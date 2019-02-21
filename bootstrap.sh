85297486f569f3cb4f7581eae857b9094f3c99ff
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-02-20 16:09:27 -0800

    Fix generate script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,6 @@ set -e
 echo "#### Updating submodules... ####"
 git submodule update --init
 
-echo "#### Generating code... ####"
-tools/generate-files
-pushd codegen
-bin/codegen
-popd
-
 echo "#### Building... ####"
 cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DGIT_SUBMODULE=OFF
 make -C build tests
