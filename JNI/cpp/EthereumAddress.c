42d961613fbc08bdcf8f52354a5c46cdc74aa767
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-02-21 15:15:24 -0800

    Generate code

---
+++ JNI/cpp/EthereumAddress.c
@@ -0,0 +1,100 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here WILL BE LOST.
+//
+
+#include <jni.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <TrustWalletCore/TWEthereumAddress.h>
+#include <TrustWalletCore/TWPublicKey.h>
+
+#include "TWJNI.h"
+#include "EthereumAddress.h"
+
+jlong JNICALL Java_wallet_core_jni_EthereumAddress_nativeCreateWithString(JNIEnv *env, jclass thisClass, jstring string) {
+    TWString *stringString = TWStringCreateWithJString(env, string);
+    struct TWEthereumAddress *instance = TWEthereumAddressCreateWithString(stringString);
+    TWStringDelete(stringString);
+    return (jlong) instance;
+}
+
+jlong JNICALL Java_wallet_core_jni_EthereumAddress_nativeCreateWithKeyHash(JNIEnv *env, jclass thisClass, jbyteArray keyHash) {
+    TWData *keyHashData = TWDataCreateWithJByteArray(env, keyHash);
+    struct TWEthereumAddress *instance = TWEthereumAddressCreateWithKeyHash(keyHashData);
+    TWDataDelete(keyHashData);
+    return (jlong) instance;
+}
+
+jlong JNICALL Java_wallet_core_jni_EthereumAddress_nativeCreateWithPublicKey(JNIEnv *env, jclass thisClass, jobject publicKey) {
+    jclass publicKeyClass = (*env)->GetObjectClass(env, publicKey);
+    jfieldID publicKeyBytesFieldID = (*env)->GetFieldID(env, publicKeyClass, "bytes", "[B");
+    jbyteArray publicKeyBytesArray = (*env)->GetObjectField(env, publicKey, publicKeyBytesFieldID);
+    jbyte* publicKeyBytesBuffer = (*env)->GetByteArrayElements(env, publicKeyBytesArray, NULL);
+    struct TWPublicKey *publicKeyInstance = (struct TWPublicKey *) publicKeyBytesBuffer;
+    struct TWEthereumAddress *instance = TWEthereumAddressCreateWithPublicKey(*publicKeyInstance);
+    (*env)->ReleaseByteArrayElements(env, publicKeyBytesArray, publicKeyBytesBuffer, JNI_ABORT);
+    (*env)->DeleteLocalRef(env, publicKeyBytesArray);
+    (*env)->DeleteLocalRef(env, publicKeyClass);
+    return (jlong) instance;
+}
+
+void JNICALL Java_wallet_core_jni_EthereumAddress_nativeDelete(JNIEnv *env, jclass thisClass, jlong handle) {
+    TWEthereumAddressDelete((struct TWEthereumAddress *) handle);
+}
+
+jboolean JNICALL Java_wallet_core_jni_EthereumAddress_equals(JNIEnv *env, jclass thisClass, jobject lhs, jobject rhs) {
+    jclass lhsClass = (*env)->GetObjectClass(env, lhs);
+    jfieldID lhsHandleFieldID = (*env)->GetFieldID(env, lhsClass, "nativeHandle", "J");
+    struct TWEthereumAddress *lhsInstance = (struct TWEthereumAddress *) (*env)->GetLongField(env, lhs, lhsHandleFieldID);
+    jclass rhsClass = (*env)->GetObjectClass(env, rhs);
+    jfieldID rhsHandleFieldID = (*env)->GetFieldID(env, rhsClass, "nativeHandle", "J");
+    struct TWEthereumAddress *rhsInstance = (struct TWEthereumAddress *) (*env)->GetLongField(env, rhs, rhsHandleFieldID);
+    jboolean resultValue = (jboolean) TWEthereumAddressEqual(lhsInstance, rhsInstance);
+
+    (*env)->DeleteLocalRef(env, lhsClass);
+    (*env)->DeleteLocalRef(env, rhsClass);
+
+    return resultValue;
+}
+
+jboolean JNICALL Java_wallet_core_jni_EthereumAddress_isValidString(JNIEnv *env, jclass thisClass, jstring string) {
+    TWString *stringString = TWStringCreateWithJString(env, string);
+    jboolean resultValue = (jboolean) TWEthereumAddressIsValidString(stringString);
+
+    TWStringDelete(stringString);
+
+    return resultValue;
+}
+
+jstring JNICALL Java_wallet_core_jni_EthereumAddress_description(JNIEnv *env, jobject thisObject) {
+    jclass thisClass = (*env)->GetObjectClass(env, thisObject);
+    jfieldID handleFieldID = (*env)->GetFieldID(env, thisClass, "nativeHandle", "J");
+    struct TWEthereumAddress *instance = (struct TWEthereumAddress *) (*env)->GetLongField(env, thisObject, handleFieldID);
+
+    jstring result = TWStringJString(TWEthereumAddressDescription(instance), env);
+
+
+    (*env)->DeleteLocalRef(env, thisClass);
+
+    return result;
+}
+
+jbyteArray JNICALL Java_wallet_core_jni_EthereumAddress_keyHash(JNIEnv *env, jobject thisObject) {
+    jclass thisClass = (*env)->GetObjectClass(env, thisObject);
+    jfieldID handleFieldID = (*env)->GetFieldID(env, thisClass, "nativeHandle", "J");
+    struct TWEthereumAddress *instance = (struct TWEthereumAddress *) (*env)->GetLongField(env, thisObject, handleFieldID);
+
+    jbyteArray result = TWDataJByteArray(TWEthereumAddressKeyHash(instance), env);
+
+
+    (*env)->DeleteLocalRef(env, thisClass);
+
+    return result;
+}
+
