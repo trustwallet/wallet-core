7dc2c3e34ce483ebd4287b76fc6ede323079cc70
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-02-21 14:11:35 -0800

    Implement Ethereum ABI encoding

---
+++ src/Ethereum/ABI/Array.h
@@ -0,0 +1,58 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include <numeric>
+
+namespace TW {
+namespace Ethereum {
+
+template <typename T>
+bool is_dynamic(std::vector<T>) {
+    return true;
+}
+
+template <typename T>
+std::size_t size(const std::vector<T>& array) {
+    return 32 + std::accumulate(array.begin(), array.end(), 0u, [](size_t sum, auto x) { return sum + size(x); });
+}
+
+template <typename T>
+void encode(const std::vector<T>& array, Data& data) {
+    encode(uint256_t(array.size()), data);
+
+    std::size_t headSize = 0;
+    for (auto& x : array) {
+        if (is_dynamic(x)) {
+            headSize += 32;
+        } else {
+            headSize += size(x);
+        }
+    }
+
+    std::size_t dynamicOffset = 0;
+    for (auto& x : array) {
+        if (is_dynamic(x)) {
+            encode(uint256_t(headSize + dynamicOffset), data);
+            dynamicOffset += size(x);
+        } else {
+            encode(x, data);
+        }
+    }
+
+    for (auto& x : array) {
+        if (is_dynamic(x))
+            encode(x, data);
+    }
+}
+
+template <typename T>
+std::string type_string(const std::vector<T>& array) {
+    return type_string(array[0]) + "[]";
+}
+
+}} // namespace
---
+++ src/Ethereum/ABI/Function.h
@@ -0,0 +1,53 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "Tuple.h"
+
+#include "../../Hash.h"
+
+#include <string>
+#include <tuple>
+
+namespace TW {
+namespace Ethereum {
+
+template <typename... Params>
+class Function {
+public:
+    std::string name;
+    std::tuple<Params...> parameters;
+
+    Function() = default;
+    Function(std::string name, std::tuple<Params...> parameters) : name(name), parameters(parameters) {}
+};
+
+template <typename... Params>
+bool is_dynamic(const Function<Params...>& f) {
+    return is_dynaic(f.parameters);
+}
+
+template <typename... Params>
+bool size(const Function<Params...>& f) {
+    return 4 + size(f.parameters);
+}
+
+template <typename... Params>
+void encode(const Function<Params...>& f, Data& data) {
+    auto string = type_string(f);
+    auto hash = Hash::keccak256(Data(string.begin(), string.end()));
+    auto signature = Data(hash.begin(), hash.begin() + 4);
+    append(data, signature);
+    encode(f.parameters, data);
+}
+
+template <typename... Params>
+std::string type_string(const Function<Params...>& f) {
+    return f.name + "(" + type_string(f.parameters) + ")";
+}
+
+}} // namespace
