4d022fa2f39decd1ff44006db14deb2099ee8b31
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-02-27 11:26:09 -0800

    Generate files

---
+++ JNI/cpp/IconSigner.c
@@ -0,0 +1,35 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here WILL BE LOST.
+//
+
+#include <jni.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <TrustWalletCore/TWIconSigner.h>
+
+#include "TWJNI.h"
+#include "IconSigner.h"
+
+jobject JNICALL Java_wallet_core_jni_IconSigner_sign(JNIEnv *env, jclass thisClass, jobject input) {
+    jclass inputClass = (*env)->GetObjectClass(env, input);
+    jmethodID inputToByteArrayMethodID = (*env)->GetMethodID(env, inputClass, "toByteArray", "()[B");
+    jbyteArray inputByteArray = (*env)->CallObjectMethod(env, input, inputToByteArrayMethodID);
+    TWData *inputData = TWDataCreateWithJByteArray(env, inputByteArray);
+    jbyteArray resultData = TWDataJByteArray(TWIconSignerSign(inputData), env);
+    jclass resultClass = (*env)->FindClass(env, "wallet/core/jni/proto/Proto$SigningOutput");
+    jmethodID parseFromMethodID = (*env)->GetStaticMethodID(env, resultClass, "parseFrom", "([B)Lwallet/core/jni/proto/Proto$SigningOutput;");
+    jobject result = (*env)->CallStaticObjectMethod(env, resultClass, parseFromMethodID, resultData);
+
+    (*env)->DeleteLocalRef(env, resultClass);
+    (*env)->DeleteLocalRef(env, inputByteArray);
+    (*env)->DeleteLocalRef(env, inputClass);
+
+    return result;
+}
+
---
+++ swift/Sources/IconSigner.swift
@@ -0,0 +1,30 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here WILL BE LOST.
+//
+
+import Foundation
+
+public final class IconSigner {
+
+    public static func sign(input: TW_Icon_Proto_SigningInput) -> TW_Icon_Proto_SigningOutput {
+        let inputData = TWDataCreateWithNSData(try! input.serializedData())
+        defer {
+            TWDataDelete(inputData)
+        }
+        let resultData = TWDataNSData(TWIconSignerSign(inputData))
+        return try! TW_Icon_Proto_SigningOutput(serializedData: resultData)
+    }
+
+    let rawValue: OpaquePointer
+
+    init(rawValue: OpaquePointer) {
+        self.rawValue = rawValue
+    }
+
+
+}
