40ade8014687c7188acdcc156870a59cbb5fe4ec
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-02-27 15:51:06 -0800

    Add missing files

---
+++ swift/Tests/PublicKeyTests.swift
@@ -0,0 +1,17 @@
+// Copyright © 2017-2018 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+import TrustWalletCore
+import XCTest
+
+class PublicKeyTests: XCTestCase {
+    func testCompressed() {
+        let privateKey = PrivateKey(data: Data(hexString: "afeefca74d9a325cf1d6b6911d61a65c32afa8e02bd5e78e2e4ac2910bab45f5")!)!
+        let publicKey = privateKey.getPublicKey(compressed: false)
+
+        XCTAssertEqual(publicKey.compressed.data.hexString, "0399c6f51ad6f98c9c583f8e92bb7758ab2ca9a04110c0a1126ec43e5453d196c1")
+    }
+}
---
+++ swift/Tests/SlipTests.swift
@@ -0,0 +1,27 @@
+// Copyright © 2017-2018 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+import XCTest
+import TrustWalletCore
+
+class SlipTests: XCTestCase {
+
+    func testCoinType() {
+        XCTAssertEqual(CoinType.bitcoin.rawValue, 0)
+        XCTAssertEqual(CoinType.litecoin.rawValue, 2)
+        XCTAssertEqual(CoinType.tron.rawValue, 195)
+        XCTAssertEqual(CoinType.ethereum.rawValue, 60)
+        XCTAssertEqual(CoinType.thunderToken.rawValue, 1001)
+        XCTAssertEqual(CoinType.wanChain.rawValue, 5718350)
+        XCTAssertEqual(CoinType.callisto.rawValue, 820)
+        XCTAssertEqual(CoinType.ethereumClassic.rawValue, 61)
+        XCTAssertEqual(CoinType.go.rawValue, 6060)
+        XCTAssertEqual(CoinType.poa.rawValue, 178)
+        XCTAssertEqual(CoinType.veChain.rawValue, 818)
+        XCTAssertEqual(CoinType.icon.rawValue, 74)
+        XCTAssertEqual(CoinType.tomoChain.rawValue, 889)
+    }
+}
