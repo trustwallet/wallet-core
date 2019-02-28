c4717a21cad4570f1f93ee26bc00fbd43391f103
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-02-28 09:21:56 -0800

    Fix unspent selector edge case (#62)

--- src/Bitcoin/UnspentSelector.cpp
+++ src/Bitcoin/UnspentSelector.cpp
@@ -86,8 +86,9 @@ std::vector<Proto::UnspentTransaction> UnspentSelector::select(const T& utxos, i
     }
 
     // 2. If not, find a combination of outputs that may produce dust change.
+    numOutputs = 1;
     for (auto numInputs = 1; numInputs <= sortedUtxos.size(); numInputs += 1) {
-        const auto fee = calculateFee(numInputs, numOutputs);
+        const auto fee = calculateFee(numInputs, numOutputs, byteFee);
         const auto targetWithFee = targetValue + fee;
         auto slices = slice(sortedUtxos, numInputs);
         slices.erase(std::remove_if(slices.begin(), slices.end(), [targetWithFee](const std::vector<Proto::UnspentTransaction>& slice) {
