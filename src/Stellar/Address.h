0610530cf0c2b5473af2a80b6af931515ad7da08
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-03-03 09:57:03 +0700

    Update src/Stellar/Address.h
    Co-Authored-By: madcake <133312+madcake@users.noreply.github.com>

--- src/Stellar/Address.h
+++ src/Stellar/Address.h
@@ -20,7 +20,7 @@ public:
     // 56 character base-32 encoded string
     static const size_t size = 56;
     // Decodes to 35 bytes
-    static const size_t STELLAR_ADDRESS_SIZE_RAW = 35;
+    static const size_t rawSize = 35;
     // Raw key size is 32 bytes
     static const size_t STELLAR_KEY_SIZE = 32;
 
