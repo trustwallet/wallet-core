16e55ddead12c33d5931c6234d97062a9b2b5dba
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-03-04 10:39:42 -0800

    FIx test

--- swift/Tests/Blockchains/EthereumTests.swift
+++ swift/Tests/Blockchains/EthereumTests.swift
@@ -23,24 +23,24 @@ class EthereumTests: XCTestCase {
     func testSigner() {
         let input = TW_Ethereum_Proto_SigningInput.with {
             $0.chainID = BigUInt(1).serialize()
-            $0.nonce = BigInt(0).magnitude.serialize()
-            $0.gasPrice = BigInt(20000000000).magnitude.serialize()
-            $0.gasLimit = BigInt(21000).magnitude.serialize()
+            $0.nonce = BigInt(9).magnitude.serialize()
+            $0.gasPrice = BigInt(20_000_000_000).magnitude.serialize()
+            $0.gasLimit = BigInt(21_000).magnitude.serialize()
             $0.toAddress = "0x3535353535353535353535353535353535353535"
-            $0.amount = BigInt(1000000000000000000).magnitude.serialize()
+            $0.amount = BigInt(1_000_000_000_000_000_000).magnitude.serialize()
             $0.privateKey = Data(hexString: "0x4646464646464646464646464646464646464646464646464646464646464646")!
         }
 
         XCTAssertEqual(input.chainID.hexString, "01")
-        XCTAssertEqual(input.nonce.hexString, "")
+        XCTAssertEqual(input.nonce.hexString, "09")
         XCTAssertEqual(input.gasPrice.hexString, "04a817c800")
         XCTAssertEqual(input.gasLimit.hexString, "5208")
         XCTAssertEqual(input.amount.hexString, "0de0b6b3a7640000")
 
         let output = EthereumSigner.sign(input: input)
 
-        XCTAssertEqual(output.encoded.hexString, "28ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa63627667cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d8300")
-        XCTAssertEqual(output.r, Data(hexString: "18515461264373351373200002665853028612451056578545711640558177340181847433846")!)
-        XCTAssertEqual(output.s, Data(hexString: "46948507304638947509940763649030358759909902576025900602547168820602576006531")!)
+        XCTAssertEqual(output.v.hexString, "25")
+        XCTAssertEqual(output.r.hexString, "28ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276")
+        XCTAssertEqual(output.s.hexString, "67cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83")
     }
 }
