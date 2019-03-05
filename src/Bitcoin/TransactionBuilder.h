8841129de717d7f7e837961d8d8b17dcad4df966
Sathvik Birudavolu <sathvikb30@gmail.com>
2019-03-05 08:53:12 -0500

    Fix transaction plan when using max amount (#30)
    * Fix use_max_amount
    
    * Fix spacing
    
    * Fix edge case, Add tests
    
    * const fix

--- src/Bitcoin/TransactionBuilder.h
+++ src/Bitcoin/TransactionBuilder.h
@@ -22,13 +22,26 @@ struct TransactionBuilder {
         auto plan = TransactionPlan();
         plan.amount = input.amount();
 
+        auto output_size = 2;
+
         if (input.use_max_amount() && UnspentSelector::sum(input.utxo()) == plan.amount) {
-            plan.amount -= UnspentSelector::calculateFee(input.utxo().size(), 2, input.byte_fee());
+            output_size = 1;
+            auto newAmount = 0;
+            auto input_size = 0;
+
+            for (auto utxo: input.utxo()) {
+                if (utxo.amount() > UnspentSelector::calculateSingleInputFee(input.byte_fee())) { 
+                    input_size++;
+                    newAmount += utxo.amount();
+                }
+            }
+
+            plan.amount = newAmount - UnspentSelector::calculateFee(input_size, output_size, input.byte_fee());
             plan.amount = std::max(Amount(0), plan.amount);
         }
-
-        plan.utxos = UnspentSelector::select(input.utxo(), plan.amount, input.byte_fee());
-        plan.fee = UnspentSelector::calculateFee(plan.utxos.size(), 2, input.byte_fee());        
+        
+        plan.utxos = UnspentSelector::select(input.utxo(), plan.amount, input.byte_fee(), output_size);
+        plan.fee = UnspentSelector::calculateFee(plan.utxos.size(), output_size, input.byte_fee());        
 
         plan.availableAmount = UnspentSelector::sum(plan.utxos);
 
