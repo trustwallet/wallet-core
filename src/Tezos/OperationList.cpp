f6c0c795638ac53de11cb2aa543f5c7cd706c93b
Robert L Kelly <robert.l.kelly3@gmail.com>
2019-03-05 02:02:10 -0500

    added operation list

---
+++ src/Tezos/OperationList.cpp
@@ -0,0 +1,31 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Address.h"
+#include "HexCoding.h"
+#include "Transaction.h"
+#include "TWTezosForger.h"
+
+using namespace TW;
+using namespace TW::Tezos;
+
+Address::Address(const std::string& str) {
+  branch = str;
+}
+
+void add_operation(Transaction transaction) {
+  operation_list.push_back(transaction);
+}
+
+std::string OperationList::forge() const {
+  std::string result = forgeBranch(branch);
+
+  for (int i = 0; i < transaction_list.size(); i++) {
+    Transaction t = operation_list[i];
+    result += forgeOperation(operation);
+  }
+  return result;
+}
