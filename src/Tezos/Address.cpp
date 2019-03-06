47466b5b1a267ddd9d95eb36c37d658d03b1d466
Robert L Kelly <robert.l.kelly3@gmail.com>
2019-03-06 11:56:19 -0500

    initial refactor of address

---
+++ src/Tezos/Address.cpp
@@ -0,0 +1,77 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Address.h"
+#include "BinaryCoding.h"
+#include "HexCoding.h"
+
+#include <TrezorCrypto/base58.h>
+#include <TrezorCrypto/ripple/base58.h>
+#include <TrezorCrypto/ecdsa.h>
+
+#include <iostream>
+
+using namespace TW;
+using namespace TW::Tezos;
+
+bool Address::isValid(const std::string& string) {
+    size_t capacity = 128;
+    uint8_t buffer[capacity];
+    int size = base58_decode_check(string.data(), HASHER_SHA2D, buffer, (int)capacity);
+
+    // verify prefix
+    std::array<std::uint8_t, 3> prefix {6, 161, 159};
+    for (size_t i = 0; i< prefix.size(); i++) {
+        uint8_t byte = buffer[i];
+        if (prefix[i] != byte) {
+            return false;
+        }
+    }
+
+    return size == Address::size;
+}
+
+Address::Address(const std::string& pkey_hash) {
+  public_key_hash = pkey_hash;
+
+  size_t capacity = 128;
+  uint8_t buffer[capacity];
+  int size = base58_decode_check(pkey_hash.data(), HASHER_SHA2D, buffer, (int)capacity);
+  assert(size == Address::size);
+  std::vector<uint8_t> vec(&buffer[0], &buffer[128]);
+  auto str = TW::hex(vec);
+  std::copy(buffer, buffer + Address::size, bytes.begin());
+}
+
+Address::Address(const PublicKey& publicKey) {
+    bytes[0] = 0x00;
+    ecdsa_get_pubkeyhash(publicKey.bytes.data(), HASHER_SHA2_RIPEMD, bytes.data() + 1);
+}
+
+std::string Address::string() const {
+  size_t size = 0;
+  b58enc(nullptr, &size, &bytes, Address::size);
+  size += 16;
+  std::string public_key_hash(size, ' ');
+  base58_encode_check(bytes.data(), Address::size, HASHER_SHA2D, &public_key_hash[0], size);
+
+  std::string result = "";
+  if (public_key_hash[0] == 'K') {
+    size_t prefixLength = 3;
+    uint8_t prefix[3] = {2, 90, 121};
+    size_t capacity = 128;
+    uint8_t decoded[capacity];
+
+    int decodedLength = checkDecodeAndDropPrefix(public_key_hash, prefixLength, prefix, decoded);
+    result += "01";
+    result += TW::hex(decoded, decoded + decodedLength);
+    result += "00";
+  } else {
+    result += "00";
+    result += forgePublicKeyHash(public_key_hash);
+  }
+  return result;
+}
--- src/interface/TWTezosAddress.cpp
+++ src/interface/TWTezosAddress.cpp
@@ -4,6 +4,7 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
+#include "../Tezos/Address.h"
 #include <TrustWalletCore/TWTezosAddress.h>
 
 #include <TrustWalletCore/TWBase58.h>
@@ -18,75 +19,58 @@
 #include <vector>
 #include <array>
 
+using namespace TW;
+using namespace TW::Tezos;
+
 bool TWTezosAddressIsValid(TWData *_Nonnull data) {
-  // Verify prefix is correct.
-  std::array<std::uint8_t, 3> prefix {6, 161, 159};
-  for (size_t i = 0; i< prefix.size(); i++) {
-    uint8_t byte = TWDataGet(data, i);
-    if (prefix[i] != byte) {
-      return false;
-    }
-  }
+    auto d = reinterpret_cast<const std::vector<uint8_t>*>(data);
+    size_t size = 0;
+    b58enc(nullptr, &size, d, Address::size);
+    size += 16;
 
-  // Verify checksum.
-  size_t checksumLength = 4;
-  TWData *dataToChecksum = TWDataCreateWithBytes(TWDataBytes(data), TWTezosAddressSize - checksumLength);
-
-  TWData *hashedData = TWHashSHA256(dataToChecksum);
-  TWData *doubleHashedData = TWHashSHA256(hashedData);
-  size_t dataSize = TWDataSize(data);
-
-  for (size_t i = 0; i < checksumLength; i++) {
-    uint8_t checksumByte = TWDataGet(doubleHashedData, i);
-    size_t expectedByteIndex = dataSize - checksumLength + i;
-    uint8_t expectedByte = TWDataGet(data, expectedByteIndex);
-
-    if (checksumByte != expectedByte) {
-      return false;
-    }
-  }
-
-  return dataSize == TWTezosAddressSize;
+    std::string str(size, ' ');
+    base58_encode_check(d->data(), Address::size, HASHER_SHA2D, &str[0], size);
+    return Address::isValid(str);
 }
 
 bool TWTezosAddressIsValidString(TWString *_Nonnull string) {
-  TWData *data = TWBase58DecodeNoCheck(string);
-  if (data == nullptr) {
-    return false;
-  }
-  return TWTezosAddressIsValid(data);
+    TWData *data = TWBase58DecodeNoCheck(string);
+    if (data == nullptr) {
+        return false;
+    }
+    return TWTezosAddressIsValid(data);
 }
 
 bool TWTezosAddressInitWithString(struct TWTezosAddress *_Nonnull address, TWString *_Nonnull string) {
-  TWData *data = TWBase58DecodeNoCheck(string);
-  if (!TWTezosAddressIsValidString(string) || data == nullptr || TWDataSize(data) != TWTezosAddressSize) {
-    return false;
-  }
+    TWData *data = TWBase58DecodeNoCheck(string);
+    if (!TWTezosAddressIsValidString(string) || data == nullptr || TWDataSize(data) != TWTezosAddressSize) {
+        return false;
+    }
 
-  TWDataCopyBytes(data, 0, TWTezosAddressSize, address->bytes);
-  return true;
+    TWDataCopyBytes(data, 0, TWTezosAddressSize, address->bytes);
+    return true;
 }
 
 bool TWTezosAddressInitWithPublicKey(struct TWTezosAddress *_Nonnull address, struct TWPublicKey publicKey) {
-  std::array<std::uint8_t, 3> prefix {6, 161, 159};
-  TWData *data = TWDataCreateWithSize(prefix.size());
-  for (size_t i = 0; i < prefix.size(); i++) {
-    TWDataSet(data, i, prefix[i]);
-  }
+    std::array<std::uint8_t, 3> prefix {6, 161, 159};
+    TWData *data = TWDataCreateWithSize(prefix.size());
+    for (size_t i = 0; i < prefix.size(); i++) {
+        TWDataSet(data, i, prefix[i]);
+    }
 
-  TWData *publicKeyData = TWPublicKeyData(publicKey);
-  TWData *publicKeyHash = TWHashBlake2b(publicKeyData, 20);
-  TWDataAppendData(data, publicKeyHash);
-  TWString *addressString = TWBase58Encode(data);
+    TWData *publicKeyData = TWPublicKeyData(publicKey);
+    TWData *publicKeyHash = TWHashBlake2b(publicKeyData, 20);
+    TWDataAppendData(data, publicKeyHash);
+    TWString *addressString = TWBase58Encode(data);
 
-  return TWTezosAddressInitWithString(address, addressString);
+    return TWTezosAddressInitWithString(address, addressString);
 }
 
 TWString *_Nonnull TWTezosAddressDescription(struct TWTezosAddress *_Nonnull address) {
-  TWData *addressData = TWTezosAddressData(address);
-  return TWBase58EncodeNoCheck(addressData);
+    TWData *addressData = TWTezosAddressData(address);
+    return TWBase58EncodeNoCheck(addressData);
 }
 
 TWData *_Nonnull TWTezosAddressData(struct TWTezosAddress *_Nonnull address) {
-  return TWDataCreateWithBytes(address -> bytes, TWTezosAddressSize);
+    return TWDataCreateWithBytes(address -> bytes, TWTezosAddressSize);
 }
