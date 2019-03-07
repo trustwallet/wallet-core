87b9b1da96f8c163ceef7e2147c63fb49e31352a
dingxin <wdx7266@vip.qq.com>
2019-03-07 17:39:04 +0800

    add ontology address

---
+++ src/Ontology/Address.cpp
@@ -0,0 +1,113 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Address.h"
+
+#include "Hash.h"
+#include "HexCoding.h"
+#include <TrezorCrypto/sha2.h>
+#include <TrezorCrypto/base58.h>
+#include <TrezorCrypto/ripemd160.h>
+#include <iostream>
+#include <stdexcept>
+
+using namespace TW::Ontology;
+
+std::vector<uint8_t> Address::sha256(std::vector<uint8_t> &data) {
+    uint8_t digest[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256_ctx;
+    sha256_Init(&sha256_ctx);
+    sha256_Update(&sha256_ctx, &data[0], data.size());
+    sha256_Final(&sha256_ctx, digest);
+    std::vector<uint8_t> uc_vec(&digest[0], &digest[SHA256_DIGEST_LENGTH]);
+    return uc_vec;
+}
+
+std::vector<uint8_t> Address::hash256(std::vector<uint8_t> &data) {
+    auto digest = Address::sha256(data);
+    return Address::sha256(digest);
+}
+
+std::vector<uint8_t> Address::ripemd160(std::vector<uint8_t> &data) {
+    uint8_t digest[RIPEMD160_DIGEST_LENGTH];
+    RIPEMD160_CTX ctx;
+    ripemd160_Init(&ctx);
+    ripemd160_Update(&ctx, &data[0], (uint32_t) data.size());
+    ripemd160_Final(&ctx, digest);
+    std::vector<uint8_t> uc_vec(&digest[0], &digest[RIPEMD160_DIGEST_LENGTH]);
+    return uc_vec;
+}
+
+Address::Address(const PublicKey &publicKey) {
+    auto hash = std::array<uint8_t, Hash::sha256Size>();
+    std::vector<uint8_t> builder(publicKey.bytes);
+    builder.insert(builder.begin(), 0x21);
+    builder.push_back(0xAC);
+    auto data = toScriptHash(builder);
+    std::copy(data.begin(), data.end(), zero.begin());
+}
+
+
+Address::Address(const std::string &b58Address) {
+    size_t dataSz = 25;
+    std::array<uint8_t, 25> data{};
+    b58tobin(data.data(), &dataSz, b58Address.data());
+    if (data[0] != version) {
+        throw std::runtime_error("Invalid address version.");
+    }
+    std::vector<uint8_t> addressWithVer(size + 1);
+    std::copy(data.begin(), data.begin() + size + 2, addressWithVer.begin());
+    auto checksum = Address::hash256(addressWithVer);
+    auto count = 1;
+    for (auto v:checksum) {
+        if (count == 5) {
+            break;
+        }
+        if (v != *(data.begin() + size + count)) {
+            throw std::runtime_error("Invalid base58 encode address.");
+        }
+        count++;
+    }
+    std::copy(addressWithVer.begin() + 1, addressWithVer.end(), zero.begin());
+}
+
+Address::Address(const std::vector<uint8_t> &data) {
+    if (!Address::isValid(data)) {
+        throw std::runtime_error("Invalid vector");
+    }
+    std::copy(data.begin(), data.end(), zero.begin());
+}
+
+std::vector<uint8_t> Address::toScriptHash(std::vector<uint8_t> &data) {
+    auto hash256 = sha256(data);
+    return ripemd160(hash256);
+}
+
+bool Address::isValid(const std::string &string) {
+    const auto data = parse_hex(string);
+    return Address::isValid(data);
+}
+
+std::string Address::hexString() const {
+    std::vector<uint8_t> data(size);
+    std::copy(zero.rbegin(), zero.rend(), data.begin());
+    return hex(data);
+}
+
+std::string Address::b58String() const {
+    std::vector<uint8_t> data(size + 1);
+    data[0] = version;
+    std::copy(zero.begin(), zero.end(), data.begin() + 1);
+    auto checksum = Address::hash256(data);
+    for (auto it = checksum.begin(); it != checksum.begin() + 4; it++) {
+        data.push_back(*it);
+    }
+    size_t b58StrSz = 35;
+    std::string b58Str(b58StrSz, ' ');
+    b58enc(&b58Str[0], &b58StrSz, data.data(), data.size());
+    b58Str.pop_back();
+    return b58Str;
+}
---
+++ src/Ontology/Address.h
@@ -0,0 +1,49 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "../PublicKey.h"
+
+#include <array>
+#include <vector>
+
+namespace TW {
+    namespace Ontology {
+        class Address {
+        private:
+            static std::vector<uint8_t> sha256(std::vector<uint8_t> &data);
+
+            static std::vector<uint8_t> hash256(std::vector<uint8_t> &data);
+
+            static std::vector<uint8_t> ripemd160(std::vector<uint8_t> &data);
+
+        public:
+            static const size_t size = 20;
+            static const uint8_t version = 0x17;
+
+            std::array<uint8_t, size> zero;
+
+            explicit Address(const PublicKey &publicKey);
+
+            explicit Address(const std::string &b58Address);
+
+            explicit Address(const std::vector<uint8_t> &data);
+
+            std::vector<uint8_t> toScriptHash(std::vector<uint8_t> &data);
+
+            static bool isValid(const std::vector<uint8_t> &data) {
+                return data.size() == size;
+            }
+
+            static bool isValid(const std::string &string);
+
+            std::string hexString() const;
+
+            std::string b58String() const;
+        };
+    }
+}
