7abb8d95ab15522e5a1f700a4541a8ca40bd6f05
Robert L Kelly <robert.l.kelly3@gmail.com>
2019-03-07 16:47:29 -0500

    formatting

--- tests/Tezos/TransactionTests.cpp
+++ tests/Tezos/TransactionTests.cpp
@@ -12,35 +12,35 @@
 using namespace TW::Tezos;
 
 TEST(TezosTransaction, forgePayment) {
-  auto tx1 = Transaction(
-    Address("tz1XVJ8bZUXs7r5NV8dHvuiBhzECvLRLR3jW"),
-    1272,
-    30738,
-    10100,
-    257,
-    1,
-    Address("tz1Yju7jmmsaUiG9qQLoYv35v5pHgnWoLWbt"),
-    OperationKind::TRANSACTION
-  );
-  auto expected = "08000081faa75f741ef614b0e35fcc8c90dfa3b0b95721f80992f001f44e81020100008fb5cea62d147c696afd9a93dbce962f4c8a9c9100";
-  auto serialized1 = tx1.forge();
+    auto tx1 = Transaction(
+        Address("tz1XVJ8bZUXs7r5NV8dHvuiBhzECvLRLR3jW"),
+        1272,
+        30738,
+        10100,
+        257,
+        1,
+        Address("tz1Yju7jmmsaUiG9qQLoYv35v5pHgnWoLWbt"),
+        OperationKind::TRANSACTION
+    );
+    auto expected = "08000081faa75f741ef614b0e35fcc8c90dfa3b0b95721f80992f001f44e81020100008fb5cea62d147c696afd9a93dbce962f4c8a9c9100";
+    auto serialized1 = tx1.forge();
 
-  ASSERT_EQ(serialized1, expected);
+    ASSERT_EQ(serialized1, expected);
 }
 
 TEST(TezosTransaction, forgeReveal) {
-  auto tx1 = Transaction(
-    Address("tz1XVJ8bZUXs7r5NV8dHvuiBhzECvLRLR3jW"),
-    1272,
-    30738,
-    10100,
-    257,
-    1,
-    PublicKey("edpku9ZF6UUAEo1AL3NWy1oxHLL6AfQcGYwA5hFKrEKVHMT3Xx889A"),
-    OperationKind::REVEAL
-  );
-  auto expected = "07000081faa75f741ef614b0e35fcc8c90dfa3b0b95721f80992f001f44e810200429a986c8072a40a1f3a3e2ab5a5819bb1b2fb69993c5004837815b9dc55923e";
-  auto serialized1 = tx1.forge();
+    auto tx1 = Transaction(
+        Address("tz1XVJ8bZUXs7r5NV8dHvuiBhzECvLRLR3jW"),
+        1272,
+        30738,
+        10100,
+        257,
+        1,
+        PublicKey("edpku9ZF6UUAEo1AL3NWy1oxHLL6AfQcGYwA5hFKrEKVHMT3Xx889A"),
+        OperationKind::REVEAL
+    );
+    auto expected = "07000081faa75f741ef614b0e35fcc8c90dfa3b0b95721f80992f001f44e810200429a986c8072a40a1f3a3e2ab5a5819bb1b2fb69993c5004837815b9dc55923e";
+    auto serialized1 = tx1.forge();
 
-  ASSERT_EQ(serialized1, expected);
+    ASSERT_EQ(serialized1, expected);
 }
