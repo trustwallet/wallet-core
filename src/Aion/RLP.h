533c103a1fc6a92177eeefea30c1949f8c92fdfe
Ivan <grachx@gmail.com>
2019-03-10 02:27:01 +0300

    Add Aion C++ implementation

---
+++ src/Aion/RLP.h
@@ -0,0 +1,42 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "Transaction.h"
+
+#include "Ethereum/RLP.h"
+
+#include <boost/multiprecision/cpp_int.hpp>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+using boost::multiprecision::uint128_t;
+
+namespace TW {
+namespace Aion {
+
+/// Aion's RLP encoging for long numbers
+/// https://github.com/aionnetwork/aion/issues/680
+struct RLP {
+    
+    static Data encodeLong(uint128_t l) noexcept {
+        if ((l & 0x00000000FFFFFFFFL) == l) {
+            return Ethereum::RLP::encode(l);
+        }
+        Data result(9);
+        result[0] = 0x80 + 8;
+        for (int i = 8; i > 0; i--) {
+            result[i] = (byte) l & 0xFF;
+            l >>= 8;
+        }
+        return result;
+    }
+
+};
+
+}} // namespace
---
+++ src/Aion/Signer.cpp
@@ -0,0 +1,25 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Signer.h"
+
+#include "../Hash.h"
+
+using namespace TW;
+using namespace TW::Aion;
+
+void Signer::sign(const PrivateKey& privateKey, Transaction& transaction) noexcept {
+    auto encoded = transaction.encode();
+    auto hashData = Hash::blake2b(encoded, 32);
+    auto hashSignature = privateKey.sign(hashData, TWCurveEd25519);
+    auto publicKeyData = privateKey.getPublicKey(PublicKeyType::ed25519).bytes;
+    
+    // Aion signature = pubKeyBytes + signatureBytes
+    Data result(publicKeyData.begin() + 1, publicKeyData.end());
+    result.insert(result.end(), hashSignature.begin(), hashSignature.end());
+    
+    transaction.signature = result;
+}
