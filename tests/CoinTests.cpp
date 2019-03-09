d7d71762e9322dbdddd887c7602a8f58c11a9d1e
keefertaylor <keefer.t@gmail.com>
2019-03-09 19:42:02 +0000

    Add coin tests for Tezos.

--- tests/CoinTests.cpp
+++ tests/CoinTests.cpp
@@ -81,4 +81,12 @@ TEST(Coin, DeriveAddress) {
     EXPECT_EQ(TW::deriveAddress(TWCoinTypeZcoin, privateKey), "aHzpPjmY132KseS4nkiQTbDahTEXqesY89");
 }
 
+TEST(Coin, ValidateAddressTezos) {
+  EXPECT_TRUE(validateAddress(TWCoinTypeTezos, "tz1d1qQL3mYVuiH4JPFvuikEpFwaDm85oabM"));
+
+  EXPECT_FALSE(validateAddress(TWCoinTypeTezos, "NmH7tmeJUmHcncBDvpr7aJNEBk7rp5zYsB1qt"));
+  EXPECT_FALSE(validateAddress(TWCoinTypeTezos, "tz1eZwq8b5cvE2bPKokatLkVMzkxz24z3AAAA"));
+  EXPECT_FALSE(validateAddress(TWCoinTypeTezos, "1tzeZwq8b5cvE2bPKokatLkVMzkxz24zAAAAA"));
+}
+
 } // namespace
