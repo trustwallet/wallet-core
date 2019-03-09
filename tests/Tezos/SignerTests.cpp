e0b08cf4395260c40125a6abc03b12d95e4fb357
keefertaylor <keefer.t@gmail.com>
2019-03-09 19:08:53 +0000

    Hook up tests

--- tests/Tezos/SignerTests.cpp
+++ tests/Tezos/SignerTests.cpp
@@ -59,5 +59,7 @@ TEST(TezosSigner, SignOperationList) {
     auto key = PrivateKey(parse_hex("0x2e8905819b8723fe2c1d161860e5ee1830318dbf49a83bd451cfb8440c28bd6f"));
     auto signedBytes = Signer().signOperationList(key, op_list);
 
+    printf("EXPECTED: %s", expectedSignedBytes.c_str());
+
     ASSERT_EQ(signedBytes, expectedSignedBytes);
 }
