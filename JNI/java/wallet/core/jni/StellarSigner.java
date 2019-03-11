9e884890eed4fe7cc3bd3a38f025dd61c9f4f813
MadCake <133312+madcake@users.noreply.github.com>
2019-03-11 23:10:38 +0700

    Add sign

---
+++ JNI/java/wallet/core/jni/StellarSigner.java
@@ -0,0 +1,32 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here WILL BE LOST.
+//
+
+package wallet.core.jni;
+
+import java.security.InvalidParameterException;
+import java.util.HashSet;
+
+public class StellarSigner {
+    private long nativeHandle;
+
+    private StellarSigner() {
+        nativeHandle = 0;
+    }
+
+    static StellarSigner createFromNative(long nativeHandle) {
+        StellarSigner instance = new StellarSigner();
+        instance.nativeHandle = nativeHandle;
+        return instance;
+    }
+
+
+    public static native wallet.core.jni.proto.Stellar.SigningOutput sign(wallet.core.jni.proto.Stellar.SigningInput input);
+
+}
+
---
+++ include/TrustWalletCore/TWStellarSigner.h
@@ -0,0 +1,23 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "TWBase.h"
+#include "TWData.h"
+#include "TWStellarProto.h"
+
+TW_EXTERN_C_BEGIN
+
+/// Helper class to sign Stellar transactions.
+TW_EXPORT_CLASS
+struct TWStellarSigner;
+
+/// Signs a transaction.
+TW_EXPORT_STATIC_METHOD
+TW_Stellar_Proto_SigningOutput TWStellarSignerSign(TW_Stellar_Proto_SigningInput input);
+
+TW_EXTERN_C_END
