8eac3fbeb8a318e3a7139983fe6012f9ec3bc28f
dingxin <wdx7266@vip.qq.com>
2019-03-12 17:28:36 +0800

    simplify code as review advice

--- src/Ontology/Address.cpp
+++ src/Ontology/Address.cpp
@@ -6,82 +6,52 @@
 
 #include "Hash.h"
 #include "Address.h"
-#include "HexCoding.h"
 
+#include <string>
 #include <stdexcept>
 
-#include <TrezorCrypto/sha2.h>
 #include <TrezorCrypto/base58.h>
-#include <TrezorCrypto/ripemd160.h>
 
 using namespace TW::Ontology;
 
 Address::Address(const PublicKey &publicKey) {
     std::vector<uint8_t> builder(publicKey.bytes);
-    builder.insert(builder.begin(), 0x21);
-    builder.push_back(0xAC);
-    auto data = toScriptHash(builder);
-    std::copy(data.begin(), data.end(), zero.begin());
+    uint8_t pushBytes33 = 0x21;
+    builder.insert(builder.begin(), pushBytes33);
+    uint8_t checkSig = 0xAC;
+    builder.push_back(checkSig);
+    auto builderData = toScriptHash(builder);
+    std::copy(builderData.begin(), builderData.end(), data.begin());
 }
-
 
 Address::Address(const std::string &b58Address) {
-    size_t dataSz = 25;
-    std::array<uint8_t, 25> data{};
-    b58tobin(data.data(), &dataSz, b58Address.data());
-    if (data[0] != version) {
-        throw std::runtime_error("Invalid address version.");
+    if (!Address::isValid(b58Address)) {
+        throw std::runtime_error("Invalid base58 encode address.");
     }
-    std::vector<uint8_t> addressWithVer(size + 1);
-    std::copy(data.begin(), data.begin() + size + 2, addressWithVer.begin());
-    auto checksum = Hash::sha256(Hash::sha256(addressWithVer));
-    auto count = 1;
-    for (auto v:checksum) {
-        if (count == 5) {
-            break;
-        }
-        if (v != *(data.begin() + size + count)) {
-            throw std::runtime_error("Invalid base58 encode address.");
-        }
-        count++;
-    }
-    std::copy(addressWithVer.begin() + 1, addressWithVer.end(), zero.begin());
-}
-
-Address::Address(const std::vector<uint8_t> &data) {
-    if (!Address::isValid(data)) {
-        throw std::runtime_error("Invalid vector");
-    }
-    std::copy(data.begin(), data.end(), zero.begin());
+    Data addressWithVersion(size + 1);
+    base58_decode_check(b58Address.c_str(), HASHER_SHA2D, addressWithVersion.data(), size + 1);
+    std::copy(addressWithVersion.begin() + 1, addressWithVersion.end(), data.begin());
 }
 
 std::vector<uint8_t> Address::toScriptHash(std::vector<uint8_t> &data) {
     return Hash::ripemd(Hash::sha256(data));
 }
 
-bool Address::isValid(const std::string &string) {
-    const auto data = parse_hex(string);
-    return Address::isValid(data);
-}
-
-std::string Address::hexString() const {
-    std::vector<uint8_t> data(size);
-    std::copy(zero.rbegin(), zero.rend(), data.begin());
-    return hex(data);
+bool Address::isValid(const std::string &b58Address) noexcept {
+    if (b58Address.length() != 34) {
+        return false;
+    }
+    Data addressWithVersion(size + 1);
+    auto len = base58_decode_check(b58Address.c_str(), HASHER_SHA2D, addressWithVersion.data(), size + 1);
+    return len == size + 1;
 }
 
 std::string Address::b58String() const {
-    std::vector<uint8_t> data(size + 1);
-    data[0] = version;
-    std::copy(zero.begin(), zero.end(), data.begin() + 1);
-    auto checksum = Hash::sha256(Hash::sha256(data));
-
-    for (auto it = checksum.begin(); it != checksum.begin() + 4; it++) {
-        data.push_back(*it);
-    }
-    size_t b58StrSz = 35;
-    std::string b58Str(b58StrSz, ' ');
-    b58enc(&b58Str[0], &b58StrSz, data.data(), data.size());
-    b58Str.pop_back();
+    std::vector<uint8_t> encodeData(size + 1);
+    encodeData[0] = version;
+    std::copy(data.begin(), data.end(), encodeData.begin() + 1);
+    size_t b58StrSize = 34;
+    std::string b58Str(b58StrSize, ' ');
+    base58_encode_check(encodeData.data(), (int) encodeData.size(), HASHER_SHA2D, &b58Str[0], (int) b58StrSize + 1);
     return b58Str;
 }
