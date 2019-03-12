15dab3e37d370c0bc294799352d7a481435d9c17
dingxin <wdx7266@vip.qq.com>
2019-03-12 17:33:02 +0800

    simplify code as review advice

--- tests/Ontology/AddressTests.cpp
+++ tests/Ontology/AddressTests.cpp
@@ -6,7 +6,6 @@
 
 
 #include "Ontology/Address.h"
-#include "PrivateKey.h"
 #include "PublicKey.h"
 #include "HexCoding.h"
 
@@ -15,28 +14,22 @@
 using namespace TW;
 using namespace TW::Ontology;
 
-TEST(OntologyAddress, Validation) {
+TEST(OntologyAddress, validation) {
     ASSERT_FALSE(Address::isValid("abc"));
     ASSERT_FALSE(Address::isValid("abeb60f3e94c1b9a09f33669435e7ef12eacd"));
-    ASSERT_TRUE(Address::isValid("abcb60f3e94c9b9a09f33669435e7ef1beaedads"));
+    ASSERT_FALSE(Address::isValid("abcb60f3e94c9b9a09f33669435e7ef1beaedads"));
+    ASSERT_TRUE(Address::isValid("ANDfjwrUroaVtvBguDtrWKRMyxFwvVwnZD"));
 }
 
 TEST(OntologyAddress, fromPubKey) {
-    auto address = Address(PublicKey(parse_hex("02a600c1bb0d195f9dd92d8465ce76e53c87be72032d9df8d3fd35540ace9bfc5b")));
-    EXPECT_EQ("9f846c6abb6800fc6c61a488d4ed2f8688d416b7", address.hexString());
+    auto address = Address(PublicKey(parse_hex("031bec1250aa8f78275f99a6663688f31085848d0ed92f1203e447125f927b7486")));
+    EXPECT_EQ("AeicEjZyiXKgUeSBbYQHxsU1X3V5Buori5", address.b58String());
 }
 
 TEST(OntologyAddress, fromB58Str) {
     auto b58Str = "AYTxeseHT5khTWhtWX1pFFP1mbQrd4q1zz";
-    EXPECT_EQ(b58Str, Address(b58Str).b58String());
+    auto address = Address(b58Str);
+    EXPECT_EQ(b58Str, address.b58String());
     auto errB58Str = "AATxeseHT5khTWhtWX1pFFP1mbQrd4q1zz";
-    EXPECT_THROW(new Address(errB58Str), std::runtime_error);
-}
-
-TEST(OntologyAddress, fromData) {
-    auto vec = parse_hex("b716d488862fedd488a4616cfc0068bb6a6c849f");
-    auto address = Address(vec);
-    EXPECT_EQ("AYTxeseHT5khTWhtWX1pFFP1mbQrd4q1zz", address.b58String());
-    auto errVec = parse_hex("b716d488862fedd488a4616cfc0068bb6a6c849f1");
-    EXPECT_THROW(new Address(errVec), std::runtime_error);
+    ASSERT_THROW(new Address(errB58Str), std::runtime_error);
 }
