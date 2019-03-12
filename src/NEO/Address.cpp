368e0edad23f59b7afa5a269ea01713ab30e45c7
Artem Goryunov <725247+ArtemGoryunov@users.noreply.github.com>
2019-03-12 20:57:35 +0300

    Address implemented

--- src/NEO/Address.cpp
+++ src/NEO/Address.cpp
@@ -6,6 +6,8 @@
 
 #include "Address.h"
 
+#include "../Hash.h"
+
 #include <TrezorCrypto/base58.h>
 
 using namespace TW::NEO;
@@ -23,20 +25,47 @@ bool Address::isValid(const std::string& string) {
 }
 
 Address::Address(const std::string& string) {
-    assert(isValid(string));
+    size_t capacity = 128;
+    uint8_t buffer[capacity];
 
+    int size = base58_decode_check(string.data(), HASHER_SHA2D, buffer, (int)capacity);
+    assert(size == Address::size);
+
+    std::copy(buffer, buffer + Address::size, bytes.begin());
 }
 
 Address::Address(const std::vector<uint8_t>& data) {
-    assert(isValid(data));
+    assert(Address::isValid(data));
+    if (data.size() != size) {
+        throw std::invalid_argument("Invalid address data");
+    }
     std::copy(data.begin(), data.end(), bytes.begin());
 }
 
 Address::Address(const PublicKey& publicKey) {
     auto publicKeyData = publicKey.bytes;
-    std::copy(publicKeyData.begin() + 1, publicKeyData.begin() + 33, bytes.data());
+
+    auto pkdata = Data(publicKeyData.begin(), publicKeyData.end());
+    pkdata.insert(pkdata.begin(), 0x21);
+    pkdata.push_back(0xAC);
+
+    auto keyHash = Hash::ripemd(Hash::sha256(pkdata));
+    keyHash.insert(keyHash.begin(), 0x17);
+
+    if (keyHash.size() != Address::size)
+        throw std::invalid_argument("Invalid address key data");
+
+    std::copy(keyHash.data(), keyHash.data() + Address::size, bytes.begin());
 }
 
 std::string Address::string() const {
-    return "";
+    size_t size = 0;
+    b58enc(nullptr, &size, bytes.data(), Address::size);
+    size += 16;
+
+    std::string str(size, '\0');
+    const auto actualSize = base58_encode_check(bytes.data(), Address::size, HASHER_SHA2D, &str[0], size);
+    str.erase(str.begin() + actualSize - 1, str.end());
+
+    return str;
 }
