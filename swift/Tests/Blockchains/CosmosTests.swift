3222cc34353b458abbbd0cea22de10dcfb620892
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-03-17 23:48:34 +1100

    Fix tests for Cosmos address HRP and derivation

--- swift/Tests/Blockchains/CosmosTests.swift
+++ swift/Tests/Blockchains/CosmosTests.swift
@@ -11,9 +11,9 @@ class CosmosTests: XCTestCase {
     func testAddressFromPrivateKey() {
         let key = PrivateKey(data: Data(hexString: "124e69c2c2dacc76600f806a31333c100b41b1d4374e99f539e41156c2792c0c")!)!
         let pubkey = key.getPublicKeySecp256k1(compressed: true)
-        //let address = TendermintAddress(hrp: .cosmos, publicKey: pubkey)
+        let address = TendermintAddress(hrp: .cosmos, publicKey: pubkey)
 
         XCTAssertEqual(pubkey.data.hexString, "03F7707B1E983FE6231D46E586DCCAB76501302481C9656859C830A42A2C1BD414".lowercased())
-        //XCTAssertEqual(address?.description, "cosmos1zt50azupanqlfam5afhv3hexwyutnukeh4c573")
+        XCTAssertEqual(address?.description, "cosmos1zt50azupanqlfam5afhv3hexwyutnukeh4c573")
     }
 }
