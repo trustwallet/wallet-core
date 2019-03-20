f04212456e4a732c244792e3c53e0bb413a129e7
NashMiao <wdx7266@vip.qq.com>
2019-03-20 01:00:34 -0700

    add Asset

---
+++ src/Ontology/Asset.h
@@ -0,0 +1,34 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include <array>
+#include <vector>
+#include <string>
+
+#include "Account.h"
+#include "Transaction.h"
+#include "BinaryCoding.h"
+
+namespace TW {
+    namespace Ontology {
+
+        class Asset {
+
+        protected:
+            const uint8_t txType = 0xD1;
+
+        public:
+            virtual std::vector<uint8_t> contractAddress() = 0;
+
+            virtual Transaction queryBalance(std::string &address) = 0;
+
+            virtual Transaction transfer(const Account &from, std::string &to, uint64_t amount, const Account &payer, uint64_t gasPrice, uint64_t gasLimit) = 0;
+
+        };
+    }
+} // namespace
