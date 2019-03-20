dc0b97c5741ae48be60e5794318986991fb7c43e
NashMiao <wdx7266@vip.qq.com>
2019-03-20 01:13:00 -0700

    add Ont

---
+++ src/Ontology/Ont.h
@@ -0,0 +1,34 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include <vector>
+
+#include "Asset.h"
+#include "Transaction.h"
+
+namespace TW {
+    namespace Ontology {
+
+        class Ont : public Asset {
+        private:
+            static const uint8_t version = 0x00;
+            const std::vector<uint8_t> ontContract = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
+        public:
+            std::vector<uint8_t> contractAddress() override {
+                return ontContract;
+            }
+
+            Transaction balanceOf(std::string &address) override;
+
+            Transaction transfer(const Account &from, std::string &to, uint64_t amount, const Account &payer, uint64_t gasPrice, uint64_t gasLimit) override;
+
+        };
+
+
+    }
+} // namespace
