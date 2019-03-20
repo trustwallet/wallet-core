5f15e9324f1c588f5257e89575637c69e6a4f0e5
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-03-20 09:29:00 -0700

    Fix Swift

--- swift/Sources/Blockchains/Decred.swift
+++ swift/Sources/Blockchains/Decred.swift
@@ -6,40 +6,7 @@
 
 import Foundation
 
-public final class Decred: Bitcoin {
-    override public var coinType: CoinType {
-        return .decred
-    }
-
-    /// Public key hash address prefix.
-    ///
-    /// - SeeAlso: https://dash-docs.github.io/en/developer-reference#address-conversion
-    override public var p2pkhPrefix: UInt8 {
-        return P2PKHPrefix.decred.rawValue
-    }
-
-    override public var p2shPrefix: UInt8 {
-        return P2SHPrefix.decred.rawValue
-    }
-
-    override public var supportSegwit: Bool {
-        return false
-    }
-
-    override public init(purpose: Purpose = .bip44) {
-        super.init(purpose: purpose)
-    }
-
-    override public func address(for publicKey: PublicKey) -> Address {
-        return DecredAddress(publicKey: publicKey)
-    }
-}
-
 extension DecredAddress: Address {
-    public static func isValid(data: Data) -> Bool {
-        fatalError("Data representation not supported")
-    }
-
     public var data: Data {
         fatalError("Data representation not supported")
     }
