623cb245872cc99f822fa296bce58f50cee00e6c
NashMiao <wdx7266@vip.qq.com>
2019-03-21 19:07:30 -0700

    using unit test

--- tests/Ontology/OntTests.cpp
+++ tests/Ontology/OntTests.cpp
@@ -30,26 +30,19 @@ TEST(OntologyOnt, queryBalance) {
 TEST(OntologyOnt, transfer) {
     std::vector<uint8_t> ontContract{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
     auto acct1 = Account("4646464646464646464646464646464646464646464646464646464646464646");
+    auto acct2 = Account("4646464646464646464646464646464646464646464646464646464646464652");
     auto fromAddress = Address("ANDfjwrUroaVtvBguDtrWKRMyxFwvVwnZD");
     std::string toAddress = "Af1n2cZHhMZumNqKgw9sfCNoTWu9de4NDn";
     uint64_t amount = 1;
     uint64_t gasPrice = 500;
     uint64_t gasLimit = 20000;
-    auto tx = Ont().transfer(acct1, toAddress, amount, acct1, gasPrice, gasLimit);
+    auto tx = Ont().transfer(acct1, toAddress, amount, acct2, gasPrice, gasLimit);
     auto serializedTx = hex(tx.serialize());
-    EXPECT_EQ(316, serializedTx.length());
+    EXPECT_EQ(724, serializedTx.length());
     EXPECT_EQ(0, serializedTx.find("00d1"));
-    EXPECT_EQ(13, serializedTx.find("401000000000000204e000000000000fbacc8214765d457c8e3f2b5a1d3c4981a2e9d2a71"));
+    EXPECT_EQ(13, serializedTx.find("401000000000000204e000000000000"));
     EXPECT_EQ(86, serializedTx.find("00c66b14fbacc8214765d457c8e3f2b5a1d3c4981a2e9d2a6a7cc814feec06b79ed299ea06fcb94abac41aaf3ead76586a7cc8516a7cc86c51c1087472616e73666572"));
     EXPECT_EQ(220, serializedTx.find("1400000000000000000000000000000000000000010068164f6e746f6c6f67792e4e61746976652e496e766f6b"));
-    tx.sign(acct1);
-    serializedTx = hex(tx.serialize());
-    EXPECT_EQ(520, serializedTx.length());
-    EXPECT_EQ(452, serializedTx.find("031bec1250aa8f78275f99a6663688f31085848d0ed92f1203e447125f927b7486"));
-    auto acct2 = Account("4646464646464646464646464646464646464646464646464646464646464652");
-    tx.addSign(acct2);
-    serializedTx = hex(tx.serialize());
-    std::cout << serializedTx << std::endl;
     EXPECT_EQ(452, serializedTx.find("031bec1250aa8f78275f99a6663688f31085848d0ed92f1203e447125f927b7486"));
     EXPECT_EQ(656, serializedTx.find("03d9fd62df332403d9114f3fa3da0d5aec9dfa42948c2f50738d52470469a1a1ee"));
 }
