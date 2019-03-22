a6cfff1ee5e2552c569861f3e4b48c943a782a66
NashMiao <wdx7266@vip.qq.com>
2019-03-21 21:04:46 -0700

    use Address class for to & fix bug

--- src/Ontology/Ong.cpp
+++ src/Ontology/Ong.cpp
@@ -21,12 +21,11 @@ Transaction Ong::balanceOf(const Address &address) {
     return tx;
 }
 
-Transaction Ong::transfer(const Account &from, std::string &to, uint64_t amount, const Account &payer, uint64_t gasPrice, uint64_t gasLimit) {
-    auto toAddress = Address(to);
-    std::unordered_map<std::string, boost::any> transferParam{{"from", from.getAddress().data},{"to", toAddress.data},{"amount", amount}};
+Transaction Ong::transfer(const Account &from, const Address &to, uint64_t amount, const Account &payer, uint64_t gasPrice, uint64_t gasLimit) {
+    std::unordered_map<std::string, boost::any> transferParam{{"from", from.getAddress().data},{"to", to.data},{"amount", amount}};
     std::vector<boost::any> args{transferParam};
     auto invokeCode = ParamsBuilder::buildNativeInvokeCode(contractAddress(), 0x00, "transfer", args);
-    auto tx = Transaction(version, txType, random32(), gasPrice, gasLimit, toAddress.string(), invokeCode);
+    auto tx = Transaction(version, txType, random32(), gasPrice, gasLimit, payer.getAddress().string(), invokeCode);
     tx.sign(from);
     tx.addSign(payer);
     return tx;
