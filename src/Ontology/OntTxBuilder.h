655c56cb1176b399f074600380d3b8c1e58869a7
NashMiao <wdx7266@vip.qq.com>
2019-03-28 16:17:01 +0800

    split cpp file for head file

--- src/Ontology/OntTxBuilder.h
+++ src/Ontology/OntTxBuilder.h
@@ -18,56 +18,13 @@ namespace Ontology {
 class OntTxBuilder {
 
   public:
-    static TW_Ontology_Proto_SigningOutput decimals(const Ontology::Proto::SigningInput& input) {
-        auto queryAddress = Address(input.query_address());
-        auto transaction = Ont().balanceOf(queryAddress);
-        auto encoded = transaction.serialize();
-        auto protoOutput = Proto::SigningOutput();
-        protoOutput.set_encoded(encoded.data(), encoded.size());
-        auto serialized = protoOutput.SerializeAsString();
-        return TWDataCreateWithBytes(reinterpret_cast<const uint8_t*>(serialized.data()),
-                                     serialized.size());
-    }
+    static TW_Ontology_Proto_SigningOutput decimals(const Ontology::Proto::SigningInput& input);
 
-    static TW_Ontology_Proto_SigningOutput balanceOf(const Ontology::Proto::SigningInput& input) {
-        auto queryAddress = Address(input.query_address());
-        auto transaction = Ont().balanceOf(queryAddress);
-        auto encoded = transaction.serialize();
-        auto protoOutput = Proto::SigningOutput();
-        protoOutput.set_encoded(encoded.data(), encoded.size());
-        auto serialized = protoOutput.SerializeAsString();
-        return TWDataCreateWithBytes(reinterpret_cast<const uint8_t*>(serialized.data()),
-                                     serialized.size());
-    }
+    static TW_Ontology_Proto_SigningOutput balanceOf(const Ontology::Proto::SigningInput& input);
 
-    static TW_Ontology_Proto_SigningOutput transfer(const Ontology::Proto::SigningInput& input) {
-        auto payerSigner = Signer(input.payer_private_key());
-        auto fromSigner = Signer(input.owner_private_key());
-        auto toAddress = Address(input.to_address());
-        auto tranferTx = Ont().transfer(fromSigner, toAddress, input.amount(), payerSigner,
-                                        input.gas_price(), input.gas_limit());
-        auto encoded = tranferTx.serialize();
-        auto protoOutput = Proto::SigningOutput();
-        protoOutput.set_encoded(encoded.data(), encoded.size());
-        auto serialized = protoOutput.SerializeAsString();
-        return TWDataCreateWithBytes(reinterpret_cast<const uint8_t*>(serialized.data()),
-                                     serialized.size());
-    }
+    static TW_Ontology_Proto_SigningOutput transfer(const Ontology::Proto::SigningInput& input);
 
-    static TW_Ontology_Proto_SigningOutput build(const Ontology::Proto::SigningInput& input) {
-        auto method = std::string(input.method().begin(), input.method().end());
-        if (method == "transfer") {
-            return OntTxBuilder::transfer(input);
-        }
-        if (method == "balanceOf") {
-            return OntTxBuilder::balanceOf(input);
-        }
-        if (method == "decimals") {
-            return OntTxBuilder::decimals(input);
-        }
-        std::vector<uint8_t> nullData;
-        return TWDataCreateWithBytes(nullData.data(), nullData.size());
-    }
+    static TW_Ontology_Proto_SigningOutput build(const Ontology::Proto::SigningInput& input);
 };
 
 } // namespace Ontology
