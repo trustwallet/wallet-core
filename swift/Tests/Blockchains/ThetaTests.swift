6782da0dc92f22e0d5fa93243fc678a2d7a302ce
growlot <growlot.71@gmail.com>
2019-03-27 23:42:03 -0700

    Fix Theta test case in swift project

--- swift/Tests/Blockchains/ThetaTests.swift
+++ swift/Tests/Blockchains/ThetaTests.swift
@@ -12,10 +12,10 @@ class ThetaTests: XCTestCase {
         let input = TW_Theta_Proto_SigningInput.with {
             $0.chainId = "privatenet"
             $0.toAddress = "0x9F1233798E905E173560071255140b4A8aBd3Ec6"
-            $0.thetaAmount = 10
-            $0.tfuelAmount = 20
+            $0.thetaAmount = Data(hexString: "0a")!
+            $0.tfuelAmount = Data(hexString: "14")!
             $0.sequence = 1
-            $0.fee = 1000000000000
+            $0.fee = Data(hexString: "e8d4a51000")!
             $0.privateKey = Data(hexString: "0x93a90ea508331dfdf27fb79757d4250b4e84954927ba0073cd67454ac432c737")!
         }
 
