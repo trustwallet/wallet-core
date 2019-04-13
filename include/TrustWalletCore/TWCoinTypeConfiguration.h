668158d9e14d6873cf62e25394d534d978e479be
armatusmiles <akurotych@gmail.com>
2019-04-13 13:43:01 +0300

    Add comments to TWCoinTypeConfiguration.h

--- include/TrustWalletCore/TWCoinTypeConfiguration.h
+++ include/TrustWalletCore/TWCoinTypeConfiguration.h
@@ -12,23 +12,29 @@
 TW_EXTERN_C_BEGIN
 
 TW_EXPORT_STRUCT
-struct TWCoinTypeConfiguration { 
+struct TWCoinTypeConfiguration {
     uint8_t unused; // C doesn't allow zero-sized struct
 };
 
+/// Returns stock symbol of coin
 TW_EXPORT_STATIC_METHOD
-TWString *_Nonnull TWCoinTypeConfigurationGetSymbol(enum TWCoinType type);
+TWString* _Nonnull TWCoinTypeConfigurationGetSymbol(enum TWCoinType type);
 
+/// Returns max count decimal places for minimal coin unit
 TW_EXPORT_STATIC_METHOD
 int TWCoinTypeConfigurationGetDecimals(enum TWCoinType type);
 
+/// Returns transaction url in blockchain explorer
 TW_EXPORT_STATIC_METHOD
-TWString *_Nonnull TWCoinTypeConfigurationGetTransactionURL(enum TWCoinType type, TWString *_Nonnull transactionID);
+TWString* _Nonnull TWCoinTypeConfigurationGetTransactionURL(enum TWCoinType type,
+                                                            TWString* _Nonnull transactionID);
 
+/// Returns full name of coin in lower case
 TW_EXPORT_STATIC_METHOD
-TWString *_Nonnull TWCoinTypeConfigurationGetID(enum TWCoinType type);
+TWString* _Nonnull TWCoinTypeConfigurationGetID(enum TWCoinType type);
 
+/// Returns full name of coin
 TW_EXPORT_STATIC_METHOD
-TWString *_Nonnull TWCoinTypeConfigurationGetName(enum TWCoinType type);
+TWString* _Nonnull TWCoinTypeConfigurationGetName(enum TWCoinType type);
 
 TW_EXTERN_C_END
