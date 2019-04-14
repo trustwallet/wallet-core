fa17ef2e8a8cffcab6e48039ce89b295e48cc9f3
daviyang35 <daviyang35@gmail.com>
2019-04-14 12:17:51 +0800

    Add NULS TransactionPlan test TransactionPlan calculate max amount

--- src/NULS/TransactionBuilder.h
+++ src/NULS/TransactionBuilder.h
@@ -16,26 +16,36 @@ struct TransactionBuilder {
 
     /// Plans a transaction by selecting UTXOs and calculating fees.
     static Proto::TransactionPlan plan(const NULS::Proto::TransactionPurpose& purpose) {
-        auto plan = Proto::TransactionPlan();
-        plan.set_amount(purpose.amount());
-
-        if (purpose.amount() == 0 || purpose.utxos_size() == 0) {
+        if (purpose.utxos_size() == 0) {
             return Proto::TransactionPlan{};
         }
 
+        if (purpose.use_max_amount()) {
+            return calculatorMaxAmountPlan(purpose);
+        }
+
+        if (purpose.amount() == 0) {
+            return Proto::TransactionPlan{};
+        }
+
+        // set common field first
+        auto plan = Proto::TransactionPlan();
+        plan.set_amount(purpose.amount());
         plan.set_timestamp(purpose.timestamp());
         plan.set_private_key(purpose.private_key());
         plan.set_from_address(purpose.from_address());
         plan.set_to_address(purpose.to_address());
         plan.set_remark(purpose.remark());
 
-        int64_t availableAmount = 0;
+        // Calculate all UTXO available amount
+        uint64_t availableAmount = 0;
         for (auto& utxo : purpose.utxos()) {
             availableAmount += utxo.amount();
         }
         plan.set_available_amount(availableAmount);
 
-        if (availableAmount < plan.amount()) {
+        // all UTXO available amount less that transfer amount, error
+        if (!purpose.use_max_amount() && availableAmount < plan.amount()) {
             return Proto::TransactionPlan{};
         }
 
@@ -51,38 +61,53 @@ struct TransactionBuilder {
         auto sortedUtxos = purpose.utxos();
         std::sort(sortedUtxos.begin(), sortedUtxos.end(),
                   [](const Proto::TransactionInput& lhs, const Proto::TransactionInput& rhs) {
-                      return lhs.amount() < rhs.amount();
+                      return lhs.amount() > rhs.amount();
                   });
 
         std::vector<Proto::TransactionInput> selectUtxos;
         int64_t values = 0;
-        for (int i = 0; i < sortedUtxos.size(); ++i) {
-            auto utxo = sortedUtxos[i];
+        int64_t fee = 0;
+        for (auto utxo : sortedUtxos) {
             selectUtxos.push_back(utxo);
             values += utxo.amount();
-            int64_t fee = calculatorTransactionFee(selectUtxos.size(), 1, purpose.remark().size());
+            // We need recalculate transaction fee when we insert an new utxo record
+            int64_t txSize =
+                calculatorTransactionSize(selectUtxos.size(), 1, purpose.remark().size());
+            fee = calculatorTransactionFee(txSize);
 
-            if (values > (purpose.amount() + fee)) {
-                fee = calculatorTransactionFee(selectUtxos.size(), 2, purpose.remark().size());
+            if (values == (purpose.amount() + fee)) {
+                // selected uxto amount equal spent and fee.
+                break;
+            } else if (values > (purpose.amount() + fee)) {
+                // all amount bigger that spent and fee, need change back
+
+                // 找零增加了一条UTXO，要重新计算手续费
+                txSize = calculatorTransactionSize(selectUtxos.size(), 2, purpose.remark().size());
+                fee = calculatorTransactionFee(txSize);
+                // 选择金额小于 转出金额 + 手续费，追加 UTXO
                 if (values < (purpose.amount() + fee)) {
+                    // don't have enough input amount because change back output
                     continue;
                 }
 
+                // 否则的话，写入找零输出
                 Proto::TransactionOutput changeOutput;
                 changeOutput.set_amount(values - purpose.amount() - fee);
                 changeOutput.set_to_address(purpose.from_address());
                 changeOutput.set_index(1);
                 changeOutput.set_lock_time(0);
                 outputs.push_back(changeOutput);
-            }
-
-            plan.set_fee(fee);
-            plan.set_change(values - purpose.amount() - fee);
-
-            if (values >= (purpose.amount() + fee)) {
                 break;
+            } else {
+                if (txSize > MAX_TRANSACTION_SIZE) {
+                    // Can't execute purpose transaction because transaction size limit
+                    return Proto::TransactionPlan{};
+                }
             }
         }
+
+        plan.set_fee(fee);
+        plan.set_change(values - purpose.amount() - fee);
 
         *plan.mutable_inputs() = {selectUtxos.begin(), selectUtxos.end()};
         *plan.mutable_outputs() = {outputs.begin(), outputs.end()};
@@ -90,18 +115,86 @@ struct TransactionBuilder {
         return plan;
     }
 
-    static int64_t calculatorTransactionFee(uint32_t inputCount, uint32_t outputCount,
-                                            uint32_t remarkSize) {
-        uint64_t size = 124 + 50 * inputCount + 38 * outputCount + remarkSize;
-        if (size > 300 * 1024) {
-            throw std::invalid_argument("transaction size too big");
+    static Proto::TransactionPlan
+    calculatorMaxAmountPlan(const NULS::Proto::TransactionPurpose& purpose) {
+        auto plan = Proto::TransactionPlan();
+        // 最大可转账计划，计算付掉手续费后最多能转的资金，不找零
+
+        uint64_t maxInputs = calculatorMaxInput(purpose.remark().size());
+        uint64_t selectCount = std::min((uint64_t)purpose.utxos_size(), maxInputs);
+
+        auto sortedUtxos = purpose.utxos();
+        std::sort(sortedUtxos.begin(), sortedUtxos.end(),
+            [](const Proto::TransactionInput& lhs, const Proto::TransactionInput& rhs) {
+              return lhs.amount() > rhs.amount();
+            });
+
+        std::vector<Proto::TransactionInput> selectUtxos;
+        uint64_t newAmount = 0;
+        for (int i = 0; i < selectCount; ++i) {
+            auto utxo = sortedUtxos[i];
+            newAmount += utxo.amount();
+            selectUtxos.push_back(utxo);
         }
-        return calculateFee(size);
+
+        uint64_t txSize = calculatorTransactionSize(selectCount, 1, purpose.remark().size());
+        uint64_t fee = calculatorTransactionFee(txSize);
+        if (newAmount < fee) {
+            return Proto::TransactionPlan{};
+        }
+
+        plan.set_available_amount(newAmount);
+        plan.set_amount(newAmount - fee);
+        plan.set_fee(fee);
+        plan.set_change(0L);
+
+        Proto::TransactionOutput output;
+        output.set_amount(plan.amount());
+        output.set_to_address(purpose.to_address());
+        output.set_index(0);
+        output.set_lock_time(0);
+
+        std::vector<Proto::TransactionOutput> outputs;
+        outputs.push_back(output);
+
+        *plan.mutable_inputs() = {selectUtxos.begin(), selectUtxos.end()};
+        *plan.mutable_outputs() = {outputs.begin(), outputs.end()};
+        return plan;
     }
 
+    static int32_t getTransactionFee(uint32_t inputCount, uint32_t outputCount,
+                                     uint32_t remarkSize) {
+        uint64_t size = 124 + TRANSACTION_INPUT_SIZE * inputCount +
+                        TRANSACTION_OUTPUT_SIZE * outputCount + remarkSize;
+        return calculatorTransactionFee(size);
+    }
+
+    static int32_t calculatorMaxInput(uint32_t remarkSize) {
+        uint32_t outputSize = 1 * TRANSACTION_OUTPUT_SIZE;
+        uint32_t maxInputs =
+            (MAX_TRANSACTION_SIZE - TRANSACTION_HEAD_SIZE - remarkSize - outputSize) / 50;
+        if ((MAX_TRANSACTION_SIZE - TRANSACTION_HEAD_SIZE - remarkSize - outputSize) % 50 != 0) {
+            maxInputs -= 1;
+        }
+        return maxInputs;
+    }
+
+    static int32_t calculatorTransactionSize(uint32_t inputCount, uint32_t outputCount,
+                                             uint32_t remarkSize) {
+        uint64_t size = 124 + TRANSACTION_INPUT_SIZE * inputCount +
+                        TRANSACTION_OUTPUT_SIZE * outputCount + remarkSize;
+        return size;
+    }
+
+    static const uint16_t TRANSACTION_HEAD_SIZE = 124;
+    static const uint16_t TRANSACTION_INPUT_SIZE = 50;
+    static const uint16_t TRANSACTION_OUTPUT_SIZE = 38;
+    /// Transaction size must less that 300KB
+    static const uint64_t MAX_TRANSACTION_SIZE = 300 * 1024;
+    /// 0.001 NULS per KB
     static const uint64_t MIN_PRICE_PRE_1024_BYTES = 100000;
 
-    static inline uint64_t calculateFee(uint64_t size) {
+    static inline uint64_t calculatorTransactionFee(uint64_t size) {
         uint64_t fee = (size / 1024) * MIN_PRICE_PRE_1024_BYTES;
         if (size % 1024 > 0) {
             fee += MIN_PRICE_PRE_1024_BYTES;
