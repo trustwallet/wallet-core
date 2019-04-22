030e3e4ec69a42ee6bee169972224698875fac1e
Johnny Jiang <johnny.nan.jiang@gmail.com>
2019-04-22 12:19:40 +1000

    Support for Cosmos - add staking (#343)
    * Cosmos support - add staking message proto and serialization, and add test
    
    * Cosmos support - update signing test with tx on gaia-13003 testnet
    
    * Cosmos support - implement staking and update the test
    
    * Cosmos support - rename message to send coins message
    
    * Cosmos support - refactor Serialization.cpp
    
    * Cosmos support - rename Coin to Amount
    
    * Cosmos support - fix iOS test
    
    * Cosmos support - add iOS test for staking
    
    * Cosmos support - update signing tx test for gaia-13003 testnet
    
    * Update  Cosmos Swift tests to use alias and .with{}

--- tests/Cosmos/SignerTests.cpp
+++ tests/Cosmos/SignerTests.cpp
@@ -19,27 +19,25 @@ namespace TW::Cosmos {
 TEST(CosmosSigner, SignTx) {
     auto input = Proto::SigningInput();
     input.set_account_number(1037);
-    input.set_chain_id("gaia-13002");
+    input.set_chain_id("gaia-13003");
     input.set_memo("");
-    input.set_sequence(2);
+    input.set_sequence(8);
 
     auto fromAddress = TW::Cosmos::Address("cosmos", parse_hex("BC2DA90C84049370D1B7C528BC164BC588833F21"));
     auto toAddress = TW::Cosmos::Address("cosmos", parse_hex("12E8FE8B81ECC1F4F774EA6EC8DF267138B9F2D9"));
 
-    auto message = Proto::SendCoinsMessage();
+    auto &message = *input.mutable_send_coins_message();
     message.set_from_address(fromAddress.string());
     message.set_to_address(toAddress.string());
-    auto amountOfTx = message.add_amount();
+    auto amountOfTx = message.add_amounts();
     amountOfTx->set_denom("muon");
     amountOfTx->set_amount(1);
-    *input.mutable_message() = message;
     
-    auto fee = Proto::Fee();
+    auto &fee = *input.mutable_fee();
     fee.set_gas(200000);
-    auto amountOfFee = fee.add_amount();
+    auto amountOfFee = fee.add_amounts();
     amountOfFee->set_denom("muon");
-    amountOfFee->set_amount(1);
-    *input.mutable_fee() = fee;
+    amountOfFee->set_amount(200);
 
     auto privateKey = parse_hex("80e81ea269e66a0a05b11236df7919fb7fbeedba87452d667489d7403a02f005");
     input.set_private_key(privateKey.data(), privateKey.size());
@@ -48,7 +46,7 @@ TEST(CosmosSigner, SignTx) {
     auto signature = signer.sign();
     auto signatureInBase64 = signer.signInBase64();
 
-    ASSERT_EQ("UCnIs7gl7opPxYlrhQGqs25ySPIkPgivO1/ROPNUf451/c5QdrFX5unbXSwfjvW9H80c21JYzNrNnqHuGUCSUw==", signatureInBase64);
+    ASSERT_EQ("/D74mdIGyIB3/sQvIboLTfS9P9EV/fYGrgHZE2/vNj9X6eM6e57G3atljNB+PABnRw3pTk51uXmhCFop8O/ZJg==", signatureInBase64);
 
     auto txInJsonString = signer.buildTransaction();
 
@@ -58,7 +56,7 @@ TEST(CosmosSigner, SignTx) {
             "\"value\":{"
                 "\"fee\":{"
                     "\"amount\":["
-                                    "{\"amount\":\"1\",\"denom\":\"muon\"}"
+                                    "{\"amount\":\"200\",\"denom\":\"muon\"}"
                                 "],"
                     "\"gas\":\"200000\""
                 "},"
@@ -81,7 +79,7 @@ TEST(CosmosSigner, SignTx) {
                                             "\"type\":\"tendermint/PubKeySecp256k1\","
                                             "\"value\":\"AlcobsPzfTNVe7uqAAsndErJAjqplnyudaGB0f+R+p3F\""
                                         "},"
-                                        "\"signature\":\"UCnIs7gl7opPxYlrhQGqs25ySPIkPgivO1/ROPNUf451/c5QdrFX5unbXSwfjvW9H80c21JYzNrNnqHuGUCSUw==\""
+                                        "\"signature\":\"/D74mdIGyIB3/sQvIboLTfS9P9EV/fYGrgHZE2/vNj9X6eM6e57G3atljNB+PABnRw3pTk51uXmhCFop8O/ZJg==\""
                                     "}"
                                 "]"
             "}"
@@ -90,9 +88,12 @@ TEST(CosmosSigner, SignTx) {
 
         auto txInBytes = signer.build();
 
-        ASSERT_EQ("a264747970656a617574682f53746454786576616c7565a463666565a266616d6f756e7481a266616d6f756e7461316564656e6f6d646d756f6e6367617366323030303030646d656d6f60636d736781a2647479706572636f736d6f732d73646b2f4d736753656e646576616c7565a366616d6f756e7481a266616d6f756e7461316564656e6f6d646d756f6e6c66726f6d5f61646472657373782d636f736d6f733168736b366a727979716a6668703564686335357463396a74636b7967783065706836646430326a746f5f61646472657373782d636f736d6f73317a743530617a7570616e716c66616d356166687633686578777975746e756b656834633537336a7369676e61747572657381a2677075625f6b6579a26474797065781a74656e6465726d696e742f5075624b6579536563703235366b316576616c7565782c416c636f6273507a66544e56653775714141736e6445724a416a71706c6e79756461474230662b522b703346697369676e6174757265785855436e497337676c376f705078596c7268514771733235795350496b506769764f312f524f504e55663435312f6335516472465835756e62585377666a7657394838306332314a597a4e724e6e7148754755435355773d3d", hex(txInBytes));
+        ASSERT_EQ("a264747970656a617574682f53746454786576616c7565a463666565a266616d6f756e7481a266616d6f756e74633230306564656e6f6d646d756f6e6367617366323030303030646d656d6f60636d736781a2647479706572636f736d6f732d73646b2f4d736753656e646576616c7565a366616d6f756e7481a266616d6f756e7461316564656e6f6d646d756f6e6c66726f6d5f61646472657373782d636f736d6f733168736b366a727979716a6668703564686335357463396a74636b7967783065706836646430326a746f5f61646472657373782d636f736d6f73317a743530617a7570616e716c66616d356166687633686578777975746e756b656834633537336a7369676e61747572657381a2677075625f6b6579a26474797065781a74656e6465726d696e742f5075624b6579536563703235366b316576616c7565782c416c636f6273507a66544e56653775714141736e6445724a416a71706c6e79756461474230662b522b703346697369676e617475726578582f4437346d644947794942332f73517649626f4c54665339503945562f6659477267485a45322f764e6a395836654d36653537473361746c6a4e422b5041426e52773370546b353175586d6843466f70384f2f5a4a673d3d", hex(txInBytes));
 
-        // the sample tx on testnet https://hubble.figment.network/chains/gaia-13002/blocks/269400/transactions/92D3D8CCE20C164409F91844A08A7FB7A3B700A3CC555C4B1CF51D44777F60F9?format=json
+        /*
+            the sample tx on testnet 
+            https://hubble.figment.network/chains/gaia-13003/blocks/142933/transactions/3A9206598C3D2E75A5EC074FD33EA53EB18EC729357F0965971C1C51F812AEA3?format=json
+        */
 }
 
 }
---
+++ tests/Cosmos/StakingTests.cpp
@@ -0,0 +1,94 @@
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Coin.h"
+#include "HDWallet.h"
+#include "HexCoding.h"
+#include "proto/Cosmos.pb.h"
+#include "Cosmos/Address.h"
+#include "Cosmos/Signer.h"
+
+#include <TrustWalletCore/TWHRP.h>
+#include <gtest/gtest.h>
+
+namespace TW::Cosmos {
+
+TEST(CosmosStaking, Staking) {
+    auto input = Proto::SigningInput();
+    input.set_account_number(1037);
+    input.set_chain_id("gaia-13003");
+    input.set_memo("");
+    input.set_sequence(7);
+
+    auto& message = *input.mutable_stake_message();
+    message.set_delegator_address("cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02");
+    message.set_validator_address("cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp");
+    auto& amountOfTx = *message.mutable_amount();
+    amountOfTx.set_denom("muon");
+    amountOfTx.set_amount(10);
+
+    auto &fee = *input.mutable_fee();
+    fee.set_gas(101721);
+    auto amountOfFee = fee.add_amounts();
+    amountOfFee->set_denom("muon");
+    amountOfFee->set_amount(1018);
+
+    auto privateKey = parse_hex("80e81ea269e66a0a05b11236df7919fb7fbeedba87452d667489d7403a02f005");
+    input.set_private_key(privateKey.data(), privateKey.size());
+
+    auto signer = Cosmos::Signer(std::move(input));
+    auto signature = signer.sign();
+    auto signatureInBase64 = signer.signInBase64();
+
+    ASSERT_EQ("wIvfbCsLRCjzeXXoXTKfHLGXRbAAmUp0O134HVfVc6pfdVNJvvzISMHRUHgYcjsSiFlLyR32heia/yLgMDtIYQ==", signatureInBase64);
+    
+    auto txInJsonString = signer.buildTransaction();
+
+    ASSERT_EQ(
+        "{"
+            "\"type\":\"auth/StdTx\","
+            "\"value\":{"
+                "\"fee\":{"
+                    "\"amount\":["
+                                    "{\"amount\":\"1018\",\"denom\":\"muon\"}"
+                                "],"
+                    "\"gas\":\"101721\""
+                "},"
+                "\"memo\":\"\","
+                "\"msg\":["
+                        "{"
+                            "\"type\":\"cosmos-sdk/MsgDelegate\","
+                            "\"value\":{"
+                                "\"amount\":{\"amount\":\"10\",\"denom\":\"muon\"},"
+                                "\"delegator_address\":\"cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02\","
+                                "\"validator_address\":\"cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp\""
+                            "}"
+                        "}"
+                    "],"
+                "\"signatures\":["
+                                    "{"
+                                        "\"pub_key\":{"
+                                            "\"type\":\"tendermint/PubKeySecp256k1\","
+                                            "\"value\":\"AlcobsPzfTNVe7uqAAsndErJAjqplnyudaGB0f+R+p3F\""
+                                        "},"
+                                        "\"signature\":\"wIvfbCsLRCjzeXXoXTKfHLGXRbAAmUp0O134HVfVc6pfdVNJvvzISMHRUHgYcjsSiFlLyR32heia/yLgMDtIYQ==\""
+                                    "}"
+                                "]"
+            "}"
+        "}", 
+        txInJsonString);
+
+        auto txInBytes = signer.build();
+
+        ASSERT_EQ("a264747970656a617574682f53746454786576616c7565a463666565a266616d6f756e7481a266616d6f756e7464313031386564656e6f6d646d756f6e6367617366313031373231646d656d6f60636d736781a2647479706576636f736d6f732d73646b2f4d736744656c65676174656576616c7565a366616d6f756e74a266616d6f756e746231306564656e6f6d646d756f6e7164656c656761746f725f61646472657373782d636f736d6f733168736b366a727979716a6668703564686335357463396a74636b7967783065706836646430327176616c696461746f725f616464726573737834636f736d6f7376616c6f706572317a6b757072383368727a6b6e33757035656c6b747a63713374756674386e78736d77647167706a7369676e61747572657381a2677075625f6b6579a26474797065781a74656e6465726d696e742f5075624b6579536563703235366b316576616c7565782c416c636f6273507a66544e56653775714141736e6445724a416a71706c6e79756461474230662b522b703346697369676e61747572657858774976666243734c52436a7a6558586f58544b66484c4758526241416d5570304f313334485666566336706664564e4a76767a49534d485255486759636a735369466c4c79523332686569612f794c674d44744959513d3d", hex(txInBytes));
+
+        /*  
+            the sample tx on testnet 
+            https://hubble.figment.network/chains/gaia-13003/blocks/125922/transactions/AAE5E18516DC8B0EF864F91B9531AB63B2248E3FC9058B6A330AE79EF1B4120A?format=json
+        */
+}
+
+}
