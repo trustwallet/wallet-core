9223843854bfccb2fd018aef9393c45487804151
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-04-23 15:47:05 -0700

    Fix another android Test, remove old files before generating

--- android/app/src/androidTest/java/com/trustwallet/core/app/utils/TestSegwitAddress.java
+++ android/app/src/androidTest/java/com/trustwallet/core/app/utils/TestSegwitAddress.java
@@ -1,13 +1,13 @@
 package com.trustwallet.core.app.utils;
 
 import org.junit.Test;
-import wallet.core.jni.Bech32Address;
 import wallet.core.jni.HRP;
 import wallet.core.jni.PublicKey;
+import wallet.core.jni.SegwitAddress;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestBech32Address {
+public class TestSegwitAddress {
 
     static {
         System.loadLibrary("TrustWalletCore");
@@ -17,7 +17,7 @@ public class TestBech32Address {
     public void testFromPublic() {
         byte[] data = Numeric.INSTANCE.hexStringToByteArray("0x02f1e733ed6030cc569c4323a34b17e192d58107d9ffbce71c8420b779f484dba1");
         PublicKey publicKey = new PublicKey(data);
-        Bech32Address address = new Bech32Address(HRP.BITCOIN, publicKey);
+        SegwitAddress address = new SegwitAddress(HRP.BITCOIN, publicKey);
         assertEquals(address.description(), "bc1qrq6gs660qewd282en83n6em9s4rlslj3cd2wmg");
     }
 
@@ -25,7 +25,7 @@ public class TestBech32Address {
     public void testBadHrp() {
         byte[] data = Numeric.INSTANCE.hexStringToByteArray("0x02f1e733ed6030cc569c4323a34b17e192d58107d9ffbce71c8420b779f484dba1");
         PublicKey publicKey = new PublicKey(data);
-        Bech32Address address = new Bech32Address(HRP.BITCOIN, publicKey);
+        SegwitAddress address = new SegwitAddress(HRP.BITCOIN, publicKey);
         assertEquals(address.description(), "bc1qrq6gs660qewd282en83n6em9s4rlslj3cd2wmg");
     }
 }
