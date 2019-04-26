bea2c0cdfaa133b1abb837050673a166c48d7d0e
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2019-04-26 10:47:17 -0700

    Increase trezor-crypto tests timeout, add script to run all tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,9 @@ if [ -x "$(command -v clang-tidy)" ]; then
 fi
 
 echo "#### Testing... ####"
+export CK_TIMEOUT_MULTIPLIER=4
 build/trezor-crypto/tests/TrezorCryptoTests
+
 ROOT="`dirname \"$0\"`"
 TESTS_ROOT="`(cd \"$ROOT/tests\" && pwd)`"
 build/tests/tests "$TESTS_ROOT"
