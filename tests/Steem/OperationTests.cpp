982042e84d527ce1e05862508851526c3751c9f8
Sandip Patel <srpatel19590@gmail.com>
2019-04-26 12:06:34 +0530

    Fixed indentation in tests/Steem/*.cpp

--- tests/Steem/OperationTests.cpp
+++ tests/Steem/OperationTests.cpp
@@ -1,5 +1,5 @@
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 
 #include "Bravo/Operation.h"
 #include "HexCoding.h"
@@ -10,17 +10,12 @@ using namespace TW;
 using namespace TW::Bravo;
 
 TEST(SteemOperation, Serialization) {
-	Data buf;
-	TransferOperation("alice", "bob", Asset(90000, Asset::decimals, "STEEM"), "Eva").serialize(buf);
-	ASSERT_EQ(
-		hex(buf),
-		"0205616c69636503626f62905f01000000000003535445454d000003457661"
-	);
+    Data buf;
+    TransferOperation("alice", "bob", Asset(90000, Asset::decimals, "STEEM"), "Eva").serialize(buf);
+    ASSERT_EQ(hex(buf), "0205616c69636503626f62905f01000000000003535445454d000003457661");
 
-	buf.clear();
-	TransferOperation("Ram", "Sita", Asset(1000, Asset::decimals, "STEEM"), "Lakshman").serialize(buf);
-	ASSERT_EQ(
-		hex(buf),
-		"020352616d0453697461e80300000000000003535445454d0000084c616b73686d616e"
-	);
+    buf.clear();
+    TransferOperation("Ram", "Sita", Asset(1000, Asset::decimals, "STEEM"), "Lakshman")
+        .serialize(buf);
+    ASSERT_EQ(hex(buf), "020352616d0453697461e80300000000000003535445454d0000084c616b73686d616e");
 }
