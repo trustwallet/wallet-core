d85e8f7c657667c0a5905778e71b73e444c7775e
Sotiris Blad <sotiris2009@hotmail.com>
2019-05-05 22:07:33 +0300

    Update CoinTests.cpp

--- tests/CoinTests.cpp
+++ tests/CoinTests.cpp
@@ -176,7 +176,7 @@ TEST(Coin, DeriveAddress) {
     EXPECT_EQ(TW::deriveAddress(TWCoinTypeIoTeX, privateKey), "io1nk9x9ajk4rgkzhqjjn7hr6w0k0jg2kj0zgdt6h");
     EXPECT_EQ(TW::deriveAddress(TWCoinTypeLitecoin, privateKey), "ltc1qhkfq3zahaqkkzx5mjnamwjsfpq2jk7z0tamvsu");
     EXPECT_EQ(TW::deriveAddress(TWCoinTypeViacoin, privateKey), "via1qhkfq3zahaqkkzx5mjnamwjsfpq2jk7z09y9mn2");
-    EXPECT_EQ(TW::deriveAddress(TWCoinTypeMonetaryUnit, privateKey), "7UQUGq9UQD57R5Kn6hHoYrpaRjDdWtZWF7");
+    EXPECT_EQ(TW::deriveAddress(TWCoinTypeMonetaryUnit, privateKey), "mue1qhkfq3zahaqkkzx5mjnamwjsfpq2jk7z0nn7v5y");
     EXPECT_EQ(TW::deriveAddress(TWCoinTypeNimiq, privateKey), "NQ74 D40G N3M0 9EJD ET56 UPLR 02VC X6DU 8G1E");
     EXPECT_EQ(TW::deriveAddress(TWCoinTypeOntology, privateKey), "AeicEjZyiXKgUeSBbYQHxsU1X3V5Buori5");
     EXPECT_EQ(TW::deriveAddress(TWCoinTypePOANetwork, privateKey), "0x9d8A62f656a8d1615C1294fd71e9CFb3E4855A4F");
