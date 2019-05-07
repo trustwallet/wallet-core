3819b60b20acb630510f5cc64b4091591b136213
Sotiris Blad <sotiris2009@hotmail.com>
2019-05-07 16:44:21 +0300

    Update Coin.cpp

--- src/Coin.cpp
+++ src/Coin.cpp
@@ -139,6 +139,7 @@ bool TW::validateAddress(TWCoinType coin, const std::string& string) {
         return Bitcoin::Address::isValid(string, {{TWP2PKHPrefixZcoin}, {TWP2SHPrefixZcoin}});
 
     case TWCoinTypeMonetaryUnit:
+            return Bitcoin::Address::isValid(string, {{TWP2PKHPrefixMonetaryUnit}, {TWP2SHPrefixMonetaryUnit}});
             
     case TWCoinTypeZcash:
         return Zcash::TAddress::isValid(string, {{Zcash::TAddress::staticPrefix, TWP2PKHPrefixZcashT}, {Zcash::TAddress::staticPrefix, TWP2SHPrefixZcashT}});
@@ -254,6 +255,7 @@ std::string TW::deriveAddress(TWCoinType coin, const PublicKey& publicKey) {
         return Tron::Address(publicKey).string();
 
     case TWCoinTypeMonetaryUnit:
+            return Bitcoin::Address(publicKey, TWP2PKHPrefixMonetaryUnit).string();
             
     case TWCoinTypeZcash:
         return Zcash::TAddress(publicKey, TWP2PKHPrefixZcashT).string();
