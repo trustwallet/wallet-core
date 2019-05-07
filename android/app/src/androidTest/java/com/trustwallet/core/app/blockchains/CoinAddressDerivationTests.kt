571119c12dc36a68b55182fb32dc4d1ec808f6ee
Sotiris Blad <sotiris2009@hotmail.com>
2019-05-07 15:16:08 +0300

    Update CoinAddressDerivationTests.kt

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
@@ -72,5 +72,6 @@ class CoinAddressDerivationTests {
         IOTEX -> assertEquals("io1qw9cccecw09q7p5kzyqtuhfhvah2mhfrc84jfk", address)
         ZILLIQA -> assertEquals("0xddb41006f7b6fa8e5fbf06a71c01f789febc66e8", address)
         SEMUX -> assertEquals("0xfe604170382452f77bc922bc19eb4b53504b09c2", address)
+        MONETARYUNIT -> assertEquals("7kC1oLHygVZ6NwuYjof7xYuqmMgovTbtEE", address)
     }
 }
