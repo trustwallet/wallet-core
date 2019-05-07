f78d954c4484b1f70267bc1d9281ee89139c3aac
Sotiris Blad <sotiris2009@hotmail.com>
2019-05-07 20:24:56 +0300

    Update Coin.cpp

--- src/Coin.cpp
+++ src/Coin.cpp
@@ -163,7 +163,7 @@ bool TW::validateAddress(TWCoinType coin, const std::string& string) {
 
     case TWCoinTypeSemux:
         return Semux::Address::isValid(string);
-            
+
     case TWCoinTypeMonetaryUnit:
         return Bitcoin::Address::isValid(string, {{TWP2PKHPrefixMonetaryUnit}, {TWP2SHPrefixMonetaryUnit}});
     }
