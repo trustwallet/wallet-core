3b4ace25d9d4965720ca64958c859a239f10461c
Sotiris Blad <sotiris2009@hotmail.com>
2019-05-10 21:35:17 +0300

    Create TWMonetaryUnitTransactionTests.cpp

---
+++ tests/interface/TWMonetaryUnitTransactionTests.cpp
@@ -0,0 +1,31 @@
+
+// Copyright © 2017-2019 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "TWTestUtilities.h"
+
+#include "Bitcoin/OutPoint.h"
+#include "Bitcoin/TransactionBuilder.h"
+#include "Bitcoin/TransactionSigner.h"
+#include "HexCoding.h"
+#include "PublicKey.h"
+#include "Bitcoin/Transaction.h"
+
+#include <TrustWalletCore/TWBitcoinScript.h>
+
+#include <gtest/gtest.h>
+
+using namespace TW;
+using namespace Bitcoin;
+
+TEST(MonetaryUnitTransaction, Encode) {
+    continue;
+}
+
+TEST(MonetaryUnitTransaction, Signing) {
+  continue;
+    );
+}
