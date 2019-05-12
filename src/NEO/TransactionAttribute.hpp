87d60e75f170b4e587565c473640566f7a36edf8
Rodolfo Araujo <rodoufu@gmail.com>
2019-05-12 19:26:04 -0300

    TransactionAttribute fix and unit test

--- src/NEO/TransactionAttribute.hpp
+++ src/NEO/TransactionAttribute.hpp
@@ -18,29 +18,31 @@ namespace TW::NEO {
         }
 
         void deserialize(const Data &data, int initial_pos = 0) override {
+            usage = (TransactionAttributeUsage) readBytes(data, 1, initial_pos)[0];
             if (data.size() + initial_pos < 1) {
                 throw std::invalid_argument("Invalid data for deserialization");
             }
             if (usage == TransactionAttributeUsage::TAU_ContractHash || usage == TransactionAttributeUsage::TAU_Vote ||
                 (usage >= TransactionAttributeUsage::TAU_Hash1 && usage <= TransactionAttributeUsage::TAU_Hash15)) {
-                this->data = readBytes(data, 32, initial_pos);
+                this->data = readBytes(data, 32, initial_pos + 1);
             } else if (usage == TransactionAttributeUsage::TAU_ECDH02 ||
                        usage == TransactionAttributeUsage::TAU_ECDH03) {
-                this->data = concat(Data(1, (byte) usage), readBytes(data, 32, initial_pos));
+                this->data = readBytes(data, 32, initial_pos + 1);
             } else if (usage == TransactionAttributeUsage::TAU_Script) {
-                this->data = readBytes(data, 20, initial_pos);
+                this->data = readBytes(data, 20, initial_pos + 1);
             } else if (usage == TransactionAttributeUsage::TAU_DescriptionUrl) {
-                this->data =readBytes(data, 1, initial_pos);;
+                this->data = readBytes(data, 1, initial_pos + 1);
             } else if (usage == TransactionAttributeUsage::TAU_Description ||
                        usage >= TransactionAttributeUsage::TAU_Remark) {
-                this->data = readBytes(data, types::MaxValue<ushort>(), initial_pos);
+//                this->data = readBytes(data, types::MaxValue<ushort>(), initial_pos + 1);
+                this->data = readBytes(data, data.size() - 1 - initial_pos, initial_pos + 1);
             } else {
                 throw std::invalid_argument("TransactionAttribute Deserialize FormatException");
             }
         }
 
         Data serialize() const override {
-            return concat(Data({static_cast<unsigned char>(usage)}), data);
+            return concat(Data({static_cast<byte>(usage)}), data);
         }
     };
 }
