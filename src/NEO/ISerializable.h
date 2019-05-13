a5dffc29a2efb146382ad8afd38898e027c072cc
Rodolfo Araujo <rodoufu@gmail.com>
2019-05-13 11:37:12 -0700

    Serialize array

--- src/NEO/ISerializable.h
+++ src/NEO/ISerializable.h
@@ -6,8 +6,11 @@
 
 #pragma once
 
+#include <vector>
 #include <cstdint>
+#include <iterator>
 #include "../Data.h"
+#include "../ReadData.h"
 
 namespace TW::NEO {
     class ISerializable {
@@ -16,6 +19,32 @@ namespace TW::NEO {
         virtual int64_t size() const = 0;
         virtual Data serialize() const = 0;
         virtual void deserialize(const Data &data, int initial_pos = 0) = 0;
+
+        template<class T>
+        static inline Data serialize(const std::vector<T> & data) {
+            return serialize(data.begin(), data.end());
+        }
+
+        template<class T>
+        static inline Data serialize(const T* data, int size) {
+            return serialize(data, data + size);
+        }
+
+        template<class T>
+        static inline Data serialize(std::iterator<std::input_iterator_tag, T> begin, std::iterator<std::input_iterator_tag, T> end) {
+            Data resp(writeVarUInt(end - begin));
+            for (auto it = begin; it < end; ++it) {
+                append(resp, it->serialize());
+            }
+            return resp;
+        }
+
+        /*template<class T>
+        static inline Data deserialize(const Data &data, int initial_pos = 0) {
+            ulong size = readVarUInt(data, initial_pos);
+            return serialize(data.begin(), data.end());
+        }*/
+        
     };
 
 } // namespace TW::NEO
