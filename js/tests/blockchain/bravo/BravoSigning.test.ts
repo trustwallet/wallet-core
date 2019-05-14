3e5497e0b50f7dcd1ef64d34252ecfb3a56660f3
Sathvik Birudavolu <sathvikb30@gmail.com>
2019-05-14 18:03:05 -0500

    Add JS tests for Bravo, EOS, Cosmos & Fix uncaught exceptions

--- js/tests/blockchain/bravo/BravoSigning.test.ts
+++ js/tests/blockchain/bravo/BravoSigning.test.ts
@@ -16,7 +16,7 @@ describe('BravoSigning', () => {
             memo: 'Eva',
             referenceBlockId: fromHexString('0000086bf9e7704509aa41311a66fa0a1b479c6b'),
             referenceBlockTime: 1552464180,
-            privateKey: Hash.sha256(fromHexString('A'))
+            privateKey: Hash.sha256(Buffer.from('A'))
         });
 
         const result: TW.Proto.Result = BravoSigner.sign(bravoSigningInput);
@@ -47,6 +47,51 @@ describe('BravoSigning', () => {
         expect(signatureValue).to.not.be.undefined;
 
         expect(signatureValue).to.equal('1f05b3dddfcbca5748b02562645fe7c3f01044b78983ce673f84c230b2dc97beed19b2e8462f1e45f2ac7600c2900f9f90510efe0891141ad2c6b1ae33b21bcace');
+    });
+
+    it('test failures', () => {
+        const goodInput: TW.Bravo.Proto.SigningInput = TW.Bravo.Proto.SigningInput.create({
+            chainId: new Uint8Array(32),
+            sender: 'alice',
+            recipient: 'bob',
+            amount: 90.0,
+            testnet: false,
+            memo: 'Eva',
+            referenceBlockId: fromHexString('0000086bf9e7704509aa41311a66fa0a1b479c6b'),
+            referenceBlockTime: 1552464180,
+            privateKey: Hash.sha256(Buffer.from('A'))
+        });
+
+
+        let badinput = goodInput;
+        badinput.amount = -90.00;
+        let result; 
+        try {
+            result = BravoSigner.sign(badinput);
+        } catch (e) {
+            console.log('ok this works');
+        }
+        if (result === undefined) {
+            console.log('its undeinfe');
+            return;
+        }
+        expect(result.success).to.be.false;
+
+
+        badinput = goodInput;
+        badinput.sender = 'A'.repeat(17);
+        result = BravoSigner.sign(badinput);
+        expect(result.success).to.be.false;
+
+        badinput = goodInput;
+        badinput.memo = 'A'.repeat(2049);
+        result = BravoSigner.sign(badinput);
+        expect(result.success).to.be.false;
+
+        badinput = goodInput;
+        badinput.referenceBlockId = fromHexString('0000086bf9e7704509aa41311a66fa0a1b479c');
+        result = BravoSigner.sign(badinput);
+        expect(result.success).to.be.false;
     });
 
 });
