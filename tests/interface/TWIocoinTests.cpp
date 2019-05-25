1a1e4bfa85584f57aa63bf99b253533ee578ae53
iocrypto <dhatton@protonmail.com>
2019-05-25 23:20:25 +0000

    iocoin - added transaction classes and signing tests

--- tests/interface/TWIocoinTests.cpp
+++ tests/interface/TWIocoinTests.cpp
@@ -28,7 +28,7 @@ TEST(Iocoin, ExtendedKeys) {
 
 TEST(Iocoin, DeriveFromXpub) {
     auto xpub = STRING("xpub6DHj382cGmV2xBnzy7KaLLBqac3qwaBcHZL1BLzjaNH9R6kye9ix45TBHjBYvS9MdfQFWtSSctJZpdF6wm4pnN1eikCvJ7N9NRAZwGyFipn");
-    auto pubKey0 = TWHDWalletGetPublicKeyFromExtended(xpub.get(), TWCoinTypeIocoin, TWHDVersionXPUB, TWHDVersionXPRV, 0, 0);
+    auto pubKey0 = TWHDWalletGetPublicKeyFromExtended(xpub.get(), STRING("m/44'/295'/0'/0/0").get());
     auto address0 = WRAPS(TWCoinTypeDeriveAddressFromPublicKey(TWCoinTypeIocoin, pubKey0));
 
     assertStringsEqual(address0, "idi85QBDyPiLyi89HvAA4jY1hJS66M3wsL");
