ebb7d0c0f01f00dab47464280e2ea75b56e3ff37
Sotiris Blad <26393434+sotblad@users.noreply.github.com>
2019-05-27 17:46:52 +0300

    Update TWMonetaryUnitTransactionTests.cpp

--- tests/interface/TWMonetaryUnitTransactionTests.cpp
+++ tests/interface/TWMonetaryUnitTransactionTests.cpp
@@ -23,43 +23,47 @@ using namespace Bitcoin;
 
 /*
  https://iancoleman.io/bip39/ and https://www.mobilefish.com/download/ethereum/hd_wallet.html
- Mnemonic - shoot island position soft burden budget tooth cruel issue economy destroy above
- m/44'/20'/0'/0/0 Private key in Base58 encoding - L37s29UXmQowW7y5BeDhe8AwM7quvRS2FmkWxSuctw9PbFprhhqA
- m/44'/20'/0'/0/0 Private key in bytes - b00420bab8b663f0870ee8e46435743ba9588eb88d8d31410ed54afa67602e8d
- 
- https://blockbook.monetaryunit.org/tx/c0090762084b77656cef7a4730f4dee6dcc66d9b4fbe48fd2ef1f4f4a8031f22
- 
+ Mnemonic - salute unfair glide naive deputy march trial phrase blouse control home staff injury draft learn achieve issue album
+ m/44'/31'/0'/0/0 Private key in Base58 encoding - KnU39LFZH2uAm6Uocmo2SrTi95yXpw84TRm3mGays5R8e1XPQJ7u
+ m/44'/31'/0'/0/0 Private key in bytes - fad56f32be3d4d1f3672716a0695c7999acda5330d7b95bcd25f46bbf3a11b20
+ https://blockbook.monetaryunit.org/tx/a3681a20f19ded012b11486af269cd585eea9c092dd75550c51c991acb860728
  */
 TEST(MonetaryUnitTransaction, SignTransaction) {
-    const int64_t utxo_amount = 336855835;
-    const int64_t amount = 236853579;
+    const int64_t utxo_amount = 1500000000;
+    const int64_t amount = 499997740;
+    const int64_t fee = 2260;
     
     auto input = Bitcoin::Proto::SigningInput();
-    input.set_hash_type(TWSignatureHashTypeFork | TWSignatureHashTypeAll);
+    input.set_hash_type(TWSignatureHashTypeAll);
     input.set_amount(amount);
     input.set_byte_fee(1);
-    input.set_to_address("7ZemuyZukEnCirZaFZsiQ36KvcdmQ7qEdS");
-    input.set_change_address("7eJR8UQnd5srGL6CQFZfTBbBRdmmoDsapW");
+    input.set_to_address("7bH5T6e99x2xssC8thzqJcxvpQMVFeaAht");
+    input.set_change_address("7XUdVpjrhY31uChwLEBUirUkKQNj8nvcrT");
     
-    auto hash0 = DATA("ecb6856eda9edec8a340709dce9a36b010e256e1331384f7598c09d6b748f118");
+    auto hash0 = DATA("170f786b94e0e287c1cce0214c78006b8c0080382304be67bc6f248ef7f29af1");
     auto utxo0 = input.add_utxo();
     utxo0->mutable_out_point()->set_hash(TWDataBytes(hash0.get()), TWDataSize(hash0.get()));
-    utxo0->mutable_out_point()->set_index(0);
+    utxo0->mutable_out_point()->set_index(1);
     utxo0->mutable_out_point()->set_sequence(UINT32_MAX);
     utxo0->set_amount(utxo_amount);
-    auto script0 = parse_hex("76a91447825943ca6a936b177fdc7c9dc05251640169c288ac");
+    auto script0 = parse_hex("76a91437993e4c8dea4eac2697d2ecb03748e0a9bb78d788ac");
     utxo0->set_script(script0.data(), script0.size());
     
-    auto utxoKey0 = DATA("b00420bab8b663f0870ee8e46435743ba9588eb88d8d31410ed54afa67602e8d");
+    auto utxoKey0 = DATA("fad56f32be3d4d1f3672716a0695c7999acda5330d7b95bcd25f46bbf3a11b20");
     input.add_private_key(TWDataBytes(utxoKey0.get()), TWDataSize(utxoKey0.get()));
     
+    auto plan = Bitcoin::TransactionBuilder::plan(input);
+    plan.amount = amount;
+    plan.fee = fee;
+    plan.change = utxo_amount - amount - fee;
+    
     // Sign
-    auto signer = TW::Bitcoin::TransactionSigner<TW::Bitcoin::Transaction>(std::move(input));
+    auto signer = TW::Bitcoin::TransactionSigner<TW::Bitcoin::Transaction>(std::move(input), plan);
     auto result = signer.sign();
     auto signedTx = result.payload();
     
     ASSERT_TRUE(result);
-    //ASSERT_EQ(fee, signer.plan.fee);
+    ASSERT_EQ(fee, signer.plan.fee);
     
     Data serialized;
     signedTx.encode(false, serialized);
@@ -67,13 +71,29 @@ TEST(MonetaryUnitTransaction, SignTransaction) {
               hex(serialized),
               "01000000"
               "01"
-              "ecb6856eda9edec8a340709dce9a36b010e256e1331384f7598c09d6b748f118"
+              "170f786b94e0e287c1cce0214c78006b8c0080382304be67bc6f248ef7f29af1"
               "01000000"
-              "69"
-              "46304302201387c7fe7a9ba470144ec5c0fdd96804e5e87bb5a48db367b42abac99a659299021f38dc7f25bb68c03d0a8154ddb692fb0d41bf9ac1a2562fe500a3f49b0686bc012102825357682ee04ac2bddc0df89b885e0eba5efd65dc7ce545807741af5e6a5e14"
+              "6a"
+              "473044022013d04c47ce9d530944d6e40e5d2c2e6cc03e0a776caf90b7f32557f41a74eb3c02203bc78aef91eb3f54abeee041a47290ca99e0052c4c57ee77a50095ecb4bb5571012103adbb6d0a2f3934be0b76148e5efb7eecfab0b1ee03279a2bdb2b2c9009f9c8bd"
               "ffffffff"
               "02"
-              "00e1f505000000001976a9148273a3ccd46cc77677bba8fa02d47ed1b8873e4688ac4b191e0e000000001976a9144f74ae2269b2278168f7c4480cbe4334ea4fddbc88ac"
+              "2c5ccd1d00000000""19"
+              "76a914614a6b5f6fb8d3690d3f31331a5476b3a597851288ac"
+              "00ca9a3b00000000""19"
+              "76a91437993e4c8dea4eac2697d2ecb03748e0a9bb78d788ac"
               "00000000"
               );
+}
+
+
+TEST(MonetaryUnitTransaction, LockScripts) {
+    // https://blockbook.monetaryunit.org/tx/18f148b7d6098c59f7841333e156e210b0369ace9d7040a3c8de9eda6e85b6ec
+    
+    auto script = WRAP(TWBitcoinScript, TWBitcoinScriptBuildForAddress(STRING("7nnXDJCkajemktQYTZcz4gP46RLZiqUhXR").get()));
+    auto scriptData = WRAPD(TWBitcoinScriptData(script.get()));
+    assertHexEqual(scriptData, "76a914df855efb3fb8e58e733d24b98c3b892da978fb1588ac");
+    
+    auto script2 = WRAP(TWBitcoinScript, TWBitcoinScriptBuildForAddress(STRING("7YQbBvhpy1u9Xf5MJSUNnnpQQMiape9NW3").get()));
+    auto scriptData2 = WRAPD(TWBitcoinScriptData(script2.get()));
+    assertHexEqual(scriptData2, "76a91441cdc737b3a0862479ffc56deffefe099ca0cfbc88ac");
 }
