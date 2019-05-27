305ff6f9ba69d4a0f3207cd64ab9739308e92771
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-05-27 18:36:44 +1000

    DigiByte integration - complete test for tx signing

--- tests/interface/TWDigiByteTransactionTests.cpp
+++ tests/interface/TWDigiByteTransactionTests.cpp
@@ -27,23 +27,21 @@ using namespace Bitcoin;
     m/44'/20'/0'/0/0 Private key in Base58 encoding - L37s29UXmQowW7y5BeDhe8AwM7quvRS2FmkWxSuctw9PbFprhhqA
     m/44'/20'/0'/0/0 Private key in bytes - b00420bab8b663f0870ee8e46435743ba9588eb88d8d31410ed54afa67602e8d
 
-    https://digiexplorer.info/tx/2e725346df28cff3275c4087f2f365e041c6fb75d10bbd6c095abdd9716db53d
-    https://digiexplorer.info/api/tx/2e725346df28cff3275c4087f2f365e041c6fb75d10bbd6c095abdd9716db53d
-    https://dgb2.trezor.io/tx/2e725346df28cff3275c4087f2f365e041c6fb75d10bbd6c095abdd9716db53d
+    https://dgb2.trezor.io/tx/6eb7e134cafbe4030032aa9f44a14420826e8d290cddff1a53339c3916b19f30
 */
 TEST(DigiByteTransaction, SignTransaction) {
-    const int64_t utxo_amount = 940900000;
-    const int64_t amount = 100000000;
+    const int64_t utxo_amount = 105000000;
+    const int64_t amount = 101000000;
     const int64_t fee = 1000;
 
     auto input = Bitcoin::Proto::SigningInput();
     input.set_hash_type(TWSignatureHashTypeAll);
     input.set_amount(amount);
     input.set_byte_fee(1);
-    input.set_to_address("D7pvLVQyJSqKpYPtpkfydjeFizNrafHcrT");
-    input.set_change_address("dgb1qhw8w4g6ny3cjq06vsas9w366yknjzcpr6pawt6");
+    input.set_to_address("D9Gv7jWSVsS9Y5q98C79WyfEj6P2iM5Nzs");
+    input.set_change_address("DBfCffUdSbhqKZhjuvrJ6AgvJofT4E2kp4");
 
-    auto hash0 = DATA("c9f3b980ce441f3fb517a16e02a05fc4246ee6f8e54651e40414056d2771a522");
+    auto hash0 = DATA("ea63bdc39035ebe02df7ad999581156f996303a70f9a3358811454a7ca806b96");
     auto utxo0 = input.add_utxo();
     utxo0->mutable_out_point()->set_hash(TWDataBytes(hash0.get()), TWDataSize(hash0.get()));
     utxo0->mutable_out_point()->set_index(0);
@@ -74,16 +72,16 @@ TEST(DigiByteTransaction, SignTransaction) {
         hex(serialized),
         "01000000"
         "01"
-            "c9f3b980ce441f3fb517a16e02a05fc4246ee6f8e54651e40414056d2771a522"
+            "ea63bdc39035ebe02df7ad999581156f996303a70f9a3358811454a7ca806b96"
             "00000000"
             "6a"
-            "473044022009a77c264da0758b47d8401dadd2803228e557bb83f95e429596ffaa63fe464102207552823b3faff1c7ec45434fd409aa3642b13fb5c47814593dac73f316aece330121024e525e582452cece7b869532d9e354cfec58b71cbed76f7238c91274a64b2116"
+            "473044022003e9756b12ecbe5788fdb6eb4b6d7b58f9f9410df32f3047edb0dd0ebffb0d630220499d00d17e50c48b4bac6c0ce148f13bb3109a8845fa3400a2d6a57dabf2c4010121024e525e582452cece7b869532d9e354cfec58b71cbed76f7238c91274a64b2116"
             "ffffffff"
         "02"
-            "b8191f3200000000""16"
-            "0014bb8eeaa3532471203f4c876057475a25a7216023"
-            "00e1f50500000000""19"
-            "76a9141d785f831292ad918302633fa98c089fc647324b88ac"
+            "4023050600000000""19"
+            "76a9142d5b215a11029ee51a1dd9404d271c7e4a74f5f288ac"
+            "18053d0000000000""19"
+            "76a91447825943ca6a936b177fdc7c9dc05251640169c288ac"
         "00000000"
     ); 
 }
