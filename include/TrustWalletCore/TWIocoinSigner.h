e6012a6b6b913c3d169a6fdabbf0dc81b50a05c9
iocrypto <dhatton@protonmail.com>
2019-05-29 15:39:13 +0000

    signer

--- include/TrustWalletCore/TWIocoinSigner.h
+++ include/TrustWalletCore/TWIocoinSigner.h
@@ -15,27 +15,27 @@ TW_EXTERN_C_BEGIN
 
 /// Helper class to sign Iocoin transactions.
 TW_EXPORT_CLASS
-struct TWIocoinTransactionSigner;
+struct TWIocoinSigner;
 
 /// Creates a transaction signer with input data (serialized from IocoinSigningInput)
 TW_EXPORT_STATIC_METHOD
-struct TWIocoinTransactionSigner *_Nonnull TWIocoinTransactionSignerCreate(TW_Iocoin_Proto_SigningInput input);
+struct TWIocoinSigner *_Nonnull TWIocoinSignerCreate(TW_Iocoin_Proto_SigningInput input);
 
 /// Creates a transaction signer with input data (serialized from Proto::SigningInput) and plan
 TW_EXPORT_STATIC_METHOD
-struct TWIocoinTransactionSigner *_Nonnull TWIocoinTransactionSignerCreateWithPlan(TW_Iocoin_Proto_SigningInput input, TW_Iocoin_Proto_TransactionPlan plan);
+struct TWIocoinSigner *_Nonnull TWIocoinSignerCreateWithPlan(TW_Iocoin_Proto_SigningInput input, TW_Iocoin_Proto_TransactionPlan plan);
 
 TW_EXPORT_METHOD
-void TWIocoinTransactionSignerDelete(struct TWIocoinTransactionSigner *_Nonnull signer);
+void TWIocoinSignerDelete(struct TWIocoinSigner *_Nonnull signer);
 
 /// Plans a transaction.
 TW_EXPORT_METHOD
-TW_Iocoin_Proto_TransactionPlan TWIocoinTransactionSignerPlan(struct TWIocoinTransactionSigner *_Nonnull signer);
+TW_Iocoin_Proto_TransactionPlan TWIocoinSignerPlan(struct TWIocoinSigner *_Nonnull signer);
 
 /// Signs the transaction.
 ///
 /// On success the result will contain a IocoinSigningOutput.
 TW_EXPORT_METHOD
-TW_Proto_Result TWIocoinTransactionSignerSign(struct TWIocoinTransactionSigner *_Nonnull signer);
+TW_Proto_Result TWIocoinSignerSign(struct TWIocoinSigner *_Nonnull signer);
 
 TW_EXTERN_C_END
