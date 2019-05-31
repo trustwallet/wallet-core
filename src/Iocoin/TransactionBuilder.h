493d49c75488b98221c2cb8f35cd0dd0e5c127eb
iocrypto <dhatton@protonmail.com>
2019-05-31 16:55:12 +0000

    signer

--- src/Iocoin/TransactionBuilder.h
+++ src/Iocoin/TransactionBuilder.h
@@ -51,7 +51,7 @@ struct TransactionBuilder {
         plan.fee =
             unspentSelector.calculator.calculate(plan.utxos.size(), output_size, input.byte_fee());
 
-        plan.availableAmount = Bitcoin::UnspentSelector::sum(plan.utxos);
+        plan.availableAmount = UnspentSelector::sum(plan.utxos);
 
         if (plan.amount > plan.availableAmount - plan.fee) {
             plan.amount = std::max(Bitcoin::Amount(0), plan.availableAmount - plan.fee);
