1d2b8eba6ca585dd18ca6af861440f672e86cbdf
iocrypto <dhatton@protonmail.com>
2019-06-01 21:35:36 +0000

    signer proto update

--- src/Iocoin/TransactionBuilder.h
+++ src/Iocoin/TransactionBuilder.h
@@ -24,8 +24,7 @@ struct TransactionBuilder {
 
         auto output_size = 2;
         auto calculator =
-          //Bitcoin::UnspentCalculator::getCalculator(static_cast<TWCoinType>(input.coin_type()));
-          Bitcoin::UnspentCalculator::getCalculator(static_cast<TWCoinType>(295));
+          Bitcoin::UnspentCalculator::getCalculator(static_cast<TWCoinType>(input.coin_type()));
         auto unspentSelector = UnspentSelector(calculator);
         if (input.use_max_amount() && UnspentSelector::sum(input.utxo()) == plan.amount) {
             output_size = 1;
