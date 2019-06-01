0e214472aa4d17919b5ee384abddff981ad68f79
iocrypto <dhatton@protonmail.com>
2019-06-01 20:52:23 +0000

    Revert "signer"
    This reverts commit 96173672450d781ce4b166474971ff2475d72fdd.

--- src/Bitcoin/UnspentCalculator.cpp
+++ src/Bitcoin/UnspentCalculator.cpp
@@ -5,12 +5,26 @@ using namespace TW::Bitcoin;
 
 UnspentCalculator UnspentCalculator::getCalculator(TWCoinType coinType) {
     switch (coinType) {
-    default: {
+    case TWCoinTypeZelcash:
+    case TWCoinTypeZcash: {
+        auto calc = [](size_t inputs, size_t outputs, int64_t byteFee) -> int64_t { return 10000; };
+        auto calcInput = [](int64_t byteFee) -> int64_t { return 0; };
+        return UnspentCalculator(calc, calcInput);
+    }
+    case TWCoinTypeGroestlcoin: {
+        auto calc = [](size_t inputs, size_t outputs, int64_t byteFee) -> int64_t { return 20000; };
+        auto calcInput = [](int64_t byteFee) -> int64_t { return 0; };
+        return UnspentCalculator(calc, calcInput);
+    }
+    case TWCoinTypeIocoin: {
         auto calc = [](size_t inputs, size_t outputs, int64_t byteFee) -> int64_t { 
     const auto txsize = ((148 * inputs) + (34 * outputs) + 10);
     return ((int64_t(txsize) / 1024) * byteFee + byteFee) * 1000; };
+        //XXXX auto calcInput = [](int64_t byteFee) -> int64_t { return 0; };
         return UnspentCalculator(calc, calculateSingleInputFee);
     }
+    default:
+        return UnspentCalculator();
     }
 }
 
