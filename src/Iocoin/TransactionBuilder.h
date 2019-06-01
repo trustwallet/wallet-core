1a1874cb388c93e5ee1ae035b8d4f6442242a291
iocrypto <dhatton@protonmail.com>
2019-06-01 14:55:45 +0000

    override cal btc

--- src/Iocoin/TransactionBuilder.h
+++ src/Iocoin/TransactionBuilder.h
@@ -24,7 +24,7 @@ struct TransactionBuilder {
 
         auto output_size = 2;
         auto calculator =
-          Bitcoin::UnspentCalculator::getCalculator(static_cast<TWCoinType>(input.coin_type()));
+          UnspentCalculator::getCalculator(static_cast<TWCoinType>(input.coin_type()));
         auto unspentSelector = UnspentSelector(calculator);
         if (input.use_max_amount() && UnspentSelector::sum(input.utxo()) == plan.amount) {
             output_size = 1;
@@ -49,6 +49,10 @@ struct TransactionBuilder {
         plan.fee =
             unspentSelector.calculator.calculate(plan.utxos.size(), output_size, input.byte_fee());
          
+<<<<<<< HEAD
+=======
+	plan.fee = 1000 * (plan.fee / 1024 + plan.fee)
+>>>>>>> parent of 752295d... signer
         plan.availableAmount = UnspentSelector::sum(plan.utxos);
 
         if (plan.amount > plan.availableAmount - plan.fee) {
