3f6eae68f1d86a6137631825d03559988c2d3b7f
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-06-04 12:51:30 +0300

    code quality

--- js/tests/blockchain/waves/WavesSigner.test.ts
+++ js/tests/blockchain/waves/WavesSigner.test.ts
@@ -12,13 +12,13 @@ describe('WavesSigner', () => {
         const signingInput = TW.Waves.Proto.SigningInput.create({
                 amount: Long.fromString('100000000'),
                 amountAsset: 'DacnEpaUVFRCYk8Fcd1F3cqUZuT4XG7qW9mRyoZD81zq',
+                attachment: fromHexString('68656c6c6f'),
                 fee: Long.fromString('100000'),
                 feeAsset: 'DacnEpaUVFRCYk8Fcd1F3cqUZuT4XG7qW9mRyoZD81zq',
-                to: '3PPCZQkvdMJpmx7Zrz1cnYsPe9Bt1XT2Ckx',
-                attachment: fromHexString('68656c6c6f'),
-                timestamp: Long.fromString('1559146613'),
+                privateKey: fromHexString('68b7a9adb4a655b205f43dac413803785921e22cd7c4d05857b203a62621075f'),
                 publicKey: fromHexString('794e8429ddf58353eacab99b4c2b3b71c8c1b5927980db2d6f8db52744dd103d'),
-                privateKey: fromHexString('68b7a9adb4a655b205f43dac413803785921e22cd7c4d05857b203a62621075f')
+                timestamp: Long.fromString('1559146613'),
+                to: '3PPCZQkvdMJpmx7Zrz1cnYsPe9Bt1XT2Ckx',
         });
         const sign: TW.Waves.Proto.SigningOutput = WavesSigner.sign(signingInput);
         const signBytes = bufToHex(sign.signature);
