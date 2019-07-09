d169fbcf1e7f0dfa039251a35fd1507d5ede852c
Liam Alford <lalford@nervecentresoftware.com>
2019-07-09 10:10:45 +0100

    Clean up

--- tests/interface/TWDeepOnionTransactionTests.cpp
+++ tests/interface/TWDeepOnionTransactionTests.cpp
@@ -32,82 +32,49 @@ TEST(DeepOnionTransaction, SignTransaction) {
         tx - https://chainz.cryptoid.info/onion/tx.dws?2743254.htm 69a4b0b8641c16268033667371e71ed1b0c1a48bbb09831c61398c9c47978d0e
     
 
-    printf("DEBUG - 1\n");
-
     const int64_t utxo_amount = 4910598;
-    printf("DEBUG - 2\n");
     const int64_t amount = 4867035;
-    printf("DEBUG - 3\n");
     const int64_t fee = 10000;
-    printf("DEBUG - 4\n");
 
     auto input = Bitcoin::Proto::SigningInput();
-    printf("DEBUG - 5\n");
     input.set_hash_type(TWSignatureHashTypeAll);
-    printf("DEBUG - 6\n");
     input.set_amount(amount);
-    printf("DEBUG - 7\n");
     input.set_byte_fee(1);
-    printf("DEBUG - 8\n");
     input.set_to_address("DohbWhdFgC4KEr4SM7vBSuhQ3hNqzNsLhs");
-    printf("DEBUG - 9\n");
     input.set_change_address("DqaUWMm9tfEvroAzA1L6G6PK9gRT7nJQ5u");
-    printf("DEBUG - 10\n");
 
     auto hash0 = DATA("445560237d8093da3487eb90bc7ff826fab43cdbe213c034d671ec4eb4827e0c");
-    printf("DEBUG - 11\n");
     auto utxo0 = input.add_utxo();
-    printf("DEBUG - 12\n");
     utxo0->mutable_out_point()->set_hash(TWDataBytes(hash0.get()), TWDataSize(hash0.get()));
-    printf("DEBUG - 13\n");
     utxo0->mutable_out_point()->set_index(0);
-    printf("DEBUG - 14\n");
     utxo0->mutable_out_point()->set_sequence(UINT32_MAX);
-    printf("DEBUG - 15\n");
     utxo0->set_amount(utxo_amount);
-    printf("DEBUG - 16\n");
     auto script0 = parse_hex("76a9145d6e33f3a108bbcc586cbbe90994d5baf5a9cce488ac");
-    printf("DEBUG - 17\n");
     utxo0->set_script(script0.data(), script0.size());
-    printf("DEBUG - 18\n");
 
     auto utxoKey0 = DATA("4646464646464646464646464646464646464646464646464646464646464646");
-    printf("DEBUG - 19\n");
     input.add_private_key(TWDataBytes(utxoKey0.get()), TWDataSize(utxoKey0.get()));
-    printf("DEBUG - 20\n");
 
     auto plan = Bitcoin::TransactionBuilder::plan(input);
-    printf("DEBUG - 21\n");
     plan.amount = amount;
-    printf("DEBUG - 22\n");
     plan.fee = fee;
-    printf("DEBUG - 23\n");
     plan.change = utxo_amount - amount - fee;
-    printf("DEBUG - 24\n");
 
     // Sign
     auto signer = TW::Bitcoin::TransactionSigner<TW::DeepOnion::Transaction>(std::move(input), plan);
-    printf("DEBUG - 25\n");
     auto result = signer.sign();
-    printf("DEBUG - 26\n");
 
     ASSERT_TRUE(result);
-    printf("DEBUG - 27\n");
     ASSERT_EQ(fee, signer.plan.fee);
-    printf("DEBUG - 28\n");
 
     auto signedTx = result.payload();
-    printf("DEBUG - 29\n");
 
     Data serialized;
-    printf("DEBUG - 30\n");
     signedTx.encode(false, serialized);
-    printf("DEBUG - 31\n");
     ASSERT_EQ(
         hex(serialized),
         "010000000c9a035d0127bde28c59d4f1b9c6ebb5b6397cc4c2f651ac6a980222422885b9e554ff8824000000008a47304402200f6b2d7b25af127e460c89059137eb9125475e339cc1bf7f030fc792233f0cfd022072f51331685e3ecf96cf71bd8d4d08537d9ae1a60c65dd999952cc4d3f4079940141044bc2a31265153f07e70e0bab08724e6b85e217f8cd628ceb62974247bb493382ce28cab79ad7119ee1ad3ebcdb98a16805211530ecc6cfefa1b88e6dff99232afeffffff021b830000000000001976a914e77245576e4b6701aaa762a885d1ba6511d566f488acdb434a00000000001976a914d2dac77deb6b0d6c86e5717f485c0605c296834e88acdeaf1300"
     ); 
-    printf("DEBUG - 32\n");
 }
 */
 
