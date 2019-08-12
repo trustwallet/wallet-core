75e1a2d017098fdc77a5da1464720936c167abc5
Sathvik Birudavolu <8377116+BSathvik@users.noreply.github.com>
2019-08-12 13:54:17 -0500

    Fix codacy issues

--- js/js/WalletCore.ts
+++ js/js/WalletCore.ts
@@ -1,5 +1,5 @@
-var binary = require('node-pre-gyp');
-var path = require('path');
+import * as binary from "node-pre-gyp";
+import * as path from "path";
 
-var binding_path = binary.find(path.resolve(path.join(__dirname,'./../package.json')));
-export const WalletCore = require(binding_path);
+const bindingPath = binary.find(path.resolve(path.join(__dirname,"./../package.json")));
+export const WalletCore = require(bindingPath);
