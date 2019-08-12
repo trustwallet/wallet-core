a92bbc8e2b14c59a19d53beaf120f889eebd7d8d
Sathvik Birudavolu <8377116+BSathvik@users.noreply.github.com>
2019-08-12 10:34:43 -0500

    Add S3 support for js binary
    - Switched to node-pre-gyp
    - Changes binary name (required by node-pre-gyp)
    - Change js/build/ to js/dist/ for npm packaging
    - node-pre-gyp uses js/build to build binary and copies into js/dist/
    - Remove binary from the npm package

--- js/js/WalletCore.ts
+++ js/js/WalletCore.ts
@@ -1,1 +1,5 @@
-export const WalletCore = require('bindings')('trust-wallet-nodejs.node');
+var binary = require('node-pre-gyp');
+var path = require('path');
+
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./../package.json')));
+export const WalletCore = require(binding_path);
