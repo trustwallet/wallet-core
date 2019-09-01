4689c20a2b99b03bfee911cd4232eb1710086bde
hewig <360470+hewigovens@users.noreply.github.com>
2019-09-02 00:15:54 +0800

    Add Decred fee calculation (#613)
    * Add decred fee estimation
    
    * Fix old issue template
    
    * Fix codacy warning

--- src/Decred/TransactionBuilder.h
+++ src/Decred/TransactionBuilder.h
@@ -8,6 +8,7 @@
 
 #include "Transaction.h"
 #include "../Bitcoin/TransactionPlan.h"
+#include "../Bitcoin/TransactionBuilder.h"
 #include "../Bitcoin/UnspentSelector.h"
 #include "../proto/Bitcoin.pb.h"
 #include "../proto/Decred.pb.h"
@@ -21,45 +22,7 @@ namespace TW::Decred {
 struct TransactionBuilder {
     /// Plans a transaction by selecting UTXOs and calculating fees.
     static Bitcoin::TransactionPlan plan(const Bitcoin::Proto::SigningInput& input) {
-        auto plan = Bitcoin::TransactionPlan();
-        plan.amount = input.amount();
-
-        auto output_size = 2;
-        auto calculator =
-            Bitcoin::UnspentCalculator::getCalculator(static_cast<TWCoinType>(input.coin_type()));
-        auto unspentSelector = Bitcoin::UnspentSelector(calculator);
-        if (input.use_max_amount() && Bitcoin::UnspentSelector::sum(input.utxo()) == plan.amount) {
-            output_size = 1;
-            int64_t newAmount = 0;
-            auto input_size = 0;
-
-            for (auto utxo : input.utxo()) {
-                if (utxo.amount() >
-                    unspentSelector.calculator.calculateSingleInput(input.byte_fee())) {
-                    input_size++;
-                    newAmount += utxo.amount();
-                }
-            }
-
-            plan.amount = newAmount - unspentSelector.calculator.calculate(input_size, output_size,
-                                                                           input.byte_fee());
-            plan.amount = std::max(Bitcoin::Amount(0), plan.amount);
-        }
-
-        plan.utxos =
-            unspentSelector.select(input.utxo(), plan.amount, input.byte_fee(), output_size);
-        plan.fee =
-            unspentSelector.calculator.calculate(plan.utxos.size(), output_size, input.byte_fee());
-
-        plan.availableAmount = Bitcoin::UnspentSelector::sum(plan.utxos);
-
-        if (plan.amount > plan.availableAmount - plan.fee) {
-            plan.amount = std::max(Bitcoin::Amount(0), plan.availableAmount - plan.fee);
-        }
-
-        plan.change = plan.availableAmount - plan.amount - plan.fee;
-
-        return plan;
+        return Bitcoin::TransactionBuilder::plan(input);
     }
 
     /// Builds a transaction by selecting UTXOs and calculating fees.
