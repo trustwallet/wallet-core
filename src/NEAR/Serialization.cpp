9312c5f85daac90cee9c6e57493e688894897563
Vladimir Grichina <vgrichina@gmail.com>
2019-08-31 23:42:21 -0700

    Implement transaction serialization (only for Transfer action for now)

--- src/NEAR/Serialization.cpp
+++ src/NEAR/Serialization.cpp
@@ -14,8 +14,75 @@ using namespace TW;
 using namespace TW::NEAR;
 using namespace TW::NEAR::Proto;
 
+
+static void writeU8(Data& data, uint8_t number) {
+    data.push_back(number);
+}
+
+static void writeU32(Data& data, uint32_t number) {
+    data.push_back(number & 0xFF);
+    data.push_back((number >> 8) & 0xFF);
+    data.push_back((number >> 16) & 0xFF);
+    data.push_back((number >> 24) & 0xFF);
+}
+
+static void writeU64(Data& data, uint64_t number) {
+    data.push_back(number & 0xFF);
+    data.push_back((number >> 8) & 0xFF);
+    data.push_back((number >> 16) & 0xFF);
+    data.push_back((number >> 24) & 0xFF);
+    data.push_back((number >> 32) & 0xFF);
+    data.push_back((number >> 40) & 0xFF);
+    data.push_back((number >> 48) & 0xFF);
+    data.push_back((number >> 56) & 0xFF);
+}
+
+static void writeU128(Data& data, const Proto::Uint128 number) {
+    const auto& numberData = number.number();
+    data.insert(std::end(data), std::begin(numberData), std::end(numberData));
+}
+
+static void writeString(Data& data, const std::string& str) {
+    writeU32(data, str.length());
+    data.insert(std::end(data), std::begin(str), std::end(str));
+}
+
+static void writePublicKey(Data& data, const Proto::PublicKey& publicKey) {
+    writeU8(data, publicKey.key_type());
+    const auto& keyData = publicKey.data();
+    data.insert(std::end(data), std::begin(keyData), std::end(keyData));
+}
+
+static void writeTransfer(Data& data, const Proto::Transfer& transfer) {
+    writeU128(data, transfer.deposit());
+}
+
+static void writeAction(Data& data, const Proto::Action& action) {
+    writeU8(data, action.payload_case() - Proto::Action::kCreateAccount);
+    switch (action.payload_case()) {
+        case Proto::Action::kTransfer:
+            writeTransfer(data, action.transfer());
+            return;
+        default:
+            // TODO: Report error properly?
+            return;
+    }
+}
+
 Data TW::NEAR::transactionData(const Proto::SigningInput& input) {
-    return Data();
+    Data data;
+    writeString(data, input.signer_id());
+    writePublicKey(data, input.public_key());
+    writeU64(data, input.nonce());
+    writeString(data, input.receiver_id());
+    // TODO: assert data sizes
+    const auto& block_hash = input.block_hash();
+    data.insert(std::end(data), std::begin(block_hash), std::end(block_hash));
+    writeU32(data, input.actions_size());
+    for (const auto& action : input.actions()) {
+        writeAction(data, action);
+    }
+    return data;
 }
 
 Data TW::NEAR::signedTransactionData(const Data& transactionData, const Data& signatureData) {
--- tests/NEAR/SerializationTests.cpp
+++ tests/NEAR/SerializationTests.cpp
@@ -10,8 +10,7 @@
 #include "Base64.h"
 #include "Base58.h"
 #include "proto/NEAR.pb.h"
-#include "NEAR/Address.h"
-#include "NEAR/Signer.h"
+#include "NEAR/Serialization.h"
 #include <iostream>
 
 #include <TrustWalletCore/TWHRP.h>
@@ -19,7 +18,7 @@
 
 namespace TW::NEAR {
 
-TEST(NEARSigner, SignTx) {
+TEST(NEARSerialization, SerializeTransferTransaction) {
     auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
 
     auto input = Proto::SigningInput();
@@ -35,18 +34,13 @@ TEST(NEARSigner, SignTx) {
     deposit[0] = 1;
     transfer.mutable_deposit()->set_number(deposit.data(), deposit.size());
 
-    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
-    input.set_private_key(privateKey.data(), 32);
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
 
-    auto signer = NEAR::Signer(std::move(input));
-    auto signature = signer.sign();
-    auto signatureInBase64 = Base64::encode(signature);
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
 
-    ASSERT_EQ(signatureInBase64, "v4UxPuX98S9+2dgnMwLL0nthTy1rdu5stLkqJ7SDQgm+LTvxi7482ZZ/RQMByShxGx+Uvu8CsnCz8jUhngQ3CQ==");
-
-    auto output = signer.build();
-
-    // TODO
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef6010000000301000000000000000000000000000000");
 }
 
 }
