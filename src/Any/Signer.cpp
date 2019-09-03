e1251267036db26ed9de3fb821b83249e22d9b49
leoneparise <leone.parise@gmail.com>
2019-09-03 18:13:24 -0300

    Add Tezos support to AnySigner

--- src/Any/Signer.cpp
+++ src/Any/Signer.cpp
@@ -12,21 +12,21 @@
 #include "Cosmos/Signer.h"
 #include "Binance/Signer.h"
 #include "Ethereum/Signer.h"
+#include "Tezos/Signer.h"
 
 #include <string>
 #include <google/protobuf/util/json_util.h>
 
 using namespace TW;
-using namespace TW::Any;
 using namespace google::protobuf;
 
-Proto::SigningOutput Signer::sign() const noexcept {
+Any::Proto::SigningOutput Any::Signer::sign() const noexcept {
     const auto coinType = (TWCoinType) input.coin_type();
     const auto transaction = input.transaction();
     const auto privateKeyData = parse_hex(input.private_key());
     const auto privateKey = PrivateKey(privateKeyData);
 
-    auto output = Proto::SigningOutput();
+    auto output = Any::Proto::SigningOutput();
 
     switch (coinType)
     {
@@ -61,6 +61,19 @@ Proto::SigningOutput Signer::sign() const noexcept {
             }
             break;
         }
+        case TWCoinTypeTezos: {
+            Tezos::Proto::SigningInput message;
+            parse(transaction, &message, output);
+            if (output.success()) {
+                message.set_private_key(privateKey.bytes.data(), privateKey.bytes.size());
+                auto operations = message.operation_list().operations();
+                auto operation_list = Tezos::OperationList(message.operation_list().branch());
+                operation_list.operation_list.assign(operations.begin(), operations.end());
+                auto signerOutput = Tezos::Signer().signOperationList(privateKey, operation_list);
+                output.set_output(hex(signerOutput.begin(), signerOutput.end()));
+            }
+            break;
+        }
         default:
             auto error = new Proto::SigningOutput_Error();
             error->set_code(SignerErrorCodeNotSupported);
@@ -71,8 +84,8 @@ Proto::SigningOutput Signer::sign() const noexcept {
     return output;
 }
 
-void Signer::parse(const std::string& transaction, Message* message,
-                   Proto::SigningOutput& output) const noexcept {
+void Any::Signer::parse(const std::string& transaction, Message* message,
+                            Any::Proto::SigningOutput& output) const noexcept {
     util::JsonParseOptions options;
     options.case_insensitive_enum_parsing = true;
     options.ignore_unknown_fields = false;
@@ -84,7 +97,7 @@ void Signer::parse(const std::string& transaction, Message* message,
         return;
     }
 
-    auto error = new Proto::SigningOutput_Error();
+    auto error = new Any::Proto::SigningOutput_Error();
     error->set_code(SignerErrorCodeInvalidJson);
     error->set_description(result.error_message());
     output.set_allocated_error(error);
