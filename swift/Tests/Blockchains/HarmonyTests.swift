899782336618da61b298200280fc760636f96cb9
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-09-03 18:07:34 -0700

    [harmony-one] Swift test code signs transaction correctly

--- swift/Tests/Blockchains/HarmonyTests.swift
+++ swift/Tests/Blockchains/HarmonyTests.swift
@@ -10,22 +10,20 @@ import TrustWalletCore
 class HarmonyTests: XCTestCase {
     func testSigner() {
         let input = TW_Harmony_Proto_SigningInput.with {
-            $0.chainID = Data(hexString: "01")!
-            $0.nonce = Data(hexString: "09")!
-            $0.gasPrice = Data(hexString: "04a817c800")!
-            $0.gasLimit = Data(hexString: "5208")!
-            $0.fromShardID = Data(hexString: "0x0")!
-            $0.toShardID = Data(hexString: "0x1")!
-            $0.toAddress = "0x3535353535353535353535353535353535353535"
-            $0.amount = Data(hexString: "0de0b6b3a7640000")!
-            $0.privateKey = Data(hexString: "0x4646464646464646464646464646464646464646464646464646464646464646")!
+            $0.chainID = Data(hexString: "0x0190")!
+            $0.nonce = Data(hexString: "0x09")!
+            $0.gasPrice = Data(hexString: "0x00")!
+            $0.gasLimit = Data(hexString: "0x5208")!
+            $0.fromShardID = Data(hexString: "0x03")!
+            $0.toShardID = Data(hexString: "0x02")!
+            $0.payload = Data(hexString: "0x")!
+            $0.toAddress = "0x587c66b4b973a7b231d02ebbc7e7d9f6c5a49ef2"
+            $0.amount = Data(hexString: "0x0168d28e3f00280000")!
+            $0.privateKey = Data(hexString: "0xe2f88b4974ae763ca1c2db49218802c2e441293a09eaa9ab681779e05d1b7b94")!
         }
-
         let output = HarmonySigner.sign(input: input)
-        XCTAssertEqual(true, true)
-        // TODO Need to make this work correctly
-        // XCTAssertEqual(output.v.hexString, "25")
-        // XCTAssertEqual(output.r.hexString, "28ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276")
-        // XCTAssertEqual(output.s.hexString, "67cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83")
+        XCTAssertEqual(output.v.hexString, "0343")
+        XCTAssertEqual(output.r.hexString, "f4757c9ffad127996f788fb388be3e3e03440f6980b36dc6cee7230e390f0c13")
+        XCTAssertEqual(output.s.hexString, "42f0ff332bd552e8ad7a1cf6a0af4ebebfb1f8aae413c54d3464b9babba5f28d")
     }
 }
