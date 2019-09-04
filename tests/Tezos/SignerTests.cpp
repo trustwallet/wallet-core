88984a03843db1e098b8e01d7182c0466d05f972
leoneparise <leone.parise@gmail.com>
2019-09-03 21:09:55 -0300

    Remove signed_hex

--- tests/Tezos/SignerTests.cpp
+++ tests/Tezos/SignerTests.cpp
@@ -67,7 +67,6 @@ TEST(TezosSigner, SignOperationList) {
     std::string expectedSignedBytes = expectedForgedBytesToSign + expectedSignature;
     auto key = PrivateKey(parse_hex("0x2e8905819b8723fe2c1d161860e5ee1830318dbf49a83bd451cfb8440c28bd6f"));
     auto signedBytes = Signer().signOperationList(key, op_list);
-    auto signetHex = hex(signedBytes.begin(), signedBytes.end());
-
+    
     ASSERT_EQ(signedBytes, parse_hex(expectedSignedBytes));
 }
