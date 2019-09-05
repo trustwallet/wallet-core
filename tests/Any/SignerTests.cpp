53843d97f7ce648af57982708de2111ae7ee1f7a
Leone Parise <leone.parise@gmail.com>
2019-09-05 00:52:47 -0300

    [Anysigner] Add support to IoTeX, Wanchain and Waves (#624)
    * Add IoTex and Wanchain
    
    * Add waves support
    
    * Add tron and nebulas support
    
    * Added more ethereum based coins

--- tests/Any/SignerTests.cpp
+++ tests/Any/SignerTests.cpp
@@ -73,6 +73,81 @@ TEST(Signer, TezosTransactionSign) {
               output.output());
 }
 
+TEST(Signer, IoTeXTransactionSign) {
+    auto transaction = R"({"version": 1,"nonce": 123,"gasLimit": 888,"gasPrice": "999","transfer": {"amount": "456","recipient": "io187wzp08vnhjjpkydnr97qlh8kh0dpkkytfam8j","payload": "aGVsbG8gd29ybGQh"}})";
+    auto input = Proto::SigningInput();
+    input.set_private_key("0806c458b262edd333a191e92f561aff338211ee3e18ab315a074a2d82aa343f");
+    input.set_transaction(transaction);
+    input.set_coin_type(TWCoinTypeIoTeX);
+
+    auto signer = Signer(input);
+    auto output = signer.sign();
+
+    ASSERT_TRUE(output.success());;
+    ASSERT_EQ("0a4c0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e723937716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c64211241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a41555cc8af4181bf85c044c3201462eeeb95374f78aa48c67b87510ee63d5e502372e53082f03e9a11c1e351de539cedf85d8dff87de9d003cb9f92243541541a000",
+              output.output());
+}
+
+TEST(Signer, WanchainTransactionSign) {
+    auto transaction = R"({"chainId": "MQ==","toAddress": "0x3535353535353535353535353535353535353535","nonce": "OQ==","gasPrice": "MjAwMDAwMDAwMDA=","gasLimit": "MjEwMDA=","amount": "MTAwMDAwMDAwMDAwMDAwMDAwMA=="})";
+    auto input = Proto::SigningInput();
+    input.set_private_key("4646464646464646464646464646464646464646464646464646464646464646");
+    input.set_transaction(transaction);
+    input.set_coin_type(TWCoinTypeWanchain);
+
+    auto signer = Signer(input);
+    auto output = signer.sign();
+
+    ASSERT_TRUE(output.success());;
+    ASSERT_EQ("f88201398b32303030303030303030308532313030309435353535353535353535353535353535353535359331303030303030303030303030303030303030808185a0f7bca5c00884bcba5c068f507a33559df775e09785fc55956c9bbe4276259a4ca06679fc4f853b7f224ac8096d15005a3d1070c81a0a13aee8e60a72654273fe5e",
+              output.output());
+}
+
+TEST(Signer, WavesTransactionSign) {
+    auto transaction = R"({"amount": 100000000,"asset": "WAVES","fee": 100000000,"fee_asset": "WAVES","to": "3P2uzAzX9XTu1t32GkWw68YFFLwtapWvDds","attachment": "ZmFsYWZlbA==","timestamp": 1526641218066})";
+    auto input = Proto::SigningInput();
+    input.set_private_key("9864a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a");
+    input.set_transaction(transaction);
+    input.set_coin_type(TWCoinTypeWaves);
+
+    auto signer = Signer(input);
+    auto output = signer.sign();
+
+    ASSERT_TRUE(output.success());;
+    ASSERT_EQ(R"({"amount":100000000,"attachment":"4t2Xazb2SX","fee":100000000,"proofs":["4WUvsbgA2EsyCd5C7jHS57SgvPNczXYnxyKSvAzm18vtRhvbjjH9Dq2vUDgqm8h4GcDzzueQnHg3WH4xTyxDJ1Hg"],"recipient":"3P2uzAzX9XTu1t32GkWw68YFFLwtapWvDds","senderPublicKey":"6mA8eQjie53kd4jbZrwL3ZhMBqCX6nzit1k55tR2X7zU","timestamp":1526641218066,"type":4,"version":2})",
+              output.output());
+}
+
+TEST(Signer, NebulasTransactionSign) {
+    auto transaction = R"({"fromAddress": "n1V5bB2tbaM3FUiL4eRwpBLgEredS5C2wLY","chainId": "MQ==","nonce": "Nw==","gasPrice": "MTAwMDAwMA==","gasLimit": "MjAwMDAw","toAddress": "n1SAeQRVn33bamxN4ehWUT7JGdxipwn8b17","amount": "MTEwMDAwMDAwMDAwMDAwMDAwMDA=","timestamp": "MTU2MDA1MjkzOA==","payload": ""})";
+    auto input = Proto::SigningInput();
+    input.set_private_key("d2fd0ec9f6268fc8d1f563e3e976436936708bdf0dc60c66f35890f5967a8d2b");
+    input.set_transaction(transaction);
+    input.set_coin_type(TWCoinTypeNebulas);
+
+    auto signer = Signer(input);
+    auto output = signer.sign();
+
+    ASSERT_TRUE(output.success());;
+    ASSERT_EQ("17aca84f1c31f2065123ff14d53f10e2c2a8cd6e99f75fb9b11a2bc7bacb14e05c29f2267c700f9f1c5b9df26bddc5972993c71be651c5c7872683e23cdd80c301",
+              output.output());
+}
+
+TEST(Signer, TronTransactionSign) {
+    auto transaction = R"({"transaction": {"timestamp": 1539295479000,"expiration": 1539331479000,"blockHeader": {"timestamp": 1539295479000,"txTrieRoot": "ZCiMLbBkExZ2KpnbsC73yQ+Wi2D58uQQg1mAYUMy+G0=","parentHash": "AAAAAAAvezr09fi54jowxTD3GfFlt0LnNYU2soDurS0=","number": 3111739,"witnessAddress": "QVhj9gkbjnF2bagIsd0xWXkPYd59","version": 3},"transfer": {"ownerAddress": "TJRyWwFs9wTFGZg3JbrVriFbNfCug5tDeC","toAddress": "THTR75o8xXAgCTQqpiot2AFRAjvW1tSbVV","amount": 2000000}}})";
+    auto input = Proto::SigningInput();
+    input.set_private_key("2d8f68944bdbfbc0769542fba8fc2d2a3de67393334471624364c7006da2aa54");
+    input.set_transaction(transaction);
+    input.set_coin_type(TWCoinTypeTron);
+
+    auto signer = Signer(input);
+    auto output = signer.sign();
+
+    ASSERT_TRUE(output.success());;
+    ASSERT_EQ("ede769f6df28aefe6a846be169958c155e23e7e5c9621d2e8dce1719b4d952b63e8a8bf9f00e41204ac1bf69b1a663dacdf764367e48e4a5afcd6b055a747fb200",
+              output.output());
+}
+
 TEST(Signer, NetworkNotSupported) {
     auto transaction = R"({"accountNumber":"8733","chainId":"cosmoshub-2","fee":{"amounts":[{"denom":"uatom","amount":"5000"}],"gas":"200000"},"memo":"Testing","sendCoinsMessage":{"fromAddress":"cosmos1ufwv9ymhqaal6xz47n0jhzm2wf4empfqvjy575","toAddress":"cosmos135qla4294zxarqhhgxsx0sw56yssa3z0f78pm0","amounts":[{"denom":"uatom","amount":"995000"}]}})";
     auto input = Proto::SigningInput();
