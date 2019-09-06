a4399c3d0f6cb861d0d8f3986b07893d3308a36b
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-09-06 09:47:19 -0700

    [harmony-one] Update Swift test

--- swift/Tests/Blockchains/HarmonyTests.swift
+++ swift/Tests/Blockchains/HarmonyTests.swift
@@ -9,25 +9,25 @@ import TrustWalletCore
 
 class HarmonyTests: XCTestCase {
     func testSigner() {
+        let localNet = "0x02"
         let input = TW_Harmony_Proto_SigningInput.with {
-            $0.chainID = Data(hexString: "0x0190")!
-            $0.nonce = Data(hexString: "0x09")!
-            $0.gasPrice = Data(hexString: "0x00")!
+            $0.chainID = Data(hexString: localNet)!
+            $0.nonce = Data(hexString: "0x9")!
+            $0.gasPrice = Data(hexString: "0x")!
             $0.gasLimit = Data(hexString: "0x5208")!
-            $0.fromShardID = Data(hexString: "0x03")!
-            $0.toShardID = Data(hexString: "0x02")!
+            $0.fromShardID = Data(hexString: "0x1")!
+            $0.toShardID = Data(hexString: "0x0")!
             $0.payload = Data(hexString: "0x")!
-            $0.toAddress = "one1tp7xdd9ewwnmyvws96au0e7e7mz6f8hjqr3g3p"
-            $0.amount = Data(hexString: "0x0168d28e3f00280000")!
-            $0.privateKey = Data(hexString: "0xe2f88b4974ae763ca1c2db49218802c2e441293a09eaa9ab681779e05d1b7b94")!
+            $0.toAddress = "one1d2rngmem4x2c6zxsjjz29dlah0jzkr0k2n88wc"
+            $0.amount = Data(hexString: "0x4c53ecdc18a60000")!
+            $0.privateKey = Data(hexString: "0xb578822c5c718e510f67a9e291e9c6efdaf753f406020f55223b940e1ddb282e")!
         }
         let output = HarmonySigner.sign(input: input)
-        let e1 = "f86c0980825208030294587c66b4b973a7b231d02ebbc7e7d9f6c5a49ef2890168d28e3f0028000080820343a0f4757c9ffad127996f788fb388"
-        let e2 = "be3e3e03440f6980b36dc6cee7230e390f0c13a042f0ff332bd552e8ad7a1cf6a0af4ebebfb1f8aae413c54d3464b9babba5f28d"
-        let shouldBeEncoded = e1 + e2
-        XCTAssertEqual(output.encoded.hexString, shouldBeEncoded)
-        XCTAssertEqual(output.v.hexString, "0343")
-        XCTAssertEqual(output.r.hexString, "f4757c9ffad127996f788fb388be3e3e03440f6980b36dc6cee7230e390f0c13")
-        XCTAssertEqual(output.s.hexString, "42f0ff332bd552e8ad7a1cf6a0af4ebebfb1f8aae413c54d3464b9babba5f28d")
+        let e1 = "f86909808252080180946a87346f3ba9958d08d09484a2b7fdbbe42b0df6884c53ecdc18a600008028a0325aed6caa01a5235b"
+        let e2 = "7a508c8ab67f0c43946b05a1ea6a3e0628de4033fe372da06c19085d3376c30f6dc47cec795991cd37d6d0ebddfa633b0a8f494bc19cd01b"
+        XCTAssertEqual(output.encoded.hexString, e1 + e2)
+        XCTAssertEqual(output.v.hexString, "28")
+        XCTAssertEqual(output.r.hexString, "325aed6caa01a5235b7a508c8ab67f0c43946b05a1ea6a3e0628de4033fe372d")
+        XCTAssertEqual(output.s.hexString, "6c19085d3376c30f6dc47cec795991cd37d6d0ebddfa633b0a8f494bc19cd01b")
     }
 }
