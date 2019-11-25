a1a04e2c800713891c1122738ea40286bcc2248c
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-11-25 16:59:55 +0100

    Protobuf version: Update to Protobuf.cmake.

--- src/Any/Signer.cpp
+++ src/Any/Signer.cpp
@@ -28,12 +28,12 @@
 using namespace TW;
 using namespace google::protobuf;
 
-Any::Proto::SigningOutput Any::Signer::sign() const noexcept {
+TW::Any::Proto::SigningOutput TW::Any::Signer::sign() const noexcept {
     const auto coinType = (TWCoinType)input.coin_type();
     const auto transaction = input.transaction();
     const auto privateKey = PrivateKey(parse_hex(input.private_key()));
 
-    auto output = Any::Proto::SigningOutput();
+    auto output = TW::Any::Proto::SigningOutput();
 
     switch (coinType) {
     case TWCoinTypeCosmos: {
@@ -172,8 +172,8 @@ Any::Proto::SigningOutput Any::Signer::sign() const noexcept {
     return output;
 }
 
-void Any::Signer::parse(const std::string &transaction, Message *message,
-                        Any::Proto::SigningOutput &output) const noexcept {
+void TW::Any::Signer::parse(const std::string &transaction, Message *message,
+                        TW::Any::Proto::SigningOutput &output) const noexcept {
     util::JsonParseOptions options;
     options.case_insensitive_enum_parsing = true;
     options.ignore_unknown_fields = false;
@@ -185,13 +185,13 @@ void Any::Signer::parse(const std::string &transaction, Message *message,
         return;
     }
 
-    auto error = new Any::Proto::SigningOutput_Error();
+    auto error = new TW::Any::Proto::SigningOutput_Error();
     error->set_code(SignerErrorCodeInvalidJson);
     error->set_description(result.error_message());
     output.set_allocated_error(error);
 }
 
-void Any::Signer::toJson(const google::protobuf::Message &message, std::string *json_string) const
+void TW::Any::Signer::toJson(const google::protobuf::Message &message, std::string *json_string) const
     noexcept {
     util::JsonPrintOptions options;
     options.preserve_proto_field_names = true;
