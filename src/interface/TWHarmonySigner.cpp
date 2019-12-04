9f9639ff4f448f8ba9e89998bcdec863973e88a1
HaoyangLiu <lhy.0318@163.com>
2019-12-04 14:22:51 +0800

    tss for IOST

--- src/interface/TWHarmonySigner.cpp
+++ src/interface/TWHarmonySigner.cpp
@@ -23,3 +23,13 @@ TW_Harmony_Proto_SigningOutput TWHarmonySignerSign(TW_Harmony_Proto_SigningInput
     return TWDataCreateWithBytes(reinterpret_cast<const uint8_t *>(serialized.data()),
                                  serialized.size());
 }
+
+TWData *_Nonnull TWIOSTSignerMessage(TW_IOST_Proto_SigningInput data, TWData *_Nonnull pubkey, uint8_t algorithm) {
+    Proto::SigningInput input;
+    input.ParseFromArray(TWDataBytes(data), static_cast<int>(TWDataSize(data)));
+
+    Data publicKey(TWDataBytes(pubkey), TWDataBytes(pubkey) + TWDataSize(pubkey));
+
+    auto unsignedTx = Signer::buildUnsignedTx(input, publicKey, algorithm);
+    return TWDataCreateWithBytes(reinterpret_cast<const uint8_t*>(unsignedTx.data()), unsignedTx.size());
+}
