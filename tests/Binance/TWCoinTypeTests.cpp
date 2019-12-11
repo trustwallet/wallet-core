7feb096ba49f145cf56a241763b0971d02fd9108
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-12-11 15:53:30 +0100

    Fix for Binance.

--- tests/Binance/TWCoinTypeTests.cpp
+++ tests/Binance/TWCoinTypeTests.cpp
@@ -28,7 +28,7 @@ TEST(TWBinanceCoinType, TWCoinType) {
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeBinance));
     assertStringsEqual(symbol, "BNB");
     assertStringsEqual(txUrl, "https://explorer.binance.org/tx/A93625C9F9ABEA1A8E31585B30BBB16C34FAE0D172EB5B6B2F834AF077BF06BB");
-    assertStringsEqual(accUrl, "https://explorer.binance.org/bnb1u7jm0cll5h3224y0tapwn6gf6pr49ytewx4gsz");
+    assertStringsEqual(accUrl, "https://explorer.binance.org/address/bnb1u7jm0cll5h3224y0tapwn6gf6pr49ytewx4gsz");
     assertStringsEqual(id, "binance");
     assertStringsEqual(name, "BNB");
 }
