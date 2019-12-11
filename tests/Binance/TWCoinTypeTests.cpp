b02e6dabe6791ea2ef7f2f19cb96ca342c3aa1b6
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-12-11 11:57:14 +0100

    Coin config: Add address url and tests to Eth&Binance.

--- tests/Binance/TWCoinTypeTests.cpp
+++ tests/Binance/TWCoinTypeTests.cpp
@@ -17,6 +17,8 @@ TEST(TWBinanceCoinType, TWCoinType) {
     auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeBinance));
     auto txId = TWStringCreateWithUTF8Bytes("A93625C9F9ABEA1A8E31585B30BBB16C34FAE0D172EB5B6B2F834AF077BF06BB");
     auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeBinance, txId));
+    auto accId = TWStringCreateWithUTF8Bytes("bnb1u7jm0cll5h3224y0tapwn6gf6pr49ytewx4gsz");
+    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeBinance, accId));
     auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeBinance));
     auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeBinance));
 
@@ -26,6 +28,7 @@ TEST(TWBinanceCoinType, TWCoinType) {
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeBinance));
     assertStringsEqual(symbol, "BNB");
     assertStringsEqual(txUrl, "https://explorer.binance.org/tx/A93625C9F9ABEA1A8E31585B30BBB16C34FAE0D172EB5B6B2F834AF077BF06BB");
+    assertStringsEqual(accUrl, "https://explorer.binance.org/bnb1u7jm0cll5h3224y0tapwn6gf6pr49ytewx4gsz");
     assertStringsEqual(id, "binance");
     assertStringsEqual(name, "BNB");
 }
