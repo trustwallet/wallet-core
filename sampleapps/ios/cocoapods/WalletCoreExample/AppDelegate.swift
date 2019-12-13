022b9ce127b611dd03bdbc1cb58d02befba1ad3c
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-12-13 11:12:39 +0100

    Update copyright notice in srcs, remove from coverage

--- sampleapps/ios/cocoapods/WalletCoreExample/AppDelegate.swift
+++ sampleapps/ios/cocoapods/WalletCoreExample/AppDelegate.swift
@@ -1,10 +1,8 @@
+// Copyright © 2017-2019 Trust Wallet.
 //
-//  AppDelegate.swift
-//  WalletCoreExample
-//
-//  Created by Tao X on 10/29/19.
-//  Copyright © 2019 Tao X. All rights reserved.
-//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
 
 import Cocoa
 
