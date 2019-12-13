b079355bc5a3922589b255c83a342518e2c03111
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-12-13 09:52:13 +0100

    Sample apps: Include sample iOS and C++ app.

---
+++ sampleapps/ios/cocoapods/WalletCoreExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WalletCoreExample
+//
+//  Created by Tao X on 10/29/19.
+//  Copyright © 2019 Tao X. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
