88c4c7c8f75b065358f4b7dc0d21cdee218abfaf
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-12-18 14:32:43 +0100

    Cardano: Curve type rename / generation fix.

--- include/TrustWalletCore/TWCurve.h
+++ include/TrustWalletCore/TWCurve.h
@@ -12,12 +12,12 @@ TW_EXTERN_C_BEGIN
 /// Elliptic cruves
 TW_EXPORT_ENUM()
 enum TWCurve {
-    TWCurveSECP256k1,              // "secp256k1"
-    TWCurveED25519,                // "ed25519"
-    TWCurveED25519Blake2bNano,     // "ed25519-blake2b-nano"
-    TWCurveCurve25519,             // "curve25519"
-    TWCurveNIST256p1,              // "nist256p1"
-    TWCurveED25519Extended,        // "ed25519 cardano seed" with extended keys, used by e.g. Cardano
+    TWCurveSECP256k1 = 0,             // "secp256k1"
+    TWCurveED25519 = 1,               // "ed25519"
+    TWCurveED25519Blake2bNano = 2,    // "ed25519-blake2b-nano"
+    TWCurveCurve25519 = 3,            // "curve25519"
+    TWCurveNIST256p1 = 4,             // "nist256p1"
+    TWCurveED25519Extended = 5,       // "ed25519 cardano seed" with extended keys, used by e.g. Cardano
 };
 
 TW_EXTERN_C_END
