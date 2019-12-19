84db0ec6d86a9bc1683cee1833b183f71357352b
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2019-12-19 23:46:30 +0000

    add real data to coin type test

--- tests/Kava/TWCoinTypeTests.cpp
+++ tests/Kava/TWCoinTypeTests.cpp
@@ -15,9 +15,9 @@
 
 TEST(TWKavaCoinType, TWCoinType) {
     auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeKava));
-    auto txId = TWStringCreateWithUTF8Bytes("t123");
+    auto txId = TWStringCreateWithUTF8Bytes("2988DF83FCBFAA38179D583A96734CBD071541D6768221BB23111BC8136D5E6A");
     auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeKava, txId));
-    auto accId = TWStringCreateWithUTF8Bytes("a12");
+    auto accId = TWStringCreateWithUTF8Bytes("kava1jf9aaj9myrzsnmpdr7twecnaftzmku2mdpy2a7");
     auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeKava, accId));
     auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeKava));
     auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeKava));
@@ -27,8 +27,8 @@ TEST(TWKavaCoinType, TWCoinType) {
     ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeKava));
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeKava));
     assertStringsEqual(symbol, "KAVA");
-    assertStringsEqual(txUrl, "https://kava.bigdipper.live/transactions/t123");
-    assertStringsEqual(accUrl, "https://kava.bigdipper.live/account/a12");
+    assertStringsEqual(txUrl, "https://kava.bigdipper.live/transactions/2988DF83FCBFAA38179D583A96734CBD071541D6768221BB23111BC8136D5E6A");
+    assertStringsEqual(accUrl, "https://kava.bigdipper.live/account/kava1jf9aaj9myrzsnmpdr7twecnaftzmku2mdpy2a7");
     assertStringsEqual(id, "kava");
     assertStringsEqual(name, "Kava");
 }
