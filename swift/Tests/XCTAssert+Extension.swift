e33fd34a2a4e19a5045bf0f7880073d4b90749de
hewigovens <360470+hewigovens@users.noreply.github.com>
2019-12-20 11:08:55 +0800

    try catch

--- swift/Tests/XCTAssert+Extension.swift
+++ swift/Tests/XCTAssert+Extension.swift
@@ -7,11 +7,15 @@
 import XCTest
 
 func XCTAssertJSONEqual(_ lhs: String, _ rhs: String) {
-    let ljson = try! JSONSerialization.jsonObject(with: lhs.data(using: .utf8)!, options: [.fragmentsAllowed])
-    let rjson = try! JSONSerialization.jsonObject(with: rhs.data(using: .utf8)!, options: [.fragmentsAllowed])
+    do {
+        let ljson = try JSONSerialization.jsonObject(with: lhs.data(using: .utf8)!, options: [.fragmentsAllowed])
+        let rjson = try JSONSerialization.jsonObject(with: rhs.data(using: .utf8)!, options: [.fragmentsAllowed])
 
-    let lstring = try! JSONSerialization.data(withJSONObject: ljson, options: [.sortedKeys, .prettyPrinted])
-    let rstring = try! JSONSerialization.data(withJSONObject: rjson, options: [.sortedKeys, .prettyPrinted])
+        let lstring = try JSONSerialization.data(withJSONObject: ljson, options: [.sortedKeys, .prettyPrinted])
+        let rstring = try JSONSerialization.data(withJSONObject: rjson, options: [.sortedKeys, .prettyPrinted])
 
-    XCTAssertEqual(lstring, rstring, "\(lhs) is not equal to \(rhs)")
+        XCTAssertEqual(lstring, rstring, "\(lhs) is not equal to \(rhs)")
+    } catch let error {
+        XCTFail(error.localizedDescription)
+    }
 }
