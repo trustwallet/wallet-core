50c0bd5a8cae80c8999304a3c00e3ef93fa5c4e2
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-12-20 11:53:09 +0100

    Update common coin derivation test.

--- tests/interface/TWCardanoTests.cpp
+++ tests/interface/TWCardanoTests.cpp
@@ -5,7 +5,7 @@
 // file LICENSE at the root of the source code distribution tree.
 
 #include <TrustWalletCore/TWCardanoAddress.h>
-#include <TrustWalletCore/TWCardanoSigner.h>
+//#include <TrustWalletCore/TWCardanoSigner.h>
 #include "HexCoding.h"
 
 #include "TWTestUtilities.h"
