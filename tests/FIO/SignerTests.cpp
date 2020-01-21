690ea37e28d609b2a59e398f596813e57314831d
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-01-21 10:27:47 +0100

    FIO Signer: Actor derivation.

---
+++ tests/FIO/SignerTests.cpp
@@ -0,0 +1,47 @@
+// Copyright © 2017-2020 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "FIO/Signer.h"
+#include "HexCoding.h"
+
+#include <gtest/gtest.h>
+
+using namespace TW;
+using namespace TW::FIO;
+using namespace std;
+
+TEST(FIOSigner, Actor) {
+    {
+        const auto addr1 = "FIO6cDpi7vPnvRwMEdXtLnAmFwygaQ8CzD7vqKLBJ2GfgtHBQ4PPy";
+        Address addr = Address(addr1);
+        EXPECT_EQ(addr1, addr.string());
+
+        uint64_t shortenedKey = Actor::shortenKey(addr.bytes);
+        EXPECT_EQ(1518832697283783336U, shortenedKey);
+
+        string name = Actor::name(shortenedKey);
+        EXPECT_EQ("2odzomo2v4pec", name);
+
+        string actor = Actor::actor(addr);
+        EXPECT_EQ("2odzomo2v4pe", actor);
+    }
+    {
+        const auto addr2 = "FIO7uMZoeei5HtXAD24C4yCkpWWbf24bjYtrRNjWdmGCXHZccwuiE";
+        Address addr = Address(addr2);
+        EXPECT_EQ(addr2, addr.string());
+
+        string actor = Actor::actor(addr);
+        EXPECT_EQ("hhq2g4qgycfb", actor);
+    }
+    {
+        const auto addr2 = "FIO88k6wXVpSpoWy4TeUnTpsWomvALwQ5VEuU3UjYg83JdMxck6Gr";
+        Address addr = Address(addr2);
+        EXPECT_EQ(addr2, addr.string());
+
+        string actor = Actor::actor(addr);
+        EXPECT_EQ("fopt11cxukby", actor);
+    }
+}
