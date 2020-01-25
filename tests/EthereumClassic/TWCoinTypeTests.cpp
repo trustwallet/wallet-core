d7cd89f7fea6556b833d78e4fd0ed5308ba71129
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2020-01-26 08:53:09 +0900

    Revert "Update ETC explorer to etcblockexplorer.com. (#824)" (#836)
    This reverts commit 39dd59b26b9d9504c3d4cbe77a80a5bd1e7a2f6b.

--- tests/EthereumClassic/TWCoinTypeTests.cpp
+++ tests/EthereumClassic/TWCoinTypeTests.cpp
@@ -27,8 +27,8 @@ TEST(TWEthereumClassicCoinType, TWCoinType) {
     ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeEthereumClassic));
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeEthereumClassic));
     assertStringsEqual(symbol, "ETC");
-    assertStringsEqual(txUrl, "https://etcblockexplorer.com/tx/0x66004165d3901819dc22e568931591d2e4287bda54995f4ce2701a12016f5997");
-    assertStringsEqual(accUrl, "https://etcblockexplorer.com/addr/0x9eab4b0fc468a7f5d46228bf5a76cb52370d068d");
+    assertStringsEqual(txUrl, "https://blockscout.com/etc/mainnet/tx/0x66004165d3901819dc22e568931591d2e4287bda54995f4ce2701a12016f5997");
+    assertStringsEqual(accUrl, "https://blockscout.com/etc/mainnet/address/0x9eab4b0fc468a7f5d46228bf5a76cb52370d068d");
     assertStringsEqual(id, "classic");
     assertStringsEqual(name, "Ethereum Classic");
 }
