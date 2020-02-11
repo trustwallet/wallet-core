6c50a10dd5593898c137bd36548f9e095baacaeb
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-02-11 16:15:30 +0800

    Add @discardableResult support

--- codegen/lib/function_decl.rb
+++ codegen/lib/function_decl.rb
@@ -3,15 +3,16 @@
 # Function or method declaration
 class FunctionDecl
   attr_reader :name, :entity
-  attr_accessor :is_method, :return_type, :parameters, :static
+  attr_accessor :is_method, :return_type, :parameters, :static, :discardable_result
 
-  def initialize(name:, entity:, is_method:, return_type: :void, parameters: [], static: false)
+  def initialize(name:, entity:, is_method:, return_type: :void, parameters: [], static: false, discardable_result: false)
     @name = name
     @entity = entity
     @is_method = is_method
     @return_type = return_type
     @parameters = parameters
     @static = static
+    @discardable_result = discardable_result
   end
 end
 
--- include/TrustWalletCore/TWEthereumAbiFunction.h
+++ include/TrustWalletCore/TWEthereumAbiFunction.h
@@ -31,40 +31,74 @@ TWString *_Nonnull TWEthereumAbiFunctionGetType(struct TWEthereumAbiFunction *_N
 /// For output parameters (isOutput=true) a value has to be specified, although usually not needd.
 /// Returns the index of the parameter (0-based).
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamUInt8(struct TWEthereumAbiFunction *_Nonnull func_in, uint8_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamUInt16(struct TWEthereumAbiFunction *_Nonnull func_in, uint16_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamUInt32(struct TWEthereumAbiFunction *_Nonnull func_in, uint32_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamUInt64(struct TWEthereumAbiFunction *_Nonnull func_in, uint64_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamUInt256(struct TWEthereumAbiFunction *_Nonnull func_in, TWData *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamUIntN(struct TWEthereumAbiFunction *_Nonnull func_in, int bits, TWData *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamInt8(struct TWEthereumAbiFunction *_Nonnull func_in, int8_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamInt16(struct TWEthereumAbiFunction *_Nonnull func_in, int16_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamInt32(struct TWEthereumAbiFunction *_Nonnull func_in, int32_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamInt64(struct TWEthereumAbiFunction *_Nonnull func_in, int64_t val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamInt256(struct TWEthereumAbiFunction *_Nonnull func_in, TWData *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamIntN(struct TWEthereumAbiFunction *_Nonnull func_in, int bits, TWData *_Nonnull val, bool isOutput);
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamBool(struct TWEthereumAbiFunction *_Nonnull func_in, bool val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamString(struct TWEthereumAbiFunction *_Nonnull func_in, TWString *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamAddress(struct TWEthereumAbiFunction *_Nonnull func_in, TWData *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamBytes(struct TWEthereumAbiFunction *_Nonnull func_in, TWData *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamBytesFix(struct TWEthereumAbiFunction *_Nonnull func_in, size_t count_in, TWData *_Nonnull val, bool isOutput);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddParamArray(struct TWEthereumAbiFunction *_Nonnull func_in, bool isOutput);
 
 /// Methods for accessing the value of an output or input parameter, of different types.
@@ -84,38 +118,71 @@ TWData *_Nonnull TWEthereumAbiFunctionGetParamAddress(struct TWEthereumAbiFuncti
 /// Methods for adding a parameter of the given type to a top-level input parameter array.  Returns the index of the parameter (0-based).
 /// Note that nested ParamArrays are not possible through this API, could be done by using index paths like "1/0"
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamUInt8(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, uint8_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamUInt16(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, uint16_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamUInt32(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, uint32_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamUInt64(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, uint64_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamUInt256(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, TWData *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamUIntN(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, int bits, TWData *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamInt8(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, int8_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamInt16(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, int16_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamInt32(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, int32_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamInt64(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, int64_t val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamInt256(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, TWData *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamIntN(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, int bits, TWData *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamBool(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, bool val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamString(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, TWString *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamAddress(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, TWData *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamBytes(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, TWData *_Nonnull val);
+
 TW_EXPORT_METHOD
+TW_METHOD_DISCARDABLE_RESULT
 int TWEthereumAbiFunctionAddInArrayParamBytesFix(struct TWEthereumAbiFunction *_Nonnull func_in, int arrayIdx, size_t count_in, TWData *_Nonnull val);
 
 TW_EXTERN_C_END
