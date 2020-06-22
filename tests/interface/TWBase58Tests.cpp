e738961e7080ab85de7af24313022df15263ee6e
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-06-22 07:24:39 +0200

    Add negative Base58 tests.

--- tests/interface/TWBase58Tests.cpp
+++ tests/interface/TWBase58Tests.cpp
@@ -22,6 +22,12 @@ TEST(TWBase58, EncodeNoCheck) {
     assertStringsEqual(result, "1Bp9U1ogV3A14FMvKbRJms7ctyso4Z4Tcx");
 }
 
+TEST(TWBase58, EncodeNoCheck2) {
+    const auto input = DATA("00769bdff96a02f9135a1d19b749db6a78fe07dc90deadbeef");
+    auto result = WRAPS(TWBase58EncodeNoCheck(input.get()));
+    assertStringsEqual(result, "1Bp9U1ogV3A14FMvKbRJms7ctyso5FdSz2");
+}
+
 TEST(TWBase58, Decode) {
     const auto input = STRING("1Bp9U1ogV3A14FMvKbRJms7ctyso4Z4Tcx");
     auto result = WRAPS(TWBase58Decode(input.get()));
@@ -32,4 +38,24 @@ TEST(TWBase58, DecodeNoCheck) {
     const auto input = STRING("1Bp9U1ogV3A14FMvKbRJms7ctyso4Z4Tcx");
     auto result = WRAPS(TWBase58DecodeNoCheck(input.get()));
     assertHexEqual(result, "00769bdff96a02f9135a1d19b749db6a78fe07dc90c3507da5");
+}
+
+TEST(TWBase58, Decode_WrongChecksum) {
+    const auto input = STRING("1Bp9U1ogV3A14FMvKbRJms7ctyso5FdSz2");
+    auto result = WRAPS(TWBase58Decode(input.get()));
+    ASSERT_EQ(result.get(), nullptr);
+}
+
+TEST(TWBase58, DecodeNoCheck_WrongChecksum) {
+    const auto input = STRING("1Bp9U1ogV3A14FMvKbRJms7ctyso5FdSz2");
+    auto result = WRAPS(TWBase58DecodeNoCheck(input.get()));
+    // decodes despite wrong checksum
+    assertHexEqual(result, "00769bdff96a02f9135a1d19b749db6a78fe07dc90deadbeef");
+}
+
+TEST(TWBase58, Decode_InvalidChar) {
+    // 0 is invalid
+    const auto input = STRING("1Bp9U1ogV3A14FMvKbRJms7ctyso4Z4Tc0");
+    auto result = WRAPS(TWBase58Decode(input.get()));
+    ASSERT_EQ(result.get(), nullptr);
 }
