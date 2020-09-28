5bceb1c8df78fbd4243af869ff706aab66234e80
Richard Patel <me@terorie.dev>
2020-09-28 08:34:59 +0200

    upgrade Filecoin ABI (again) (#1114)

--- tests/Filecoin/TWAnySignerTests.cpp
+++ tests/Filecoin/TWAnySignerTests.cpp
@@ -26,15 +26,19 @@ TEST(TWAnySignerFilecoin, Sign) {
     // 600 FIL
     // auto value = parse_hex("2086ac351052600000");
     auto value = store(uint256_t(600) * uint256_t(1'000'000'000) * uint256_t(1'000'000'000));
-    auto gasPrice = store(uint256_t(2));
     uint64_t gasLimit = 1000;
+    // auto gasFeeCap = parse_hex("25f273933db5700000");
+    auto gasFeeCap = store(uint256_t(700) * uint256_t(1'000'000'000) * uint256_t(1'000'000'000));
+    // auto gasPremium = parse_hex("2b5e3af16b18800000");
+    auto gasPremium = store(uint256_t(800) * uint256_t(1'000'000'000) * uint256_t(1'000'000'000));
 
     input.set_private_key(privateKey.data(), privateKey.size());
     input.set_to_address(toAddress);
     input.set_nonce(nonce);
     input.set_value(value.data(), value.size());
-    input.set_gas_price(gasPrice.data(), gasPrice.size());
     input.set_gas_limit(gasLimit);
+    input.set_gas_fee_cap(gasFeeCap.data(), gasFeeCap.size());
+    input.set_gas_premium(gasPremium.data(), gasPremium.size());
 
     auto inputString = input.SerializeAsString();
     auto inputData = TWDataCreateWithBytes((const byte*)inputString.data(), inputString.size());
@@ -42,24 +46,22 @@ TEST(TWAnySignerFilecoin, Sign) {
     auto outputData = TWAnySignerSign(inputData, TWCoinTypeFilecoin);
 
     ASSERT_EQ(hex(TWDataBytes(outputData), TWDataBytes(outputData) + TWDataSize(outputData)),
-              "0aa401"
-              "828900583103a33d476e13eb8bde5d21becf2b86dd60642f0297cc6a5b914de8"
-              "6bb1d096861ba99bb13c577fee003e72f51e89f837c45501cf01bf485f61435e"
-              "6770b52615bf455e043a2361024a002086ac3510526000004200021903e80040"
-              "584201d7320c388135b20dea596493156495519db9e3cfcb78ccb18462dde4aa"
-              "1c507750a79ef014caf717acbe2ce562e5e55a4c990607c32a741f1c073fa922"
-              "176cc900");
+              "0ab701"
+              "828a00583103a33d476e13eb8bde5d21becf2b86dd60642f0297cc6a5b914de86bb1d096861ba99bb13c"
+              "577fee003e72f51e89f837c45501cf01bf485f61435e6770b52615bf455e043a2361024a002086ac3510"
+              "526000001903e84a0025f273933db57000004a002b5e3af16b1880000000405842018cc46ef8e67f95fa"
+              "69826a927c6b27b45ad12d6667520dd96262534d12ec56de15d77d74bd1dd5286269684e92e2c36f0ce1"
+              "3ab0482637d6267d390f54adaa3201");
 
     Proto::SigningOutput output;
     output.ParseFromArray(TWDataBytes(outputData), static_cast<int>(TWDataSize(outputData)));
 
     ASSERT_EQ(hex(output.encoded()),
-              "828900583103a33d476e13eb8bde5d21becf2b86dd60642f0297cc6a5b914de8"
-              "6bb1d096861ba99bb13c577fee003e72f51e89f837c45501cf01bf485f61435e"
-              "6770b52615bf455e043a2361024a002086ac3510526000004200021903e80040"
-              "584201d7320c388135b20dea596493156495519db9e3cfcb78ccb18462dde4aa"
-              "1c507750a79ef014caf717acbe2ce562e5e55a4c990607c32a741f1c073fa922"
-              "176cc900");
+              "828a00583103a33d476e13eb8bde5d21becf2b86dd60642f0297cc6a5b914de86bb1d096861ba99bb13c"
+              "577fee003e72f51e89f837c45501cf01bf485f61435e6770b52615bf455e043a2361024a002086ac3510"
+              "526000001903e84a0025f273933db57000004a002b5e3af16b1880000000405842018cc46ef8e67f95fa"
+              "69826a927c6b27b45ad12d6667520dd96262534d12ec56de15d77d74bd1dd5286269684e92e2c36f0ce1"
+              "3ab0482637d6267d390f54adaa3201");
 
     TWDataDelete(inputData);
     TWDataDelete(outputData);
@@ -67,15 +69,23 @@ TEST(TWAnySignerFilecoin, Sign) {
 
 TEST(TWAnySignerFilecoin, SignJSON) {
     auto json = STRING(
-        R"({"toAddress":"f3um6uo3qt5of54xjbx3hsxbw5mbsc6auxzrvfxekn5bv3duewqyn2tg5rhrlx73qahzzpkhuj7a34iq7oifsq","nonce":"2","value":"IIasNRBSYAAA","gasPrice":"Ag==","gasLimit":"1000"})");
+        R"({
+            "toAddress": "f3um6uo3qt5of54xjbx3hsxbw5mbsc6auxzrvfxekn5bv3duewqyn2tg5rhrlx73qahzzpkhuj7a34iq7oifsq",
+            "nonce": "2",
+            "value": "IIasNRBSYAAA",
+            "gasLimit": 1000,
+            "gasFeeCap": "JfJzkz21cAAA",
+            "gasPremium": "K1468WsYgAAA"
+        })");
     auto key = DATA("1d969865e189957b9824bd34f26d5cbf357fda1a6d844cbf0c9ab1ed93fa7dbe");
     auto result = WRAPS(TWAnySignerSignJSON(json.get(), key.get(), TWCoinTypeFilecoin));
 
     ASSERT_TRUE(TWAnySignerSupportsJSON(TWCoinTypeFilecoin));
-    assertStringsEqual(result, "828900583103a33d476e13eb8bde5d21becf2b86dd60642f0297cc6a5b914de8"
-                               "6bb1d096861ba99bb13c577fee003e72f51e89f837c45501cf01bf485f61435e"
-                               "6770b52615bf455e043a2361024a002086ac3510526000004200021903e80040"
-                               "584201d7320c388135b20dea596493156495519db9e3cfcb78ccb18462dde4aa"
-                               "1c507750a79ef014caf717acbe2ce562e5e55a4c990607c32a741f1c073fa922"
-                               "176cc900");
+    assertStringsEqual(
+        result,
+        "828a00583103a33d476e13eb8bde5d21becf2b86dd60642f0297cc6a5b914de86bb1d096861ba99bb13c"
+        "577fee003e72f51e89f837c45501cf01bf485f61435e6770b52615bf455e043a2361024a002086ac3510"
+        "526000001903e84a0025f273933db57000004a002b5e3af16b1880000000405842018cc46ef8e67f95fa"
+        "69826a927c6b27b45ad12d6667520dd96262534d12ec56de15d77d74bd1dd5286269684e92e2c36f0ce1"
+        "3ab0482637d6267d390f54adaa3201");
 }
--- tests/Filecoin/TransactionTests.cpp
+++ tests/Filecoin/TransactionTests.cpp
@@ -13,6 +13,16 @@
 
 namespace TW::Filecoin {
 
+TEST(FilecoinTransaction, EncodeBigInt) {
+    ASSERT_EQ(hex(encodeBigInt(0)), "");
+    ASSERT_EQ(hex(encodeBigInt(1)), "0001");
+    ASSERT_EQ(hex(encodeBigInt(16)), "0010");
+    ASSERT_EQ(hex(encodeBigInt(1111111111111)), "000102b36211c7");
+    uint256_t reallyBig = 1;
+    reallyBig <<= 128;
+    ASSERT_EQ(hex(encodeBigInt(reallyBig)), "000100000000000000000000000000000000");
+}
+
 TEST(FilecoinTransaction, Serialize) {
     const PrivateKey privateKey(
         parse_hex("2f0f1d2c8de955c7c3fb4d9cae02539fadcb13fa998ccd9a1e871bed95f1941e"));
@@ -23,14 +33,15 @@ TEST(FilecoinTransaction, Serialize) {
     Transaction tx(toAddress, fromAddress,
                    /*nonce*/ 0x1234567890,
                    /*value*/ 1000,
-                   /*gasPrice*/ 1,
-                   /*gasLimit*/ 50);
+                   /*gasLimit*/ 3333333333,
+                   /*gasFeeCap*/ 11111111,
+                   /*gasPremium*/ 333333);
 
     ASSERT_EQ(hex(tx.message().encoded()),
-              "890055013d403ac3911e9f806228326fa68619d36a4641d455013d413d4c3fe3"
-              "d89f99495a48c6046224a71f0cd71b0000001234567890430003e842000118320040");
+              "8a0055013d403ac3911e9f806228326fa68619d36a4641d455013d413d4c3fe3d89f99495a48c6046224"
+              "a71f0cd71b0000001234567890430003e81ac6aea1554400a98ac744000516150040");
     ASSERT_EQ(hex(tx.cid()),
-              "0171a0e40220bebd2d8facba996f5e04e7c64e1b0f088c63078263a4587b838ed3573419d743");
+              "0171a0e40220a3b06c2837a94e3a431a78b00536d0298455ceec3d304adf26a3868147c4e6e1");
 }
 
 } // namespace TW::Filecoin
