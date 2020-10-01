3248d526f21884323bcb11cf0957a6ff9dd675f5
Dave Puchyr <dave@slim.avaritia.com>
2020-10-01 10:03:58 +0200

    Fix indentation

--- tests/IOV/TWCoinTypeTests.cpp
+++ tests/IOV/TWCoinTypeTests.cpp
@@ -14,21 +14,22 @@
 
 
 TEST(TWIOVCoinType, TWCoinType) {
-auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeIOV));
-auto txId = TWStringCreateWithUTF8Bytes("5E189F10BE129169096096B6AD39547631780E31089B1C56C1DB35C92FD39801");
-auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeIOV, txId));
-auto accId = TWStringCreateWithUTF8Bytes("star1mj92w3zhd89znd26qq6yarpr86yc573khrm0hv");
-auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeIOV, accId));
-auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeIOV));
-auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeIOV));
+    auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeIOV));
+    auto txId = TWStringCreateWithUTF8Bytes("5E189F10BE129169096096B6AD39547631780E31089B1C56C1DB35C92FD39801");
+    auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeIOV, txId));
+    auto accId = TWStringCreateWithUTF8Bytes("star1mj92w3zhd89znd26qq6yarpr86yc573khrm0hv");
+    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeIOV, accId));
+    auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeIOV));
+    auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeIOV));
 
-ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeIOV), 6);
-ASSERT_EQ(TWBlockchainCosmos, TWCoinTypeBlockchain(TWCoinTypeIOV));
-ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeIOV));
-ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeIOV));
-assertStringsEqual(symbol, "IOV");
-assertStringsEqual(txUrl, "https://big-dipper.iov-mainnet-2.iov.one/transactions/5E189F10BE129169096096B6AD39547631780E31089B1C56C1DB35C92FD39801");
-assertStringsEqual(accUrl, "https://big-dipper.iov-mainnet-2.iov.one/account/star1mj92w3zhd89znd26qq6yarpr86yc573khrm0hv");
-assertStringsEqual(id, "iov");
-assertStringsEqual(name, "IOV");
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeIOV), 6);
+    ASSERT_EQ(TWBlockchainCosmos, TWCoinTypeBlockchain(TWCoinTypeIOV));
+    ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeIOV));
+    ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeIOV));
+    assertStringsEqual(symbol, "IOV");
+    assertStringsEqual(txUrl, "https://big-dipper.iov-mainnet-2.iov.one/transactions/5E189F10BE129169096096B6AD39547631780E31089B1C56C1DB35C92FD39801");
+    assertStringsEqual(accUrl, "https://big-dipper.iov-mainnet-2.iov.one/account/star1mj92w3zhd89znd26qq6yarpr86yc573khrm0hv");
+    assertStringsEqual(id, "iov");
+    assertStringsEqual(name, "IOV");
 }
+
