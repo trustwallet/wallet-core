c4f94f2f25d63ca84dd2ee7fa684f8c4e1d574a8
Adam R <13562139+catenocrypt@users.noreply.github.com>
2020-11-18 10:02:18 +0100

    Remove empty line in TestCoinType.kt
    Co-authored-by: hewigovens <360470+hewigovens@users.noreply.github.com>

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/TestCoinType.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/TestCoinType.kt
@@ -32,7 +32,6 @@ class TestCoinType {
         assertEquals(CoinType.TEZOS.value(), 1729)
         assertEquals(CoinType.QTUM.value(), 2301)
         assertEquals(CoinType.NEBULAS.value(), 2718)
-
     }
 
     @Test
