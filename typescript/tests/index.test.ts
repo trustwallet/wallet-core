596aeae6b4666211adab00eae4dda3fe70d72062
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-12-08 14:44:31 +0100

    Typescript test

--- typescript/tests/index.test.ts
+++ typescript/tests/index.test.ts
@@ -51,14 +51,14 @@ describe('Wallet Core types tests', () => {
             nonce: Buffer.from('09', 'hex'),
             gasPrice: Buffer.from('04a817c800', 'hex'),
             gasLimit: Buffer.from('5208', 'hex'),
-            privateKey: Buffer.from('4646464646464646464646464646464646464646464646464646464646464646', 'hex'),
-            contractTransfer: TW.Ethereum.Proto.SigningInput.TransferContract.create({
+            contractTransfer: TW.Ethereum.Proto.TransferContract.create({
                 amount: Buffer.from('0de0b6b3a7640000', 'hex')
-            })
+            }),
+            privateKey: Buffer.from('4646464646464646464646464646464646464646464646464646464646464646', 'hex')
         });
 
         const encoded = TW.Ethereum.Proto.SigningInput.encode(input).finish()
-        expect(Buffer.from(encoded).toString('hex')).to.equal("0a01011201091a0504a817c800220252082a2a30783335333533353335333533353335333533353335333533353335333533353335333533353335333532080de0b6b3a764000042204646464646464646464646464646464646464646464646464646464646464646")
+        expect(Buffer.from(encoded).toString('hex')).to.equal("0a01011201091a0504a817c800220252082a2a307833353335333533353335333533353335333533353335333533353335333533353335333533353335322046464646464646464646464646464646464646464646464646464646464646465a0a0a080de0b6b3a7640000")
     })
 
     it('test Bitcoin / Bitcoin SigningInput', () => {
