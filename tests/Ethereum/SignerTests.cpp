b74c5fc2887d2ee05dff5fb819f468f0511618be
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-12-23 21:15:07 +0900

    Add amount to contract and fix data variable in case transfer (#1223)

--- tests/Ethereum/SignerTests.cpp
+++ tests/Ethereum/SignerTests.cpp
@@ -23,7 +23,7 @@ public:
 
 TEST(EthereumSigner, Hash) {
     auto address = parse_hex("0x3535353535353535353535353535353535353535");
-    auto transaction = Transaction::buildTransfer(
+    auto transaction = Transaction(
         /* nonce: */ 9,
         /* gasPrice: */ 20000000000,
         /* gasLimit: */ 21000,
@@ -38,7 +38,7 @@ TEST(EthereumSigner, Hash) {
 
 TEST(EthereumSigner, Sign) {
     auto address = parse_hex("0x3535353535353535353535353535353535353535");
-    auto transaction = Transaction::buildTransfer(
+    auto transaction = Transaction(
         /* nonce: */ 9,
         /* gasPrice: */ 20000000000,
         /* gasLimit: */ 21000,
