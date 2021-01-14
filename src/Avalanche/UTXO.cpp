1cb1de94c92e980142f51f53e15f4a53647933a3
Evan Richard <evan@avalabs.org>
2021-01-14 15:03:39 -0500

    Add UTXO class.

---
+++ src/Avalanche/UTXO.cpp
@@ -0,0 +1,23 @@
+// Copyright © 2017-2020 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "UTXO.h"
+
+#include "../BinaryCoding.h"
+
+using namespace TW::Avalanche;
+
+void UTXO::encode(Data& data) const {
+    encode16LE(CodecID, data);
+    for (auto byte : TxID) {
+        data.push_back(byte);
+    }
+    encode32LE(UTXOIndex, data);
+    for (auto byte : AssetID) {
+        data.push_back(byte);
+    }
+    Output.encode(data);
+}
---
+++ src/Avalanche/UTXO.h
@@ -0,0 +1,30 @@
+// Copyright © 2017-2020 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "TransferableOutput.h"
+
+namespace TW::Avalanche {
+
+    class UTXO {
+      public: 
+        const uint16_t CodecID = 0;
+        Data TxID;
+        uint32_t UTXOIndex;
+        Data AssetID;
+        TransferableOutput Output;
+        
+        UTXO(Data &txID, uint32_t UTXOIndex, Data &assetID, TransferableOutput &output)
+        : TxID(txID), UTXOIndex(UTXOIndex), AssetID(assetID), Output(output) {}
+
+        /// Encodes the InitialState into the provided buffer.
+        void encode(Data& data) const;
+
+        bool operator<(const UTXO& other);
+    };
+
+} // namespace TW::Avalanche
