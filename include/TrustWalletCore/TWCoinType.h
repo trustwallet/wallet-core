9dc53a0779bc8ee85ed9654f149f334912bebb8e
Emmanuel <emmanuelm41@gmail.com>
2021-01-25 12:31:32 -0300

    fix oasis coin value
    Co-authored-by: hewigovens <360470+hewigovens@users.noreply.github.com>

--- include/TrustWalletCore/TWCoinType.h
+++ include/TrustWalletCore/TWCoinType.h
@@ -84,7 +84,7 @@ enum TWCoinType {
     TWCoinTypeBandChain = 494,
     TWCoinTypeSmartChainLegacy = 10000714,
     TWCoinTypeSmartChain = 20000714,
-    TWCoinTypeOasis = 5786211,
+    TWCoinTypeOasis = 474,
 };
 
 /// Returns the blockchain for a coin type.
