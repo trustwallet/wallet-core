602e6dcc418c3af663a0eb82dc851009b0201d2e
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-02-17 01:29:41 +0100

    Missing method from ecdsa.h

--- trezor-crypto/include/TrezorCrypto/ecdsa.h
+++ trezor-crypto/include/TrezorCrypto/ecdsa.h
@@ -128,6 +128,7 @@ int ecdsa_recover_pub_from_sig(const ecdsa_curve *curve, uint8_t *pub_key,
                                const uint8_t *sig, const uint8_t *digest,
                                int recid);
 int ecdsa_sig_to_der(const uint8_t *sig, uint8_t *der);
+int ecdsa_sig_from_der(const uint8_t *der, size_t der_len, uint8_t sig[64]);
 
 // [wallet-core]
 int zil_schnorr_sign(const ecdsa_curve *curve, const uint8_t *priv_key, const uint8_t *msg, const uint32_t msg_len, uint8_t *sig);
