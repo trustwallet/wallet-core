186c43a2695f9125f858a284f9823a0be643ac09
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-02-23 22:14:11 +0100

    various headers, negative

--- trezor-crypto/include/TrezorCrypto/sha3.h
+++ trezor-crypto/include/TrezorCrypto/sha3.h
@@ -21,7 +21,6 @@
 #define __SHA3_H__
 
 #include <stdint.h>
-#include "options.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -68,7 +67,6 @@ void sha3_512_Init(SHA3_CTX *ctx);
 void sha3_Update(SHA3_CTX *ctx, const unsigned char* msg, size_t size);
 void sha3_Final(SHA3_CTX *ctx, unsigned char* result);
 
-#if USE_KECCAK
 #define keccak_224_Init sha3_224_Init
 #define keccak_256_Init sha3_256_Init
 #define keccak_384_Init sha3_384_Init
@@ -77,13 +75,12 @@ void sha3_Final(SHA3_CTX *ctx, unsigned char* result);
 void keccak_Final(SHA3_CTX *ctx, unsigned char* result);
 void keccak_256(const unsigned char* data, size_t len, unsigned char* digest);
 void keccak_512(const unsigned char* data, size_t len, unsigned char* digest);
-#endif
 
 void sha3_256(const unsigned char* data, size_t len, unsigned char* digest);
 void sha3_512(const unsigned char* data, size_t len, unsigned char* digest);
 
 #ifdef __cplusplus
 } /* extern "C" */
-#endif /* __cplusplus */
+#endif
 
 #endif /* __SHA3_H__ */
