7da561b90107ed3601a6c8191b792b6f367ae826
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-03-02 10:40:59 +0100

    Re-add umbrella TrezorCrypto.h

---
+++ trezor-crypto/include/TrezorCrypto/TrezorCrypto.h
@@ -0,0 +1,31 @@
+// Copyright © 2017-2020 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include <TrezorCrypto/options.h>
+
+#include <TrezorCrypto/address.h>
+#include <TrezorCrypto/aes.h>
+#include <TrezorCrypto/base32.h>
+#include <TrezorCrypto/bignum.h>
+#include <TrezorCrypto/bip32.h>
+#include <TrezorCrypto/bip39.h>
+#include <TrezorCrypto/blake2b.h>
+#include <TrezorCrypto/blake256.h>
+#include <TrezorCrypto/cash_addr.h>
+#include <TrezorCrypto/curves.h>
+#include <TrezorCrypto/ed25519.h>
+#include <TrezorCrypto/hasher.h>
+#include <TrezorCrypto/hmac.h>
+#include <TrezorCrypto/memzero.h>
+//#include <TrezorCrypto/monero.h>
+//#include <TrezorCrypto/nem.h>
+#include <TrezorCrypto/nist256p1.h>
+#include <TrezorCrypto/pbkdf2.h>
+#include <TrezorCrypto/rand.h>
+#include <TrezorCrypto/ripemd160.h>
+#include <TrezorCrypto/secp256k1.h>
+#include <TrezorCrypto/sha2.h>
+#include <TrezorCrypto/sha3.h>
