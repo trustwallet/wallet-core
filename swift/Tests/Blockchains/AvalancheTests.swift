186dd3915b4fe2aa73db1f57f3786857861f8302
Evan Richard <evan@avalabs.org>
2021-03-19 12:40:21 -0400

    Add AddressTest.

--- swift/Tests/Blockchains/AvalancheTests.swift
+++ swift/Tests/Blockchains/AvalancheTests.swift
@@ -8,17 +8,14 @@ import TrustWalletCore
 import XCTest
 
 class AvalancheTests: XCTestCase {
-    // TODO: Check and finalize implementation
 
     func testAddress() {
-        // TODO: Check and finalize implementation
-
-        let key = PrivateKey(data: Data(hexString: "__PRIVATE_KEY_DATA__")!)!
-        let pubkey = key.getPublicKeyEd25519()
+        let key = PrivateKey(data: Data(hexString: "0x98cb077f972feb0481f1d894f272c6a1e3c15e272a1658ff716444f465200070")!)!
+        let pubkey = key.getPublicKeySecp256k1(compressed: false)
         let address = AnyAddress(publicKey: pubkey, coin: .avalanche)
-        let addressFromString = AnyAddress(string: "__ADDRESS_DATA__", coin: .avalanche)!
+        let addressFromString = AnyAddress(string: "X-avax1apmh7wxg3js48fhacfv5y9md9065jxuft30vup", coin: .avalanche)!
 
-        XCTAssertEqual(pubkey.data.hexString, "__EXPECTED_PUBKEY_DATA__")
+        XCTAssertEqual(pubkey.data.hexString, "0x02b33c917f2f6103448d7feb42614037d05928433cb25e78f01a825aa829bb3c27")
         XCTAssertEqual(address.description, addressFromString.description)
     }
 
