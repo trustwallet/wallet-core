1f3d8215333cec17b00ce476efef9bd6145023a7
Evan Richard <evan@avalabs.org>
2021-03-29 20:14:16 -0400

    Repair testAddress.

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/avalanche/TestAvalancheAddress.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/avalanche/TestAvalancheAddress.kt
@@ -21,11 +21,11 @@ class TestAvalancheAddress {
     @Test
     fun testAddress() {
         val key = PrivateKey("0x98cb077f972feb0481f1d894f272c6a1e3c15e272a1658ff716444f465200070".toHexByteArray())
-        val pubkey = key.publicKeyEd25519
+        val pubkey = key.getPublicKeySecp256k1(false)
         val address = AnyAddress(pubkey, CoinType.AVALANCHEXCHAIN)
         val expected = AnyAddress("X-avax1apmh7wxg3js48fhacfv5y9md9065jxuft30vup", CoinType.AVALANCHEXCHAIN)
 
-        assertEquals(pubkey.data().toHex(), "0x57c66cf849b2186cb52398caf8cb9cd390aac3e217e9b66bab9602faff715098")
+        assertEquals(pubkey.data().toHex(), "0x04b33c917f2f6103448d7feb42614037d05928433cb25e78f01a825aa829bb3c27299a9fb02dd081d37876f145b8d3aa47fe9764145a9d90d4f941105d7de85c82")
         assertEquals(address.description(), expected.description())
     }
 }
