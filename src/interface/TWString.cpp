a52f5f92360fbd8237e633818e7c4212c8c76ade
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-04-09 09:10:07 +0200

    Return guaranteed null-terminated string in TWStringUTF8Bytes

--- src/interface/TWString.cpp
+++ src/interface/TWString.cpp
@@ -30,7 +30,7 @@ char TWStringGet(TWString *_Nonnull string, size_t index) {
 
 const char *_Nonnull TWStringUTF8Bytes(TWString *_Nonnull string) {
     auto s = reinterpret_cast<const std::string*>(string);
-    return s->data();
+    return s->c_str();
 }
 
 void TWStringDelete(TWString *_Nonnull string) {
