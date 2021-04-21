952e2584648ac54e6415921f76154fd914aec5c0
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-04-21 14:31:17 +0200

    Extend CB58 tests

--- tests/Avalanche/CB58Tests.cpp
+++ tests/Avalanche/CB58Tests.cpp
@@ -13,7 +13,7 @@
 using namespace TW;
 using namespace TW::Avalanche;
 
-TEST(CB58, decode) {
+TEST(AvalancheCB58, decode) {
     Data decoded = CB58::avalanche.decodeCheck("32UWxgjUJd9s6Kyvxjj1u");
     EXPECT_EQ(data("Hello world"), decoded);
     decoded = CB58::avalanche.decodeCheck("2Eef9JnJ3Lz2c5op9");
@@ -23,9 +23,26 @@ TEST(CB58, decode) {
     decoded = CB58::avalanche.decodeCheck("148vjpuxYXixb8DcbaWyeDE2fEG");
     Data original = parse_hex("00112233445566778899aabbccddeeff");
     EXPECT_EQ(original, decoded);
+    decoded = CB58::avalanche.decodeCheck("   32UWxgjUJd9s6Kyvxjj1u   ");
+    EXPECT_EQ(data("Hello world"), decoded);
 }
 
-TEST(CB58, encode) {
+TEST(AvalancheCB58, decodeNegative) {
+    // invalid char
+    Data decoded = CB58::avalanche.decodeCheck("32U*WxgjUJd9s6Kyvxjj1u");
+    EXPECT_EQ(Data(), decoded);
+    // invalid char, >128
+    decoded = CB58::avalanche.decodeCheck("32UőWxgjUJd9s6Kyvxjj1u");
+    EXPECT_EQ(Data(), decoded);
+    // too short (no checksum)
+    decoded = CB58::avalanche.decodeCheck("5y3");
+    EXPECT_EQ(Data(), decoded);
+    // wrong checksum
+    decoded = CB58::avalanche.decodeCheck("32UWxgjUJd9s6Kyvxjj1v");
+    EXPECT_EQ(Data(), decoded);
+}
+
+TEST(AvalancheCB58, encode) {
     std::string encoded = CB58::avalanche.encodeCheck(data("Hello world"));
     EXPECT_EQ("32UWxgjUJd9s6Kyvxjj1u", encoded);
     encoded = CB58::avalanche.encodeCheck(data("Ava Labs"));
