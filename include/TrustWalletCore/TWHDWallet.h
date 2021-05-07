24900d236f3a2f2a44928df5e14ee812c25603a2
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-05-07 19:14:39 +0800

    fix typo

--- include/TrustWalletCore/TWHDWallet.h
+++ include/TrustWalletCore/TWHDWallet.h
@@ -65,7 +65,7 @@ TWString *_Nonnull TWHDWalletGetAddressForCoin(struct TWHDWallet *_Nonnull walle
 TW_EXPORT_METHOD
 struct TWPrivateKey *_Nonnull TWHDWalletGetKey(struct TWHDWallet *_Nonnull wallet, enum TWCoinType coin, TWString *_Nonnull derivationPath);
 
-/// Shortcut method to generates private key with the specified account/change/address (bip44 standard). Returned object needs to be deleted.
+/// Shortcut method to generate private key with the specified account/change/address (bip44 standard). Returned object needs to be deleted.
 ///
 /// @see https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
 TW_EXPORT_METHOD
