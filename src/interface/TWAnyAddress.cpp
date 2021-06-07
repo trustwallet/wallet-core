cacedd0e57351079184c0824002657fd6c565f14
Harish Sridharan <harish.sridhar@gmail.com>
2021-06-07 18:16:33 +0530

    Added Bluzelle to AnyAddress decoder of cosmos

--- src/interface/TWAnyAddress.cpp
+++ src/interface/TWAnyAddress.cpp
@@ -79,6 +79,7 @@ TWData* _Nonnull TWAnyAddressData(struct TWAnyAddress* _Nonnull address) {
     case TWCoinTypeTerra:
     case TWCoinTypeBandChain:
     case TWCoinTypeTHORChain:
+    case TWCoinTypeBluzelle:
     case TWCoinTypeIoTeX: {
         Cosmos::Address addr;
         if (!Cosmos::Address::decode(string, addr)) {
