ff6351ff79b1e3100d22b0463d51ccb60692b2d2
Harish Sridharan <harish.sridhar@gmail.com>
2021-06-17 14:07:52 +0530

    Fixed android signing test with real world transaction scenario

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/bluzelle/TestBluzelleSigner.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/bluzelle/TestBluzelleSigner.kt
@@ -28,6 +28,8 @@ class TestBluzelleSigner {
 
     @Test
     fun testSigningTransaction() {
+
+        // Submitted Realworld tx for the following test : https://bigdipper.net.bluzelle.com/transactions/B3A7F30539CCDF72D210BC995FAF65B43F9BE04FA9F8AFAE0EC969660744002F
         val key =
             PrivateKey("80e81ea269e66a0a05b11236df7919fb7fbeedba87452d667489d7403a02f005".toHexByteArray())
         val publicKey = key.getPublicKeySecp256k1(true)
@@ -49,20 +51,20 @@ class TestBluzelleSigner {
         }.build()
 
         val feeAmount = Cosmos.Amount.newBuilder().apply {
-            amount = 200
+            amount = 1000
             denom = "ubnt"
         }.build()
 
         val cosmosFee = Cosmos.Fee.newBuilder().apply {
-            gas = 200000
+            gas = 500000
             addAllAmounts(listOf(feeAmount))
         }.build()
 
         val signingInput = Cosmos.SigningInput.newBuilder().apply {
-            accountNumber = 1037
-            chainId = "bluzelle"
+            accountNumber = 590
+            chainId = "net-6"
             memo = ""
-            sequence = 8
+            sequence = 2
             fee = cosmosFee
             privateKey = ByteString.copyFrom(key.data())
             addAllMessages(listOf(message))
@@ -71,7 +73,7 @@ class TestBluzelleSigner {
         val output = AnySigner.sign(signingInput, BLUZELLE, SigningOutput.parser())
         val jsonPayload = output.json
 
-        val expectedJsonPayload = """{"mode":"block","tx":{"fee":{"amount":[{"amount":"200","denom":"ubnt"}],"gas":"200000"},"memo":"","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"1","denom":"ubnt"}],"from_address":"bluzelle1hsk6jryyqjfhp5dhc55tc9jtckygx0epzzw0fm","to_address":"bluzelle1xccvees6ev4wm2r49rc6ptulsdxa8x8jfpmund"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"AlcobsPzfTNVe7uqAAsndErJAjqplnyudaGB0f+R+p3F"},"signature":"0StN9LHr4fIbEzCzyx5bzWJmfgiUqkwoavYGNUDm2shXKmAFogVtOviC4zMNNHKBtFTnHQ07DO1UjqEEWS/2BA=="}]}}"""
+        val expectedJsonPayload = """{"mode":"block","tx":{"fee":{"amount":[{"amount":"1000","denom":"ubnt"}],"gas":"500000"},"memo":"","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"1","denom":"ubnt"}],"from_address":"bluzelle1hsk6jryyqjfhp5dhc55tc9jtckygx0epzzw0fm","to_address":"bluzelle1xccvees6ev4wm2r49rc6ptulsdxa8x8jfpmund"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"AlcobsPzfTNVe7uqAAsndErJAjqplnyudaGB0f+R+p3F"},"signature":"5e3e13x1F+Y4+cPNvE1jQ/Mrz0J2RQCY69re3g4xuTY3Gw7MNGQ+8E34d9DgvcNLPM05nehdOv/0SvekY/ihIQ=="}]}}"""
         assertEquals(expectedJsonPayload, jsonPayload)
 
     }
@@ -79,20 +81,22 @@ class TestBluzelleSigner {
     @Test
     fun testSigningJSON() {
 
+        // Submitted realworld tx for the following test : https://bigdipper.net.bluzelle.com/transactions/DEF394BE0DD1075CDC8B8618A7858AAA4A03A43D04476381224E316E06FD3A5B
         val myPrivateKey = "80e81ea269e66a0a05b11236df7919fb7fbeedba87452d667489d7403a02f005"
         val myAddress = "bluzelle1hsk6jryyqjfhp5dhc55tc9jtckygx0epzzw0fm"
 
         val toAddress = "bluzelle1xccvees6ev4wm2r49rc6ptulsdxa8x8jfpmund"
         val json = """
         {
-            "accountNumber": "8733",
-            "chainId": "bluzelle",
+            "accountNumber": "590",
+            "chainId": "net-6",
+            "sequence": "3",
             "fee": {
                 "amounts": [{
                     "denom": "ubnt",
-                    "amount": "5000"
+                    "amount": "1000"
                 }],
-                "gas": "200000"
+                "gas": "500000"
             },
             "memo": "Testing",
             "messages": [{
@@ -101,7 +105,7 @@ class TestBluzelleSigner {
                     "toAddress": "$toAddress",
                     "amounts": [{
                         "denom": "ubnt",
-                        "amount": "995000"
+                        "amount": "2"
                     }]
                 }
             }]
@@ -110,7 +114,7 @@ class TestBluzelleSigner {
         val key = myPrivateKey.toHexByteArray()
         val actualResult  = AnySigner.signJSON(json, key, BLUZELLE.value())
 
-        val expectedResult = """{"mode":"block","tx":{"fee":{"amount":[{"amount":"5000","denom":"ubnt"}],"gas":"200000"},"memo":"Testing","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"995000","denom":"ubnt"}],"from_address":"$myAddress","to_address":"$toAddress"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"AlcobsPzfTNVe7uqAAsndErJAjqplnyudaGB0f+R+p3F"},"signature":"gKWKk5lku9H/wosvnsD6auWUt7AGNjLEudWS+Fdv4TQr16HUBVpkq9A6gZYqZIawFukq2I/qrtyw9MKR4pVkhg=="}]}}"""
+        val expectedResult = """{"mode":"block","tx":{"fee":{"amount":[{"amount":"1000","denom":"ubnt"}],"gas":"500000"},"memo":"Testing","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"2","denom":"ubnt"}],"from_address":"bluzelle1hsk6jryyqjfhp5dhc55tc9jtckygx0epzzw0fm","to_address":"bluzelle1xccvees6ev4wm2r49rc6ptulsdxa8x8jfpmund"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"AlcobsPzfTNVe7uqAAsndErJAjqplnyudaGB0f+R+p3F"},"signature":"2JcfVwq9N3UAk5Lfki7+CngqcefgjfH1q/8chtJMJvEHRe6PvuYKMv5pjeN0Z5Vk2BArJT3V3EHxbpbiY2eLWw=="}]}}"""
         assertEquals(expectedResult, actualResult)
     }
 }
