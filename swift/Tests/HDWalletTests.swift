be91973ff931703d3fc0a1a97ab0494991f16330
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-07-09 11:49:46 +0200

    Extend iOS tests

--- swift/Tests/HDWalletTests.swift
+++ swift/Tests/HDWalletTests.swift
@@ -12,16 +12,32 @@ extension HDWallet {
 }
 
 class HDWalletTests: XCTestCase {
-    func testSeed() {
+    func testCreateFromMnemonic() {
         let wallet = HDWallet.test
 
         XCTAssertEqual(wallet.seed.hexString, "7ae6f661157bda6492f6162701e570097fc726b6235011ea5ad09bf04986731ed4d92bc43cbdee047b60ea0dd1b1fa4274377c9bf5bd14ab1982c272d8076f29")
+        XCTAssertEqual(wallet.mnemonic, "ripple scissors kick mammal hire column oak again sun offer wealth tomorrow wagon turn fatal")
     }
 
-    func testSeedNoPassword() {
+    func testCreateFromMnemonicNoPassword() {
         let wallet = HDWallet(mnemonic: HDWallet.test.mnemonic, passphrase: "")!
 
         XCTAssertEqual(wallet.seed.hexString, "354c22aedb9a37407adc61f657a6f00d10ed125efa360215f36c6919abd94d6dbc193a5f9c495e21ee74118661e327e84a5f5f11fa373ec33b80897d4697557d")
+    }
+
+    func testCreateFromMnemonicInvalid() {
+        let wallet = HDWallet(mnemonic: "THIS IS AN INVALID MNEMONIC", passphrase: "")
+        XCTAssertNil(wallet)
+    }
+
+    func testGenerate() {
+        let wallet = HDWallet(strength: 128, passphrase: "")!
+        XCTAssertTrue(Mnemonic.isValid(mnemonic: wallet.mnemonic))
+    }
+
+    func testCreateFromEntropy() {
+        let wallet = HDWallet(data: Data(hexString: "ba5821e8c356c05ba5f025d9532fe0f21f65d594")!, passphrase: "TREZOR")!
+        XCTAssertEqual(wallet.mnemonic, "ripple scissors kick mammal hire column oak again sun offer wealth tomorrow wagon turn fatal")
     }
 
     func testMasterKey() {
