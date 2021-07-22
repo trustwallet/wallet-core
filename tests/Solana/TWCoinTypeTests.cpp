d0ce32a3bd75fe2d289f00d876594fc1e7c11653
Adam R <13562139+catenocrypt@users.noreply.github.com>
2021-07-22 23:45:14 +0200

    Solana: Change explorer to solscan.io (#1543)
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>

--- tests/Solana/TWCoinTypeTests.cpp
+++ tests/Solana/TWCoinTypeTests.cpp
@@ -14,9 +14,9 @@
 
 TEST(TWSolanaCoinType, TWCoinType) {
     auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeSolana));
-    auto txId = WRAPS(TWStringCreateWithUTF8Bytes("t123"));
+    auto txId = WRAPS(TWStringCreateWithUTF8Bytes("5LmxrEKGchhMuYfw6Qut6CbsvE9pVfb8YvwZKvWssSesDVjHioBCmWKSJQh1WhvcM6CpemhpHNmEMA2a36rzwTa8"));
     auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeSolana, txId.get()));
-    auto accId = WRAPS(TWStringCreateWithUTF8Bytes("a12"));
+    auto accId = WRAPS(TWStringCreateWithUTF8Bytes("Bxp8yhH9zNwxyE4UqxP7a7hgJ5xTZfxNNft7YJJ2VRjT"));
     auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeSolana, accId.get()));
     auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeSolana));
     auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeSolana));
@@ -26,8 +26,8 @@ TEST(TWSolanaCoinType, TWCoinType) {
     ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeSolana));
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeSolana));
     assertStringsEqual(symbol, "SOL");
-    assertStringsEqual(txUrl, "https://explorer.solana.com/transactions/t123");
-    assertStringsEqual(accUrl, "https://explorer.solana.com/accounts/a12");
+    assertStringsEqual(txUrl, "https://solscan.io/tx/5LmxrEKGchhMuYfw6Qut6CbsvE9pVfb8YvwZKvWssSesDVjHioBCmWKSJQh1WhvcM6CpemhpHNmEMA2a36rzwTa8");
+    assertStringsEqual(accUrl, "https://solscan.io/account/Bxp8yhH9zNwxyE4UqxP7a7hgJ5xTZfxNNft7YJJ2VRjT");
     assertStringsEqual(id, "solana");
     assertStringsEqual(name, "Solana");
 }
