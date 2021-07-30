e0515e9a0f26d82f1455c5306ba15e6042924c8f
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-07-30 10:38:22 +0200

    Add comments to pubkey types

--- include/TrustWalletCore/TWPublicKeyType.h
+++ include/TrustWalletCore/TWPublicKeyType.h
@@ -13,15 +13,15 @@ TW_EXTERN_C_BEGIN
 /// Public key types
 TW_EXPORT_ENUM(uint32_t)
 enum TWPublicKeyType {
-    TWPublicKeyTypeSECP256k1 = 0,
-    TWPublicKeyTypeSECP256k1Extended = 1,
+    TWPublicKeyTypeSECP256k1 = 0, // 'Compressed' Secp256k1 key, with 1-byte Y coordinate sign (33 bytes)
+    TWPublicKeyTypeSECP256k1Extended = 1, // Extended Secp256k1 key (65 bytes)
     TWPublicKeyTypeNIST256p1 = 2,
     TWPublicKeyTypeNIST256p1Extended = 3,
     TWPublicKeyTypeED25519 = 4,
     TWPublicKeyTypeED25519Blake2b = 5,
     TWPublicKeyTypeCURVE25519 = 6,
     TWPublicKeyTypeED25519Extended = 7,
-    TWPublicKeyTypeSECP256k1Compact = 8,
+    TWPublicKeyTypeSECP256k1Compact = 8, // Secp256pk1 with implicit Y coordinate, for Taproot (32 bytes)
 };
 
 TW_EXTERN_C_END
