73492720045cb24a83f72170d64195958b729636
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-08-11 11:22:22 +0200

    HDWallet creation thread-safe

--- src/Mnemonic.h
+++ src/Mnemonic.h
@@ -16,6 +16,7 @@ public:
     static constexpr size_t MaxWords = 24;
     static constexpr size_t MinWords = 12;
     static constexpr size_t BitsPerWord = 11; // each word encodes this many bits (there are 2^11=2048 different words)
+    static constexpr size_t MaxWordLength = 9; // Length of longest mnemonic word
 
 public:
     /// Determines whether a mnemonic phrase is valid.
