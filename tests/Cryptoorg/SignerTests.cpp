9162a8c122dddb92a9cd7995bd5d03a11bf89190
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2021-09-22 01:20:17 +0200

    Create real-world tx

--- tests/Cryptoorg/SignerTests.cpp
+++ tests/Cryptoorg/SignerTests.cpp
@@ -19,6 +19,9 @@ using namespace TW::Cosmos;
 
 TEST(CryptoorgSigner, SignTx) {
     auto input = Cosmos::Proto::SigningInput();
+    input.set_account_number(125798);
+    input.set_sequence(0);
+    input.set_chain_id("crypto-org-chain-mainnet-1");
 
     auto msg = input.add_messages();
     auto& message = *msg->mutable_send_coins_message();
@@ -32,12 +35,12 @@ TEST(CryptoorgSigner, SignTx) {
     fee.set_gas(200000);
     auto amountOfFee = fee.add_amounts();
     amountOfFee->set_denom("basecro");
-    amountOfFee->set_amount(2000);
+    amountOfFee->set_amount(5000);
 
     std::string json;
     google::protobuf::util::MessageToJsonString(input, &json);
 
-    EXPECT_EQ(R"({"fee":{"amounts":[{"denom":"basecro","amount":"2000"}],"gas":"200000"},"messages":[{"sendCoinsMessage":{"fromAddress":"cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0","toAddress":"cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus","amounts":[{"denom":"basecro","amount":"100000000"}]}}]})", json);
+    EXPECT_EQ(R"({"accountNumber":"125798","chainId":"crypto-org-chain-mainnet-1","fee":{"amounts":[{"denom":"basecro","amount":"5000"}],"gas":"200000"},"messages":[{"sendCoinsMessage":{"fromAddress":"cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0","toAddress":"cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus","amounts":[{"denom":"basecro","amount":"100000000"}]}}]})", json);
 
     auto privateKey = parse_hex("200e439e39cf1aad465ee3de6166247f914cbc0f823fc2dd48bf16dcd556f39d");
     {
@@ -50,6 +53,9 @@ TEST(CryptoorgSigner, SignTx) {
 
     auto output = Cosmos::Signer::sign(input);
 
-    EXPECT_EQ(R"({"mode":"block","tx":{"fee":{"amount":[{"amount":"2000","denom":"basecro"}],"gas":"200000"},"memo":"","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"100000000","denom":"basecro"}],"from_address":"cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0","to_address":"cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"A4gxsGFiPn6L5Z2IjHEISkXI0IkwfL9exV3GLB171Wvj"},"signature":"l1IbecABw0nyWqAS52/BGCqB84sPmMQow5JKGAAdMG0BNBDsGzgFyOWbeQLhi6vw1O8Q3E1oZHTGjBTobn+qKw=="}]}})", output.json());
-    EXPECT_EQ(hex(output.signature()), "97521b79c001c349f25aa012e76fc1182a81f38b0f98c428c3924a18001d306d013410ec1b3805c8e59b7902e18babf0d4ef10dc4d686474c68c14e86e7faa2b");
+    EXPECT_EQ(R"({"mode":"block","tx":{"fee":{"amount":[{"amount":"5000","denom":"basecro"}],"gas":"200000"},"memo":"","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"100000000","denom":"basecro"}],"from_address":"cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0","to_address":"cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"A4gxsGFiPn6L5Z2IjHEISkXI0IkwfL9exV3GLB171Wvj"},"signature":"5+5rSFFg0FE9cTklQWQHNktBDJsz7UCnMSgF0t0+gYcrIhEWUyTtibXaHZQbKAAaciJ1BkHXYREjU55VswByVg=="}]}})", output.json());
+    EXPECT_EQ(hex(output.signature()), "e7ee6b485160d0513d713925416407364b410c9b33ed40a7312805d2dd3e81872b2211165324ed89b5da1d941b28001a7222750641d7611123539e55b3007256");
+
+    /// https://crypto.org/explorer/tx/DDCCE4052040B05914CADEFE78C0A75BE363AE39504E7EF6B2EDB8A9072AD44B
+    /// curl -H 'Content-Type: application/json' -D '{"mode":"block","tx":{"fee": ... }}' https://mainnet.crypto.org:1317/txs
 }
