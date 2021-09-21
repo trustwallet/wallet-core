47eed8fae7feba8d2b888b697c6b1064e9a5f321
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2021-09-21 23:28:48 +0200

    Add signer tests

--- tests/Cryptoorg/SignerTests.cpp
+++ tests/Cryptoorg/SignerTests.cpp
@@ -4,34 +4,52 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
-/*
-#include "Cryptoorg/Signer.h"
-#include "Cryptoorg/Address.h"
+#include "proto/Cosmos.pb.h"
+#include "Cosmos/Signer.h"
+#include "Cosmos/Address.h"
 #include "HexCoding.h"
 #include "PrivateKey.h"
 #include "PublicKey.h"
 
 #include <gtest/gtest.h>
+#include <google/protobuf/util/json_util.h>
 
 using namespace TW;
-using namespace TW::Cryptoorg;
+using namespace TW::Cosmos;
 
-// TODO: Add tests
+TEST(CryptoorgSigner, SignTx) {
+    auto input = Cosmos::Proto::SigningInput();
 
-TEST(CryptoorgSigner, Sign) {
-    // TODO: Finalize test implementation
+    auto msg = input.add_messages();
+    auto& message = *msg->mutable_send_coins_message();
+    message.set_from_address("cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0");
+    message.set_to_address("cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus");
+    auto amountOfTx = message.add_amounts();
+    amountOfTx->set_denom("basecro");
+    amountOfTx->set_amount(100000000);
 
-    //auto key = PrivateKey(parse_hex("__PRIVKEY_DATA__"));
-    //auto publicKey = key.getPublicKey(TWPublicKeyTypeED25519);
-    //auto from = Address(publicKey);
-    //auto to = Address("__TO_ADDRESS__");
-    //...
-    //auto transaction = Transaction(...)
-    //auto signature = Signer::sign(key, transaction);
-    //auto result = transaction.serialize(signature);
+    auto& fee = *input.mutable_fee();
+    fee.set_gas(200000);
+    auto amountOfFee = fee.add_amounts();
+    amountOfFee->set_denom("basecro");
+    amountOfFee->set_amount(2000);
 
-    //ASSERT_EQ(hex(serialized), "__RESULT__");
-    //ASSERT_EQ(...)
+    std::string json;
+    google::protobuf::util::MessageToJsonString(input, &json);
+
+    EXPECT_EQ(R"({"fee":{"amounts":[{"denom":"basecro","amount":"2000"}],"gas":"200000"},"messages":[{"sendCoinsMessage":{"fromAddress":"cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0","toAddress":"cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus","amounts":[{"denom":"basecro","amount":"100000000"}]}}]})", json);
+
+    auto privateKey = parse_hex("200e439e39cf1aad465ee3de6166247f914cbc0f823fc2dd48bf16dcd556f39d");
+    {
+        auto privateKeyKey = PrivateKey(privateKey);
+        auto publicKey = privateKeyKey.getPublicKey(TWPublicKeyTypeSECP256k1);
+        auto address = Address("cro", publicKey);
+        EXPECT_EQ("cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0", address.string());
+    }
+    input.set_private_key(privateKey.data(), privateKey.size());
+
+    auto output = Cosmos::Signer::sign(input);
+
+    EXPECT_EQ(R"({"mode":"block","tx":{"fee":{"amount":[{"amount":"2000","denom":"basecro"}],"gas":"200000"},"memo":"","msg":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"100000000","denom":"basecro"}],"from_address":"cro1ctwtcwpgksky988dhth6jslxveumgu0d45zgf0","to_address":"cro1xpahy6c7wldxacv6ld99h435mhvfnsup24vcus"}}],"signatures":[{"pub_key":{"type":"tendermint/PubKeySecp256k1","value":"A4gxsGFiPn6L5Z2IjHEISkXI0IkwfL9exV3GLB171Wvj"},"signature":"l1IbecABw0nyWqAS52/BGCqB84sPmMQow5JKGAAdMG0BNBDsGzgFyOWbeQLhi6vw1O8Q3E1oZHTGjBTobn+qKw=="}]}})", output.json());
+    EXPECT_EQ(hex(output.signature()), "97521b79c001c349f25aa012e76fc1182a81f38b0f98c428c3924a18001d306d013410ec1b3805c8e59b7902e18babf0d4ef10dc4d686474c68c14e86e7faa2b");
 }
-
-*/
