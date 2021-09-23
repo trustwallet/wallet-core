9355f11c27b0d141603fd3c108a57c5e812d429b
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2021-09-23 17:09:23 +0200

    Review comment, minor

--- src/interface/TWAnyAddress.cpp
+++ src/interface/TWAnyAddress.cpp
@@ -81,15 +81,7 @@ TWData* _Nonnull TWAnyAddressData(struct TWAnyAddress* _Nonnull address) {
     case TWCoinTypeBandChain:
     case TWCoinTypeTHORChain:
     case TWCoinTypeBluzelle:
-    case TWCoinTypeIoTeX: {
-        Cosmos::Address addr;
-        if (!Cosmos::Address::decode(string, addr)) {
-            break;
-        }
-        data = addr.getKeyHash();
-        break;
-    }
-
+    case TWCoinTypeIoTeX:
     case TWCoinTypeCryptoorg:
     {
         Cosmos::Address addr;
