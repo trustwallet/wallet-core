68dec3827fdc42bca6423dc61bde3a571301fee2
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-10-02 16:28:09 +0800

    [Terra] Update terra block explorer (#1661)
    * update terra block explorer
    * remove explicit network

--- tests/Terra/TWCoinTypeTests.cpp
+++ tests/Terra/TWCoinTypeTests.cpp
@@ -15,9 +15,9 @@
 
 TEST(TWTerraCoinType, TWCoinType) {
     auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeTerra));
-    auto txId = WRAPS(TWStringCreateWithUTF8Bytes("t123"));
+    auto txId = WRAPS(TWStringCreateWithUTF8Bytes("D28D8AFC7CE89F2A22FA2DBF78D2C0A36E549BB830C4D9FA7459E3F723CA7182"));
     auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeTerra, txId.get()));
-    auto accId = WRAPS(TWStringCreateWithUTF8Bytes("a12"));
+    auto accId = WRAPS(TWStringCreateWithUTF8Bytes("terra16t3gx5rqvz6ru37yzn3shuu20erv4ngmfr59zf"));
     auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeTerra, accId.get()));
     auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeTerra));
     auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeTerra));
@@ -27,8 +27,8 @@ TEST(TWTerraCoinType, TWCoinType) {
     ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeTerra));
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeTerra));
     assertStringsEqual(symbol, "LUNA");
-    assertStringsEqual(txUrl, "https://terra.stake.id/#/tx/t123");
-    assertStringsEqual(accUrl, "https://terra.stake.id/#/address/a12");
+    assertStringsEqual(txUrl, "https://finder.terra.money/tx/tx/D28D8AFC7CE89F2A22FA2DBF78D2C0A36E549BB830C4D9FA7459E3F723CA7182");
+    assertStringsEqual(accUrl, "https://finder.terra.money/tx/address/terra16t3gx5rqvz6ru37yzn3shuu20erv4ngmfr59zf");
     assertStringsEqual(id, "terra");
     assertStringsEqual(name, "Terra");
 }
