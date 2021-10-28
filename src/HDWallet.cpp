5de4ec653c3f019037e6235ed2d79401a4092931
defacedbeef <0xdefacedbeef@protonmail.com>
2021-10-28 07:42:15 +0200

    Clearing unused sensitive data from memory (#1731)
    * Clearing unused sensitive data from memory
    
    A mnemonic generated in two HDWallet c-tors is not cleared properly.
    Proposed change clears out the sensitive data from memory right after std::string copy-ctor.
    
    * Change memset to memzero
    
    Co-authored-by: catenocrypt <13562139+catenocrypt@users.noreply.github.com>

--- src/HDWallet.cpp
+++ src/HDWallet.cpp
@@ -19,6 +19,7 @@
 #include <TrezorCrypto/bip32.h>
 #include <TrezorCrypto/bip39.h>
 #include <TrezorCrypto/curves.h>
+#include <TrezorCrypto/memzero.h>
 
 #include <array>
 #include <cstring>
@@ -46,6 +47,7 @@ HDWallet::HDWallet(int strength, const std::string& passphrase)
         throw std::invalid_argument("Invalid strength");
     }
     mnemonic = mnemonic_chars;
+    memzero(buf, MnemonicBufLength);
     updateSeedAndEntropy();
 }
 
@@ -66,6 +68,7 @@ HDWallet::HDWallet(const Data& entropy, const std::string& passphrase)
         throw std::invalid_argument("Invalid mnemonic data");
     }
     mnemonic = mnemonic_chars;
+    memzero(buf, MnemonicBufLength);
     updateSeedAndEntropy();
 }
 
