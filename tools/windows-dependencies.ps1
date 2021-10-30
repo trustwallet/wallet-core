e7b4e3400bbe72236e66c7edf81fb847b1976a60
Jan Boon <jan.boon@kaetemi.be>
2021-10-30 19:59:44 +0800

    Build script for protobuf plugins. Patch bug in protobuf cmake

--- tools/windows-dependencies.ps1
+++ tools/windows-dependencies.ps1
@@ -5,9 +5,11 @@
 
 # Ported from the `install-dependencies` bash script
 
-# Downloads the single-header JSON library
+# TODO: GoogleTest
+# TODO: Check
+# Downloads the single-header Nlohmann JSON library
 # Downloads and builds Protobuf in static debug and release mode, with dynamic C runtime
-# Builds the Wallet Core protobuf plugins
+# Builds the Wallet Core protobuf plugins in release mode
 # (next script) Builds Wallet Core in static release mode, to build the console wallet
 # (next script) Builds Wallet Core in dynamic release and debug mode, to build a DLL for applications
 
@@ -18,14 +20,29 @@ $cmakeGenerator = "Visual Studio 17 2022"
 $cmakePlatform = "x64"
 $cmakeToolset = "v143"
 
+# Nlohmann JSON
+$jsonVersion = "3.10.4"
+$jsonDir = Join-Path $prefix "include\nlohmann"
+$jsonUrl = "https://github.com/nlohmann/json/releases/download/v$jsonVersion/json.hpp"
+$jsonFile = Join-Path $jsonDir "json.hpp"
+if (Test-Path -Path $jsonFile -PathType Leaf) {
+    Remove-Item –Path $jsonFile
+}
+if (-not(Test-Path -Path $jsonDir -PathType Container)) {
+    mkdir -p $jsonDir
+}
+Invoke-WebRequest -Uri $jsonUrl -OutFile $jsonFile
+
 # Protobuf
 $protobufVersion = "3.19.1"
 $protobufDir = Join-Path $prefix "src\protobuf"
-
 $protobufZip = "protobuf-cpp-$protobufVersion.zip"
 $protobufUrl = "https://github.com/protocolbuffers/protobuf/releases/download/v$protobufVersion/$protobufZip"
 
 # Download and extract
+if (-not(Test-Path -Path $protobufDir -PathType Container)) {
+    mkdir -p $protobufDir
+}
 cd $protobufDir
 if (-not(Test-Path -Path $protobufZip -PathType Leaf)) {
     Invoke-WebRequest -Uri $protobufUrl -OutFile $protobufZip
@@ -37,13 +54,25 @@ Expand-Archive -LiteralPath $protobufZip -DestinationPath $protobufDir
 
 # Build debug and release libraries
 cd protobuf-$protobufVersion
-if (Test-Path -Path build_temp -PathType Container) {
-     Remove-Item –Path build_temp -Recurse
-}
-mkdir build_temp
-cd build_temp
+mkdir build_msvc
+cd build_msvc
+$protobufCMake = Get-Content ..\cmake\CMakeLists.txt # Bugfix
+$protobufCMake = $protobufCMake.Replace("set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)",
+    "if (MSVC AND protobuf_MSVC_STATIC_RUNTIME)`r`nset(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)`r`nendif()") # Bugfix
+$protobufCMake | Out-File -encoding UTF8 ..\cmake\CMakeLists.txt # Bugfix
 cmake -G $cmakeGenerator -A $cmakePlatform -T $cmakeToolset "-DCMAKE_INSTALL_PREFIX=$prefix" "-DCMAKE_BUILD_TYPE=Release" "-Dprotobuf_WITH_ZLIB=OFF" "-Dprotobuf_MSVC_STATIC_RUNTIME=OFF" "-Dprotobuf_BUILD_TESTS=OFF" "-Dprotobuf_BUILD_SHARED_LIBS=OFF" ../cmake
 cmake --build . --target INSTALL --config Debug
+cmake --build . --target INSTALL --config Release
+
+# Protobuf plugins
+$pluginSrc = Join-Path $root "protobuf-plugin"
+cd $pluginSrc
+if (Test-Path -Path build -PathType Container) {
+     Remove-Item –Path build -Recurse
+}
+mkdir build
+cd build
+cmake -G $cmakeGenerator -A $cmakePlatform -T $cmakeToolset "-DCMAKE_INSTALL_PREFIX=$prefix" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --target INSTALL --config Release
 
 cd $root
