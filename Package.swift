7eff6b4019a22902da7cf24899a933992e8d2487
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-11-07 07:02:41 +0900

    point master to latest release

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.31/WalletCore.xcframework.zip",
-            checksum: "29d88807485f88992e00a5b7ed1ddf53ca57dacb89ba5e5368dc931102600879"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.32/WalletCore.xcframework.zip",
+            checksum: "eaa96d79dbe156f5a0fa2ead3c832096e3ada5d00e4ddd8da8ed45ed06c077c7"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.31/SwiftProtobuf.xcframework.zip",
-            checksum: "d1035aa8a32f2483305bbe9cd3d5774bf6c1a65ce5a37213c9ee651c78873a55"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.32/SwiftProtobuf.xcframework.zip",
+            checksum: "31c47b25f0accc669299b6fb60b63b87a146fdb15fe3149e996c1e28407d844e"
         )
     ]
 )
