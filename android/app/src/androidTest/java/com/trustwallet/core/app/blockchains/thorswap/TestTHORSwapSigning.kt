062a147b7aad2be1570be4600bda240ffe1574db
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2021-11-24 11:56:03 +0100

    test2

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/thorswap/TestTHORSwapSigning.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/thorswap/TestTHORSwapSigning.kt
@@ -31,8 +31,8 @@ class TestTHORSwap {
             toAsset = THORSwap.Asset.newBuilder().apply {
                 chain = THORSwap.Chain.BNB
                 symbol = "BNB"
-                tokenID = ""
-            }
+                tokenId = ""
+            }.build()
             toAddress = "bnb1us47wdhfx08ch97zdueh3x3u5murfrx30jecrx"
             vaultAddress = "0x1091c4De6a3cF09CdA00AbDAeD42c7c3B69C83EC"
             routerAddress = "0x42A5Ed456650a09Dc10EBc6361A7480fDd61f27B"
@@ -40,26 +40,31 @@ class TestTHORSwap {
             toAmountLimit = "600003"
         }
 
-        /* TODO remove
-        val signingInput = Ethereum.SigningInput.newBuilder()
-        signingInput.apply {
-            privateKey = ByteString.copyFrom(PrivateKey("0x4646464646464646464646464646464646464646464646464646464646464646".toHexByteArray()).data())
-            toAddress = "0x3535353535353535353535353535353535353535"
-            chainId = ByteString.copyFrom("0x1".toHexByteArray())
-            nonce = ByteString.copyFrom("0x9".toHexByteArray())
-            // txMode not set, Legacy is the default
-            gasPrice = ByteString.copyFrom("0x04a817c800".toHexByteArray())
-            gasLimit = ByteString.copyFrom("0x5208".toHexByteArray())
-            transaction = Ethereum.Transaction.newBuilder().apply {
-                transfer = Ethereum.Transaction.Transfer.newBuilder().apply {
-                    amount = ByteString.copyFrom("0x0de0b6b3a7640000".toHexByteArray())
-                }.build()
-            }.build()
-        }
+        // serialize input
+        val inputSerialized = ProtoBuf.encodeToByteArray(input)
+        assertEquals(Numeric.toHexString(inputSerialized.toByteArray()), "0x0802122a3078623966353737316332373636346266323238326439386530396437663530636563376362303161371a0708031203424e42222a626e62317573343777646866783038636839377a6475656833783375356d757266727833306a656372782a2a307831303931633444653661336346303943644130304162444165443432633763334236394338334543322a3078343241354564343536363530613039446331304542633633363141373438306644643631663237423a1135303030303030303030303030303030304206363030303033")
 
-        val output = AnySigner.sign(signingInput.build(), ETHEREUM, SigningOutput.parser())
+        // invoke swap
+        val outputData = THORSwap.buildSwap(inputSerialized)
+        assertEquals(outputData.count, 311)
 
-        assertEquals(Numeric.toHexString(output.encoded.toByteArray()), "0xf86c098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a76400008025a028ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276a067cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83")
-        */
+        // parse result in proto
+        val outputProto = ProtoBuf.decodeFromByteArray<THORSwap.SwapOutput>(outputData)
+        assertEquals(outputProto.fromChain, THORSwap.Chain.ETH)
+        assertEquals(outputProto.toChain, THORSwap.Chain.BNB)
+        assertEquals(outputProto.error.code, THORSwap.ErrorCode.OK)
+        val txInput = outputProto.ethereum
+
+        // set few fields before signing
+        txInput.chainID = ByteString.copyFrom("0x01".toHexByteArray())
+        txInput.nonce = ByteString.copyFrom("0x03".toHexByteArray())
+        txInput.gasPrice = ByteString.copyFrom("0x06FC23AC00".toHexByteArray())
+        txInput.gasLimit = ByteString.copyFrom("0x013880".toHexByteArray())
+        txInput.privateKey = ByteString.copyFrom(PrivateKey("0x4f96ed80e9a7555a6f74b3d658afdd9c756b0a40d4ca30c42c2039eb449bb904".toHexByteArray()).data())
+
+        // sign and encode resulting input
+        val output = AnySigner.sign(txInput, ETHEREUM, SigningOutput.parser())
+
+        assertEquals(Numeric.toHexString(output.encoded.toByteArray()), "0xf90151038506fc23ac00830138809442a5ed456650a09dc10ebc6361a7480fdd61f27b87b1a2bc2ec50000b8e41fece7b40000000000000000000000001091c4de6a3cf09cda00abdaed42c7c3b69c83ec000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b1a2bc2ec500000000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000003e535741503a424e422e424e423a626e62317573343777646866783038636839377a6475656833783375356d757266727833306a656372783a363030303033000025a06ae104be3201baca38315352f81fac70ca4dd47339981914e64e91149813e780a066a3f0b2c44ddf5a96a38481274f623f552a593d723237d6742185f4885c0064")
     }
 }
