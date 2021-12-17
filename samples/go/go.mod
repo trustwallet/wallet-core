656db4603cf6cbdbc2bb2bcc13561c9741fdcbb4
Amir Parsa <apm@Amirs-MacBook-Pro.local>
2021-12-17 12:23:22 +0330

    Go Sample: refactor structure, add ethereum and tron wallet, delete old transaction sample to rewrite in new style

--- samples/go/go.mod
+++ samples/go/go.mod
@@ -1,5 +1,3 @@
 module tw
 
 go 1.14
-
-require github.com/golang/protobuf v1.4.2
--- samples/go/main.go
+++ samples/go/main.go
@@ -1,87 +1,36 @@
 package main
 
-// #cgo CFLAGS: -I../../include
-// #cgo LDFLAGS: -L../../build -L../../build/trezor-crypto -lTrustWalletCore -lprotobuf -lTrezorCrypto -lc++ -lm
-// #include <TrustWalletCore/TWHDWallet.h>
-// #include <TrustWalletCore/TWPrivateKey.h>
-// #include <TrustWalletCore/TWPublicKey.h>
-// #include <TrustWalletCore/TWBitcoinScript.h>
-// #include <TrustWalletCore/TWAnySigner.h>
-import "C"
-
 import (
-	"encoding/hex"
 	"fmt"
-	"tw/protos/bitcoin"
-	"tw/types"
-
-	"github.com/golang/protobuf/proto"
+	"tw/core"
 )
 
 func main() {
-	fmt.Println("==> calling wallet core from go")
-	str := types.TWStringCreateWithGoString("confirm bleak useless tail chalk destroy horn step bulb genuine attract split")
-	emtpy := types.TWStringCreateWithGoString("")
-	defer C.TWStringDelete(str)
-	defer C.TWStringDelete(emtpy)
+	mn := "confirm bleak useless tail chalk destroy horn step bulb genuine attract split"
 
-	fmt.Println("==> mnemonic is valid: ", C.TWMnemonicIsValid(str))
-
-	wallet := C.TWHDWalletCreateWithMnemonic(str, emtpy)
-	defer C.TWHDWalletDelete(wallet)
-
-	key := C.TWHDWalletGetKeyForCoin(wallet, C.TWCoinTypeBitcoin)
-	keyData := C.TWPrivateKeyData(key)
-	defer C.TWDataDelete(keyData)
-
-	fmt.Println("<== bitcoin private key: ", types.TWDataHexString(keyData))
-
-	pubKey, _ := hex.DecodeString("0288be7586c41a0498c1f931a0aaf08c15811ee2651a5fe0fa213167dcaba59ae8")
-	pubKeyData := types.TWDataCreateWithGoBytes(pubKey)
-	defer C.TWDataDelete(pubKeyData)
-	fmt.Println("==> bitcoin public key is valid: ", C.TWPublicKeyIsValid(pubKeyData, C.TWPublicKeyTypeSECP256k1))
-
-	address := C.TWHDWalletGetAddressForCoin(wallet, C.TWCoinTypeBitcoin)
-	defer C.TWStringDelete(address)
-	fmt.Println("<== bitcoin address: ", types.TWStringGoString(address))
-
-	script := C.TWBitcoinScriptLockScriptForAddress(address, C.TWCoinTypeBitcoin)
-	scriptData := C.TWBitcoinScriptData(script)
-	defer C.TWBitcoinScriptDelete(script)
-	defer C.TWDataDelete(scriptData)
-	fmt.Println("<== bitcoin address lock script: ", types.TWDataHexString(scriptData))
-
-	utxoHash, _ := hex.DecodeString("fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f")
-
-	utxo := bitcoin.UnspentTransaction{
-		OutPoint: &bitcoin.OutPoint{
-			Hash:     utxoHash,
-			Index:    0,
-			Sequence: 4294967295,
-		},
-		Amount: 625000000,
-		Script: types.TWDataGoBytes(scriptData),
+	bw, err := core.CreateWalletWithMnemonic(mn, core.CoinTypeBitcoin)
+	if err != nil {
+		panic(err)
 	}
+	printWallet(bw)
 
-	input := bitcoin.SigningInput{
-		HashType:      1, // TWBitcoinSigHashTypeAll
-		Amount:        1000000,
-		ByteFee:       1,
-		ToAddress:     "1Bp9U1ogV3A14FMvKbRJms7ctyso4Z4Tcx",
-		ChangeAddress: "1FQc5LdgGHMHEN9nwkjmz6tWkxhPpxBvBU",
-		PrivateKey:    [][]byte{types.TWDataGoBytes(keyData)},
-		Utxo:          []*bitcoin.UnspentTransaction{&utxo},
-		CoinType:      0, // TWCoinTypeBitcoin
+	ew, err := core.CreateWalletWithMnemonic(mn, core.CoinTypeEthereum)
+	if err != nil {
+		panic(err)
 	}
+	printWallet(ew)
 
-	inputBytes, _ := proto.Marshal(&input)
-	inputData := types.TWDataCreateWithGoBytes(inputBytes)
-	defer C.TWDataDelete(inputData)
+	tw, err := core.CreateWalletWithMnemonic(mn, core.CoinTypeTron)
+	if err != nil {
+		panic(err)
+	}
+	printWallet(tw)
+}
 
-	outputData := C.TWAnySignerSign(inputData, C.TWCoinTypeBitcoin)
-	defer C.TWDataDelete(outputData)
-
-	var output bitcoin.SigningOutput
-	_ = proto.Unmarshal(types.TWDataGoBytes(outputData), &output)
-	fmt.Println("<== bitcoin signed tx: ", hex.EncodeToString(output.Encoded))
+func printWallet(w *core.Wallet) {
+	fmt.Printf("%s wallet: \n", w.CoinType.GetName())
+	fmt.Printf("\t address: %s \n", w.Address)
+	fmt.Printf("\t pri key: %s \n", w.PriKey)
+	fmt.Printf("\t pub key: %s \n", w.PubKey)
+	fmt.Println("")
 }
