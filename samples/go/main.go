69b9d19abda9b46212f74419c384554e5341c3cb
Amir Parsa <develop.apm@gmail.com>
2021-12-18 08:33:38 +0330

    Go Sample: add btc transaction sample

--- samples/go/main.go
+++ samples/go/main.go
@@ -6,23 +6,28 @@ import (
 	"math"
 	"math/big"
 	"tw/core"
+	"tw/protos/bitcoin"
+	"tw/protos/common"
 	"tw/protos/ethereum"
 )
 
 func main() {
 	mn := "confirm bleak useless tail chalk destroy horn step bulb genuine attract split"
+	
 	// bitcoin wallet
 	bw, err := core.CreateWalletWithMnemonic(mn, core.CoinTypeBitcoin)
 	if err != nil {
 		panic(err)
 	}
 	printWallet(bw)
+	
 	// ethereum wallet
 	ew, err := core.CreateWalletWithMnemonic(mn, core.CoinTypeEthereum)
 	if err != nil {
 		panic(err)
 	}
 	printWallet(ew)
+
 	// tron wallet
 	tw, err := core.CreateWalletWithMnemonic(mn, core.CoinTypeTron)
 	if err != nil {
@@ -30,10 +35,21 @@ func main() {
 	}
 	printWallet(tw)
 
-	// ethereum transaction
+	// Ethereum transaction
+	ethTxn := createEthTransaction(ew)
+	fmt.Println("Ethereum signed tx:")
+	fmt.Println("\t", ethTxn)
+
+	// Bitcion transaction
+	btcTxn := createBtcTransaction(bw)
+	fmt.Println("\nBitcoin signed tx:")
+	fmt.Println("\t", btcTxn)
+}
+
+func createEthTransaction(ew *core.Wallet) string {
 	priKeyByte, _ := hex.DecodeString(ew.PriKey)
 
-	ethSigningInput := ethereum.SigningInput{
+	input := ethereum.SigningInput{
 		ChainId:    big.NewInt(4).Bytes(), // mainnet: 1, rinkeby: 4 https://chainlist.org/
 		Nonce:      big.NewInt(0).Bytes(), // get nonce from network
 		TxMode:     ethereum.TransactionMode_Legacy,
@@ -44,7 +60,7 @@ func main() {
 		Transaction: &ethereum.Transaction{
 			TransactionOneof: &ethereum.Transaction_Transfer_{
 				Transfer: &ethereum.Transaction_Transfer{
-					// eth amount should be in wei, eth * (10^decimals) = wei
+					// amount should be in wei unit, eth * (10^decimals) = wei
 					Amount: big.NewInt(int64(
 						0.01 * math.Pow10(ew.CoinType.Decimals()),
 					)).Bytes(),
@@ -54,12 +70,53 @@ func main() {
 		},
 	}
 
-	var ethSigningOutput ethereum.SigningOutput
-	err = core.CreateSignedTx(&ethSigningInput, ew.CoinType, &ethSigningOutput)
+	var output ethereum.SigningOutput
+	err := core.CreateSignedTx(&input, ew.CoinType, &output)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Ethereum signed tx:\n\t", hex.EncodeToString(ethSigningOutput.GetEncoded()))
+	return hex.EncodeToString(output.GetEncoded())
+}
+
+func createBtcTransaction(bw *core.Wallet) string {
+	lockScript := core.BitcoinLockScriptForAddress(bw.Address, bw.CoinType)
+	fmt.Println("\nBitcoin address lock script:")
+	fmt.Println("\t", hex.EncodeToString(lockScript))
+
+	utxoHash, _ := hex.DecodeString("fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f")
+
+	utxo := bitcoin.UnspentTransaction{
+		OutPoint: &bitcoin.OutPoint{
+			Hash:     utxoHash,
+			Index:    0,
+			Sequence: 4294967295,
+		},
+		Amount: 625000000,
+		Script: lockScript,
+	}
+
+	priKeyByte, _ := hex.DecodeString(bw.PriKey)
+
+	input := bitcoin.SigningInput{
+		HashType:      uint32(core.BitcoinSigHashTypeAll),
+		Amount:        1000000,
+		ByteFee:       1,
+		ToAddress:     "1Bp9U1ogV3A14FMvKbRJms7ctyso4Z4Tcx",
+		ChangeAddress: "1FQc5LdgGHMHEN9nwkjmz6tWkxhPpxBvBU",
+		PrivateKey:    [][]byte{priKeyByte},
+		Utxo:          []*bitcoin.UnspentTransaction{&utxo},
+		CoinType:      uint32(core.CoinTypeBitcoin),
+	}
+
+	var output bitcoin.SigningOutput
+	err := core.CreateSignedTx(&input, bw.CoinType, &output)
+	if err != nil {
+		panic(err)
+	}
+	if output.GetError() != common.SigningError_OK {
+		panic(output.GetError().String())
+	}
+	return hex.EncodeToString(output.GetEncoded())
 }
 
 func printWallet(w *core.Wallet) {
--- samples/go/protos/compile.sh
+++ samples/go/protos/compile.sh
@@ -1,3 +1,5 @@
+# Run these commands inside current(protos) folder
+# all proto files copied from wallet-core/src/protos directory
 protoc -I=. --go_out=ethereum --go_opt=paths=source_relative Ethereum.proto
 protoc -I=. --go_out=common --go_opt=paths=source_relative Common.proto
 protoc -I=. --go_out=bitcoin --go_opt=paths=source_relative Bitcoin.proto
