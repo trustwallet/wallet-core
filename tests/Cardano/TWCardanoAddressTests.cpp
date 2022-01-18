4c3175997fa49b69df97b07b787cc014f712242c
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-01-18 09:32:16 +0100

    Test data update

--- tests/Cardano/TWCardanoAddressTests.cpp
+++ tests/Cardano/TWCardanoAddressTests.cpp
@@ -16,7 +16,7 @@
 TEST(TWCardano, Address) {
     auto privateKey = WRAP(TWPrivateKey, TWPrivateKeyCreateWithData(DATA(
         "b0884d248cb301edd1b34cf626ba6d880bb3ae8fd91b4696446999dc4f0b5744309941d56938e943980d11643c535e046653ca6f498c014b88f2ad9fd6e71effbf36a8fa9f5e11eb7a852c41e185e3969d518e66e6893c81d3fc7227009952d4"
-        "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" // dummy second
+        "639aadd8b6499ae39b78018b79255fbd8f585cbda9cbb9e907a72af86afb7a05d41a57c2dec9a6a19d6bf3b1fa784f334f3a0048d25ccb7b78a7b44066f9ba7bed7f28be986cbe06819165f2ee41b403678a098961013cf4a2f3e9ea61fb6c1a"
         ).get()));
     ASSERT_NE(nullptr, privateKey.get());
     auto publicKey = WRAP(TWPublicKey, TWPrivateKeyGetPublicKeyEd25519Extended(privateKey.get()));
@@ -24,12 +24,12 @@ TEST(TWCardano, Address) {
     ASSERT_EQ(128, publicKey.get()->impl.bytes.size());
     auto address = WRAP(TWAnyAddress, TWAnyAddressCreateWithPublicKey(publicKey.get(), TWCoinTypeCardano));
     auto addressString = WRAPS(TWAnyAddressDescription(address.get()));
-    assertStringsEqual(addressString, "addr1qx4z6twzknkkux0hhp0kq6hvdfutczp56g56y5em8r8mgvqgu83fq6ft8wnx2h6e039x78ul8a04yqlt4jg4378pszts5a80gr");
+    assertStringsEqual(addressString, "addr1qx4z6twzknkkux0hhp0kq6hvdfutczp56g56y5em8r8mgvxalp7nkkk25vuspleke2zltaetmlwrfxv7t049cq9jmwjswmfw6t");
 
-    auto address2 = WRAP(TWAnyAddress, TWAnyAddressCreateWithString(STRING("addr1qx4z6twzknkkux0hhp0kq6hvdfutczp56g56y5em8r8mgvqgu83fq6ft8wnx2h6e039x78ul8a04yqlt4jg4378pszts5a80gr").get(), TWCoinTypeCardano));
+    auto address2 = WRAP(TWAnyAddress, TWAnyAddressCreateWithString(STRING("addr1qx4z6twzknkkux0hhp0kq6hvdfutczp56g56y5em8r8mgvxalp7nkkk25vuspleke2zltaetmlwrfxv7t049cq9jmwjswmfw6t").get(), TWCoinTypeCardano));
     ASSERT_NE(nullptr, address2.get());
     auto address2String = WRAPS(TWAnyAddressDescription(address2.get()));
-    assertStringsEqual(address2String, "addr1qx4z6twzknkkux0hhp0kq6hvdfutczp56g56y5em8r8mgvqgu83fq6ft8wnx2h6e039x78ul8a04yqlt4jg4378pszts5a80gr");
+    assertStringsEqual(address2String, "addr1qx4z6twzknkkux0hhp0kq6hvdfutczp56g56y5em8r8mgvxalp7nkkk25vuspleke2zltaetmlwrfxv7t049cq9jmwjswmfw6t");
 
     ASSERT_TRUE(TWAnyAddressEqual(address.get(), address2.get()));
 }
