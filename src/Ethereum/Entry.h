c504e60a01b74c3dfd723f92d843f6a88c87ffa1
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-02-20 16:15:38 +0100

    Merge pull request

--- src/Ethereum/Entry.h
+++ src/Ethereum/Entry.h
@@ -34,7 +34,6 @@ public:
             TWCoinTypeXDai,
             TWCoinTypeFantom,
             TWCoinTypeCelo,
-            TWCoinTypeRonin,
         };
     }
     virtual bool validateAddress(TWCoinType coin, const std::string& address, TW::byte p2pkh, TW::byte p2sh, const char* hrp) const;
---
+++ src/Ronin/Address.h
@@ -0,0 +1,31 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include "../PublicKey.h"
+#include "../Ethereum/Address.h"
+#include <cstdint>
+#include <vector>
+
+namespace TW::Ronin {
+
+class Address: public Ethereum::Address {
+  public:
+    /// Determines whether a string makes a valid  address.
+    static bool isValid(const std::string& string);
+
+    /// Initializes an address with a string representation.
+    explicit Address(const std::string& string);
+
+    /// Initializes an address with a public key.
+    explicit Address(const PublicKey& publicKey): Ethereum::Address(publicKey) {}
+
+    /// Returns a string representation of the address.
+    std::string string() const;
+};
+
+} // namespace TW::Ronin
