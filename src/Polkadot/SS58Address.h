a33665cee28a26ff42a10027a77840c976f60935
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-03-08 15:37:41 +0100

    Move implementation to .cpp file

--- src/Polkadot/SS58Address.h
+++ src/Polkadot/SS58Address.h
@@ -10,7 +10,6 @@
 #include "Data.h"
 #include "PublicKey.h"
 
-#include <array>
 #include <string>
 
 const std::string SS58Prefix = "SS58PRE";
@@ -32,111 +31,29 @@ class SS58Address {
     Data bytes;
 
     /// Determines whether a string makes a valid address
-    static bool isValid(const std::string& string, uint32_t network) {
-        const auto decoded = Base58::bitcoin.decode(string);
-        byte decodedNetworkSize = 0;
-        uint32_t decodedNetwork = 0;
-        if (!decodeNetwork(decoded, decodedNetworkSize, decodedNetwork)) {
-            return false;
-        }
-        // check size
-        if ((decodedNetworkSize + PublicKey::ed25519Size + checksumSize) != decoded.size()) {
-            return false;
-        }
-        // compare network
-        if (decodedNetwork != network) {
-            return false;
-        }
-        auto checksum = computeChecksum(Data(decoded.begin(), decoded.end() - checksumSize));
-        // compare checksum
-        if (!std::equal(decoded.end() - checksumSize, decoded.end(), checksum.begin())) {
-            return false;
-        }
-        return true;
-    }
+    static bool isValid(const std::string& string, uint32_t network);
 
     template <typename T>
-    static Data computeChecksum(const T& data) {
-        auto prefix = Data(SS58Prefix.begin(), SS58Prefix.end());
-        append(prefix, Data(data.begin(), data.end()));
-        auto hash = Hash::blake2b(prefix, 64);
-        auto checksum = Data(checksumSize);
-        std::copy(hash.begin(), hash.begin() + checksumSize, checksum.data());
-        return checksum;
-    }
+    static Data computeChecksum(const T& data);
 
     SS58Address() = default;
 
     /// Initializes an address with a string representation.
-    SS58Address(const std::string& string, uint32_t network) {
-        if (!isValid(string, network)) {
-            throw std::invalid_argument("Invalid address string");
-        }
-        const auto decoded = Base58::bitcoin.decode(string);
-        bytes.resize(decoded.size() - checksumSize);
-        std::copy(decoded.begin(), decoded.end() - checksumSize, bytes.begin());
-    }
+    SS58Address(const std::string& string, uint32_t network);
 
     /// Initializes an address with a public key and network.
-    SS58Address(const PublicKey& publicKey, uint32_t network) {
-        if (publicKey.type != TWPublicKeyTypeED25519) {
-            throw std::invalid_argument("SS58Address expects an ed25519 public key.");
-        }
-        if (!encodeNetwork(network, bytes)) {
-            throw std::invalid_argument(std::string("network out of range ") + std::to_string(network));
-        }
-        TW::append(bytes, publicKey.bytes);
-    }
+    SS58Address(const PublicKey& publicKey, uint32_t network);
 
     /// Returns a string representation of the address.
-    std::string string() const {
-        auto result = Data(bytes.begin(), bytes.end());
-        auto checksum = computeChecksum(bytes);
-        append(result, checksum);
-        return Base58::bitcoin.encode(result);
-    }
+    std::string string() const;
 
     /// Returns public key bytes
-    Data keyBytes() const {
-        byte networkSize;
-        uint32_t networkTemp;
-        decodeNetwork(bytes, networkSize, networkTemp);
-        return Data(bytes.begin() + networkSize, bytes.end());
-    }
+    Data keyBytes() const;
 
     // Return true and the network size (1 or 2) and network if input is valid
-    static bool decodeNetwork(const Data& data, byte& networkSize, uint32_t& network) {
-        networkSize = 0;
-        network = 0;
-        if (data.size() >= 1 && data[0] < networkSimpleLimit) { // 0 -- 63
-            networkSize = 1;
-            network = data[0];
-            return true;
-        }
-        if (data.size() >= 2 && data[0] >= networkSimpleLimit && data[0] < networkFullLimit) { // 64 -- 127
-            networkSize = 2;
-            network = ((uint32_t)data[1] << 6) | (uint32_t)(data[0] & 0b00111111);
-            return true;
-        }
-        return false;
-    }
+    static bool decodeNetwork(const Data& data, byte& networkSize, uint32_t& network);
 
-    static bool encodeNetwork(uint32_t network, Data& data) {
-        if (network < networkSimpleLimit) { // 0 -- 63
-            // Simple account/address/network
-            data = {(byte)network};
-            return true;
-        }
-        if (network < 0x4000) { // 64 -- 16383
-            // Full address/address/network identifier.
-            byte byte0 = networkSimpleLimit + (byte)(network & 0b0000000000111111);
-            byte byte1 = (network & 0b0011111111000000) >> 6;
-            data = {byte0, byte1};
-            return true;
-        }
-        // not supported
-        return false;
-    }
+    static bool encodeNetwork(uint32_t network, Data& data);
 };
 
 inline bool operator==(const SS58Address& lhs, const SS58Address& rhs) {
