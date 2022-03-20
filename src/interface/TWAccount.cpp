ce55dd3a9a30bf202ea091a7f862da088dd41547
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-03-20 17:56:26 +0900

    cleanup

--- src/interface/TWAccount.cpp
+++ src/interface/TWAccount.cpp
@@ -4,7 +4,6 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
-#include "TrustWalletCore/TWString.h"
 #include <TrustWalletCore/TWAccount.h>
 
 #include "../Keystore/Account.h"
