b50969dcb6509924abd06042d71e392b88987926
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-03-31 21:32:04 +0900

    Fix Cronos address data (#2110)

---
+++ tests/Cronos/TWAnyAddressTests.cpp
@@ -0,0 +1,21 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "../interface/TWTestUtilities.h"
+
+#include <TrustWalletCore/TWAnyAddress.h>
+#include <TrustWalletCore/TWCoinType.h>
+
+#include <gtest/gtest.h>
+
+TEST(CronosAnyAddress, Validate) {
+    auto string = STRING("0xEC49280228b0D05Aa8e8b756503254e1eE7835ab");
+
+    auto addr = WRAP(TWAnyAddress, TWAnyAddressCreateWithString(string.get(), TWCoinTypeCronosChain));
+    
+    auto keyHash = WRAPD(TWAnyAddressData(addr.get()));
+    assertHexEqual(keyHash, "ec49280228b0d05aa8e8b756503254e1ee7835ab");    
+}
