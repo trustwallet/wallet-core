f3b0256520614d0a2e465a8834b319637b26234c
Ben <huangzhangcheng@matrixelements.com>
2022-04-18 11:29:54 +0800

    add swift platonTests

---
+++ swift/Tests/Blockchains/PlatonTests.swift
@@ -0,0 +1,23 @@
+//
+//  PlatonTests.swift
+//  WalletCoreTests
+//
+//  Created by Ben on 2022/4/18.
+//
+
+import WalletCore
+import XCTest
+
+class PlatonTests: XCTestCase {
+
+    func testAddress() {
+        let key = PrivateKey(data: Data(hexString: "03a4130e4abb887a296eb38c15bbd83253ab09492a505b10a54b008b7dcc1668")!)!
+        let pubkey = key.getPublicKeySecp256k1(compressed: false)
+        let address = AnyAddress(publicKey: pubkey, coin: .platon)
+        let addressFromString = AnyAddress(string: "0xC115CEADF9e5923330e5f42903Fe7f926DDA65D2", coin: .platon)!
+
+        XCTAssertEqual(pubkey.data.hexString, "04013f756ad4762f5109dba9e652ae43905730c882a12dedc17edca788f51dcb3b8a74ce86d8b6674fa167798d30f1149d9ab86dffcc976278966b5584450107a6")
+        XCTAssertEqual(address.description, addressFromString.description)
+    }
+
+}
