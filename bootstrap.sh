0f000717b296160306541c5605e692a19c5debd9
yushion-yang <43662114+yushion-yang@users.noreply.github.com>
2022-04-29 10:49:03 +0800

    use root account to cp and link in boostrap.sh (#3)
    Co-authored-by: yushion <yushion.y@nodereal.io>

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,5 +28,5 @@ TESTS_ROOT="`(cd \"$ROOT/tests\" && pwd)`"
 build/tests/tests "$TESTS_ROOT"
 
 echo "#### Installing... ####"
-cp build/libTrustWalletCore.a build/libprotobuf.a build/trezor-crypto/libTrezorCrypto.a /usr/local/lib
-ln -s ${PWD}/include/TrustWalletCore /usr/local/include/TrustWalletCore
+sudo cp build/libTrustWalletCore.a build/libprotobuf.a build/trezor-crypto/libTrezorCrypto.a /usr/local/lib
+sudo ln -s ${PWD}/include/TrustWalletCore /usr/local/include/TrustWalletCore
