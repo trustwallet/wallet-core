d366fd746f7a5ae5b7687b714c8a23e051e396a3
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-05-04 11:53:02 +0200

    Add EvmosZone CoinType tests

--- tests/Evmos/TWCoinTypeTests.cpp
+++ tests/Evmos/TWCoinTypeTests.cpp
@@ -8,7 +8,7 @@
 #include <TrustWalletCore/TWCoinTypeConfiguration.h>
 #include <gtest/gtest.h>
 
-TEST(TWEvmosCoinType, TWCoinType) {
+TEST(TWEvmosCoinType, TWCoinTypeEvmos) {
     auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeEvmos));
     auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0x24af42cf4977a96d62e3a82c3cd9b519c3e7c53dd83398b88f0cb435d867b422"));
     auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeEvmos, txId.get()));
@@ -25,4 +25,23 @@ TEST(TWEvmosCoinType, TWCoinType) {
     assertStringsEqual(accUrl, "https://evm.evmos.org/address/0x30627903124Aa1e71384bc52e1cb96E4AB3252b6");
     assertStringsEqual(id, "evmos");
     assertStringsEqual(name, "Evmos");
+}
+
+TEST(TWEvmosCoinType, TWCoinTypeEvmosZone) {
+    auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeEvmosZone));
+    auto txId = WRAPS(TWStringCreateWithUTF8Bytes("A16C211C83AD1E684DE46F694FAAC17D8465C864BD7385A81EC062CDE0638811"));
+    auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeEvmosZone, txId.get()));
+    auto accId = WRAPS(TWStringCreateWithUTF8Bytes("evmos17xpfvakm2amg962yls6f84z3kell8c5ljcjw34"));
+    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeEvmosZone, accId.get()));
+    auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeEvmosZone));
+    auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeEvmosZone));
+
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeEvmosZone), 18);
+    ASSERT_EQ(TWBlockchainCosmos, TWCoinTypeBlockchain(TWCoinTypeEvmosZone));
+
+    assertStringsEqual(symbol, "EVMOS");
+    assertStringsEqual(txUrl, "https://mintscan.io/evmos/txs/A16C211C83AD1E684DE46F694FAAC17D8465C864BD7385A81EC062CDE0638811");
+    assertStringsEqual(accUrl, "https://mintscan.io/evmos/account/evmos17xpfvakm2amg962yls6f84z3kell8c5ljcjw34");
+    assertStringsEqual(id, "evmoszone");
+    assertStringsEqual(name, "EvmosZone");
 }
