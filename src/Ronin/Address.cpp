8e80357d2fa949b8521269c58a12cd8e8a7a35f2
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-05-21 01:21:36 +0200

    Ronin accepts & normalizes Eth address as well

--- src/Ronin/Address.cpp
+++ src/Ronin/Address.cpp
@@ -14,22 +14,31 @@ using namespace TW::Ronin;
 
 bool Address::isValid(const std::string& string) {
     // check prefix
-    if (string.compare(0, prefix.length(), prefix) != 0) {
-        return false;
+    if (string.compare(0, prefix.length(), prefix) == 0) {
+        const auto suffix = string.substr(prefix.length());
+        const auto data = parse_hex(suffix);
+        return Ethereum::Address::isValid(data);
     }
-
-    const auto suffix = string.substr(prefix.length());
-    const auto data = parse_hex(suffix);
-    return Ethereum::Address::isValid(data);
+    // accept Ethereum format as well
+    if (Ethereum::Address::isValid(string)) {
+        return true;
+    }
+    return false;
 }
 
 Address::Address(const std::string& string) {
-    if (!isValid(string)) {
+    // check prefix
+    if (string.compare(0, prefix.length(), prefix) == 0) {
+        const auto suffix = string.substr(prefix.length());
+        const auto data = parse_hex(suffix);
+        std::copy(data.begin(), data.end(), bytes.begin());
+    } else if (Ethereum::Address::isValid(string)) {
+        // accept Ethereum format as well
+        Ethereum::Address ethereumAddress(string);
+        bytes = ethereumAddress.bytes;
+    } else {
         throw std::invalid_argument("Invalid address data");
     }
-    const auto suffix = string.substr(prefix.length());
-    const auto data = parse_hex(suffix);
-    std::copy(data.begin(), data.end(), bytes.begin());
 }
 
 std::string Address::string() const {
