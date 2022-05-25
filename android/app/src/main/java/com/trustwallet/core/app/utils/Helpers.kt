932c19c04f52fc2204314710dedfaf5b1b44160d
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-05-25 10:14:36 +0200

    ns ambiguity

--- android/app/src/main/java/com/trustwallet/core/app/utils/Helpers.kt
+++ android/app/src/main/java/com/trustwallet/core/app/utils/Helpers.kt
@@ -3,7 +3,7 @@ package com.trustwallet.core.app.helpers
 import wallet.core.jni.*
 import wallet.core.jni.Cardano
 //import wallet.core.jni.CoinType.CARDANO
-//import wallet.core.jni.proto.Cardano
+import wallet.core.jni.proto.Cardano
 //import wallet.core.jni.proto.Cardano.SigningInput
 //import wallet.core.jni.proto.Cardano.SigningOutput
 //import wallet.core.jni.proto.Common.SigningError
@@ -21,6 +21,6 @@ public func CardanoMinAdaAmount(tokenBundle: Data) -> UInt64 {
 fun CardanoMinAdaAmount(tokenBundleData: ByteArray): Long {
     //Cardano.minAdaAmount(tokenBundleData);
     //minAdaAmount(tokenBundleData);
-    return Cardano.minAdaAmount(tokenBundleData);
+    return jni.Cardano.minAdaAmount(tokenBundleData);
 }
 
