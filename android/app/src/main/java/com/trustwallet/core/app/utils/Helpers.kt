b82b9fecd0e19acb099664d6313adc56dc46043c
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-05-25 09:51:25 +0200

    comment out

--- android/app/src/main/java/com/trustwallet/core/app/utils/Helpers.kt
+++ android/app/src/main/java/com/trustwallet/core/app/utils/Helpers.kt
@@ -2,11 +2,11 @@ package com.trustwallet.core.app.helpers
 
 import wallet.core.jni.*
 import wallet.core.jni.Cardano
-import wallet.core.jni.CoinType.CARDANO
-import wallet.core.jni.proto.Cardano
-import wallet.core.jni.proto.Cardano.SigningInput
-import wallet.core.jni.proto.Cardano.SigningOutput
-import wallet.core.jni.proto.Common.SigningError
+//import wallet.core.jni.CoinType.CARDANO
+//import wallet.core.jni.proto.Cardano
+//import wallet.core.jni.proto.Cardano.SigningInput
+//import wallet.core.jni.proto.Cardano.SigningOutput
+//import wallet.core.jni.proto.Common.SigningError
 
 /*
 public func CardanoMinAdaAmount(tokenBundle: Data) -> UInt64 {
@@ -19,8 +19,8 @@ public func CardanoMinAdaAmount(tokenBundle: Data) -> UInt64 {
  */
 
 fun CardanoMinAdaAmount(tokenBundleData: ByteArray): Long {
-    Cardano.minAdaAmount(tokenBundleData);
-    minAdaAmount(tokenBundleData);
+    //Cardano.minAdaAmount(tokenBundleData);
+    //minAdaAmount(tokenBundleData);
     return Cardano.minAdaAmount(tokenBundleData);
 }
 
