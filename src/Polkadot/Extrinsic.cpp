f11b25109a49e656c8ad35a0c2f8765345a2d426
Ruslan Serebriakov <rsrbk1@gmail.com>
2022-05-30 22:46:26 +0700

    Update Extrinsic.cpp

--- src/Polkadot/Extrinsic.cpp
+++ src/Polkadot/Extrinsic.cpp
@@ -25,7 +25,6 @@ static const std::string stakingUnbond = "Staking.unbond";
 static const std::string stakingWithdrawUnbond = "Staking.withdraw_unbonded";
 static const std::string stakingNominate = "Staking.nominate";
 static const std::string stakingChill = "Staking.chill";
-static const std::string stakingChillAndUnbond = "Staking.chill_and_unbond";
 
 // Readable decoded call index can be found from https://polkascan.io
 static std::map<const std::string, Data> polkadotCallIndices = {
@@ -36,7 +35,6 @@ static std::map<const std::string, Data> polkadotCallIndices = {
     {stakingWithdrawUnbond, Data{0x07, 0x03}},
     {stakingNominate,       Data{0x07, 0x05}},
     {stakingChill,          Data{0x07, 0x06}},
-    {stakingChillAndUnbond, Data{0x07, 0x07}},
     {utilityBatch,          Data{0x1a, 0x02}},
 };
 
@@ -48,7 +46,6 @@ static std::map<const std::string, Data> kusamaCallIndices = {
     {stakingWithdrawUnbond, Data{0x06, 0x03}},
     {stakingNominate,       Data{0x06, 0x05}},
     {stakingChill,          Data{0x06, 0x06}},
-    {stakingChillAndUnbond, Data{0x06, 0x07}},
     {utilityBatch,          Data{0x18, 0x02}},
 };
 
@@ -217,7 +214,7 @@ Data Extrinsic::encodeStakingCall(const Proto::Staking& staking, TWSS58AddressTy
                 Data call1;
                 {
                     auto staking1 = Proto::Staking();
-                    staking1.chill();
+                    staking1.mutable_chill();
                     // recursive call
                     call1 = encodeStakingCall(staking1, network, specVersion);
                 }
@@ -227,7 +224,7 @@ Data Extrinsic::encodeStakingCall(const Proto::Staking& staking, TWSS58AddressTy
                 {
                     auto staking2 = Proto::Staking();
                     auto* unbond = staking2.mutable_unbond();
-                    unbond->set_value(staking.unbond().value());
+                    unbond->set_value(staking.chill_and_unbond().value());
                     // recursive call
                     call2 = encodeStakingCall(staking2, network, specVersion);
                 }
