04e9ecc468bb8b1c1dbcb55d584414a0a4554336
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-05-31 08:27:47 +0900

    consolidate ETH symbols

--- tests/Aurora/TWCoinTypeTests.cpp
+++ tests/Aurora/TWCoinTypeTests.cpp
@@ -26,7 +26,7 @@ TEST(TWAuroraCoinType, TWCoinType) {
     ASSERT_EQ(TWBlockchainEthereum, TWCoinTypeBlockchain(TWCoinTypeAurora));
     ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeAurora));
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeAurora));
-    assertStringsEqual(symbol, "AETH");
+    assertStringsEqual(symbol, "ETH");
     assertStringsEqual(txUrl, "https://aurorascan.dev/tx/0x99deebdb70f8027037abb3d3d0f3c7523daee857d85e9056d2671593ff2f2f28");
     assertStringsEqual(accUrl, "https://aurorascan.dev/address/0x8707cdE20dd43E3dB1F74c28fcd509ef38B0bA51");
     assertStringsEqual(id, "aurora");
