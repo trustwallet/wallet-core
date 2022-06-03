8283e6318d6d4f254cbaf038e348791df1e5addf
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2022-06-03 23:39:31 +0530

    Add test for Moonriver and Moonbeam

---
+++ tests/Moonriver/TWCoinTypeTests.cpp
@@ -0,0 +1,37 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here MAY BE LOST.
+// Generated one-time (codegen/bin/cointests)
+//
+
+#include "../interface/TWTestUtilities.h"
+#include <TrustWalletCore/TWCoinTypeConfiguration.h>
+#include <gtest/gtest.h>
+
+
+TEST(TWMoonriverCoinType, TWCoinType) {
+    const auto coin = TWCoinTypeMoonriver;
+    const auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(coin));
+    const auto id = WRAPS(TWCoinTypeConfigurationGetID(coin));
+    const auto name = WRAPS(TWCoinTypeConfigurationGetName(coin));
+    const auto chainId = WRAPS(TWCoinTypeChainId(coin));
+    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0x2e2daa3943ba65d9bbb910a4f6765aa6a466a0ef8935090547ca9d30e201e032"));
+    const auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(coin, txId.get()));
+    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0x899831D937937d011305E73EE782cce0455DF15a"));
+    const auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(coin, accId.get()));
+
+    assertStringsEqual(id, "moonriver");
+    assertStringsEqual(name, "Moonriver");
+    assertStringsEqual(symbol, "MOVR");
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(coin), 18);
+    ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainEthereum);
+    ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 0x0);
+    ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0x0);
+    assertStringsEqual(chainId, "1285");
+    assertStringsEqual(txUrl, "https://moonriver.moonscan.io/tx/0x2e2daa3943ba65d9bbb910a4f6765aa6a466a0ef8935090547ca9d30e201e032");
+    assertStringsEqual(accUrl, "https://moonriver.moonscan.io/address/0x899831D937937d011305E73EE782cce0455DF15a");
+}
