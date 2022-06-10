c9e32ea6b52958a386ffce8a31e6d70494bbb3da
Sztergbaum Roman <rmscastle@gmail.com>
2022-06-10 05:40:24 +0100

    [Format]: Bitcoin Input Selector (#2303)
    * format(bitcoin_input_selector): apply clang-format
    * Update src/Bitcoin/InputSelector.h
    Co-authored-by: Adam V <13562139+catenocrypt@users.noreply.github.com>

--- src/Bitcoin/InputSelector.h
+++ src/Bitcoin/InputSelector.h
@@ -16,34 +16,43 @@ namespace TW::Bitcoin {
 
 template <typename TypeWithAmount> // TypeWithAmount has to have a uint64_t amount
 class InputSelector {
-  public:
+public:
     /// Selects unspent transactions to use given a target transaction value, using complete logic.
     ///
-    /// \returns the list of indices of selected inputs, or an empty list if there are insufficient funds.
-    std::vector<TypeWithAmount> select(int64_t targetValue, int64_t byteFee, int64_t numOutputs = 2);
+    /// \returns the list of indices of selected inputs, or an empty list if there are insufficient
+    /// funds.
+    std::vector<TypeWithAmount> select(int64_t targetValue, int64_t byteFee,
+                                       int64_t numOutputs = 2);
 
     /// Selects unspent transactions to use given a target transaction value;
     /// Simplified version suitable for large number of inputs
     ///
-    /// \returns the list of indices of selected inputs, or an empty list if there are insufficient funds.
-    std::vector<TypeWithAmount> selectSimple(int64_t targetValue, int64_t byteFee, int64_t numOutputs = 2);
+    /// \returns the list of indices of selected inputs, or an empty list if there are insufficient
+    /// funds.
+    std::vector<TypeWithAmount> selectSimple(int64_t targetValue, int64_t byteFee,
+                                             int64_t numOutputs = 2);
 
-    /// Selects UTXOs for max amount; select all except those which would reduce output (dust). Return indIces.
-    /// One output and no change is assumed.
+    /// Selects UTXOs for max amount; select all except those which would reduce output (dust).
+    /// Return indices. One output and no change is assumed.
     std::vector<TypeWithAmount> selectMaxAmount(int64_t byteFee);
 
     /// Construct, using provided feeCalculator (see getFeeCalculator()).
-    explicit InputSelector(const std::vector<TypeWithAmount>& inputs, const FeeCalculator& feeCalculator) : inputs(inputs), feeCalculator(feeCalculator) {}
-    InputSelector(const std::vector<TypeWithAmount>& inputs) : InputSelector(inputs, getFeeCalculator(TWCoinTypeBitcoin)) {}
+    explicit InputSelector(const std::vector<TypeWithAmount>& inputs,
+                           const FeeCalculator& feeCalculator)
+        : inputs(inputs), feeCalculator(feeCalculator) {}
+    InputSelector(const std::vector<TypeWithAmount>& inputs)
+        : InputSelector(inputs, getFeeCalculator(TWCoinTypeBitcoin)) {}
 
     /// Sum of input amounts
     static uint64_t sum(const std::vector<TypeWithAmount>& amounts);
     /// Filters out utxos that are dust
-    std::vector<TypeWithAmount> filterOutDust(const std::vector<TypeWithAmount>& inputs, int64_t byteFee);
+    std::vector<TypeWithAmount> filterOutDust(const std::vector<TypeWithAmount>& inputs,
+                                              int64_t byteFee);
     /// Filters out inputs below (or equal) a certain threshold limit
-    std::vector<TypeWithAmount> filterThreshold(const std::vector<TypeWithAmount>& inputs, uint64_t minimumAmount);
+    std::vector<TypeWithAmount> filterThreshold(const std::vector<TypeWithAmount>& inputs,
+                                                uint64_t minimumAmount);
 
-  private:
+private:
     const std::vector<TypeWithAmount> inputs;
     const FeeCalculator& feeCalculator;
 };
