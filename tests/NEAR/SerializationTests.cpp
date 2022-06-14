b0a91d3c8c173bc3f0e83c0d11d8dcec01e3002a
vng0 <>
2022-06-14 14:42:52 +0700

    Add NEAR stake, add key, delete key, create account, delete account transactions serialization

--- tests/NEAR/SerializationTests.cpp
+++ tests/NEAR/SerializationTests.cpp
@@ -76,4 +76,173 @@ TEST(NEARSerialization, SerializeFunctionCallTransaction) {
     ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef601000000020300000071717103000000010203e80300000000000001000000000000000000000000000000");
 }
 
+TEST(NEARSerialization, SerializeStakeTransaction) {
+    auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
+
+    auto input = Proto::SigningInput();
+    input.set_signer_id("test.near");
+    input.set_nonce(1);
+    input.set_receiver_id("whatever.near");
+
+    input.add_actions();
+    auto& stake = *input.mutable_actions(0)->mutable_stake();
+    Data amount(16, 0);
+    amount[0] = 1;
+    stake.set_stake(amount.data(), amount.size());
+
+    auto& pKey = *stake.mutable_public_key();
+    pKey.set_data(publicKey.data(), publicKey.size());
+    pKey.set_key_type(0);
+
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
+
+    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
+    input.set_private_key(privateKey.data(), 32);
+
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
+
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef601000000040100000000000000000000000000000000917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d");
+}
+
+TEST(NEARSerialization, SerializeAddKeyFunctionCallTransaction) {
+    auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
+
+    auto input = Proto::SigningInput();
+    input.set_signer_id("test.near");
+    input.set_nonce(1);
+    input.set_receiver_id("whatever.near");
+
+    input.add_actions();
+    auto& addKey = *input.mutable_actions(0)->mutable_add_key();
+
+    auto& pKey = *addKey.mutable_public_key();
+    pKey.set_data(publicKey.data(), publicKey.size());
+    pKey.set_key_type(0);
+
+    auto& accessKey = *addKey.mutable_access_key();
+    accessKey.set_nonce(0);
+    auto& functionCallPermission = *accessKey.mutable_function_call();
+    functionCallPermission.set_receiver_id("zzz");
+    functionCallPermission.add_method_names("www");
+
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
+
+    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
+    input.set_private_key(privateKey.data(), 32);
+
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
+
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef6010000000500917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d00000000000000000000030000007a7a7a0100000003000000777777");
+}
+
+TEST(NEARSerialization, SerializeAddKeyFullAccessTransaction) {
+    auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
+
+    auto input = Proto::SigningInput();
+    input.set_signer_id("test.near");
+    input.set_nonce(1);
+    input.set_receiver_id("whatever.near");
+
+    input.add_actions();
+    auto& addKey = *input.mutable_actions(0)->mutable_add_key();
+
+    auto& pKey = *addKey.mutable_public_key();
+    pKey.set_data(publicKey.data(), publicKey.size());
+    pKey.set_key_type(0);
+
+    auto& accessKey = *addKey.mutable_access_key();
+    accessKey.set_nonce(0);
+
+    accessKey.mutable_full_access();
+
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
+
+    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
+    input.set_private_key(privateKey.data(), 32);
+
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
+
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef6010000000500917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d000000000000000001");
+}
+
+TEST(NEARSerialization, SerializeDeleteKeyTransaction) {
+    auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
+
+    auto input = Proto::SigningInput();
+    input.set_signer_id("test.near");
+    input.set_nonce(1);
+    input.set_receiver_id("whatever.near");
+
+    input.add_actions();
+    auto& deleteKey = *input.mutable_actions(0)->mutable_delete_key();
+
+    auto& pKey = *deleteKey.mutable_public_key();
+    pKey.set_data(publicKey.data(), publicKey.size());
+    pKey.set_key_type(0);
+
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
+
+    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
+    input.set_private_key(privateKey.data(), 32);
+
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
+
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef6010000000600917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d");
+}
+
+TEST(NEARSerialization, SerializeCreateAccountTransaction) {
+    auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
+
+    auto input = Proto::SigningInput();
+    input.set_signer_id("test.near");
+    input.set_nonce(1);
+    input.set_receiver_id("whatever.near");
+
+    input.add_actions();
+    input.mutable_actions(0)->mutable_create_account();
+
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
+
+    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
+    input.set_private_key(privateKey.data(), 32);
+
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
+
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef60100000000");
+}
+
+TEST(NEARSerialization, SerializeDeleteAccountTransaction) {
+    auto publicKey = Base58::bitcoin.decode("Anu7LYDfpLtkP7E16LT9imXF694BdQaa9ufVkQiwTQxC");
+
+    auto input = Proto::SigningInput();
+    input.set_signer_id("test.near");
+    input.set_nonce(1);
+    input.set_receiver_id("whatever.near");
+
+    input.add_actions();
+    auto& deleteAccount = *input.mutable_actions(0)->mutable_delete_account();
+    deleteAccount.set_beneficiary_id("123");
+
+    auto blockHash = Base58::bitcoin.decode("244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM");
+    input.set_block_hash(blockHash.data(), blockHash.size());
+
+    auto privateKey = Base58::bitcoin.decode("3hoMW1HvnRLSFCLZnvPzWeoGwtdHzke34B2cTHM8rhcbG3TbuLKtShTv3DvyejnXKXKBiV7YPkLeqUHN1ghnqpFv");
+    input.set_private_key(privateKey.data(), 32);
+
+    auto serialized = transactionData(input);
+    auto serializedHex = hex(serialized);
+
+    ASSERT_EQ(serializedHex, "09000000746573742e6e65617200917b3d268d4b58f7fec1b150bd68d69be3ee5d4cc39855e341538465bb77860d01000000000000000d00000077686174657665722e6e6561720fa473fd26901df296be6adc4cc4df34d040efa2435224b6986910e630c2fef6010000000703000000313233");
+}
+
 }
