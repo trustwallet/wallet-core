eb882569862bf3d149b5b86e32a882b3b554f5af
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-06-18 13:29:50 +0800

    Revert "Codegen: preserve comments (#2297)" (#2324)
    This reverts commit 5375d2912d6ef26333cdc98de1c1819488a23066.

--- codegen/lib/entity_decl.rb
+++ codegen/lib/entity_decl.rb
@@ -2,17 +2,16 @@
 
 # Class or struct declaration
 class EntityDecl
-  attr_reader :name, :comment
+  attr_reader :name
   attr_accessor :is_struct, :methods, :properties, :static_methods, :static_properties
 
-  def initialize(name:, is_struct:, comment:)
+  def initialize(name:, is_struct:)
     @name = name
     @is_struct = is_struct
     @methods = []
     @properties = []
     @static_methods = []
     @static_properties = []
-    @comment = comment
   end
 
   def struct?
