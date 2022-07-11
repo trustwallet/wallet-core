0cf00bc044fccbcad94aaaaf4e7807e72f84ae13
Milerius <rmscastle@gmail.com>
2022-07-11 11:56:50 +0200

    feat(base32): add swift basis unit test

---
+++ swift/Tests/Base32Tests.swift
@@ -0,0 +1,24 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+import XCTest
+import WalletCore
+
+class Base32Tests: XCTestCase {
+    func testDecode() {
+        if let decoded = Base32.decode(string: "JBSWY3DPK5XXE3DE") {
+            let toCompare = decoded.map({ String(format: "%c", $0) }).joined();
+            XCTAssertEqual(toCompare, "HelloWorld");
+        }
+    }
+    
+    func testDecodeWithAlphabet() {
+        if let decoded = Base32.decodeWithAlphabet(string: "g52w64jworydimrxov5hmn3gpj2gwyttnzxdmndjo5xxiztsojuxg5dxobzhs6i", alphabet:"abcdefghijklmnopqrstuvwxyz234567") {
+            let toCompare = decoded.map({ String(format: "%c", $0) }).joined();
+            XCTAssertEqual(toCompare, "7uoq6tp427uzv7fztkbsnn64iwotfrristwpryy");
+        }
+    }
+}
