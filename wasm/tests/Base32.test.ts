e6d87661a609d9b2d3c618b35bba001b63318fbe
milerius <rmscastle@gmail.com>
2022-07-11 20:42:09 +0200

    feat(base32): add typescript unit tests

---
+++ wasm/tests/Base32.test.ts
@@ -0,0 +1,30 @@
+import {assert} from "chai";
+import {Buffer} from "buffer";
+import { WalletCore } from "../dist";
+
+describe("Base32", () => {
+    it("test decrypting", () => {
+        const {Base32} = WalletCore;
+
+        const decoded = Base32.decode("JBSWY3DPK5XXE3DE");
+        assert.equal(Buffer.from(decoded).toString(), "HelloWorld");
+    });
+    it("test decrypting with alphabet", () => {
+        const {Base32} = WalletCore;
+
+        const decoded = Base32.decodeWithAlphabet("g52w64jworydimrxov5hmn3gpj2gwyttnzxdmndjo5xxiztsojuxg5dxobzhs6i", "abcdefghijklmnopqrstuvwxyz234567");
+        assert.equal(Buffer.from(decoded).toString(), "7uoq6tp427uzv7fztkbsnn64iwotfrristwpryy");
+    });
+    it("test encrypting", () => {
+        const {Base32} = WalletCore;
+
+        const encoded = Base32.encode(Buffer.from("HelloWorld"));
+        assert.equal(encoded, "JBSWY3DPK5XXE3DE");
+    });
+    it("test encrypting with alphabet", () => {
+        const {Base32} = WalletCore;
+
+        const encoded = Base32.encodeWithAlphabet(Buffer.from("7uoq6tp427uzv7fztkbsnn64iwotfrristwpryy"), "abcdefghijklmnopqrstuvwxyz234567");
+        assert.equal(encoded, "g52w64jworydimrxov5hmn3gpj2gwyttnzxdmndjo5xxiztsojuxg5dxobzhs6i");
+    });
+});
