69dac79f511847046e519a3109bb311af5b0c94b
Milerius <rmscastle@gmail.com>
2022-07-13 08:25:10 +0200

    feat(wasm): simplify wasm cmake logic / use toolchain

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -4,3 +4,9 @@
 # Currently supporting: Clang-Tidy.
 
 option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." OFF)
+
+#
+# Specific platforms support
+#
+# Currently supporting: Wasm.
+option(${PROJECT_NAME}_USE_WASM "Target Wasm" OFF)
