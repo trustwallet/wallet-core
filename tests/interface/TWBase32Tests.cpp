4f38375fcac6721849e157c2dbe593c7b7642f0d
Sztergbaum Roman <rmscastle@gmail.com>
2022-07-14 01:51:53 +0200

    [Feature]: Base32 to C interface (#2365)
    * feat(base32): add base32 encoding to c interface
    * feat(base32): finalize encoding/decoding function
    * feat(base32): add swift basis unit test
    * feat(base32): fix review comments
    * feat(base32): separate lines between assignment and assertion
    * feat(base32): add typescript unit tests
    * feat(base32): coding style + line break
    * feat(base32): use emptiness instead of nullable TWString
    * feat(base32): add kt unit tests

---
+++ tests/interface/TWBase32Tests.cpp
@@ -0,0 +1,67 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "TWTestUtilities.h"
+
+#include "Data.h"
+#include <TrustWalletCore/TWBase32.h>
+
+#include <gtest/gtest.h>
+
+TEST(TWBase32, InvalidDecode) {
+    const auto encodedInput = STRING("JBSWY3DPK5XXE3DE=======");
+    auto result = WRAPD(TWBase32Decode(encodedInput.get()));
+    ASSERT_EQ(result, nullptr);
+}
+
+TEST(TWBase32, Decode) {
+    const auto encodedInput = STRING("JBSWY3DPK5XXE3DE");
+    auto result = WRAPD(TWBase32Decode(encodedInput.get()));
+
+    ASSERT_NE(result, nullptr);
+    ASSERT_EQ(TWDataSize(result.get()), 10);
+
+    auto data = *reinterpret_cast<const TW::Data*>(result.get());
+    std::string str(data.begin(), data.end());
+
+    ASSERT_EQ(str, "HelloWorld");
+}
+
+TEST(TWBase32, DecodeWithAlphabet) {
+    const auto encodedInput = STRING("g52w64jworydimrxov5hmn3gpj2gwyttnzxdmndjo5xxiztsojuxg5dxobzhs6i");
+    const auto filecoinAlphabet = STRING("abcdefghijklmnopqrstuvwxyz234567");
+    auto result = WRAPD(TWBase32DecodeWithAlphabet(encodedInput.get(), filecoinAlphabet.get()));
+
+    ASSERT_NE(result, nullptr);
+    ASSERT_EQ(TWDataSize(result.get()), 39);
+
+    auto data = *reinterpret_cast<const TW::Data*>(result.get());
+    std::string str(data.begin(), data.end());
+
+    ASSERT_EQ(str, "7uoq6tp427uzv7fztkbsnn64iwotfrristwpryy");
+}
+
+TEST(TWBase32, Encode) {
+    TW::Data data{'H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd'};
+    auto encodedStr = TWBase32Encode(&data);
+    std::string result = TWStringUTF8Bytes(encodedStr);
+
+    ASSERT_EQ(result, "JBSWY3DPK5XXE3DE");
+
+    TWStringDelete(encodedStr);
+}
+
+TEST(TWBase32, EncodeWithAlphabet) {
+    const auto filecoinAlphabet = STRING("abcdefghijklmnopqrstuvwxyz234567");
+    TW::Data data{'7', 'u', 'o', 'q', '6', 't', 'p', '4', '2', '7', 'u', 'z', 'v', '7', 'f',
+                  'z', 't', 'k', 'b', 's', 'n', 'n', '6', '4', 'i', 'w', 'o', 't', 'f', 'r', 'r', 'i', 's', 't', 'w', 'p', 'r', 'y', 'y'};
+    auto encodedStr = TWBase32EncodeWithAlphabet(&data, filecoinAlphabet.get());
+    std::string result = TWStringUTF8Bytes(encodedStr);
+
+    ASSERT_EQ(result, "g52w64jworydimrxov5hmn3gpj2gwyttnzxdmndjo5xxiztsojuxg5dxobzhs6i");
+
+    TWStringDelete(encodedStr);
+}
