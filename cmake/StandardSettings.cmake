f464e68232fd6bfdb04ab8e5072a9222ec144120
Sztergbaum Roman <rmscastle@gmail.com>
2022-07-15 09:45:07 +0200

    [Wasm]: Change CMake logic (#2376)
    * feat(wasm): simplify wasm cmake logic / use toolchain
    * feat(cmake): simplify options name
    * feat(wasm): use TW_COMPILE_WASM for test check

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -3,4 +3,10 @@
 #
 # Currently supporting: Clang-Tidy.
 
-option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." OFF)
+option(TW_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." OFF)
+
+#
+# Specific platforms support
+#
+# Currently supporting: Wasm.
+option(TW_COMPILE_WASM "Target Wasm" OFF)
