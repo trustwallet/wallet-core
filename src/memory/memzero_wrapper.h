ea86e17a3922fedcfc3fd92fcf7a32f8babd2c2f
Milerius <rmscastle@gmail.com>
2022-07-20 15:49:58 +0200

    feat(memzero): add memzero c++ wrapper

---
+++ src/memory/memzero_wrapper.h
@@ -0,0 +1,23 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#pragma once
+
+#include <cstddef>
+
+#include <type_traits>
+
+#include <TrezorCrypto/memzero.h>
+
+namespace TW {
+
+template <typename T>
+static inline void memzero(T* data, std::size_t len = sizeof(T)) noexcept {
+    static_assert(std::is_trivial_v<T>, "type should be a pod");
+    ::memzero(data, len);
+}
+
+} // namespace TW
---
+++ tests/memory/memzero_tests.cpp
@@ -0,0 +1,23 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "memory/memzero_wrapper.h"
+
+#include "gtest/gtest.h"
+
+struct int_wrapper {
+    int value;
+};
+
+TEST(Memory, Memzero) {
+    int_wrapper obj{.value = 42};
+    TW::memzero(&obj);
+    ASSERT_EQ(obj.value, 0);
+    obj.value = 42;
+    ASSERT_EQ(obj.value, 42);
+    TW::memzero(&obj, sizeof(int_wrapper));
+    ASSERT_EQ(obj.value, 0);
+}
