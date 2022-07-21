b9162a4dbf685a3557d8f811115dab301c430d0e
Sztergbaum Roman <rmscastle@gmail.com>
2022-07-21 09:14:40 +0200

    [Improvements]: Modernize CMake (#2380)
    * feat(cmake): first part modernize
    - Use Interface library
    - Move options to StandardSettings.cmake
    - Don't use harmfull link_directories and include_directories
    * feat(cmake): add CompilerWarnings.cmake
    * feat(cmake): add warning as error option
    * feat(cmake): add opt flags and compile features
    * feat(cmake): simplify coverage/sanitizer instruments
    * feat(cmake): add missing target
    * feat(cmake): add pvs studio options
    * feat(cmake): finalize pvs studio cfg
    * feat(cmake): clean
    * feat(cmake): apply DRY on targets that link against TrustWalletCore
    * feat(cmake): add helpers, variables for IDE
    * feat(cmake): add sarif vscode gen for pvs studio
    * feat(cmake): use option instead of set
    * feat(ci): add pvs-studio-analyze
    * Fix brew prefix for m1 mac
    Co-authored-by: hewigovens <360470+hewigovens@users.noreply.github.com>

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,9 +1,33 @@
-if(TW_ENABLE_CLANG_TIDY)
+if (TW_ENABLE_CLANG_TIDY)
     find_program(CLANGTIDY clang-tidy)
-    if(CLANGTIDY)
+    if (CLANGTIDY)
         set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option)
         message("Clang-Tidy finished setting up.")
-    else()
+    else ()
         message(SEND_ERROR "Clang-Tidy requested but executable not found.")
-    endif()
-endif()
+    endif ()
+endif ()
+
+if (TW_ENABLE_PVS_STUDIO)
+    macro(tw_add_pvs_studio_target target)
+        message(STATUS "PVS-Studio analyzer enabled - ${CMAKE_SOURCE_DIR}/tools/pvs-studio/config.cfg")
+        include(cmake/PVS-Studio.cmake)
+        if (TW_IDE_VSCODE)
+            pvs_studio_add_target(TARGET TrustWalletCore.analyze ALL
+                    OUTPUT FORMAT sarif-vscode
+                    ANALYZE ${target}
+                    MODE GA:1,2
+                    LOG result.sarif
+                    CONFIG ${CMAKE_SOURCE_DIR}/tools/pvs-studio/config.cfg
+                    )
+        else ()
+            pvs_studio_add_target(TARGET TrustWalletCore.analyze ALL
+                    OUTPUT FORMAT json
+                    ANALYZE ${target}
+                    MODE GA:1,2
+                    LOG result.json
+                    CONFIG ${CMAKE_SOURCE_DIR}/tools/pvs-studio/config.cfg
+                    )
+        endif ()
+    endmacro()
+endif ()
