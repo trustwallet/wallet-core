3317925c9d444e78f5f0371788250aceb5fcbbe1
jianguo <zjg555543@163.com>
2022-07-22 15:18:51 +0800

    Optimize code

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -91,8 +91,7 @@ class CoinAddressDerivationTests: XCTestCase {
                      .moonbeam,
                      .kavaEvm,
                      .klaytn,
-                     .meter,
-                     .okc:
+                     .meter:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .ronin:
