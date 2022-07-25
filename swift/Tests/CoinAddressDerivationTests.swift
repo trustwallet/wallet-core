4736eb4b09ebed59874bee79576f4cefe7267759
jianguo <zjg555543@163.com>
2022-07-25 12:44:55 +0800

    Modify config for android and ios

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -92,7 +92,7 @@ class CoinAddressDerivationTests: XCTestCase {
                      .kavaEvm,
                      .klaytn,
                      .meter,
-                     .okxChain:
+                     .okxchain:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .ronin:
