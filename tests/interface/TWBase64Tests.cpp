ba7224cd8893e149926b9773206498c550b64ff7
Vladimir Miloserdov <milosvova@gmail.com>
2022-07-27 18:51:00 +0100

    Add TWBase64 interface exposing Base64 implementation (#2390)
    * Add TWBase64 interface exposing Base64 implementation
    
    Similarly to https://github.com/trustwallet/wallet-core/blob/master/include/TrustWalletCore/TWBase32.h, expose Base64 implementation from core library to Swift/Kotlin, issue: #2369
    
    * Add Swift/Kotlin tests, minor fixes
    
    * Add wasm tests
    
    * Rewrite Base64 to fix wasm crash
    
    * format code
    
    Co-authored-by: hewigovens <360470+hewigovens@users.noreply.github.com>

---
+++ tests/interface/TWBase64Tests.cpp
@@ -0,0 +1,56 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "TWTestUtilities.h"
+
+#include "Data.h"
+#include <TrustWalletCore/TWBase64.h>
+
+#include <gtest/gtest.h>
+
+TEST(TWBase64, Decode) {
+    const auto encodedInput = STRING("Kyc/YWI=");
+    auto result = WRAPD(TWBase64Decode(encodedInput.get()));
+
+    ASSERT_EQ(TWDataSize(result.get()), 5);
+
+    auto data = *reinterpret_cast<const TW::Data*>(result.get());
+    std::string str(data.begin(), data.end());
+
+    ASSERT_EQ(str, "+\'?ab");
+}
+
+TEST(TWBase64, Encode) {
+    TW::Data data{'+', '\'', '?', 'a', 'b'};
+    auto encodedStr = TWBase64Encode(&data);
+    std::string result = TWStringUTF8Bytes(encodedStr);
+
+    ASSERT_EQ(result, "Kyc/YWI=");
+
+    TWStringDelete(encodedStr);
+}
+
+TEST(TWBase64, DecodeUrl) {
+    const auto encodedInput = STRING("Kyc_YWI=");
+    auto result = WRAPD(TWBase64DecodeUrl(encodedInput.get()));
+
+    ASSERT_EQ(TWDataSize(result.get()), 5);
+
+    auto data = *reinterpret_cast<const TW::Data*>(result.get());
+    std::string str(data.begin(), data.end());
+
+    ASSERT_EQ(str, "+\'?ab");
+}
+
+TEST(TWBase64, EncodeUrl) {
+    TW::Data data{'+', '\'', '?', 'a', 'b'};
+    auto encodedStr = TWBase64EncodeUrl(&data);
+    std::string result = TWStringUTF8Bytes(encodedStr);
+
+    ASSERT_EQ(result, "Kyc_YWI=");
+
+    TWStringDelete(encodedStr);
+}
