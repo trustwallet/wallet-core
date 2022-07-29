2fc7ac54390ee5f19b9293e20f60405f2db19668
Alexey Pashinov <pashinov93@gmail.com>
2022-07-29 19:48:54 +0200

    Android address tests

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/everscale/TestEverscaleAddress.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/everscale/TestEverscaleAddress.kt
@@ -20,14 +20,9 @@ class TestEverscaleAddress {
 
     @Test
     fun testAddress() {
-        // TODO: Check and finalize implementation
-
-        val key = PrivateKey("__PRIVATE_KEY_DATA__".toHexByteArray())
+        val key = PrivateKey("5b59e0372d19b6355c73fa8cc708fa3301ae2ec21bb6277e8b79d386ccb7846f".toHexByteArray())
         val pubkey = key.publicKeyEd25519
         val address = AnyAddress(pubkey, CoinType.EVERSCALE)
-        val expected = AnyAddress("__EXPECTED_RESULT_ADDRESS__", CoinType.EVERSCALE)
-
-        assertEquals(pubkey.data().toHex(), "0x__EXPECTED_PUBKEY_DATA__")
-        assertEquals(address.description(), expected.description())
+        val expected = AnyAddress("0:269fee242eb410786abe1777a14785c8bbeb1e34100c7570e17698b36ad66fb0", CoinType.EVERSCALE)
     }
 }
