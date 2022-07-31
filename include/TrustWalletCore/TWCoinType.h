7c3486f2fc36f06b2f2964930f66aef9d08dd22a
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2022-07-31 15:20:42 +0530

    Add Astar chain

--- include/TrustWalletCore/TWCoinType.h
+++ include/TrustWalletCore/TWCoinType.h
@@ -114,6 +114,7 @@ enum TWCoinType {
     TWCoinTypeKlaytn = 10008217,
     TWCoinTypeMeter = 18000,
     TWCoinTypeOKXChain = 996,
+    TWCoinTypeAstar = 810,
 };
 
 /// Returns the blockchain for a coin type.
---
+++ tests/Astar/TWCoinTypeTests.cpp
@@ -0,0 +1,37 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+//
+// This is a GENERATED FILE, changes made here MAY BE LOST.
+// Generated one-time (codegen/bin/cointests)
+//
+
+#include "../interface/TWTestUtilities.h"
+#include <TrustWalletCore/TWCoinTypeConfiguration.h>
+#include <gtest/gtest.h>
+
+
+TEST(TWAstarCoinType, TWCoinType) {
+    const auto coin = TWCoinTypeAstar;
+    const auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(coin));
+    const auto id = WRAPS(TWCoinTypeConfigurationGetID(coin));
+    const auto name = WRAPS(TWCoinTypeConfigurationGetName(coin));
+    const auto chainId = WRAPS(TWCoinTypeChainId(coin));
+    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0xdc7cf434cfbb85ad57c133cd6da5fde6b5a080d5b322263f80dc48969913c708"));
+    const auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(coin, txId.get()));
+    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0x470aC13c63329A99C2Bd474127B08Fd3513d52A5"));
+    const auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(coin, accId.get()));
+
+    assertStringsEqual(id, "astar");
+    assertStringsEqual(name, "Astar");
+    assertStringsEqual(symbol, "OKT");
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(coin), 18);
+    ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainEthereum);
+    ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 0x0);
+    ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0x0);
+    assertStringsEqual(chainId, "592");
+    assertStringsEqual(txUrl, "https://astar.subscan.io/extrinsic/0xdc7cf434cfbb85ad57c133cd6da5fde6b5a080d5b322263f80dc48969913c708");
+    assertStringsEqual(accUrl, "https://astar.subscan.io/account/0x470aC13c63329A99C2Bd474127B08Fd3513d52A5");
+}
