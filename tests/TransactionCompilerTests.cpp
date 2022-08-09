a8aeacc1223eb8a2de64c1d18a87a9d3ea95bb28
jasonchuang <rice8266@gmail.com>
2022-08-09 16:52:47 +0800

    code fmt

--- tests/TransactionCompilerTests.cpp
+++ tests/TransactionCompilerTests.cpp
@@ -2843,7 +2843,6 @@ TEST(TransactionCompiler, CardanoCompileWithSignaturesAndPubKeyType) {
     EXPECT_EQ(hex(output.encoded()), expectedTx);
 }
 
-
 TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
     const auto coin = TWCoinTypeIoTeX;
 
@@ -2855,8 +2854,18 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
         parse_hex("034e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c");
     const auto pubKey1 =
         parse_hex("0253ad2f3b734a197f64911242aabc9b5b10bf5744949f5396e56427f35448eafa");
-    const auto ExpectedTx0 = "0a4c0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e723937716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c64211241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a41555cc8af4181bf85c044c3201462eeeb95374f78aa48c67b87510ee63d5e502372e53082f03e9a11c1e351de539cedf85d8dff87de9d003cb9f92243541541a000";
-    const auto ExpectedTx1 = "0a4c0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e723937716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c642112410453ad2f3b734a197f64911242aabc9b5b10bf5744949f5396e56427f35448eafa84a5d74b49ecb56e011b18c3d5a300e8cff7c6b39d330d1d3799c4700a0b1be21a41de4be56ce74dce8e526590f5b5f947385b00947c4c2ead014429aa706a2470055c56c7e57d1b119b487765d59b21bcdeafac25108f6929a14f9edf4b2309534501";
+    const auto ExpectedTx0 =
+        "0a4c0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e7239"
+        "37716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c64211241044e18306ae9ef4ec9d0"
+        "7bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34f"
+        "beb71270d4bad3d648d91a41555cc8af4181bf85c044c3201462eeeb95374f78aa48c67b87510ee63d5e502372"
+        "e53082f03e9a11c1e351de539cedf85d8dff87de9d003cb9f92243541541a000";
+    const auto ExpectedTx1 =
+        "0a4c0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e7239"
+        "37716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c642112410453ad2f3b734a197f64"
+        "911242aabc9b5b10bf5744949f5396e56427f35448eafa84a5d74b49ecb56e011b18c3d5a300e8cff7c6b39d33"
+        "0d1d3799c4700a0b1be21a41de4be56ce74dce8e526590f5b5f947385b00947c4c2ead014429aa706a2470055c"
+        "56c7e57d1b119b487765d59b21bcdeafac25108f6929a14f9edf4b2309534501";
 
     const auto prkey0 = PrivateKey(privateKey0);
     const PublicKey pbkey0 = prkey0.getPublicKey(TWPublicKeyTypeSECP256k1Extended);
@@ -2889,12 +2898,16 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
     auto preImage = data(preSigningOutput.data());
     auto preImageHash = data(preSigningOutput.data_hash());
 
-    std::string expectedPreImage = "0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e723937716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c6421";
-    std::string expectedPreImageHash = "0f17cd7f43bdbeff73dfe8f5cb0c0045f2990884e5050841de887cf22ca35b50";
+    std::string expectedPreImage =
+        "0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e72393771"
+        "6c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c6421";
+    std::string expectedPreImageHash =
+        "0f17cd7f43bdbeff73dfe8f5cb0c0045f2990884e5050841de887cf22ca35b50";
     ASSERT_EQ(hex(preImage), expectedPreImage);
     ASSERT_EQ(hex(preImageHash), expectedPreImageHash);
 
-    Data signature = parse_hex("555cc8af4181bf85c044c3201462eeeb95374f78aa48c67b87510ee63d5e502372e53082f03e9a11c1e351de539cedf85d8dff87de9d003cb9f92243541541a000");
+    Data signature = parse_hex("555cc8af4181bf85c044c3201462eeeb95374f78aa48c67b87510ee63d5e502372e"
+                               "53082f03e9a11c1e351de539cedf85d8dff87de9d003cb9f92243541541a000");
 
     // Verify signature (pubkey & hash & signature)
     EXPECT_TRUE(pbkey0.verify(signature, preImageHash));
@@ -2910,18 +2923,18 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
         ASSERT_TRUE(input.ParseFromArray(inputData.data(), (int)inputData.size()));
         EXPECT_EQ(hex(PrivateKey(privateKey0).getPublicKey(TWPublicKeyTypeSECP256k1).bytes),
                   hex(pubKey0));
-        input.set_privatekey(prkey0.bytes.data(), prkey0.bytes.size());                  
+        input.set_privatekey(prkey0.bytes.data(), prkey0.bytes.size());
         TW::IoTeX::Proto::SigningOutput output;
         ANY_SIGN(input, coin);
 
         ASSERT_EQ(hex(output.encoded()), ExpectedTx0);
     }
 
-    {   // more signatures
+    { // more signatures
         TW::IoTeX::Proto::SigningInput input;
         ASSERT_TRUE(input.ParseFromArray(inputData.data(), (int)inputData.size()));
         EXPECT_EQ(hex(PrivateKey(privateKey1).getPublicKey(TWPublicKeyTypeSECP256k1).bytes),
-                  hex(pubKey1));              
+                  hex(pubKey1));
         input.set_privatekey(prkey1.bytes.data(), prkey1.bytes.size());
         TW::IoTeX::Proto::SigningOutput output;
         ANY_SIGN(input, coin);
@@ -2938,8 +2951,9 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
             "Invalid public key");
     }
 
-    {   // Negative: not enough signatures
-        const Data outputData = TransactionCompiler::compileWithSignatures(coin, inputData, {}, {pbkey0.bytes});
+    { // Negative: not enough signatures
+        const Data outputData =
+            TransactionCompiler::compileWithSignatures(coin, inputData, {}, {pbkey0.bytes});
 
         TW::IoTeX::Proto::SigningOutput output;
         ASSERT_TRUE(output.ParseFromArray(outputData.data(), (int)outputData.size()));
@@ -2949,7 +2963,8 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
     }
 
     { // Negative: not enough publicKey
-        const Data outputData = TransactionCompiler::compileWithSignatures(coin, inputData, {signature}, {});
+        const Data outputData =
+            TransactionCompiler::compileWithSignatures(coin, inputData, {signature}, {});
 
         TW::IoTeX::Proto::SigningOutput output;
         ASSERT_TRUE(output.ParseFromArray(outputData.data(), (int)outputData.size()));
@@ -2959,7 +2974,8 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
     }
 
     { // Negative: not one to on
-        const Data outputData = TransactionCompiler::compileWithSignatures(coin, inputData, {signature}, {pbkey0.bytes, pbkey1.bytes});
+        const Data outputData = TransactionCompiler::compileWithSignatures(
+            coin, inputData, {signature}, {pbkey0.bytes, pbkey1.bytes});
 
         TW::IoTeX::Proto::SigningOutput output;
         ASSERT_TRUE(output.ParseFromArray(outputData.data(), (int)outputData.size()));
@@ -2967,5 +2983,4 @@ TEST(TransactionCompiler, IoTeXCompileWithSignatures) {
         EXPECT_EQ(output.error(), Common::Proto::Error_no_support_n2n);
         EXPECT_EQ(output.error_message(), "signatures and publickeys size can only be one");
     }
-
 }
