ee16e413537682dfa2713cc12cb9f973c9f2acaa
jasonchuang <rice8266@gmail.com>
2022-08-11 18:26:43 +0800

    chuange iotex proto file

--- tests/IoTeX/SignerTests.cpp
+++ tests/IoTeX/SignerTests.cpp
@@ -38,23 +38,24 @@ TEST(IoTeXSigner, Sign) {
 TEST(IoTeXSigner, Build) {
     auto input = Proto::SigningInput();
     input.set_version(1);
-    input.set_nonce(123);
-    input.set_gaslimit(888);
-    input.set_gasprice("999");
-    auto keyhex = parse_hex("0806c458b262edd333a191e92f561aff338211ee3e18ab315a074a2d82aa343f");
+    input.set_nonce(1);
+    input.set_gaslimit(10000);
+    input.set_gasprice("1000000000000");
+    input.set_chainid(2); //1 for miannet 2 for testnet
+    auto keyhex = parse_hex("cf0cee2e0bbdcf2f5d5f2f97de17ea16be53dae932cff20739109a8c03ae5a8b");
     input.set_privatekey(keyhex.data(), keyhex.size());
     auto tsf = input.mutable_transfer();
-    tsf->set_amount("456");
-    tsf->set_recipient("io187wzp08vnhjjpkydnr97qlh8kh0dpkkytfam8j");
-    auto text = parse_hex("68656c6c6f20776f726c6421"); // "hello world!"
+    tsf->set_amount("1");
+    tsf->set_recipient("io1yltnetj5pmuae7pxrdc66acag69gvxtjh2zkjq");
+    auto text = parse_hex(""); // "hello world!"
     tsf->set_payload(text.data(), text.size());
 
     auto signer = IoTeX::Signer(std::move(input));
     auto output = signer.build();
     auto encoded = output.encoded(); // signed action's serialized bytes
-    ASSERT_EQ(hex(encoded.begin(), encoded.end()), "0a4c0801107b18f8062203393939523e0a033435361229696f313837777a703038766e686a6a706b79646e723937716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c64211241044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d91a41555cc8af4181bf85c044c3201462eeeb95374f78aa48c67b87510ee63d5e502372e53082f03e9a11c1e351de539cedf85d8dff87de9d003cb9f92243541541a000");
+    ASSERT_EQ(hex(encoded.begin(), encoded.end()), "0a480801100118904e220d313030303030303030303030302802522e0a01311229696f31796c746e65746a35706d7561653770787264633636616361673639677678746a68327a6b6a7112410465491bf9caee6102a87b138cb443de8a88306bc4b0588b3dbb2f4b2d3e706eab142d165985fcbe60f040e7db6488b95b2a186c75632bbfdc7437b04d0eca96a01a4195837d804d919a6652e7bded5da2f3619df1401244acfdf71fb9e33386a62358083473a9d6e4ad84ced05dba46755e79168a46c2391a67d56dffc6c8ec43e6cc01");
     auto h = output.hash(); // signed action's hash
-    ASSERT_EQ(hex(h.begin(), h.end()), "6c84ac119058e859a015221f87a4e187c393d0c6ee283959342eac95fad08c33");
+    ASSERT_EQ(hex(h.begin(), h.end()), "778c1ef69688d4a10ee45e81e4b50d40472b3ac5d240e51fc8f823bdb962ff60");
 }
 
 TEST(IoTeXSigner, Compile) {
