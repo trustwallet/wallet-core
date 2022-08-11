8d7d9d0efa59a368ae358002fe7c0d75438ec8d7
jasonchuang <rice8266@gmail.com>
2022-08-11 13:27:50 +0800

    fix fmt

--- src/IoTeX/Entry.cpp
+++ src/IoTeX/Entry.cpp
@@ -16,13 +16,11 @@ using namespace std;
 
 // Note: avoid business logic from here, rather just call into classes like Address, Signer, etc.
 
-bool Entry::validateAddress(TWCoinType coin, const string& address, TW::byte, TW::byte,
-                            const char*) const {
+bool Entry::validateAddress(TWCoinType coin, const string& address, TW::byte, TW::byte, const char*) const {
     return Address::isValid(address);
 }
 
-string Entry::deriveAddress(TWCoinType coin, const PublicKey& publicKey, TW::byte,
-                            const char*) const {
+string Entry::deriveAddress(TWCoinType coin, const PublicKey& publicKey, TW::byte, const char*) const {
     return Address(publicKey).string();
 }
 
