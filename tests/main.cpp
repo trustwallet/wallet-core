da9f0f0907d88ec32e7a152c8871651995589609
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-08-21 18:48:59 +0900

    only use filesystem in tests target

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,24 +4,22 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
-#include <boost/algorithm/string.hpp>
+#include <filesystem>
 #include <gtest/gtest.h>
 #include <string>
 
 std::string TESTS_ROOT;
-std::string find_executable();
-
-void init_tests_root() {
-    const auto path = find_executable();
-    std::vector<std::string> components;
-    boost::split(components, path, boost::is_any_of("/"));
-    components.erase(components.end() - 3, components.end());
-    components.emplace_back("tests");
-    TESTS_ROOT = boost::join(components, "/");
-}
 
 int main(int argc, char** argv) {
-    init_tests_root();
+    // current path
+    auto path = std::filesystem::current_path();
+    // executable path
+    path.append(argv[0]);
+    // normalize
+    path = std::filesystem::canonical(path);
+    // root path
+    path = path.parent_path().parent_path().parent_path();
+    TESTS_ROOT = path.append("tests").string();
     ::testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
     return ret;
