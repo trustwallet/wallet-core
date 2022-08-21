2862a497889c2ddb84e3e716a139999929da747e
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-08-21 09:54:32 +0900

    Add TWNervosAddress test

--- tests/Nervos/AddressTests.cpp
+++ tests/Nervos/AddressTests.cpp
@@ -4,6 +4,7 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
+#include "HDWallet.h"
 #include "HexCoding.h"
 #include "Nervos/Address.h"
 #include "PrivateKey.h"
@@ -57,6 +58,31 @@ TEST(NervosAddress, FromString) {
                             "lvpfmaa582lu860dja5h0fk0v");
     ASSERT_EQ(address3.string(), "ckb1qjda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xw3394p3wg6"
                                  "p60qclvpfmaa582lu860dja5h0fk0v");
+}
+
+TEST(TWNervosAddress, AddressFromPublicKey) {
+    auto privateKey =
+        PrivateKey(parse_hex("8a2a726c44e46d1efaa0f9c2a8efed932f0e96d6050b914fde762ee285e61feb"));
+    auto publicKey = privateKey.getPublicKey(TWPublicKeyTypeSECP256k1);
+    ASSERT_EQ(publicKey.bytes.size(), 33ul);
+    auto address = Address(publicKey);
+    ASSERT_EQ(address.string(), "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9er"
+                                "g8furras980hksatlslfaktks7epf25");
+}
+
+TEST(NervosAddress, AddressFromString) {
+    auto address = Address("ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9erg8fur"
+                           "ras980hksatlslfaktks7epf25");
+    ASSERT_EQ(address.string(), "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9er"
+                                "g8furras980hksatlslfaktks7epf25");
+}
+
+TEST(NervosAddress, AddressFromWallet) {
+    auto hdWallet = HDWallet(
+        "alpha draw toss question picnic endless recycle wrong enable roast success palm", "");
+    auto addressString = hdWallet.deriveAddress(TWCoinTypeNervos);
+    ASSERT_EQ(addressString, "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9erg8f"
+                             "urras980hksatlslfaktks7epf25");
 }
 
 } // namespace TW::Nervos::tests
--- tests/Nervos/TWNervosAddressTests.cpp
+++ tests/Nervos/TWNervosAddressTests.cpp
@@ -4,38 +4,29 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
-#include "HDWallet.h"
 #include "HexCoding.h"
-#include "Nervos/Address.h"
 #include "PrivateKey.h"
+#include <TrustWalletCore/TWNervosAddress.h>
 
 #include "../interface/TWTestUtilities.h"
 #include <gtest/gtest.h>
 
-using namespace TW;
-using namespace TW::Nervos;
+namespace TW::Nervos::tests {
 
-TEST(TWNervosAddress, AddressFromPublicKey) {
-    auto privateKey =
-        PrivateKey(parse_hex("8a2a726c44e46d1efaa0f9c2a8efed932f0e96d6050b914fde762ee285e61feb"));
-    auto publicKey = privateKey.getPublicKey(TWPublicKeyTypeSECP256k1);
-    ASSERT_EQ(publicKey.bytes.size(), 33ul);
-    auto address = Address(publicKey);
-    ASSERT_EQ(address.string(), "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9er"
-                                "g8furras980hksatlslfaktks7epf25");
+const auto ckbAddress = "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9erg8furras980hksatlslfaktks7epf25";
+
+TEST(TWBitcoinAddress, Create) {
+    TWNervosAddress* addr = TWNervosAddressCreateWithString(STRING(ckbAddress).get());
+
+    const auto codeCash = WRAPD(TWNervosAddressCodeHash(addr));
+    const auto args = WRAPD(TWNervosAddressArgs(addr));
+    const auto hashType = WRAPS(TWNervosAddressHashType(addr));
+
+    assertHexEqual(codeCash, "9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8");
+    assertHexEqual(args, "c4b50c5c8d074f063ec0a77ded0eaff0fa7b65da");
+    assertStringsEqual(hashType, "type");
+
+    TWNervosAddressDelete(addr);
 }
 
-TEST(TWNervosAddress, AddressFromString) {
-    auto address = Address("ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9erg8fur"
-                           "ras980hksatlslfaktks7epf25");
-    ASSERT_EQ(address.string(), "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9er"
-                                "g8furras980hksatlslfaktks7epf25");
-}
-
-TEST(TWNervosAddress, AddressFromWallet) {
-    auto hdWallet = HDWallet(
-        "alpha draw toss question picnic endless recycle wrong enable roast success palm", "");
-    auto addressString = hdWallet.deriveAddress(TWCoinTypeNervos);
-    ASSERT_EQ(addressString, "ckb1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwyk5x9erg8f"
-                             "urras980hksatlslfaktks7epf25");
-}
+} // namespace TW::Nervos::tests
