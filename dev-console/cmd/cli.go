96f335f20965ce6cc3a7fa145e8e61088757a25f
Milerius <rmscastle@gmail.com>
2022-08-31 11:27:28 +0200

    feat(dev_console): add size function

--- dev-console/cmd/cli.go
+++ dev-console/cmd/cli.go
@@ -1,9 +1,13 @@
 package main
 
-import "dev-console/native"
+import (
+	"dev-console/native"
+	"fmt"
+)
 
 func main() {
 	s := native.NewTWString("Hey")
 	defer s.Delete()
 	println(s.String())
+	fmt.Printf("%d\n", s.Size())
 }
--- dev-console/native/twstring.go
+++ dev-console/native/twstring.go
@@ -12,30 +12,20 @@ type TWString struct {
 }
 
 func NewTWString(s string) TWString {
-	return TWString{s: TWStringCreateWithGoString(s)}
-}
-
-func (self TWString) Delete() {
-	TWStringDelete(self.s)
-}
-
-func (self TWString) String() string {
-	return TWStringGoString(self.s)
-}
-
-// C.TWString -> Go string
-func TWStringGoString(s unsafe.Pointer) string {
-	return C.GoString(C.TWStringUTF8Bytes(s))
-}
-
-// Go string -> C.TWString
-func TWStringCreateWithGoString(s string) unsafe.Pointer {
 	cStr := C.CString(s)
 	defer C.free(unsafe.Pointer(cStr))
 	str := C.TWStringCreateWithUTF8Bytes(cStr)
-	return str
+	return TWString{s: str}
 }
 
-func TWStringDelete(s unsafe.Pointer) {
-	C.TWStringDelete(s)
+func (self TWString) Delete() {
+	C.TWStringDelete(self.s)
+}
+
+func (self TWString) String() string {
+	return C.GoString(C.TWStringUTF8Bytes(self.s))
+}
+
+func (self TWString) Size() int64 {
+	return int64(C.size_t(C.TWStringSize(self.s)))
 }
