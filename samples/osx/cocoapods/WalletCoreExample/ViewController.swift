3d6ea1ec1a35ba61c7722e9fda29220bc58e1bcf
Adam V <13562139+catenocrypt@users.noreply.github.com>
2022-09-01 03:56:22 +0200

    Swift sample app: Use BigInt for Eth amount serialization (#2549)

--- samples/osx/cocoapods/WalletCoreExample/ViewController.swift
+++ samples/osx/cocoapods/WalletCoreExample/ViewController.swift
@@ -6,6 +6,7 @@
 
 import Cocoa
 import WalletCore
+import BigInt
 
 class ViewController: NSViewController {
 
@@ -27,13 +28,13 @@ class ViewController: NSViewController {
         let dummyReceiverAddress = "0xC37054b3b48C3317082E7ba872d7753D13da4986"
         let signerInput = EthereumSigningInput.with {
             $0.chainID = Data(hexString: "01")!
-            $0.gasPrice = Data(hexString: "d693a400")! // decimal 3600000000
-            $0.gasLimit = Data(hexString: "5208")! // decimal 21000
+            $0.gasPrice = BigInt(3600000000).magnitude.serialize()
+            $0.gasLimit = BigInt(21000).magnitude.serialize()
             $0.toAddress = dummyReceiverAddress
 
             $0.transaction = EthereumTransaction.with {
                 $0.transfer = EthereumTransaction.Transfer.with {
-                    $0.amount = Data(hexString: "0348bca5a16000")!
+                    $0.amount = BigInt(0.0009244*1000000000000000000).magnitude.serialize()
                 }
             }
             $0.privateKey = secretPrivateKeyEth.data
