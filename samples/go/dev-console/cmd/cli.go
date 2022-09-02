4b6c74326d63b4c629dc3bba3502a342da143240
Milerius <rmscastle@gmail.com>
2022-09-02 11:14:47 +0200

    feat(dev_console): clean main

--- samples/go/dev-console/cmd/cli.go
+++ samples/go/dev-console/cmd/cli.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"dev-console/cli"
-	prompt "github.com/c-bata/go-prompt"
+	"github.com/c-bata/go-prompt"
 )
 
 func main() {
@@ -12,13 +12,4 @@ func main() {
 		completer.Complete,
 	)
 	p.Run()
-	//s := native.NewTWString("Hey")
-	//defer s.Delete()
-	//println(s.String())
-	//fmt.Printf("%d\n", s.Size())
-	//fmt.Println(native.IsMnemonicValid("name dash bleak force moral disease shine response menu rescue more will"))
-	//wallet, _ := native.NewWalletWithMnemonic("name dash bleak force moral disease shine response menu rescue more will")
-	//fmt.Printf("seed: [%s]\n", wallet.Seed())
-	//fmt.Printf("mnemonic: [%s]\n", wallet.Mnemonic())
-	//defer wallet.Delete()
 }
