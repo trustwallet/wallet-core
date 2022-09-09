b7e8ed3e1fd548a5bd55925f4cc022535e705333
milerius <rmscastle@gmail.com>
2022-09-09 12:13:00 +0200

    improvements(boost): remove boost::variant usage in favor of std::variant

--- src/Keystore/EncryptionParameters.h
+++ src/Keystore/EncryptionParameters.h
@@ -12,7 +12,7 @@
 #include "Data.h"
 #include <TrustWalletCore/TWStoredKeyEncryptionLevel.h>
 
-#include <boost/variant.hpp>
+#include <variant>
 #include <nlohmann/json.hpp>
 #include <string>
 
@@ -40,12 +40,12 @@ struct EncryptionParameters {
     AESParameters cipherParams = AESParameters();
 
     /// Key derivation function parameters.
-    boost::variant<ScryptParameters, PBKDF2Parameters> kdfParams = ScryptParameters();
+    std::variant<ScryptParameters, PBKDF2Parameters> kdfParams = ScryptParameters();
 
     EncryptionParameters() = default;
 
     /// Initializes with standard values.
-    EncryptionParameters(AESParameters cipherParams, boost::variant<ScryptParameters, PBKDF2Parameters> kdfParams)
+    EncryptionParameters(AESParameters cipherParams, std::variant<ScryptParameters, PBKDF2Parameters> kdfParams)
         : cipherParams(std::move(cipherParams))
         , kdfParams(std::move(kdfParams)) {}
 
