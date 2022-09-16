fa33b8f1ed11ede15cae11f8b7c0f8a8d2ab55d3
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-09-16 11:41:36 +0200

    Devconsole.ts: Expose all WC namespaces

--- samples/typescript/devconsole.ts/src/index.ts
+++ samples/typescript/devconsole.ts/src/index.ts
@@ -7,6 +7,19 @@ const repl = require('node:repl');
 //const inquirer = require('inquirer');
 const { initWasm, TW } = require("@trustwallet/wallet-core");
 
+function enumerateNamespaces(topLevelNamespace: any) {
+    var ns : string[] = [];
+    for (var member in topLevelNamespace) {
+        if (typeof topLevelNamespace[member] == 'function') {
+            var firstLetter = member[0];
+            if (firstLetter >= 'A' && firstLetter <= 'Z') {
+                ns.push(member);
+            }
+        }
+    }
+    return ns;
+}
+
 (async function () {
     class AddressCoin {
         coin: any;
@@ -29,11 +42,11 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
             this.addDefaultCoins();
         }
         public static createRandom(name: string = '', strength: number = 256): TestWallet {
-            let wallet = HDWallet.create(strength, '');
+            let wallet = WC.HDWallet.create(strength, '');
             return new TestWallet(name, wallet);
         }
         public static createWithMnemonic(name: string = '', mnemonic: string): TestWallet {
-            let wallet = HDWallet.createWithMnemonic(mnemonic, '');
+            let wallet = WC.HDWallet.createWithMnemonic(mnemonic, '');
             return new TestWallet(name, wallet);
         }
         addCoin(coin: string): void {
@@ -41,9 +54,9 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
             this.addresses.push(new AddressCoin(coin, address));
         }
         addDefaultCoins(): void {
-            this.addCoin(CoinType.bitcoin);
-            this.addCoin(CoinType.ethereum);
-            this.addCoin(CoinType.binance);
+            this.addCoin(WC.CoinType.bitcoin);
+            this.addCoin(WC.CoinType.ethereum);
+            this.addCoin(WC.CoinType.binance);
         }
         status(): string {
             return `Wallet '${this.name}', with ${this.addresses.length} addresses`;
@@ -56,7 +69,7 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
             } else {
                 console.log('Addresses:');
                 this.addresses.forEach(element => {
-                    console.log(`  ${CoinTypeConfiguration.getSymbol(element.coin)}:  ${element.address}`);
+                    console.log(`  ${WC.CoinTypeConfiguration.getSymbol(element.coin)}:  ${element.address}`);
                 });
             }
         }
@@ -70,7 +83,7 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
         console.log("                                                 'wallet-core'.length");
         console.log("                                                 x = 5; ++x");
         console.log();
-        console.log('- Any method from Wallet-Core, through following namespaces:');
+        console.log('- Any method from Wallet-Core, through various namespaces, see full list below.  Some examples:');
         console.log("    CoinType                                     CoinType.bitcoin.value");
         console.log("    HDWallet                                     wallet = HDWallet.create(256, ''); wallet.mnemonic()");
         console.log("    PrivateKey                                   pk = PrivateKey.createWithData(HexCoding.decode('afeefca74d9a325cf1d6b6911d61a65c32afa8e02bd5e78e2e4ac2910bab45f5'))");
@@ -80,7 +93,6 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
         console.log("    CoinTypeConfiguration                        CoinTypeConfiguration.getSymbol(CoinType.bitcoin)");
         console.log("    Mnemonmic                                    Mnemonic.isValidWord('acid')");
         console.log("    HexCoding                                    HexCoding.encode(HexCoding.decode('01ab02'))");
-        console.log("    and some others... (Curve, PublicKeyType)");
         console.log();
         console.log("- Convenience methods:");
         console.log("    walletCreate([strength], [name])             walletCreate(128)");
@@ -98,6 +110,10 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
         console.log("    a1 = AnyAddress.createWithPublicKey(pubkey, coin)");
         console.log("    a1.description()");
         console.log();
+        console.log("Full list of exposed Wallet-core namespaces:");
+        enumerateNamespaces(WC).sort().forEach(ns => process.stdout.write(ns + ', '));
+        console.log();
+        console.log();
     }
 
     function exit(code: number): void {
@@ -112,7 +128,7 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
     }
 
     function walletImport(mnemonic: string, name: string = ''): any {
-        if (!Mnemonic.isValid(mnemonic)) {
+        if (!WC.Mnemonic.isValid(mnemonic)) {
             console.error(`Mnemonic is not valid ${mnemonic}`);
             return null;
         }
@@ -150,18 +166,7 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
     console.log();
 
     process.stdout.write("Initializing WalletCore library ...");
-    const {
-        CoinType,
-        HDWallet,
-        PrivateKey,
-        PublicKey,
-        AnyAddress,
-        CoinTypeConfiguration,
-        Mnemonic,
-        HexCoding,
-        Curve,
-        PublicKeyType,
-    } = await initWasm();
+    const WC = await initWasm();
     console.log(` done.`);
     console.log();
     console.log(`Type 'help()' for help`);
@@ -171,25 +176,19 @@ const { initWasm, TW } = require("@trustwallet/wallet-core");
 
     const local = repl.start('> ');
 
-    local.context.CoinType = CoinType;
-    local.context.HDWallet = HDWallet;
-    local.context.PrivateKey = PrivateKey;
-    local.context.PublicKey = PublicKey;
-    local.context.AnyAddress = AnyAddress;
-    local.context.CoinTypeConfiguration = CoinTypeConfiguration;
-    local.context.Mnemonic = Mnemonic;
-    local.context.HexCoding = HexCoding;
-    local.context.HexCoding = HexCoding;
-    local.context.Curve = Curve;
-    local.context.PublicKeyType = PublicKeyType;
+    // Expose WC namespaces
+    var nss = enumerateNamespaces(WC);
+    nss.forEach(ns => local.context[ns] = WC[ns]);
+    local.context.WC = WC;
 
+    // Expose more stuff; utilities
     local.context.help = help;
     local.context.exit = exit;
     local.context.walletCreate = walletCreate;
     local.context.walletImport = walletImport;
     local.context.walletDump = walletDump;
     local.context.wallet = wallet;
-    local.context.coin = CoinType.bitcoin;
+    local.context.coin = WC.CoinType.bitcoin;
 
     local.on('exit', () => {
         console.log('Bye!');
