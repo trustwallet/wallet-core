8760e5936b2ad3fe7b3d6ca499dfbab12c7ed861
Sztergbaum Roman <rmscastle@gmail.com>
2022-09-17 14:23:08 +0200

    [evmos]: authz tx unit tests (#2573)
    * feat(evmos): add a unit test of authz tx for evmos

--- tests/Evmos/SignerTests.cpp
+++ tests/Evmos/SignerTests.cpp
@@ -9,6 +9,7 @@
 #include "proto/Cosmos.pb.h"
 #include "Cosmos/Address.h"
 #include "Cosmos/Signer.h"
+#include "../interface/TWTestUtilities.h"
 
 #include <nlohmann/json.hpp>
 
@@ -87,4 +88,39 @@ TEST(EvmosSigner, SignTxJsonEthermintKeyType) {
     EXPECT_EQ(signatures, expectedSignatures);
 }
 
+TEST(EvmosSigner, CompoundingAuthz) {
+    // Successfully broadcasted https://www.mintscan.io/evmos/txs/8D811CEC078420C41220F0B584EA0AC037763380FAC31E0E352E4BB4D1D18B79
+    auto input = Proto::SigningInput();
+    input.set_signing_mode(Proto::Protobuf);
+    input.set_account_number(2139877);
+    input.set_chain_id("evmos_9001-2");
+    input.set_memo("");
+    input.set_sequence(3);
+
+    auto msg = input.add_messages();
+    auto& message = *msg->mutable_auth_grant();
+    message.set_granter("evmos12m9grgas60yk0kult076vxnsrqz8xpjy9rpf3e");
+    message.set_grantee("evmos18fzq4nac28gfma6gqfvkpwrgpm5ctar2z9mxf3");
+    auto& grant_stake = *message.mutable_grant_stake();
+    grant_stake.mutable_allow_list()->add_address("evmosvaloper1umk407eed7af6anvut6llg2zevnf0dn0feqqny");
+    grant_stake.set_authorization_type(TW::Cosmos::Proto::Message_AuthorizationType_DELEGATE);
+    message.set_expiration(1692309600);
+
+    auto& fee = *input.mutable_fee();
+    fee.set_gas(180859);
+    auto amountOfFee = fee.add_amounts();
+    amountOfFee->set_denom("aevmos");
+    amountOfFee->set_amount("4521475000000000");
+
+    auto privateKey = parse_hex("79bcbded1a5678ab34e6d9db9ad78e4e480e7b22723cc5fbf59e843732e1a8e5");
+    input.set_private_key(privateKey.data(), privateKey.size());
+
+    auto output = Signer::sign(input, TWCoinTypeNativeEvmos);
+    auto expected = R"(
+                {
+                    "mode":"BROADCAST_MODE_BLOCK",
+                    "tx_bytes":"CvUBCvIBCh4vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnR3JhbnQSzwEKLGV2bW9zMTJtOWdyZ2FzNjB5azBrdWx0MDc2dnhuc3Jxejh4cGp5OXJwZjNlEixldm1vczE4ZnpxNG5hYzI4Z2ZtYTZncWZ2a3B3cmdwbTVjdGFyMno5bXhmMxpxCmcKKi9jb3Ntb3Muc3Rha2luZy52MWJldGExLlN0YWtlQXV0aG9yaXphdGlvbhI5EjUKM2V2bW9zdmFsb3BlcjF1bWs0MDdlZWQ3YWY2YW52dXQ2bGxnMnpldm5mMGRuMGZlcXFueSABEgYI4LD6pgYSfQpZCk8KKC9ldGhlcm1pbnQuY3J5cHRvLnYxLmV0aHNlY3AyNTZrMS5QdWJLZXkSIwohA4B2WHbj6sH/GWE7z/YW5PRnXYFGaGRAov7gZZI2Fv2nEgQKAggBGAMSIAoaCgZhZXZtb3MSEDQ1MjE0NzUwMDAwMDAwMDAQ+4QLGkAm17CZgB7m+CPVlITnrHosklMTL9zrUeGRs8FL8N0GcRami9zdJ+e3xuXOtJmwP7G6QNh85CRYjFj8a8lpmmJM"
+                })";
+    assertJSONEqual(output.serialized(), expected);
+}
 }
