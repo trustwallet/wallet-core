a66464acb5db419aed27f6b19b6c406eb58939aa
nathanduft44 <nathan.duft@gmail.com>
2022-09-21 10:53:16 -0500

    finished derivation tests for Telos

--- tests/Telos/TWCoinTypeTests.cpp
+++ tests/Telos/TWCoinTypeTests.cpp
@@ -18,18 +18,20 @@ TEST(TWTelosCoinType, TWCoinType) {
     const auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(coin));
     const auto id = WRAPS(TWCoinTypeConfigurationGetID(coin));
     const auto name = WRAPS(TWCoinTypeConfigurationGetName(coin));
-    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("t123"));
+    const auto chainId = WRAPS(TWCoinTypeChainId(coin));
+    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0x9cd372081c6fcc8571a3f01084f22d246d7b1076c803e5dd3b2d53fc4d0ec585"));
     const auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(coin, txId.get()));
-    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("a12"));
+    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0x1dc07e303c994da351b4f7cb29fd38fe5727749f"));
     const auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(coin, accId.get()));
 
     assertStringsEqual(id, "telos");
     assertStringsEqual(name, "Telos");
     assertStringsEqual(symbol, "TELOS");
     ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(coin), 18);
-    ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainTelos);
+    ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainEthereum);
     ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 0x0);
     ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0x0);
-    assertStringsEqual(txUrl, "https://www.teloscan.io//tx/t123");
-    assertStringsEqual(accUrl, "https://www.teloscan.io//address/a12");
+    assertStringsEqual(chainId, "40");
+    assertStringsEqual(txUrl, "https://www.teloscan.io/tx/0x9cd372081c6fcc8571a3f01084f22d246d7b1076c803e5dd3b2d53fc4d0ec585");
+    assertStringsEqual(accUrl, "https://www.teloscan.io/address/0x1dc07e303c994da351b4f7cb29fd38fe5727749f");
 }
