23bbddac564c92220be175bbf0cfb852e756cdc2
Milerius <rmscastle@gmail.com>
2022-09-27 16:16:14 -0500

    feat(aptos): upgrade transaction payload tests to use amount

--- tests/Aptos/TransactionPayloadTests.cpp
+++ tests/Aptos/TransactionPayloadTests.cpp
@@ -14,7 +14,7 @@ TEST(AptosTransactionPayload, PayLoadBasis) {
     // strict equivalent of the following rust test
     // #[test]
     // fn test_payload() {
-    //    let from = AccountAddress::from_str("0xeeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b").unwrap();
+    //    let amount = 1000u64;
     //    let to = AccountAddress::from_str("0xeeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b").unwrap();
     //    let st = StructTag {
     //        address: AccountAddress::ONE,
@@ -28,29 +28,29 @@ TEST(AptosTransactionPayload, PayLoadBasis) {
     //        Identifier::new("transfer").unwrap(),
     //        vec![tag],
     //        vec![
-    //            bcs::to_bytes(&from).unwrap(),
     //            bcs::to_bytes(&to).unwrap(),
+    //            bcs::to_bytes(&amount).unwrap(),
     //        ],
     //    ));
     //    let val = hex::encode(bcs::to_bytes(&val).unwrap());
-    //    assert_eq!(val, "02000000000000000000000000000000000000000000000000000000000000000104636f696e087472616e73666572010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220eeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b20eeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b");
+    //    assert_eq!(val, "02000000000000000000000000000000000000000000000000000000000000000104636f696e087472616e73666572010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220eeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b08e803000000000000");
     //}
     ModuleId module(gAddressOne, "coin");
-    Address from("0xeeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b");
+    std::uint64_t amount{1000};
     Address to("0xeeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b");
     BCS::Serializer serializer;
-    serializer << from;
+    serializer << to;
     std::vector<Data> args;
     args.emplace_back(serializer.bytes);
     serializer.clear();
-    serializer << to;
+    serializer << amount;
     args.emplace_back(serializer.bytes);
     TransactionPayload payload = EntryFunction(module, "transfer", {gTransferTag}, args);
     ASSERT_EQ(std::get<EntryFunction>(payload).module().name(), "coin");
     ASSERT_EQ(std::get<EntryFunction>(payload).module().shortString(), "0x1::coin");
     serializer.clear();
     serializer << payload;
-    ASSERT_EQ(hex(serializer.bytes), "02000000000000000000000000000000000000000000000000000000000000000104636f696e087472616e73666572010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220eeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b20eeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b");
+    ASSERT_EQ(hex(serializer.bytes), "02000000000000000000000000000000000000000000000000000000000000000104636f696e087472616e73666572010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220eeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b08e803000000000000");
 }
 
 } // namespace TW::Aptos::tests
