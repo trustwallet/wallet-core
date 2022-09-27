987eabab9792f616ae50b07e7ef292896462da63
Milerius <rmscastle@gmail.com>
2022-09-27 05:50:01 -0500

    feat(aptos): use blockchain ID 43 for aptos, and remove unwanted comment

--- include/TrustWalletCore/TWBlockchain.h
+++ include/TrustWalletCore/TWBlockchain.h
@@ -54,7 +54,7 @@ enum TWBlockchain {
     TWBlockchainKusama = 40, // Polkadot
     TWBlockchainNervos = 41,
     TWBlockchainEverscale = 42,
-    TWBlockchainAptos = 44,  // TODO remove if the blockchain already exists, or just remove this comment if not
+    TWBlockchainAptos = 43, // Aptos
 };
 
 TW_EXTERN_C_END
