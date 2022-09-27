ff231c18315b1b2c869780f7406e2fa13053c5af
Milerius <rmscastle@gmail.com>
2022-09-27 07:53:57 -0500

    feat(aptos): clean test name

--- tests/Aptos/AddressTests.cpp
+++ tests/Aptos/AddressTests.cpp
@@ -46,7 +46,7 @@ TEST(AptosAddress, FromString) {
     ASSERT_EQ(address.string(), "0xeeff357ea5c1a4e7bc11b2b17ff2dc2dcca69750bfef1e1ebcaccf8c8018175b");
 }
 
-TEST(AptosAddress, shortString) {
+TEST(AptosAddress, ShortString) {
     ASSERT_EQ(gAddressOne.string(), "0x0000000000000000000000000000000000000000000000000000000000000001");
     ASSERT_EQ(gAddressOne.shortString(), "1");
 }
