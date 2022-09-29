2aef3781044e1e1bda2255e01e2b0a597b9b83ac
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-09-29 11:21:19 +0200

    Typescript test for Bitcoin signature and validation

--- wasm/tests/PublicKey.test.ts
+++ wasm/tests/PublicKey.test.ts
@@ -12,10 +12,10 @@ describe("PublicKey", () => {
   it("test sign, verify and recover", () => {
     const { PrivateKey, PublicKey, Curve, HexCoding } = globalThis.core;
     
-    const privKeyData = HexCoding.decode('afeefca74d9a325cf1d6b6911d61a65c32afa8e02bd5e78e2e4ac2910bab45f5');
+    const privKeyData = 'afeefca74d9a325cf1d6b6911d61a65c32afa8e02bd5e78e2e4ac2910bab45f5';
     const messageDigest = HexCoding.decode('0001020304050607080910111213141519171819202122232425262728293031');
 
-    const privkey = PrivateKey.createWithData(privKeyData);
+    const privkey = PrivateKey.createWithData(HexCoding.decode(privKeyData));
 
     // Sign the message
     const signature = privkey.sign(messageDigest, Curve.secp256k1);
