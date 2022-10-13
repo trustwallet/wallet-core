7feb515d3c60264dc127423cc581d622f2a63ae1
Milerius <rmscastle@gmail.com>
2022-10-13 10:54:23 +0200

    feat(any_address): add juno tests with AnyAddressData

--- tests/chains/Juno/TWAnyAddressTests.cpp
+++ tests/chains/Juno/TWAnyAddressTests.cpp
@@ -6,6 +6,7 @@
 
 #include <TrustWalletCore/TWAnyAddress.h>
 #include "HexCoding.h"
+#include "Hash.h"
 
 #include "TestUtilities.h"
 #include <gtest/gtest.h>
@@ -21,14 +22,20 @@ TEST(TWJunoAnyAddress, IsValid) {
 }
 
 TEST(TWJunoAnyAddress, createFromPubKeyJuno) {
+    const auto hrp = STRING("juno");
     const auto data = DATA("02753f5c275e1847ba4d2fd3df36ad00af2e165650b35fe3991e9c9c46f68b12bc");
     const auto pubkey = TWPublicKeyCreateWithData(data.get(), TWPublicKeyTypeSECP256k1);
-    const auto twAddress = TWAnyAddressCreateBech32WithPublicKey(pubkey, TWCoinTypeCosmos, STRING("juno").get());
-    TWPublicKeyDelete(pubkey);
+    const auto twAddress = TWAnyAddressCreateBech32WithPublicKey(pubkey, TWCoinTypeCosmos, hrp.get());
+    auto twData = TWAnyAddressData(twAddress);
+    auto hexData = hex(*reinterpret_cast<const Data*>(twData));
+    ASSERT_EQ(hexData, "c494c4cb388e23fe24a93158d6cd1fbdca8ebb73");
+    ASSERT_EQ(hex(Bech32Address("juno", TW::Hash::HasherSha256ripemd, pubkey->impl).getKeyHash()), hexData);
     auto address = TWAnyAddressDescription(twAddress);
     EXPECT_EQ("juno1cj2vfjec3c3luf9fx9vddnglhh9gawmncn4k5n", *reinterpret_cast<const std::string*>(address));
     TWStringDelete(address);
     TWAnyAddressDelete(twAddress);
+    TWDataDelete(twData);
+    TWPublicKeyDelete(pubkey);
 }
 
 TEST(TWJunoAnyAddress, createFromStringJuno) {
@@ -39,4 +46,5 @@ TEST(TWJunoAnyAddress, createFromStringJuno) {
     ASSERT_TRUE(TWAnyAddressIsValidBech32(addrDescription, TWCoinTypeCosmos, hrp.get()));
     TWStringDelete(addrDescription);
     TWAnyAddressDelete(anyAddr);
+
 }
