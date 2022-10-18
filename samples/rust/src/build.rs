fec7b9c660b912afe947813e38dec17b125143c2
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-10-18 10:21:16 +0200

    Linking with std lib C++ conditionally

--- samples/rust/src/build.rs
+++ samples/rust/src/build.rs
@@ -1,11 +1,16 @@
 static WALLET_CORE_PROJECT_DIR: &str = "../../build";
     // libs to link with
-    static LIBS: [&str; 4] = ["c++", "protobuf", "TrezorCrypto", "TrustWalletCore"];
+    static LIBS: [&str; 3] = ["protobuf", "TrezorCrypto", "TrustWalletCore"];
 
 fn main() {
     println!("cargo:rustc-link-search=native={}", WALLET_CORE_PROJECT_DIR);
     println!("cargo:rustc-link-search=native={}/trezor-crypto", WALLET_CORE_PROJECT_DIR);
 
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-lib=c++");
+    } else { // "linux", etc
+        println!("cargo:rustc-link-lib=stdc++");
+    }
     for i in 0..LIBS.len() {
         println!("cargo:rustc-link-lib={}", LIBS[i]);
     }
