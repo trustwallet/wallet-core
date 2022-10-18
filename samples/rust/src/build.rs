f55b1b3ce0904ced3e21385123fd7aa604df0332
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-10-18 10:36:08 +0200

    Try reorder of the libs

--- samples/rust/src/build.rs
+++ samples/rust/src/build.rs
@@ -6,12 +6,12 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", WALLET_CORE_PROJECT_DIR);
     println!("cargo:rustc-link-search=native={}/trezor-crypto", WALLET_CORE_PROJECT_DIR);
 
+    for i in 0..LIBS.len() {
+        println!("cargo:rustc-link-lib={}", LIBS[i]);
+    }
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=c++");
     } else { // "linux", etc
         println!("cargo:rustc-link-lib=stdc++");
-    }
-    for i in 0..LIBS.len() {
-        println!("cargo:rustc-link-lib={}", LIBS[i]);
     }
 }
