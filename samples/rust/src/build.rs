9dad827a7283892150628672731114b6df6286e5
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-10-18 13:18:24 +0200

    Codegen for proto files

--- samples/rust/src/build.rs
+++ samples/rust/src/build.rs
@@ -1,11 +1,26 @@
-static WALLET_CORE_PROJECT_DIR: &str = "../../build";
-    // libs to link with
-    static LIBS: [&str; 3] = ["TrustWalletCore", "TrezorCrypto", "protobuf"];
+use std::path::Path;
 
-fn main() {
+static WALLET_CORE_PROJECT_DIR: &str = "../../build";
+// libs to link with
+static LIBS: [&str; 3] = ["TrustWalletCore", "TrezorCrypto", "protobuf"];
+
+fn main() {        
+    let proto_src: &str = "../../src/proto";
+    protobuf_codegen::Codegen::new()
+        // Use `protoc` parser, optional.
+        //.protoc()
+        .protoc_path(Path::new("../../build/local/bin/protoc"))
+        .out_dir("src/proto")
+        .input(proto_src.to_string() + "/Bitcoin.proto")
+        .input(proto_src.to_string() + "/Ethereum.proto")
+        .include(proto_src)
+        .run()
+        .expect("Codegen failed.");
+
     println!("cargo:rustc-link-search=native={}", WALLET_CORE_PROJECT_DIR);
     println!("cargo:rustc-link-search=native={}/trezor-crypto", WALLET_CORE_PROJECT_DIR);
 
+    // Libraries; order matters
     for i in 0..LIBS.len() {
         println!("cargo:rustc-link-lib={}", LIBS[i]);
     }
