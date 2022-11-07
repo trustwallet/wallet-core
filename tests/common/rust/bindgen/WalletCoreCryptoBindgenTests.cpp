3452281192b36a666f9fa33c99922cbb104fca21
Milerius <rmscastle@gmail.com>
2022-11-07 11:11:29 +0100

    feat(bindgen_poc): continue basic poc

---
+++ tests/common/rust/bindgen/WalletCoreCryptoBindgenTests.cpp
@@ -0,0 +1,13 @@
+// Copyright © 2017-2022 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "rust/bindgen/WalletCoreCryptoBindgen.h"
+
+#include "gtest/gtest.h"
+
+TEST(RustBindgen, ExampleFunction) {
+    ASSERT_EQ(rust_function(), 42);
+}
