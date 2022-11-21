52323f86430130edf96de5125f7bbf0b6744a24b
Milerius <rmscastle@gmail.com>
2022-11-21 14:05:22 +0100

    feat(wasm): use javascript for random generation

--- wasm/src/Random.cpp
+++ wasm/src/Random.cpp
@@ -5,20 +5,90 @@
 // file LICENSE at the root of the source code distribution tree.
 //
 
+#ifdef __EMSCRIPTEN__
+# include <emscripten.h>
+#endif
+
 #include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <random>
-#include <sys/random.h>
+
+
+#ifdef __EMSCRIPTEN__
+
+static uint32_t
+javascript_random(void)
+{
+    return EM_ASM_INT_V({
+        return Module.getRandomValue();
+    });
+}
+
+static void
+javascript_stir(void)
+{
+    EM_ASM({
+        if (Module.getRandomValue === undefined) {
+            try {
+                var window_ = 'object' === typeof window ? window : self;
+                var crypto_ = typeof window_.crypto !== 'undefined' ? window_.crypto : window_.msCrypto;
+                var randomValuesStandard = function() {
+                    var buf = new Uint32Array(1);
+                    crypto_.getRandomValues(buf);
+                    return buf[0] >>> 0;
+                };
+                randomValuesStandard();
+                Module.getRandomValue = randomValuesStandard;
+            } catch (e) {
+                try {
+                    var crypto = require('crypto');
+                    var randomValueNodeJS = function() {
+                        var buf = crypto['randomBytes'](4);
+                        return (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3]) >>> 0;
+                    };
+                    randomValueNodeJS();
+                    Module.getRandomValue = randomValueNodeJS;
+                } catch (e) {
+                    throw 'No secure random number generator found';
+                }
+            }
+        }
+    });
+}
+
+
+static void
+randombytes_init_if_needed(void)
+{
+    static bool initialized = false;
+    if (!initialized) {
+        javascript_stir();
+        initialized = true;
+    }
+}
+
+static void
+javascript_buf(void * const buf, const size_t size)
+{
+    unsigned char *p = (unsigned char *) buf;
+    size_t         i;
+
+    for (i = (size_t) 0U; i < size; i++) {
+        p[i] = (unsigned char) javascript_random();
+    }
+}
+#endif
 
 extern "C" {
 uint32_t random32(void) {
-    std::mt19937 rng(std::random_device{}());
-    return rng();
+    randombytes_init_if_needed();
+    return javascript_random();
 }
 
 void random_buffer(uint8_t* buf, size_t len) {
-    getentropy(buf, len);
+    randombytes_init_if_needed();
+    javascript_buf(buf, len);
     return;
 }
 
