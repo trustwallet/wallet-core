1b9ef6b9c0045dd06b8e6a5d81a6c941868a4ab1
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-11-22 12:19:37 +0100

    Bitcoin Op_Return: Proper handling for len 76-80 bytes in two chunks

--- tests/chains/Bitcoin/BitcoinScriptTests.cpp
+++ tests/chains/Bitcoin/BitcoinScriptTests.cpp
@@ -306,18 +306,42 @@ TEST(BitcoinScript, OpReturn) {
     {
         Data data = parse_hex("00010203");
         Script script = Script::buildOpReturnScript(data);
+        EXPECT_EQ(script.bytes.size(), 2 + data.size());
         EXPECT_EQ(hex(script.bytes), "6a0400010203");
     }
     {
         Data data = parse_hex("535741503a54484f522e52554e453a74686f72317470657263616d6b6b7865633071306a6b366c74646e6c7176737732396775617038776d636c3a");
         Script script = Script::buildOpReturnScript(data);
+        EXPECT_EQ(script.bytes.size(), 2 + data.size());
         EXPECT_EQ(hex(script.bytes), "6a3b535741503a54484f522e52554e453a74686f72317470657263616d6b6b7865633071306a6b366c74646e6c7176737732396775617038776d636c3a");
     }
     {
         Data data = Data(69);
         data.push_back(0xab);
         Script script = Script::buildOpReturnScript(data);
+        EXPECT_EQ(script.bytes.size(), 2 + data.size());
         EXPECT_EQ(hex(script.bytes), "6a46000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ab");
+    }
+    {
+        Data data = Data(74);
+        data.push_back(0xab);
+        Script script = Script::buildOpReturnScript(data);
+        EXPECT_EQ(script.bytes.size(), 2 + data.size());
+        EXPECT_EQ(hex(script.bytes), 
+            "6a4b"
+            "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ab");
+    }
+    {
+        // >75 bytes, in 2 data pushes
+        Data data = Data(79);
+        data.push_back(0xab);
+        Script script = Script::buildOpReturnScript(data);
+        EXPECT_EQ(script.bytes.size(), 3 + data.size());  // 1 more byte for the extra data push
+        EXPECT_EQ(hex(script.bytes), 
+            "6a4b"
+            "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+            "05"
+            "00000000ab");
     }
 }
 
