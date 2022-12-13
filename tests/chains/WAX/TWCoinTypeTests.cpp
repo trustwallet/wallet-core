44d669da278dcc4869a63ae998fa99c4d2f63d5a
Vladimir Miloserdov <milosvova@gmail.com>
2022-12-13 11:14:16 +0000

    Fix TWCoinTypeTests decimals value

--- tests/chains/WAX/TWCoinTypeTests.cpp
+++ tests/chains/WAX/TWCoinTypeTests.cpp
@@ -18,7 +18,7 @@ TEST(TWWAXCoinType, TWCoinType) {
     auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeWAX));
     auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeWAX));
 
-    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeWAX), 4);
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeWAX), 8);
     ASSERT_EQ(TWBlockchainEOS, TWCoinTypeBlockchain(TWCoinTypeWAX));
     ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeWAX));
     ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeWAX));
