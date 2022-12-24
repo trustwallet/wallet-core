0f1f283b44186d877537f25ca665e5b5ef724557
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2022-12-24 09:01:15 +0530

    add coin address derivation tests

--- tests/common/CoinAddressDerivationTests.cpp
+++ tests/common/CoinAddressDerivationTests.cpp
@@ -258,6 +258,9 @@ TEST(Coin, DeriveAddress) {
         case TWCoinTypeSecret:
             EXPECT_EQ(address, "secret1hkfq3zahaqkkzx5mjnamwjsfpq2jk7z0m7t23a");
             break;
+        case TWCoinTypeAgoric:
+            EXPECT_EQ(address, "agoric18zvvgk6j3eq5wd7mqxccgt20gz2w94cy88aek5");
+            break;
             // no default branch here, intentionally, to better notice any missing coins
         }
     }
