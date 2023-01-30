7ba8fc1f1cc3b1fdab44caa8b28f0feebdf9ff13
Ruslan Serebriakov <rsrbk1@gmail.com>
2023-01-30 14:00:50 +0000

    Update TestTHORSwapSigning.kt

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/thorchain/TestTHORSwapSigning.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/thorchain/TestTHORSwapSigning.kt
@@ -48,7 +48,7 @@ class TestTHORChainSwap {
 
         // invoke swap
         val outputData = buildSwap(inputSerialized)
-        assertEquals(outputData.count(), 192)
+        assertEquals(outputData.count(), 193)
 
         // parse result in proto
         val outputProto = THORChainSwap.SwapOutput.newBuilder().mergeFrom(outputData)
