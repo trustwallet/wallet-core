00790e5f0f961e349add5ba8d4e58337f3cebc94
Maxim Pestryakov <maxim.pestryakov@binance.com>
2023-02-10 14:38:47 +0800

    Added licence headers

--- wasm/src/enum-ext.d.ts
+++ wasm/src/enum-ext.d.ts
@@ -1,3 +1,9 @@
+// Copyright © 2017-2023 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
 export class BitcoinSigHashTypeExt {
     static isSingle(type: BitcoinSigHashType): boolean;
     static isNone(type: BitcoinSigHashType): boolean;
