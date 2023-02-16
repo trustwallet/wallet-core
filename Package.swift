6804af75da862c5b8fae139e84395ea3e13a4f9a
Alvaro <alvarogharo@gmail.com>
2023-02-16 15:53:54 +0100

    Reedited swift package library names

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCoreCommon",
-            url: "https://github.com/GeoDB-Limited/wallet-core-odin/releases/download/3.0.5/WalletCoreCommon.xcframework.zip",
+            url: "https://github.com/GeoDB-Limited/wallet-core-odin/releases/download/3.0.5/WalletCore.xcframework.zip",
             checksum: "72cb25c552167a29912eec44768be0de40f6e063a5afe1a703c926e91c522df2"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.9.5/SwiftProtobuf.xcframework.zip",
+            url: "https://github.com/GeoDB-Limited/wallet-core-odin/releases/download/3.0.5/SwiftProtobuf.xcframework.zip",
             checksum: "7903f5e9487db4764dc57be000c384a0619a96c275993711f3a5a858d3c865bd"
         )
     ]
