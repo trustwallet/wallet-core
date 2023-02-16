d9a00705035ce13615f807b8762ae9fcb8596aa1
Alvaro <alvarogharo@gmail.com>
2023-02-16 14:03:29 +0100

    Edited swift package
    Conflicts:
    	Package.swift

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ let package = Package(
     name: "WalletCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "WalletCore", targets: ["WalletCore"]),
+        .library(name: "WalletCore", targets: ["WalletCoreCommon"]),
         .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "WalletCore",
+            name: "WalletCoreCommon",
             url: "https://github.com/GeoDB-Limited/wallet-core-odin/releases/download/3.0.5/WalletCoreCommon.xcframework.zip",
+            checksum: "72cb25c552167a29912eec44768be0de40f6e063a5afe1a703c926e91c522df2"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/3.2.1/SwiftProtobuf.xcframework.zip",
-            checksum: "12bbc92dc2225c661e301cc2826e034fcb69b2a144f8c4ff2e51ce6ccf122124"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.9.5/SwiftProtobuf.xcframework.zip",
+            checksum: "7903f5e9487db4764dc57be000c384a0619a96c275993711f3a5a858d3c865bd"
         )
     ]
 )
