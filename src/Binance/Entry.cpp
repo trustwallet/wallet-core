302394eb9af3ba524d516909eccca5d00432f667
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-03-13 15:27:48 +0000

    Fix after review.

--- src/Binance/Entry.cpp
+++ src/Binance/Entry.cpp
@@ -17,13 +17,12 @@ bool Entry::validateAddress([[maybe_unused]] TWCoinType coin, const std::string&
 }
 
 std::string Entry::deriveAddress([[maybe_unused]] TWCoinType coin, const PublicKey& publicKey, [[maybe_unused]] TWDerivation derivation, [[maybe_unused]] const PrefixVariant& addressPrefix) const {
-    if (std::holds_alternative<Bech32Prefix>(addressPrefix)) {
-        const std::string hrp = std::get<Bech32Prefix>(addressPrefix);
-        if (!hrp.empty()) {
-            return Address(publicKey, hrp).string();
+    if (const auto* hrp = std::get_if<Bech32Prefix>(&addressPrefix); hrp) {
+        if (hrpForString(*hrp) != TWHRPUnknown) {
+            return Address(publicKey, *hrp).string();
         }
     }
-    
+
     return Address(publicKey).string();
 }
 
