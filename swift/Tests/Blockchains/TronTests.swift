d67078daa580b37063c97be66a625aaee9664882
Sztergbaum Roman <rmscastle@gmail.com>
2023-03-15 13:11:48 +0100

    [Tron]: Signing message  (#2999)

--- swift/Tests/Blockchains/TronTests.swift
+++ swift/Tests/Blockchains/TronTests.swift
@@ -64,4 +64,13 @@ class TronTests: XCTestCase {
         """
         XCTAssertJSONEqual(output.json, expectedJSON)
     }
+    
+    func testMessageAndVerifySigner() {
+        let privateKey = PrivateKey(data: Data(hexString: "75065f100e38d3f3b4c5c4235834ba8216de62272a4f03532c44b31a5734360a")!)!
+        let msg = "Hello World"
+        let signature = TronMessageSigner.signMessage(privateKey: privateKey, message: msg)
+        XCTAssertEqual(signature, "9bb6d11ec8a6a3fb686a8f55b123e7ec4e9746a26157f6f9e854dd72f5683b450397a7b0a9653865658de8f9243f877539882891bad30c7286c3bf5622b900471b")
+        let pubKey = privateKey.getPublicKey(coinType: .tron)
+        XCTAssertTrue(TronMessageSigner.verifyMessage(pubKey: pubKey, message: msg, signature: signature))
+    }
 }
