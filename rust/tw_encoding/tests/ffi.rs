28d7311515c60b3d0f6b6a3cca77293bf373632c
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-03-17 18:43:40 +0100

    Further improvements

--- rust/tw_encoding/tests/ffi.rs
+++
@@ -1,8 +0,0 @@
-#[path = "ffi/base32_tests.rs"]
-mod base32_tests;
-#[path = "ffi/base58_tests.rs"]
-mod base58_tests;
-#[path = "ffi/base64_tests.rs"]
-mod base64_tests;
-#[path = "ffi/hex_tests.rs"]
-mod hex;
