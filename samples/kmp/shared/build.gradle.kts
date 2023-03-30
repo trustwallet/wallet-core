2ab05c0b6800750a9b7e8e0dbf6eb3a70e51fc6f
Milerius <rmscastle@gmail.com>
2023-03-30 09:35:32 +0200

    feat(zksync): upgrade to wallet core kotlin 3.1.22

--- samples/kmp/shared/build.gradle.kts
+++ samples/kmp/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.trustwallet:wallet-core-kotlin:3.1.20")
+                implementation("com.trustwallet:wallet-core-kotlin:3.1.22")
             }
         }
         val commonTest by getting {
