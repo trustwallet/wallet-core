c8028b84cb7955e86f2264ce028c5c5be11be483
lamafab <42901763+lamafab@users.noreply.github.com>
2023-04-05 12:32:37 +0000

    track test module

---
+++ codegen-v2/src/tests/mod.rs
@@ -0,0 +1,31 @@
+use crate::{ParseTree, Driver, GType, GSeparator};
+
+#[test]
+fn test_separator() {
+	let driver = Driver::from(" ");
+	let x = GSeparator::derive(driver).unwrap();
+	dbg!(x);
+
+	let driver = Driver::from("  ");
+	let x = GSeparator::derive(driver).unwrap();
+	dbg!(x);
+
+	let driver = Driver::from(" \n  \t");
+	let x = GSeparator::derive(driver).unwrap();
+	dbg!(x);
+}
+
+#[test]
+fn test_types() {
+	let driver = Driver::from("int\n");
+	let der = GType::derive(driver).unwrap();
+	dbg!(der.derived, GType::Int);
+
+	let driver = Driver::from("bool\n");
+	let der = GType::derive(driver).unwrap();
+	dbg!(der.derived, GType::Bool);
+
+	let driver = Driver::from("char\n");
+	let der = GType::derive(driver).unwrap();
+	dbg!(der.derived, GType::Char);
+}
