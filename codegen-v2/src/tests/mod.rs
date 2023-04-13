45db3d052fadeb676f25facd060ffc0e21092613
lamafab <42901763+lamafab@users.noreply.github.com>
2023-04-13 10:59:31 +0000

    update in mod.rs

--- codegen-v2/src/tests/mod.rs
+++ codegen-v2/src/tests/mod.rs
@@ -36,64 +36,29 @@ macro_rules! must_err {
 
 #[test]
 fn test_non_alphanumeric_items() {
-    let driver = Reader::from("a");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_err());
+    // OK!
+    must_ok!(GNonAlphanumericItem, " ");
+    must_ok!(GNonAlphanumericItem, ",");
 
-    let driver = Reader::from("1");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_err());
-
-    let driver = Reader::from("-");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_err());
-
-    let driver = Reader::from("_");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_err());
-
+    // ERR!
+    must_err!(GNonAlphanumericItem, "a");
+    must_err!(GNonAlphanumericItem, "1");
+    must_err!(GNonAlphanumericItem, "-");
+    must_err!(GNonAlphanumericItem, "_");
     // Does not handle EOF.
-    let driver = Reader::from("");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_err());
-
-    // OK!
-    let driver = Reader::from(" ");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_ok());
-
-    // OK!
-    let driver = Reader::from(",");
-    let res = GNonAlphanumericItem::derive(driver);
-    assert!(res.is_ok());
+    must_err!(GNonAlphanumericItem, "");
 }
 
 #[test]
 fn test_non_alphanumeric() {
-    let driver = Reader::from(",,, ");
-    let res = GNonAlphanumeric::derive(driver);
-    assert!(res.is_ok());
-
-    let driver = Reader::from(",,,,");
-    let res = GNonAlphanumeric::derive(driver);
-    assert!(res.is_ok());
+    must_ok!(GNonAlphanumeric, ",,, ");
+    must_ok!(GNonAlphanumeric, ",,,,");
 
     // ERR!
-    let driver = Reader::from(",,,,a");
-    let res = GNonAlphanumeric::derive(driver);
-    assert!(res.is_ok());
+    must_err!(GNonAlphanumeric, ",,,a");
 }
 
 #[test]
 fn test_eof() {
-    let driver = Reader::from("");
-    let res = GEof::derive(driver).unwrap();
-    assert_eq!(res.derived, GEof);
-}
-
-#[test]
-fn test_header_include() {
-    let driver = Reader::from("#include \"some_file.h\"");
-    let res = GHeaderInclude::derive(driver).unwrap();
-    dbg!(res);
+    must_ok!(GEof, "");
 }
