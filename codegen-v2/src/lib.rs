0aa422fb302e655dea2ad4eea44470b5a970b1dc
lamafab <42901763+lamafab@users.noreply.github.com>
2023-04-13 13:09:34 +0000

    complete parse_file and parse_dir impl

--- codegen-v2/src/lib.rs
+++ codegen-v2/src/lib.rs
@@ -5,6 +5,7 @@ use grammar::{GHeaderFileItem, ParseTree};
 use reader::Reader;
 use std::{
     collections::HashMap,
+    fmt::format,
     path::{Path, PathBuf},
 };
 
@@ -21,11 +22,33 @@ pub enum Error {
     Eof,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct CHeaderDirectory {
     map: HashMap<PathBuf, Vec<GHeaderFileItem>>,
 }
 
+pub fn parse_dir(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderDirectory> {
+    let entries = std::fs::read_dir(path).unwrap();
+    for entry in entries {
+        let child_path = entry.unwrap().path();
+
+        if child_path.is_dir() {
+            main_dir = parse_dir(&child_path, main_dir)?;
+        } else if child_path.is_file() {
+            main_dir = parse_file(&child_path, main_dir)?;
+        } else {
+            panic!()
+        }
+    }
+
+    Ok(main_dir)
+}
+
 pub fn parse_file(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderDirectory> {
+    if !path.is_file() {
+        return Err(Error::Todo);
+    }
+
     let parent = path.parent().unwrap();
 
     let content = std::fs::read_to_string(path).map_err(|_err| Error::Todo)?;
@@ -42,12 +65,17 @@ pub fn parse_file(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeader
             reader = pending.merge(branch);
 
             if let GHeaderFileItem::HeaderInclude(include) = &derived {
-                let path = parent.join(Path::new(&include.0));
+                let child_path = parent.join(Path::new(&include.0));
+
+                // Skip referencing includes.
+                if child_path == path {
+                    continue;
+                }
 
                 // If the `#include`d file is not yet parsed, parse it.
                 // Otherwise, skip it.
-                if !main_dir.map.contains_key(&path) {
-                    main_dir = parse_file(&path, main_dir)?;
+                if !main_dir.map.contains_key(&child_path) {
+                    main_dir = parse_file(&child_path, main_dir)?;
                 }
             }
 
@@ -71,7 +99,13 @@ pub fn parse_file(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeader
 #[test]
 #[ignore]
 fn test_parse_file() {
-    //let res = parse_file("../include/TrustWalletCore/TWBase.h").unwrap();
-    //let string = serde_json::to_string_pretty(&res).unwrap();
-    //println!("{}", string);
+    let path = Path::new("../include/");
+
+    let dir = CHeaderDirectory {
+        map: HashMap::new(),
+    };
+
+    let dir = parse_dir(path, dir).unwrap();
+    let string = serde_json::to_string_pretty(&dir).unwrap();
+    println!("{}", string);
 }
