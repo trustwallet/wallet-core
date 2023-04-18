edd33b577b5b2111d37d93bc8a32719b5d8bbefd
lamafab <42901763+lamafab@users.noreply.github.com>
2023-04-18 12:45:00 +0000

    add minimal main() method

--- codegen-v2/README.md
+++ codegen-v2/README.md
@@ -3,11 +3,12 @@
 This is a _work-in-progress_ parser meant to deprecate the existing Ruby parser
 in `codegen/`. This project will progess over multiple stages (PRs).
 
-- [x] Write a parser with full coverage of the C headers in `include/` ([#3065](https://github.com/trustwallet/wallet-core/pull/3065)).
+- [x] Write a (minimal) parser with full coverage of the C headers in `include/` ([#3065](https://github.com/trustwallet/wallet-core/pull/3065)).
 - [ ] Implement a templating engine, write easy to read templates.
 	- [ ] Start with Swift codegen.
 	- [ ] Complete codegen for remaining languages.
 - [ ] Extend the parser to handle all valid C header syntax.
+	- [ ] Add explicit error types (right now it's just `Error::Todo`).
 	- [ ] Seperate parser logic from C header grammer (no refactoring required, just reorganization).
 - [ ] Update the entire building system.
 
--- codegen-v2/src/lib.rs
+++ codegen-v2/src/lib.rs
@@ -23,13 +23,22 @@ pub enum Error {
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct CHeaderDirectory {
-    map: HashMap<PathBuf, Vec<GHeaderFileItem>>,
+    pub map: HashMap<PathBuf, Vec<GHeaderFileItem>>,
 }
 
-pub fn parse_dir(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderDirectory> {
-    let entries = std::fs::read_dir(path).unwrap();
+pub fn parse(path: &Path) -> Result<CHeaderDirectory> {
+    let dir = CHeaderDirectory {
+        map: HashMap::new(),
+    };
+
+    let dir = parse_dir(path, dir)?;
+    Ok(dir)
+}
+
+fn parse_dir(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderDirectory> {
+    let entries = std::fs::read_dir(path).map_err(|_| Error::Todo)?;
     for entry in entries {
-        let child_path = entry.unwrap().path();
+        let child_path = entry.map_err(|_| Error::Todo)?.path();
 
         if child_path.is_dir() {
             main_dir = parse_dir(&child_path, main_dir)?;
@@ -43,12 +52,12 @@ pub fn parse_dir(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderD
     Ok(main_dir)
 }
 
-pub fn parse_file(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderDirectory> {
+fn parse_file(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeaderDirectory> {
     if !path.is_file() {
         return Err(Error::Todo);
     }
 
-    let parent = path.parent().unwrap();
+    let parent = path.parent().ok_or(Error::Todo)?;
 
     let content = std::fs::read_to_string(path).map_err(|_err| Error::Todo)?;
     // TODO: Find a better solution for this.
@@ -97,18 +106,11 @@ pub fn parse_file(path: &Path, mut main_dir: CHeaderDirectory) -> Result<CHeader
 
 #[test]
 #[ignore]
-fn test_parse_file() {
+fn test_parse_with_output() {
     let path = Path::new("../include/");
-
-    let dir = CHeaderDirectory {
-        map: HashMap::new(),
-    };
-
-    let dir = parse_dir(path, dir).unwrap();
-    //let string = serde_json::to_string_pretty(&dir).unwrap();
+    let dir = parse(&path).unwrap();
 
     println!("UNRECOGNIZED items:");
-
     for (path, items) in dir.map {
         println!("> ## FILE: {}", path.to_str().unwrap());
         for item in items {
