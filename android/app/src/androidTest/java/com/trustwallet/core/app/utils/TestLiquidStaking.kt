7bdc7d1ebb133794c5411945bd902c480df6d5e1
Milerius <rmscastle@gmail.com>
2023-04-21 09:12:49 +0200

    feat(liquid_staking): license and year update

--- android/app/src/androidTest/java/com/trustwallet/core/app/utils/TestLiquidStaking.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/utils/TestLiquidStaking.kt
@@ -1,3 +1,9 @@
+// Copyright © 2017-2023 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
 package com.trustwallet.core.app.utils
 
 import com.google.protobuf.ByteString
@@ -48,7 +54,7 @@ class TestLiquidStaking {
             gasLimit = ByteString.copyFrom("0x01c520".toHexByteArray())
             privateKey = ByteString.copyFrom(PrivateKey("0x4a160b803c4392ea54865d0c5286846e7ad5e68fbd78880547697472b22ea7ab".toHexByteArray()).data())
         }.build()
-        
+
         val output = AnySigner.sign(txInputFull, CoinType.ETHEREUM, Ethereum.SigningOutput.parser())
 
         assertEquals(Numeric.toHexString(output.encoded.toByteArray()), "0x02f87a81890185085e42c7c0858fbcc8fcd88301c52094fd225c9e6601c9d38d8f98d8731bf59efcf8c0e3880de0b6b3a764000084c78cf1a0c001a04bcf92394d53d4908130cc6d4f7b2491967f9d6c59292b84c1f56adc49f6c458a073e09f45d64078c41a7946ffdb1dee8e604eb76f318088490f8f661bb7ddfc54")
