4f4d82c00187823b8faca937c669aec301428422
lamafab <42901763+lamafab@users.noreply.github.com>
2023-05-10 12:19:18 +0000

    move out inner loop logic of process_methods

--- codegen-v2/src/codegen/swift/functions.rs
+++ codegen-v2/src/codegen/swift/functions.rs
@@ -22,136 +22,149 @@ pub(super) fn process_methods(
     let mut skipped_funcs = vec![];
 
     for func in functions {
-        if !func.name.starts_with(object.name()) {
-            // Function is not assciated with the object.
-            skipped_funcs.push(func);
-            continue;
-        }
-
-        let mut ops = vec![];
-
-        // Initalize the 'self' type, which is then passed on to the underlying
-        // C FFI function, assuming the function is not static.
-        //
-        // E.g:
-        // - `let obj = self.rawValue`
-        // - `let obj = TWSomeEnum(rawValue: self.RawValue")`
-        if !func.is_static {
-            ops.push(match object {
-                ObjectVariant::Struct(_) => SwiftOperation::Call {
-                    var_name: "obj".to_string(),
-                    call: "self.rawValue".to_string(),
-                    defer: None,
-                },
-                ObjectVariant::Enum(name) => SwiftOperation::Call {
-                    var_name: "obj".to_string(),
-                    call: format!("{}(rawValue: self.rawValue)", name),
-                    defer: None,
-                },
-            });
-        }
-
-        // For each parameter, we track a list of `params` which is used for the
-        // function interface and add the necessary operations on how to process
-        // those parameters.
-        let mut params = vec![];
-        for param in func.params {
-            // Skip self parameter
-            match &param.ty.variant {
-                TypeVariant::Enum(name) | TypeVariant::Struct(name) if name == object.name() => {
-                    continue
-                }
-                _ => {}
+        match do_process_methods(object, &func)? {
+            Some(f) => {
+                swift_funcs.push(f);
             }
-
-            // Convert parameter to Swift parameter for the function interface.
-            params.push(SwiftParam {
-                name: param.name.clone(),
-                param_type: SwiftType::from(param.ty.variant.clone()),
-                is_nullable: param.ty.is_nullable,
-            });
-
-            // Process parameter.
-            if let Some(op) = param_c_ffi_call(&param) {
-                ops.push(op)
+            None => {
+                skipped_funcs.push(func);
             }
         }
-
-        // Prepepare parameter list to be passed on to the underlying C FFI function.
-        let param_name = if func.is_static { vec![] } else { vec!["obj"] };
-        let param_names = param_name
-            .into_iter()
-            .chain(params.iter().map(|p| p.name.as_str()))
-            .collect::<Vec<&str>>()
-            .join(",");
-
-        // Call the underlying C FFI function, passing on the parameter list.
-        let (var_name, call) = (
-            "result".to_string(),
-            format!("{}({})", func.name, param_names),
-        );
-        if func.return_type.is_nullable {
-            ops.push(SwiftOperation::GuardedCall { var_name, call });
-        } else {
-            ops.push(SwiftOperation::Call {
-                var_name,
-                call,
-                defer: None,
-            });
-        }
-
-        // Wrap result.
-        ops.push(wrap_return(&func.return_type));
-
-        // Convert return type for function interface.
-        let return_type = SwiftReturn {
-            param_type: SwiftType::from(func.return_type.variant),
-            is_nullable: func.return_type.is_nullable,
-        };
-
-        // Prettify name, remove object name prefix from this property.
-        let pretty_name = func
-            .name
-            .strip_prefix(object.name())
-            // Panicing implies bug, checked at the start of the loop.
-            .unwrap()
-            .to_lower_camel_case();
-
-        // Special handling: some functions do not follow standard camelCase
-        // convention.
-        #[rustfmt::skip]
-        let pretty_name = if object.name() == "TWStoredKey" {
-            pretty_name
-                .replace("Json", "JSON")
-                .replace("Hd", "HD")
-        } else if object.name() == "TWPublicKey" {
-            pretty_name
-                .replace("Der", "DER")
-        } else if object.name() == "TWHash" {
-            pretty_name
-                .replace("ripemd", "RIPEMD")
-                .replace("Ripemd", "RIPEMD")
-                .replace("sha512256", "sha512_256")
-                .replace("sha3256", "sha3_256")
-                .replace("sha256sha256", "sha256SHA256")
-        } else if object.name() == "TWAES" {
-            pretty_name
-                .replace("Cbc", "CBC")
-                .replace("Ctr", "CTR")
-        } else {
-            pretty_name
-        };
-
-        swift_funcs.push(SwiftFunction {
-            name: pretty_name,
-            is_public: func.is_public,
-            is_static: func.is_static,
-            operations: ops,
-            params,
-            return_type,
-            comments: vec![],
-        });
     }
 
     Ok((swift_funcs, skipped_funcs))
+}
+
+fn do_process_methods(
+    object: &ObjectVariant,
+    func: &FunctionInfo,
+) -> Result<Option<SwiftFunction>> {
+    if !func.name.starts_with(object.name()) {
+        // Function is not assciated with the object.
+        return Ok(None);
+    }
+
+    let mut ops = vec![];
+
+    // Initalize the 'self' type, which is then passed on to the underlying
+    // C FFI function, assuming the function is not static.
+    //
+    // E.g:
+    // - `let obj = self.rawValue`
+    // - `let obj = TWSomeEnum(rawValue: self.RawValue")`
+    if !func.is_static {
+        ops.push(match object {
+            ObjectVariant::Struct(_) => SwiftOperation::Call {
+                var_name: "obj".to_string(),
+                call: "self.rawValue".to_string(),
+                defer: None,
+            },
+            ObjectVariant::Enum(name) => SwiftOperation::Call {
+                var_name: "obj".to_string(),
+                call: format!("{}(rawValue: self.rawValue)", name),
+                defer: None,
+            },
+        });
+    }
+
+    // For each parameter, we track a list of `params` which is used for the
+    // function interface and add the necessary operations on how to process
+    // those parameters.
+    let mut params = vec![];
+    for param in &func.params {
+        // Skip self parameter
+        match &param.ty.variant {
+            TypeVariant::Enum(name) | TypeVariant::Struct(name) if name == object.name() => {
+                continue
+            }
+            _ => {}
+        }
+
+        // Convert parameter to Swift parameter for the function interface.
+        params.push(SwiftParam {
+            name: param.name.clone(),
+            param_type: SwiftType::from(param.ty.variant.clone()),
+            is_nullable: param.ty.is_nullable,
+        });
+
+        // Process parameter.
+        if let Some(op) = param_c_ffi_call(&param) {
+            ops.push(op)
+        }
+    }
+
+    // Prepepare parameter list to be passed on to the underlying C FFI function.
+    let param_name = if func.is_static { vec![] } else { vec!["obj"] };
+    let param_names = param_name
+        .into_iter()
+        .chain(params.iter().map(|p| p.name.as_str()))
+        .collect::<Vec<&str>>()
+        .join(",");
+
+    // Call the underlying C FFI function, passing on the parameter list.
+    let (var_name, call) = (
+        "result".to_string(),
+        format!("{}({})", func.name, param_names),
+    );
+    if func.return_type.is_nullable {
+        ops.push(SwiftOperation::GuardedCall { var_name, call });
+    } else {
+        ops.push(SwiftOperation::Call {
+            var_name,
+            call,
+            defer: None,
+        });
+    }
+
+    // Wrap result.
+    ops.push(wrap_return(&func.return_type));
+
+    // Convert return type for function interface.
+    let return_type = SwiftReturn {
+        param_type: SwiftType::from(func.return_type.variant.clone()),
+        is_nullable: func.return_type.is_nullable,
+    };
+
+    // Prettify name, remove object name prefix from this property.
+    let pretty_name = func
+        .name
+        .strip_prefix(object.name())
+        // Panicing implies bug, checked at the start of the loop.
+        .unwrap()
+        .to_lower_camel_case();
+
+    // Special handling: some functions do not follow standard camelCase
+    // convention.
+    #[rustfmt::skip]
+    let pretty_name = if object.name() == "TWStoredKey" {
+        pretty_name
+            .replace("Json", "JSON")
+            .replace("Hd", "HD")
+    } else if object.name() == "TWPublicKey" {
+        pretty_name
+            .replace("Der", "DER")
+    } else if object.name() == "TWHash" {
+        pretty_name
+            .replace("ripemd", "RIPEMD")
+            .replace("Ripemd", "RIPEMD")
+            .replace("sha512256", "sha512_256")
+            .replace("sha3256", "sha3_256")
+            .replace("sha256sha256", "sha256SHA256")
+    } else if object.name() == "TWAES" {
+        pretty_name
+            .replace("Cbc", "CBC")
+            .replace("Ctr", "CTR")
+    } else {
+        pretty_name
+    };
+
+    Ok(Some(SwiftFunction {
+        name: pretty_name,
+        is_public: func.is_public,
+        is_static: func.is_static,
+        operations: ops,
+        params,
+        return_type,
+        comments: vec![],
+    }))
 }
