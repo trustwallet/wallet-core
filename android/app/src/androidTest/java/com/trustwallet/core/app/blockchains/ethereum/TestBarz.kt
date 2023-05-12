2fac27422d950f19ea10fb22187586afc50b40f0
Milerius <rmscastle@gmail.com>
2023-05-12 13:40:38 +0200

    feat(barz): fix barz

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/ethereum/TestBarz.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/ethereum/TestBarz.kt
@@ -28,8 +28,8 @@ class TestBarz {
             owner = Barz.ContractOwner.newBuilder().apply {
                 publicKey = "0x019b4ee7ad22ffd4c215e5f424faf4c75577dc36"
             }.build()
-        }.build()
-        val result = WCBarz.getCounterfactualAddress(input)
+        }
+        val result = WCBarz.getCounterfactualAddress(input.build().toByteArray())
         assertEquals(result, "0xE9C377EFCE644fd54614d77C177c14148502496F")
     }
 
@@ -48,8 +48,8 @@ class TestBarz {
             owner = Barz.ContractOwner.newBuilder().apply {
                 attestationObject = "0xa363666d74646e6f6e656761747453746d74a068617574684461746158a4f95bc73828ee210f9fd3bbe72d97908013b0a3759e9aea3d0ae318766cd2e1ad4500000000adce000235bcc60a648b0b25f1f055030020c720eb493e167ce93183dd91f5661e1004ed8cc1be23d3340d92381da5c0c80ca5010203262001215820a620a8cfc88fd062b11eab31663e56cad95278bef612959be214d98779f645b82258204e7b905b42917570148b0432f99ba21f2e7eebe018cbf837247e38150a89f771"
             }.build()
-        }.build()
-        val result = WCBarz.getCounterfactualAddress(input)
+        }
+        val result = WCBarz.getCounterfactualAddress(input.build().toByteArray())
         assertEquals(result, "0xc3d477e5D4CEd32835574bBAFE51a0A8c9c17272")
     }
 }
