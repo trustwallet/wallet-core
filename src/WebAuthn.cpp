34f2c7f578422f098785510c8254cd71b059bc28
Ruslan Serebriakov <rsrbk@users.noreply.github.com>
2023-05-12 13:09:15 +0100

    Webauthn public key support (#3145)
    * Webauthn public key support
    
    * Update Webauthn.h
    
    * Update Webauthn.cpp
    
    * Code review fixes
    
    * make public key optional
    
    * rename files
    
    * Counterfactual address computation for ERC-4337 (#3150)
    
    Added to your initial branch you can follow up the fixes in the other branch
    
    * Move Barz to separate files
    
    * update tests
    
    * update tests
    
    * Update EIP4337Tests.cpp
    
    * Update TWEthereum.cpp
    
    * Update TestBarz.kt
    
    * Update TWEthereum.cpp
    
    * Create TWBarz.yaml
    
    * Update WalletCore.h
    
    * Update TestBarz.kt
    
    * Update TestBarz.kt
    
    * Update TestBarz.kt
    
    * Update TWBarz.yaml
    
    * feat(barz): fix barz
    
    ---------
    
    Co-authored-by: Milerius <rmscastle@gmail.com>

---
+++ src/WebAuthn.cpp
@@ -0,0 +1,115 @@
+// Copyright © 2017-2023 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+
+#include "Cbor.h"
+#include "PublicKey.h"
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+namespace TW::WebAuthn {
+
+// https://www.w3.org/TR/webauthn-2/#authenticator-data
+struct AuthData {
+    Data rpIdHash;
+    std::array<std::uint8_t, 1> flagsBuf;
+    struct {
+        bool up;
+        bool uv;
+        bool at;
+        bool ed;
+        std::uint8_t flagsInt;
+    } flags;
+    std::uint32_t counter;
+    Data counterBuf;
+    Data aaguid;
+    Data credID;
+    Data COSEPublicKey;
+};
+
+AuthData parseAuthData(const Data& buffer) {
+    AuthData authData;
+
+    authData.rpIdHash = subData(buffer, 0, 32);
+
+    auto it = buffer.begin() + 32;
+    authData.flagsBuf = { *it };
+    ++it;
+    std::uint8_t flagsInt = authData.flagsBuf[0];
+    authData.flags.up = !!(flagsInt & 0x01);
+    authData.flags.uv = !!(flagsInt & 0x04);
+    authData.flags.at = !!(flagsInt & 0x40);
+    authData.flags.ed = !!(flagsInt & 0x80);
+    authData.flags.flagsInt = flagsInt;
+
+    authData.counterBuf = Data(it, it + 4);
+    authData.counter = static_cast<std::uint32_t>((authData.counterBuf[0] << 24) |
+                                                  (authData.counterBuf[1] << 16) |
+                                                  (authData.counterBuf[2] << 8) |
+                                                  authData.counterBuf[3]);
+    it += 4;
+
+    if (authData.flags.at) {
+        authData.aaguid = Data(it, it + 16);
+        it += 16;
+
+        std::array<std::uint8_t, 2> credIDLenBuf = { *(it), *(it + 1) };
+        std::uint16_t credIDLen = static_cast<std::uint16_t>((credIDLenBuf[0] << 8) |
+                                                             credIDLenBuf[1]);
+        it += 2;
+
+        authData.credID = Data(it, it + credIDLen);
+        it += credIDLen;
+
+        authData.COSEPublicKey = Data(it, buffer.end());
+    }
+
+    return authData;
+}
+
+auto findIntKey = [](const auto& map, const auto& key) {
+    return std::find_if(map.begin(), map.end(), [&](const auto& p) {
+        return p.first.dumpToString() == key;
+    });
+};
+
+auto findStringKey = [](const auto& map, const auto& key) {
+    return std::find_if(map.begin(), map.end(), [&](const auto& p) {
+        return p.first.getString() == key;
+    });
+};
+
+std::optional<PublicKey> getPublicKey(const Data& attestationObject) {
+    const Data authData = findStringKey(TW::Cbor::Decode(attestationObject).getMapElements(), "authData")->second.getBytes();
+    if (authData.empty()) {
+        return std::nullopt;
+    }
+
+    const AuthData authDataParsed = parseAuthData(authData);
+    const auto COSEPublicKey = TW::Cbor::Decode(authDataParsed.COSEPublicKey).getMapElements();
+
+    if (COSEPublicKey.empty()) {
+        return std::nullopt;
+    }
+
+    // https://www.w3.org/TR/webauthn-2/#sctn-encoded-credPubKey-examples
+    const std::string xKey = "-2";
+    const std::string yKey = "-3";
+
+    const auto x = findIntKey(COSEPublicKey, xKey);
+    const auto y = findIntKey(COSEPublicKey, yKey);
+
+    Data publicKey;
+    append(publicKey, 0x04);
+    append(publicKey, x->second.getBytes());
+    append(publicKey, y->second.getBytes());
+
+    return PublicKey(publicKey, TWPublicKeyTypeNIST256p1Extended);
+}
+
+} // namespace TW::Webauthn
---
+++ tests/common/WebAuthnTests.cpp
@@ -0,0 +1,20 @@
+// Copyright © 2017-2023 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "TestUtilities.h"
+#include "HexCoding.h"
+#include <WebAuthn.h>
+
+namespace TW::WebAuthn::tests {
+
+TEST(WebAuthn, GetPublicKey) {
+    {
+        const Data& attestationObject = parse_hex("0xa363666d74646e6f6e656761747453746d74a068617574684461746158a4f95bc73828ee210f9fd3bbe72d97908013b0a3759e9aea3d0ae318766cd2e1ad4500000000adce000235bcc60a648b0b25f1f055030020c720eb493e167ce93183dd91f5661e1004ed8cc1be23d3340d92381da5c0c80ca5010203262001215820a620a8cfc88fd062b11eab31663e56cad95278bef612959be214d98779f645b82258204e7b905b42917570148b0432f99ba21f2e7eebe018cbf837247e38150a89f771");
+        ASSERT_EQ(hex(getPublicKey(attestationObject)->bytes), "04a620a8cfc88fd062b11eab31663e56cad95278bef612959be214d98779f645b84e7b905b42917570148b0432f99ba21f2e7eebe018cbf837247e38150a89f771");
+    }
+}
+
+}
