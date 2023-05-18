7f3a6e6a0418f5d0d04ae567a3a7e9a1e62659e5
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-05-18 13:56:43 +0200

    [r2r] Decrease test coverage
    * Add nist256p1 Private -> Public key test

--- rust/tw_keypair/tests/nist256p1_tests.rs
+++ rust/tw_keypair/tests/nist256p1_tests.rs
@@ -6,17 +6,25 @@
 
 use serde::Deserialize;
 use tw_hash::{H256, H264, H520};
-use tw_keypair::ecdsa::nist256p1::{PublicKey, VerifySignature};
+use tw_keypair::ecdsa::nist256p1::{PrivateKey, PublicKey, VerifySignature};
 use tw_keypair::traits::VerifyingKeyTrait;
 
 /// The tests were generated in C++ using the `trezor-crypto` library.
 const NIST256P1_VERIFY: &str = include_str!("nist256p1_verify.json");
+const NIST256P1_PRIV_TO_PUB_COMPRESSED: &str =
+    include_str!("nist256p1_priv_to_pub_compressed.json");
 
 #[derive(Deserialize)]
 struct Nist256p1VerifyTest {
     public: H264,
     msg: H256,
     signature: H520,
+}
+
+#[derive(Deserialize)]
+struct Nist256p1PrivToPubCompressedTest {
+    secret: H256,
+    public: H264,
 }
 
 #[test]
@@ -27,6 +35,17 @@ fn test_nist256p1_verify() {
 
         let verify_sign = VerifySignature::try_from(test.signature.as_slice()).unwrap();
         assert!(public.verify(verify_sign, test.msg));
+    }
+}
+
+#[test]
+fn test_nist256p1_priv_to_pub() {
+    let tests: Vec<Nist256p1PrivToPubCompressedTest> = serde_json::from_str(NIST256P1_PRIV_TO_PUB_COMPRESSED).unwrap();
+    for test in tests {
+        let private = PrivateKey::try_from(test.secret.as_slice()).unwrap();
+        let actual_public = private.public().compressed();
+
+        assert_eq!(actual_public, test.public);
     }
 }
 
