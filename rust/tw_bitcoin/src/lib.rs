24dfc558343dc4cdd740adfdae33c69e054fe5d0
lamafab <42901763+lamafab@users.noreply.github.com>
2023-05-29 21:33:27 +0000

    attempt at integrating tw_keypair and tw_hash, WIP

--- rust/tw_bitcoin/src/lib.rs
+++ rust/tw_bitcoin/src/lib.rs
@@ -5,21 +5,55 @@ use bitcoin::hash_types::Txid;
 use bitcoin::hashes::sha256d::Hash;
 use bitcoin::key::UntweakedPublicKey;
 use bitcoin::psbt::Input;
-use bitcoin::sighash::SighashCache;
+use bitcoin::sighash::{LegacySighash, SighashCache};
 use bitcoin::taproot::{LeafVersion, TaprootBuilder};
 use bitcoin::transaction::Transaction;
 use bitcoin::{Sequence, TxIn, TxOut, Witness};
-use secp256k1::{generate_keypair, KeyPair, Secp256k1};
+//use secp256k1::{generate_keypair, KeyPair, Secp256k1};
+use tw_hash::H256;
+use tw_keypair::ecdsa::secp256k1;
+use tw_keypair::traits::{KeyPairTrait, SigningKeyTrait};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 const SIGHASH_ALL: u32 = 1;
+
+fn convert_legacy_btc_hash_to_h256(hash: LegacySighash) -> H256 {
+    let slice: &[u8] = hash.as_raw_hash().as_ref();
+    debug_assert_eq!(slice.len(), 32);
+    let bytes: [u8; 32] = slice.try_into().unwrap();
+    H256::from(bytes)
+}
+
+// TODO: `tw_keypair` should offer this function/method.
+fn to_der_signature(sig: &[u8; 64]) -> Vec<u8> {
+    let mut der_sig = vec![];
+
+    // Add the DER prefix
+    der_sig.push(0x30);
+
+    // Length of the rest of the signature
+    der_sig.push(68);
+
+    // Add the 'r' component
+    der_sig.push(0x02); // integer marker
+    der_sig.push(32); // length of the 'r' component
+    der_sig.extend(&sig[0..32]);
+
+    // Add the 's' component
+    der_sig.push(0x02); // integer marker
+    der_sig.push(32); // length of the 's' component
+    der_sig.extend(&sig[32..64]);
+
+    der_sig
+}
 
 #[derive(Debug, Clone)]
 pub enum Error {
     Todo,
 }
 
+/*
 #[test]
 fn poc() {
     let secp = Secp256k1::new();
@@ -43,13 +77,63 @@ fn poc() {
 
     let control_block = spend_info.control_block(&(script1, LeafVersion::TapScript));
 }
+*/
 
-pub struct PrivateKey;
-pub struct PublicKey;
+pub enum KeyPair {
+    Secp256k1(secp256k1::KeyPair),
+}
 
-impl PublicKey {
+impl KeyPair {
+    // TODO: Prefix `as_`
+    pub fn private(&self) -> PrivateKeyRef {
+        match self {
+            KeyPair::Secp256k1(pair) => PrivateKeyRef::Secp256k1(pair.private()),
+        }
+    }
+    pub fn public(&self) -> PublicKeyRef {
+        match self {
+            KeyPair::Secp256k1(pair) => PublicKeyRef::Secp256k1(pair.public()),
+        }
+    }
+}
+
+pub enum PrivateKeyRef<'a> {
+    Secp256k1(&'a secp256k1::PrivateKey),
+}
+
+impl<'a> PrivateKeyRef<'a> {
+    fn sign_h256(&self, msg: H256) -> Result<Signature> {
+        match self {
+            PrivateKeyRef::Secp256k1(key) => Ok(Signature::Secp256k1(
+                key.sign(msg).map_err(|_| Error::Todo)?,
+            )),
+        }
+    }
+}
+
+pub enum PublicKey {
+    Secp256k1(secp256k1::PublicKey),
+}
+
+pub enum PublicKeyRef<'a> {
+    Secp256k1(&'a secp256k1::PublicKey),
+}
+
+impl<'a> PublicKeyRef<'a> {
     pub fn hash(&self) -> PublicKeyHash {
         todo!()
+    }
+}
+
+pub enum Signature {
+    Secp256k1(secp256k1::Signature),
+}
+
+impl Signature {
+    pub fn to_der(&self) -> &[u8] {
+        match self {
+            Signature::Secp256k1(sig) => sig.to_der(),
+        }
     }
 }
 
@@ -59,21 +143,19 @@ pub struct ScriptHash;
 pub struct TransactionBuilder {
     version: i32,
     lock_time: LockTime,
-    privkey: PrivateKey,
-    pubkey: PublicKey,
+    keypair: KeyPair,
     inputs: Vec<TxInput>,
     outputs: Vec<TxOutput>,
 }
 
 impl TransactionBuilder {
-    fn new(privkey: PrivateKey, pubkey: PublicKey) -> Self {
+    fn new(keypair: KeyPair) -> Self {
         TransactionBuilder {
             // TODO: Check this.
             version: 2,
             // No lock time, transaction is immediately spendable.
             lock_time: LockTime::Blocks(Height::ZERO),
-            privkey,
-            pubkey,
+            keypair,
             inputs: vec![],
             outputs: vec![],
         }
@@ -95,7 +177,7 @@ impl TransactionBuilder {
         match TxInputBuilder::from_utxo(utxo, point) {
             TxInputBuilder::P2pkh(builder) => {
                 let input = builder
-                    .my_pubkey_hash(self.pubkey.hash())
+                    .my_pubkey_hash(self.keypair.public().hash())
                     .build()
                     // Panicing implies bug.
                     .unwrap();
@@ -150,13 +232,15 @@ impl TransactionBuilder {
 
         let cache = SighashCache::new(tx);
 
+        //let mut input_scriptsigs = vec![];
+
         for (index, input) in self.inputs.into_iter().enumerate() {
             // TODO: Prettify this.
             let script_pubkey = match input {
                 TxInput::P2pkh { ctx, hash: _ } => ctx.script_pub_key,
             };
 
-            let _hash = cache
+            let btc_hash = cache
                 .legacy_signature_hash(
                     index,
                     // TODO: Add note that this is same as `scriptPubKey` handled
@@ -165,6 +249,11 @@ impl TransactionBuilder {
                     SIGHASH_ALL,
                 )
                 .map_err(|_| Error::Todo)?;
+
+            let hash = convert_legacy_btc_hash_to_h256(btc_hash);
+            let sig = self.keypair.private().sign_h256(hash)?.to_der();
+
+            //input_scriptsigs.push()
         }
 
         todo!()
