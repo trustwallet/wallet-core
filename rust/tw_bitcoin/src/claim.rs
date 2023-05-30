15eaab0b5af24744f9b1d83c973cff34ce84110d
lamafab <42901763+lamafab@users.noreply.github.com>
2023-05-30 12:50:37 +0000

    replace ScriptSig input with TxInputP2PKH in TransactionSigner

--- rust/tw_bitcoin/src/claim.rs
+++ rust/tw_bitcoin/src/claim.rs
@@ -1,6 +1,6 @@
-use bitcoin::{ScriptBuf, TxOut};
+use bitcoin::{ScriptBuf, TxIn, TxOut};
 //use secp256k1::{generate_keypair, KeyPair, Secp256k1};
-use crate::{Error, Result, SigHashType};
+use crate::{Error, Result, SigHashType, TxInputP2PKH};
 use ripemd::{Digest, Ripemd160};
 use tw_hash::H256;
 use tw_keypair::ecdsa::secp256k1;
@@ -9,20 +9,15 @@ use tw_keypair::traits::{KeyPairTrait, SigningKeyTrait};
 pub trait TransactionSigner {
     fn claim_p2pkh_with_sighash(
         &self,
-        script_pubkey: &ScriptBuf,
+        input: &TxInputP2PKH,
         // TODO: Should be wrapped.
         hash: H256,
         sighash: SigHashType,
     ) -> Result<ClaimP2PKH>;
 
     // P2PKH signer with `SIGHASH_ALL` as default.
-    fn claim_p2pkh(&self, script_pubkey: &ScriptBuf, hash: H256) -> Result<ClaimP2PKH> {
-        <Self as TransactionSigner>::claim_p2pkh_with_sighash(
-            self,
-            script_pubkey,
-            hash,
-            SigHashType::All,
-        )
+    fn claim_p2pkh(&self, input: &TxInputP2PKH, hash: H256) -> Result<ClaimP2PKH> {
+        <Self as TransactionSigner>::claim_p2pkh_with_sighash(self, input, hash, SigHashType::All)
     }
 }
 
@@ -43,25 +38,10 @@ impl ClaimP2PKH {
 impl TransactionSigner for secp256k1::KeyPair {
     fn claim_p2pkh_with_sighash(
         &self,
-        script_pubkey: &ScriptBuf,
+        input: &TxInputP2PKH,
         hash: H256,
         sighash: SigHashType,
     ) -> Result<ClaimP2PKH> {
-        // Sanity check; make sure the scriptPubKey is actually a `P2PKH` script.
-        if script_pubkey.is_p2pkh() {
-            return Err(Error::Todo);
-        }
-
-        // Extract the expected recipient from the scriptPubKey.
-        //
-        // script[0] == OP_DUP
-        // script[1] == OP_HASH160
-        // script[2] == OP_PUSHBYTES_20
-        // script[3..23] == <PUBKEY-RIPEMD160>
-        // script[23] == OP_EQUALVERIFY
-        // script[24] == OP_CHECKSIG
-        let expected_recipient = &script_pubkey.as_bytes()[3..23];
-
         // The expected recipient is a RIPEMD160 hash, so first we check
         // whether it's a hash from the COMPRESSED public key.
         let mut hasher = Ripemd160::new();
@@ -69,11 +49,12 @@ impl TransactionSigner for secp256k1::KeyPair {
         let finalized = hasher.finalize();
         let hashed_pubkey = &finalized[..];
 
-        debug_assert_eq!(expected_recipient.len(), hashed_pubkey.len());
+        debug_assert_eq!(input.recipient.0.len(), hashed_pubkey.len());
 
         // If the expected recipient is a RIPEMD160 hash of the COMPRESSED
         // key...
-        let pubkey = if expected_recipient == hashed_pubkey {
+        // TODO: `RecipientHash160` should implement Deref
+        let pubkey = if input.recipient.0 == hashed_pubkey {
             self.public().compressed().to_vec()
         }
         // ... if not, then we check whether it is a RIPEMD160 hash of the
@@ -84,7 +65,7 @@ impl TransactionSigner for secp256k1::KeyPair {
             let finalized = hasher.finalize();
             let hashed_pubkey = &finalized[..];
 
-            if expected_recipient == hashed_pubkey {
+            if input.recipient.0 == hashed_pubkey {
                 self.public().uncompressed().to_vec()
             } else {
                 // Invalid, wrong signer!
