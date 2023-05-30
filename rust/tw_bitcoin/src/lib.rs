318cfaa8517931a6737bd0804065fecdacdc601d
lamafab <42901763+lamafab@users.noreply.github.com>
2023-05-30 22:22:18 +0000

    rename BTC* prefix to B*, remove unused deps

--- rust/tw_bitcoin/src/lib.rs
+++ rust/tw_bitcoin/src/lib.rs
@@ -1,14 +1,14 @@
-use bitcoin::address::{Address as BTCAddress, Payload as BTCPayload};
-use bitcoin::blockdata::locktime::absolute::{Height as BTCHeight, LockTime as BTCLockTime};
-use bitcoin::blockdata::script::ScriptBuf as BTCScriptBuf;
-use bitcoin::blockdata::transaction::OutPoint as BTCOutPoint;
+use bitcoin::address::{Address as BAddress, Payload as BPayload};
+use bitcoin::blockdata::locktime::absolute::{Height as BHeight, LockTime as BLockTime};
+use bitcoin::blockdata::script::ScriptBuf as BScriptBuf;
+use bitcoin::blockdata::transaction::OutPoint as BOutPoint;
 use bitcoin::consensus::{Decodable, Encodable};
-use bitcoin::hash_types::PubkeyHash as BTCPubkeyHash;
-use bitcoin::hashes::hash160::Hash as BTCHash;
-use bitcoin::hashes::Hash as BTCHashTrait;
-use bitcoin::sighash::{LegacySighash as BTCLegacySighash, SighashCache as BTCSighashCache};
-use bitcoin::transaction::Transaction as BTCTransaction;
-use bitcoin::{Sequence as BTCSequence, TxIn as BTCTxIn, TxOut as BTCTxOut, Witness as BTCWitness};
+use bitcoin::hash_types::PubkeyHash as BPubkeyHash;
+use bitcoin::hashes::hash160::Hash as BHash;
+use bitcoin::hashes::Hash as BHashTrait;
+use bitcoin::sighash::{LegacySighash as BLegacySighash, SighashCache as BSighashCache};
+use bitcoin::transaction::Transaction as BTransaction;
+use bitcoin::{Sequence as BSequence, TxIn as BTxIn, TxOut as BTxOut, Witness as BWitness};
 use claim::TransactionSigner;
 use std::str::FromStr;
 use tw_hash::H256;
@@ -44,7 +44,7 @@ pub fn keypair_from_wif(wif: &str) -> Result<secp256k1::KeyPair> {
     secp256k1::KeyPair::from_wif(wif).map_err(|_| Error::Todo)
 }
 
-fn convert_legacy_btc_hash_to_h256(hash: BTCLegacySighash) -> H256 {
+fn convert_legacy_btc_hash_to_h256(hash: BLegacySighash) -> H256 {
     let slice: &[u8] = hash.as_raw_hash().as_ref();
     debug_assert_eq!(slice.len(), 32);
     let bytes: [u8; 32] = slice.try_into().unwrap();
@@ -63,8 +63,8 @@ fn poc() {
     let (private, public) = generate_keypair(&mut rand::thread_rng());
     let tweaked = UntweakedPublicKey::from(public);
 
-    let script1 = BTCScriptBuf::new();
-    let script2 = BTCScriptBuf::new();
+    let script1 = BScriptBuf::new();
+    let script2 = BScriptBuf::new();
 
     let spend_info = TaprootBuilder::new()
         .add_leaf(1, script1.clone())
@@ -76,7 +76,7 @@ fn poc() {
 
     let root = spend_info.merkle_root().unwrap();
 
-    let tapscript = BTCScriptBuf::new_v1_p2tr(&secp, tweaked, Some(root));
+    let tapscript = BScriptBuf::new_v1_p2tr(&secp, tweaked, Some(root));
 
     let control_block = spend_info.control_block(&(script1, LeafVersion::TapScript));
 }
@@ -84,25 +84,32 @@ fn poc() {
 
 pub struct ScriptHash;
 
+#[derive(Debug, Clone)]
 pub struct TransactionBuilder {
     version: i32,
-    lock_time: BTCLockTime,
+    lock_time: BLockTime,
     inputs: Vec<TxInput>,
     outputs: Vec<TxOutput>,
-    btc_tx: Option<BTCTransaction>,
+    btc_tx: Option<BTransaction>,
 }
 
-impl TransactionBuilder {
-    pub fn new() -> Self {
+impl Default for TransactionBuilder {
+    fn default() -> Self {
         TransactionBuilder {
             // TODO: Check this.
             version: 2,
             // No lock time, transaction is immediately spendable.
-            lock_time: BTCLockTime::Blocks(BTCHeight::ZERO),
+            lock_time: BLockTime::Blocks(BHeight::ZERO),
             inputs: vec![],
             outputs: vec![],
             btc_tx: None,
         }
+    }
+}
+
+impl TransactionBuilder {
+    pub fn new() -> Self {
+        Self::default()
     }
     pub fn version(mut self, version: i32) -> Self {
         self.version = version;
@@ -110,7 +117,7 @@ impl TransactionBuilder {
     }
     // TODO: handle locktime seconds?.
     pub fn lock_time(mut self, height: u32) -> Self {
-        self.lock_time = BTCLockTime::Blocks(BTCHeight::from_consensus(height).unwrap());
+        self.lock_time = BLockTime::Blocks(BHeight::from_consensus(height).unwrap());
         self
     }
     pub fn add_input(mut self, input: TxInput) -> Self {
@@ -123,7 +130,7 @@ impl TransactionBuilder {
     }
     pub fn prepare_for_signing(mut self) -> Self {
         // Prepare boilerplate transaction for `bitcoin` crate.
-        let mut tx = BTCTransaction {
+        let mut tx = BTransaction {
             version: self.version,
             lock_time: self.lock_time,
             input: vec![],
@@ -133,9 +140,9 @@ impl TransactionBuilder {
         // Prepare the inputs for `bitcoin` crate.
         for input in self.inputs.iter().cloned() {
             let btc_txin = match input {
-                // TODO: `BTCTxIn` should implement `From<TxInput>`.
-                TxInput::P2PKH(p2pkh) => BTCTxIn::from(p2pkh.ctx),
-                TxInput::NonStandard { ctx } => BTCTxIn::from(ctx),
+                // TODO: `BTxIn` should implement `From<TxInput>`.
+                TxInput::P2PKH(p2pkh) => BTxIn::from(p2pkh.ctx),
+                TxInput::NonStandard { ctx } => BTxIn::from(ctx),
             };
 
             tx.input.push(btc_txin);
@@ -144,7 +151,7 @@ impl TransactionBuilder {
         // Prepare the outputs for `bitcoin` crate.
         for output in &self.outputs {
             // TODO: Doable without clone?
-            let btc_txout = BTCTxOut::from(output.clone());
+            let btc_txout = BTxOut::from(output.clone());
             tx.output.push(btc_txout);
         }
 
@@ -158,7 +165,7 @@ impl TransactionBuilder {
         self.sign_inputs_fn(|input, sighash| match input {
             TxInput::P2PKH(p2pkh) => signer
                 .claim_p2pkh(p2pkh, sighash)
-                // TODO: Should not convert into BTCScriptBuf here.
+                // TODO: Should not convert into BScriptBuf here.
                 .map(|claim| claim.into_script_buf()),
             TxInput::NonStandard { ctx: _ } => {
                 panic!()
@@ -168,9 +175,9 @@ impl TransactionBuilder {
     // TODO: Does this have to return `Result<T>`?
     pub fn sign_inputs_fn<F>(mut self, signer: F) -> Result<Self>
     where
-        F: Fn(&TxInput, H256) -> Result<BTCScriptBuf>,
+        F: Fn(&TxInput, H256) -> Result<BScriptBuf>,
     {
-        let cache = BTCSighashCache::new(self.btc_tx.unwrap());
+        let cache = BSighashCache::new(self.btc_tx.unwrap());
 
         let mut updated_scriptsigs = vec![];
 
@@ -238,25 +245,25 @@ impl TransactionSigHashType {
 #[derive(Debug, Clone)]
 // TODO: Should be private.
 pub struct InputContext {
-    pub previous_output: BTCOutPoint,
+    pub previous_output: BOutPoint,
     // The condition for claiming the output.
-    pub script_pubkey: BTCScriptBuf,
+    pub script_pubkey: BScriptBuf,
     // TODO: Document this.
-    pub sequence: BTCSequence,
-    // BTCWitness data for Segwit/Taproot transactions.
-    pub witness: BTCWitness,
+    pub sequence: BSequence,
+    // BWitness data for Segwit/Taproot transactions.
+    pub witness: BWitness,
 }
 
 impl InputContext {
-    pub fn new(utxo: BTCTxOut, point: BTCOutPoint) -> Self {
+    pub fn new(utxo: BTxOut, point: BOutPoint) -> Self {
         InputContext {
             previous_output: point,
             // TODO: Document this.
             script_pubkey: utxo.script_pubkey,
             // Default value of `0xFFFFFFFF = 4294967295`.
-            sequence: BTCSequence::default(),
+            sequence: BSequence::default(),
             // Empty witness.
-            witness: BTCWitness::new(),
+            witness: BWitness::new(),
         }
     }
     pub fn from_slice(mut slice: &[u8]) -> Result<Self> {
@@ -267,17 +274,17 @@ impl InputContext {
                 .map_err(|_| Error::Todo)?,
             sequence: Decodable::consensus_decode_from_finite_reader(&mut slice)
                 .map_err(|_| Error::Todo)?,
-            witness: BTCWitness::default(),
+            witness: BWitness::default(),
         })
     }
 }
 
-impl From<InputContext> for BTCTxIn {
+impl From<InputContext> for BTxIn {
     fn from(ctx: InputContext) -> Self {
-        BTCTxIn {
+        BTxIn {
             previous_output: ctx.previous_output,
             // TODO: Document this.
-            script_sig: BTCScriptBuf::default(),
+            script_sig: BScriptBuf::default(),
             sequence: ctx.sequence,
             witness: ctx.witness,
         }
@@ -298,22 +305,22 @@ impl From<TxOutputP2PKH> for TxOutput {
 #[derive(Debug, Clone)]
 pub struct TxOutputP2PKH {
     satoshis: u64,
-    script_pubkey: BTCScriptBuf,
+    script_pubkey: BScriptBuf,
 }
 
 impl TxOutputP2PKH {
     pub fn new(satoshis: u64, recipient: &PubkeyHash) -> Self {
         TxOutputP2PKH {
             satoshis,
-            script_pubkey: BTCScriptBuf::new_p2pkh(&recipient.0),
+            script_pubkey: BScriptBuf::new_p2pkh(&recipient.0),
         }
     }
 }
 
-impl From<TxOutput> for BTCTxOut {
+impl From<TxOutput> for BTxOut {
     fn from(out: TxOutput) -> Self {
         match out {
-            TxOutput::P2PKH(p2pkh) => BTCTxOut {
+            TxOutput::P2PKH(p2pkh) => BTxOut {
                 value: p2pkh.satoshis,
                 script_pubkey: p2pkh.script_pubkey,
             },
@@ -334,15 +341,15 @@ pub struct TxInputP2PKH {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct PubkeyHash(BTCPubkeyHash);
+pub struct PubkeyHash(BPubkeyHash);
 
 impl PubkeyHash {
     pub fn from_address_str(slice: &str) -> Result<Self> {
-        let checked = BTCAddress::from_str(slice).map_err(|_| Error::Todo)?;
+        let checked = BAddress::from_str(slice).map_err(|_| Error::Todo)?;
 
         // TODO: Network should be checked.
         let hash = match checked.payload {
-            BTCPayload::PubkeyHash(hash) => hash,
+            BPayload::PubkeyHash(hash) => hash,
             _ => todo!(),
         };
 
@@ -350,21 +357,21 @@ impl PubkeyHash {
     }
     pub fn from_keypair(keypair: &secp256k1::KeyPair, compressed: bool) -> Result<Self> {
         let bhash = if compressed {
-            BTCHash::hash(keypair.public().compressed().as_slice())
+            BHash::hash(keypair.public().compressed().as_slice())
         } else {
-            BTCHash::hash(keypair.public().uncompressed().as_slice())
+            BHash::hash(keypair.public().uncompressed().as_slice())
         };
 
-        let pubkey = BTCPubkeyHash::from_raw_hash(bhash);
+        let pubkey = BPubkeyHash::from_raw_hash(bhash);
 
         Ok(PubkeyHash(pubkey))
     }
     pub fn from_bytes(bytes: [u8; 20]) -> Result<Self> {
-        Ok(PubkeyHash(BTCPubkeyHash::from_byte_array(bytes)))
+        Ok(PubkeyHash(BPubkeyHash::from_byte_array(bytes)))
     }
-    pub fn from_script(script: &BTCScriptBuf) -> Result<Self> {
-        let pubkey = match BTCPayload::from_script(script).map_err(|_| Error::Todo)? {
-            BTCPayload::PubkeyHash(hash) => PubkeyHash(hash),
+    pub fn from_script(script: &BScriptBuf) -> Result<Self> {
+        let pubkey = match BPayload::from_script(script).map_err(|_| Error::Todo)? {
+            BPayload::PubkeyHash(hash) => PubkeyHash(hash),
             _ => return Err(Error::Todo),
         };
 
