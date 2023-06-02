75445c40036e24f843587302f40f5eacf2c21278
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2023-06-02 14:12:16 +0200

    Update `starknet-crypto` and `starknet-ff` (#3189)
    * [r2r] Update starknet-crypto and starknet-ff
    
    * [r2r] Update code coverage

--- rust/tw_starknet/tests/starknet_ffi_tests.rs
+++ rust/tw_starknet/tests/starknet_ffi_tests.rs
@@ -1,6 +1,7 @@
 use std::ffi::{c_char, CString};
 use tw_memory::c_string_standalone;
 use tw_memory::ffi::c_result::OK_CODE;
+use tw_memory::ffi::free_string;
 use tw_starknet::ffi::{
     starknet_pubkey_from_private, starknet_sign, starknet_verify, CStarknetCode,
 };
@@ -20,6 +21,8 @@ fn test_starknet_pubkey_from_private() {
         actual,
         "0x2a4c7332c55d6c1c510d24272d1db82878f2302f05b53bcc38695ed5f78fffd"
     );
+
+    unsafe { free_string(priv_key_hex) };
 }
 
 #[test]
@@ -37,6 +40,8 @@ fn test_starknet_pubkey_from_private_invalid() {
     let pub_key_raw = unsafe { starknet_pubkey_from_private(invalid_priv_key_ptr) };
     assert_eq!(pub_key_raw.code, CStarknetCode::InvalidInput as i32);
     assert!(pub_key_raw.result.is_null());
+
+    unsafe { free_string(priv_key_raw) };
 }
 
 #[test]
@@ -53,6 +58,9 @@ fn test_starknet_sign() {
         .unwrap();
     let expected = "061ec782f76a66f6984efc3a1b6d152a124c701c00abdd2bf76641b4135c770f04e44e759cea02c23568bb4d8a09929bbca8768ab68270d50c18d214166ccd9a";
     assert_eq!(actual, expected);
+
+    unsafe { free_string(priv_key_raw) };
+    unsafe { free_string(digest_raw) };
 }
 
 #[test]
@@ -69,6 +77,11 @@ fn test_starknet_verify() {
     let res = unsafe { starknet_verify(pubkey_raw, hash_raw, signature_r_raw, signature_s_raw) };
     assert_eq!(res.code, OK_CODE);
     assert!(res.result);
+
+    unsafe { free_string(pubkey_raw) };
+    unsafe { free_string(hash_raw) };
+    unsafe { free_string(signature_r_raw) };
+    unsafe { free_string(signature_s_raw) };
 }
 
 #[test]
@@ -85,4 +98,31 @@ fn test_starknet_verify_fail() {
     let res = unsafe { starknet_verify(pubkey_raw, hash_raw, signature_r_raw, signature_s_raw) };
     assert_eq!(res.code, OK_CODE);
     assert!(!res.result);
+
+    unsafe { free_string(pubkey_raw) };
+    unsafe { free_string(hash_raw) };
+    unsafe { free_string(signature_r_raw) };
+    unsafe { free_string(signature_s_raw) };
+}
+
+#[test]
+fn test_starknet_private_key_invalid() {
+    let private_raw =
+        c_string_standalone("02c5dbad71c92a45cc4b40573ae661f8147869a91d57b8d9b8f48c8af7f831590123");
+
+    let res = unsafe { starknet_pubkey_from_private(private_raw) };
+    assert_eq!(res.code, CStarknetCode::PrivKeyError as i32);
+
+    unsafe { free_string(private_raw) };
+}
+
+#[test]
+fn test_starknet_private_key_zero() {
+    let private_raw =
+        c_string_standalone("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
+    let res = unsafe { starknet_pubkey_from_private(private_raw) };
+    assert_eq!(res.code, CStarknetCode::PrivKeyError as i32);
+
+    unsafe { free_string(private_raw) };
 }
