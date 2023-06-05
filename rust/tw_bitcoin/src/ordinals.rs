df7a5e6e992e0c030d267316e72535b9e69b7a98
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-05 11:38:35 +0000

    get get_op_push

--- rust/tw_bitcoin/src/ordinals.rs
+++ rust/tw_bitcoin/src/ordinals.rs
@@ -5,7 +5,7 @@ use bitcoin::secp256k1::XOnlyPublicKey;
 use bitcoin::taproot::{TaprootBuilder, TaprootSpendInfo};
 use bitcoin::PublicKey;
 
-fn get_push(size: u32) -> Result<(AnyOpcode, Option<Vec<u8>>)> {
+fn get_op_push(size: u32) -> Result<(AnyOpcode, Option<Vec<u8>>)> {
     use bitcoin::opcodes::all::*;
 
     let ret = match size {
@@ -20,18 +20,17 @@ fn get_push(size: u32) -> Result<(AnyOpcode, Option<Vec<u8>>)> {
     Ok(ret)
 }
 
-/// Creates an Ordinal Inscription as defined in
-/// https://docs.ordinals.com/inscriptions.html. This function is used for two
-/// purposes:
-/// 
-/// 1. It creates the spending condition for the given `internal_key`
-///    (recipient). This associates the public key of the recipient with the
-///    Merkle root of the Inscription on-chain, but it does not actually reveal
-///    the script to anyone ("commit stage").
+/// Creates an [Ordinal Inscription](https://docs.ordinals.com/inscriptions.html).
+/// This function is used for two purposes:
+///
+/// 1. It creates the spending condition for the given `internal_key`. This
+///    associates the public key of the recipient with the Merkle root of the
+///    Inscription on-chain, but it does not actually reveal the script to
+///    anyone ("commit stage").
 /// 2. The same function can then be used by the spender/claimer to actually
 ///    transfer the Inscripion by sending a transaction with the Inscription
 ///    script in the Witness ("reveal stage").
-/// 
+///
 /// Do note that the `internal_key` can be different for each stage, but it
 /// could also be the same entity. Stage one, the `internal_key` is the
 /// recipient. Stage two, the `internal_key` is the claimer of the transaction
@@ -41,7 +40,7 @@ fn create_envelope(mime: &str, data: &str, internal_key: PublicKey) -> Result<Ta
     use bitcoin::opcodes::*;
 
     // TODO: Check overflow
-    let (op_push, size_buf) = get_push(mime.len() as u32)?;
+    let (op_push, size_buf) = get_op_push(mime.len() as u32)?;
 
     // Prepare content-type buffer.
     let mut mime_buf = PushBytesBuf::new();
@@ -59,7 +58,7 @@ fn create_envelope(mime: &str, data: &str, internal_key: PublicKey) -> Result<Ta
         .extend_from_slice(data.as_bytes())
         .map_err(|_| Error::Todo)?;
 
-	// Create an Ordinal Inscription.
+    // Create an Ordinal Inscription.
     let script = ScriptBuf::builder()
         .push_opcode(OP_FALSE)
         .push_opcode(OP_IF)
@@ -74,10 +73,10 @@ fn create_envelope(mime: &str, data: &str, internal_key: PublicKey) -> Result<Ta
         .push_opcode(OP_ENDIF)
         .into_script();
 
-	// Generate the necessary spending information. As mentioned in the
-	// documentation of this function at the top, this serves two purposes;
-	// setting the spending condition and actually claiming the spending
-	// condition.
+    // Generate the necessary spending information. As mentioned in the
+    // documentation of this function at the top, this serves two purposes;
+    // setting the spending condition and actually claiming the spending
+    // condition.
     let spend_info = TaprootBuilder::new()
         .add_leaf(1, script)
         .map_err(|_| Error::Todo)?
@@ -88,4 +87,41 @@ fn create_envelope(mime: &str, data: &str, internal_key: PublicKey) -> Result<Ta
         .map_err(|_| Error::Todo)?;
 
     Ok(spend_info)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bitcoin::opcodes::all::*;
+
+    #[test]
+    fn test_get_op_push() {
+        fn eq(size: u32, expected: AnyOpcode, has_size_le: bool) {
+            dbg!(size, expected, has_size_le);
+
+            let (op, size_le) = get_op_push(size).unwrap();
+            assert_eq!(op, expected);
+            if has_size_le {
+                assert_eq!(size_le.unwrap().as_slice(), size.to_le_bytes());
+            }
+        }
+
+        // Up to 75, only the OP_PUSHBYTES_* opcode is returned.
+        eq(0, OP_PUSHBYTES_0, false);
+        eq(1, OP_PUSHBYTES_1, false);
+        eq(2, OP_PUSHBYTES_2, false);
+        eq(75, OP_PUSHBYTES_75, false);
+
+        // From 76 to 255, it returns a OP_PUSHDATA_1 + SIZE (LE).
+        eq(76, OP_PUSHDATA1, true);
+        eq(255, OP_PUSHDATA1, true);
+
+        // From 256 to 65535, it returns a OP_PUSHDATA_2 + SIZE (LE).
+        eq(256, OP_PUSHDATA2, true);
+        eq(65535, OP_PUSHDATA2, true);
+
+        // From 65536 to 2^32-1, it returns a OP_PUSHDATA_4 + SIZE (LE).
+        eq(65536, OP_PUSHDATA4, true);
+        eq(u32::MAX, OP_PUSHDATA4, true);
+    }
 }
