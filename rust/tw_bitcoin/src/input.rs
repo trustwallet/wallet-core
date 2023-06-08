5b00667d70ddfbe4d20a68166b35192843b0701a
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-08 13:29:22 +0000

    add builder for TxInputP2WPKH

--- rust/tw_bitcoin/src/input.rs
+++ rust/tw_bitcoin/src/input.rs
@@ -338,3 +338,45 @@ impl TxInputP2WPKH {
         }
     }
 }
+
+#[derive(Debug, Clone, Default)]
+pub struct TxInputP2WPKHBuilder {
+    txid: Option<Txid>,
+    vout: Option<u32>,
+    recipient: Option<Recipient<WPubkeyHash>>,
+    satoshis: Option<u64>,
+}
+
+impl TxInputP2WPKHBuilder {
+    pub fn new() -> TxInputP2WPKHBuilder {
+        Self::default()
+    }
+    pub fn txid(mut self, txid: Txid) -> TxInputP2WPKHBuilder {
+        self.txid = Some(txid);
+        self
+    }
+    pub fn vout(mut self, vout: u32) -> TxInputP2WPKHBuilder {
+        self.vout = Some(vout);
+        self
+    }
+    pub fn recipient(mut self, recipient: Recipient<WPubkeyHash>) -> TxInputP2WPKHBuilder {
+        self.recipient = Some(recipient);
+        self
+    }
+    pub fn satoshis(mut self, satoshis: u64) -> TxInputP2WPKHBuilder {
+        self.satoshis = Some(satoshis);
+        self
+    }
+    pub fn build(self) -> Result<TxInputP2WPKH> {
+        Ok(
+            TxInputP2WPKH::new(
+                self.txid.ok_or(Error::Todo)?,
+                self.vout.ok_or(Error::Todo)?,
+                self.recipient.ok_or(Error::Todo)?,
+                self.satoshis,
+            )
+        )
+    }
+}
+
+
