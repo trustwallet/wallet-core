f1795980898381b881db201febd22019179b47fa
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-08 15:56:34 +0000

    update p2tr_script_path to builder pattern

--- rust/tw_bitcoin/src/tests/p2tr_script_path.rs
+++ rust/tw_bitcoin/src/tests/p2tr_script_path.rs
@@ -1,9 +1,8 @@
 use super::*;
 use crate::{
     brc20::{BRC20DeployInscription, BRC20MintInscription},
-    keypair_from_wif, Recipient, TXOutputP2TRScriptPath, TransactionBuilder,
-    TxInputP2PKH, TxInputP2TRScriptPath,
-    TxOutputP2PKH,
+    keypair_from_wif, Recipient, TXOutputP2TRScriptPath, TransactionBuilder, TxInputP2PKH,
+    TxInputP2TRScriptPath, TxOutputP2PKH,
 };
 use bitcoin::{PubkeyHash, PublicKey, Txid};
 use std::str::FromStr;
@@ -31,26 +30,35 @@ fn mainnet_brc20_deploy() {
     let recipient = Recipient::<PubkeyHash>::from(&alice);
     let satoshis = DEPLOY_INPUT_AMOUNT;
 
-    let input = TxInputP2PKH::new(txid, vout, recipient, Some(satoshis));
+    let input = TxInputP2PKH::builder()
+        .txid(Txid::from_str(ORIGIN_TXID).unwrap())
+        .vout(0)
+        .recipient(&alice)
+        .satoshis(DEPLOY_INPUT_AMOUNT)
+        .build()
+        .unwrap();
 
     // Prepare BRC20 Deploy output for Alice.
-    let recipient = Recipient::<PublicKey>::from_keypair(&alice);
     let ticker = "qqqq".to_string();
     let max = 1000;
     let limit = None;
     let decimals = None;
 
     let brc20_deploy =
-        BRC20DeployInscription::new(recipient.clone(), ticker.clone(), max, limit, decimals)
-            .unwrap();
-    //dbg!(&brc20_deploy);
+        BRC20DeployInscription::new((&alice).into(), ticker.clone(), max, limit, decimals).unwrap();
 
-    let output = TXOutputP2TRScriptPath::new(ORDINALS_SATOSHIS, brc20_deploy.0.recipient());
+    let output = TXOutputP2TRScriptPath::builder()
+        .recipient(brc20_deploy.0.recipient().clone())
+        .satoshis(ORDINALS_SATOSHIS)
+        .build()
+        .unwrap();
 
     // Return address (back to Alice)
-    let satoshis = DEPLOY_RETURN_AMOUNT;
-
-    let return_output = TxOutputP2PKH::new(satoshis, recipient.clone());
+    let return_output = TxOutputP2PKH::builder()
+        .recipient(&alice)
+        .satoshis(DEPLOY_RETURN_AMOUNT)
+        .build()
+        .unwrap();
 
     // Alice signs the transaction.
     let signed_transaction = TransactionBuilder::new()
@@ -68,50 +76,52 @@ fn mainnet_brc20_deploy() {
     println!("{hex}");
 
     // ## SECOND TRANSACTION: Deploy the BRC20 token.
-    let input_deploy;
-    let input_fee_payer;
-    let output_mint;
-    let output_return;
-    {
-        let DEPLOY_TXID: &str = env!("DEPLOY_TXID");
-        let DEPLOY_RETURN_AMOUNT: u64 = env!("DEPLOY_RETURN_AMOUNT").parse().unwrap();
-        let MINT_RETURN_AMOUNT: u64 = DEPLOY_RETURN_AMOUNT - MINER_FEE;
-        dbg!(DEPLOY_RETURN_AMOUNT, MINER_FEE, MINT_RETURN_AMOUNT);
+    let DEPLOY_TXID: &str = env!("DEPLOY_TXID");
+    let DEPLOY_RETURN_AMOUNT: u64 = env!("DEPLOY_RETURN_AMOUNT").parse().unwrap();
+    let MINT_RETURN_AMOUNT: u64 = DEPLOY_RETURN_AMOUNT - MINER_FEE;
 
-        // Claim BRC20 DEPLOY input.
-        let txid = Txid::from_str(DEPLOY_TXID).unwrap();
-        let vout = 0;
-        let satoshis = ORDINALS_SATOSHIS;
+    // Claim BRC20 DEPLOY input.
+    let txid = Txid::from_str(DEPLOY_TXID).unwrap();
 
-        // Input of BRC20 DEPLOY
-        input_deploy = TxInputP2TRScriptPath::new(
-            txid,
-            vout,
-            brc20_deploy.0.recipient().clone(),
-            satoshis,
-            brc20_deploy.0.envelope.script,
-            brc20_deploy.0.envelope.spend_info,
-        );
+    // Input of BRC20 DEPLOY
+    let input_deploy = TxInputP2TRScriptPath::builder()
+        .txid(txid.clone())
+        .vout(0)
+        .recipient(brc20_deploy.0.recipient().clone())
+        .satoshis(ORDINALS_SATOSHIS)
+        .script(brc20_deploy.0.envelope.script)
+        .spend_info(brc20_deploy.0.envelope.spend_info)
+        .build()
+        .unwrap();
 
-        // Input for paying fees.
-        let vout = 1;
-        let satoshis = DEPLOY_RETURN_AMOUNT;
-        input_fee_payer = TxInputP2PKH::new(txid, vout, recipient.clone(), Some(satoshis));
+    // Input for paying fees.
+    let input_fee_payer = TxInputP2PKH::builder()
+        .txid(txid.clone())
+        .vout(1)
+        .recipient(&alice)
+        .satoshis(DEPLOY_RETURN_AMOUNT)
+        .build()
+        .unwrap();
 
-        // Prepare BRC20 MINT output.
+    // Prepare BRC20 MINT output.
 
-        // Mint 50 tokens of "qqqq".
-        let amount = 50;
+    // Mint 50 tokens of "qqqq".
+    let amount = 50;
 
-        let brc20_mint =
-            BRC20MintInscription::new(recipient.clone(), ticker.clone(), amount).unwrap();
+    let brc20_mint = BRC20MintInscription::new((&alice).into(), ticker.clone(), amount).unwrap();
 
-        output_mint = TXOutputP2TRScriptPath::new(ORDINALS_SATOSHIS, brc20_mint.0.recipient());
+    let output_mint = TXOutputP2TRScriptPath::builder()
+        .recipient(brc20_mint.0.recipient().clone())
+        .satoshis(ORDINALS_SATOSHIS)
+        .build()
+        .unwrap();
 
-        // Return address (back to Alice)
-        let satoshis = MINT_RETURN_AMOUNT;
-        output_return = TxOutputP2PKH::new(satoshis, recipient.clone());
-    };
+    // Return address (back to Alice)
+    let output_return = TxOutputP2PKH::builder()
+        .recipient(&alice)
+        .satoshis(MINT_RETURN_AMOUNT)
+        .build()
+        .unwrap();
 
     // Alice signs the transaction.
     let signed_transaction = TransactionBuilder::new()
@@ -128,4 +138,6 @@ fn mainnet_brc20_deploy() {
     let hex = hex::encode(signed_transaction, false);
     //assert_eq!(&hex, EXPECTED_RAW_SIGNED);
     println!("{hex}");
+
+    todo!()
 }
