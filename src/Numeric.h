3a46487d25c71e03086a65861e9962334c135b71
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2023-06-08 20:54:22 +0200

    [Cardano]: Fix `TWCardanoOutputMinAdaAmount` (#3210)

--- src/Numeric.h
+++ src/Numeric.h
@@ -18,4 +18,15 @@ bool checkAddUnsignedOverflow(T x, T y) {
     return x > std::numeric_limits<T>::max() - y;
 }
 
+template <
+    typename T,
+    typename = std::enable_if_t<std::is_unsigned_v<T>>
+>
+bool checkMulUnsignedOverflow(T x, T y) {
+    if (x == 0 || y == 0) {
+        return false;
+    }
+    return x > std::numeric_limits<T>::max() / y;
+}
+
 } // namespace TW
