fa9c3c656a45b00744303ca29fcd40fe52565feb
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-08 21:02:58 +0000

    add tw_tx_input_p2wpkh_create

--- rust/tw_bitcoin/src/ffi/p2tr_key_path.rs
+++ rust/tw_bitcoin/src/ffi/p2tr_key_path.rs
@@ -1,27 +1,27 @@
 use crate::{output::TxOutputP2TRKeyPath, Recipient};
-use bitcoin::ScriptBuf;
 use bitcoin::PublicKey;
-use tw_memory::ffi::c_byte_array_ref::CByteArrayRef;
+use bitcoin::ScriptBuf;
 use tw_memory::ffi::c_byte_array::CByteArray;
+use tw_memory::ffi::c_byte_array_ref::CByteArrayRef;
 use tw_memory::ffi::RawPtrTrait;
 use tw_misc::{try_or_else, try_or_false};
 
-pub struct Test(pub(crate) TxOutputP2TRKeyPath);
-
+/*
 #[no_mangle]
 pub unsafe extern "C" fn tw_build_pay_to_taproot_key_spend_script(
-	input: *const u8,
-	input_len: usize,
+    pubkey: *const u8,
+    pubkey_len: usize,
 ) -> CByteArray {
-	let input_ref = CByteArrayRef::new(input, input_len);
+    let input_ref = CByteArrayRef::new(pubkey, pubkey_len);
 
-	// Convert bytes to public key and tweak with zeroed merkle root
-	// (the "key-spend" way).
-	let input = try_or_else!(input_ref.as_slice(), std::ptr::null_mut);
-	let pubkey = try_or_else!(Recipient::<PublicKey>::from_slice(input), std::ptr::null_mut);
+    // Convert bytes to public key and tweak with zeroed merkle root
+    // (the "key-spend" way).
+    let slice = try_or_else!(input_ref.as_slice(), std::ptr::null_mut);
+    let pubkey = try_or_else!(Recipient::<PublicKey>::from_slice(slice), std::ptr::null_mut);
 
-	// We create the `scriptPubKey` directly an avoid the Rust builder.
-	let script = ScriptBuf::new_v1_p2tr_tweaked(pubkey.tweaked_pubkey());
+    // We create the `scriptPubKey` directly an avoid the Rust builder.
+    let script = ScriptBuf::new_v1_p2tr_tweaked(pubkey.tweaked_pubkey());
 
-	CByteArray::from(script.to_bytes())
+    CByteArray::from(script.to_bytes())
 }
+ */
