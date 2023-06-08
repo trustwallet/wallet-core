1dc4419f07577521c455373c1583b13aaec08c61
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-08 21:36:47 +0000

    add tw_transaction_add_*_output methods

--- rust/tw_bitcoin/src/ffi/transaction.rs
+++ rust/tw_bitcoin/src/ffi/transaction.rs
@@ -1,5 +1,5 @@
 use crate::{output::TxOutputP2TRKeyPath, Recipient, TransactionBuilder};
-use super::p2tr_key_path::TWTxInputP2TRKeyPath;
+use super::p2tr_key_path::{TWTxInputP2TRKeyPath, TWTxOutputP2TRKeyPath};
 use super::p2wpkh::{TWTxInputP2WPKH, TWTxOutputP2WPKH};
 use bitcoin::PublicKey;
 use bitcoin::ScriptBuf;
@@ -40,6 +40,30 @@ pub unsafe extern "C" fn tw_transaction_add_p2tr_key_path_input(
 	let input = try_or_else!(TWTxInputP2TRKeyPath::from_ptr(input), std::ptr::null_mut);
 
 	let builder = builder.0.add_input(input.0.into());
+
+    TWTransactionBuilder(builder).into_ptr()
+}
+
+pub unsafe extern "C" fn tw_transaction_add_p2wpkh_output(
+	builder: *mut TWTransactionBuilder,
+	output: *mut TWTxOutputP2WPKH,
+) -> *mut TWTransactionBuilder {
+	let builder = try_or_else!(TWTransactionBuilder::from_ptr(builder), std::ptr::null_mut);
+	let output = try_or_else!(TWTxOutputP2WPKH::from_ptr(output), std::ptr::null_mut);
+
+	let builder = builder.0.add_output(output.0.into());
+
+    TWTransactionBuilder(builder).into_ptr()
+}
+
+pub unsafe extern "C" fn tw_transaction_add_p2tr_key_path_output(
+	builder: *mut TWTransactionBuilder,
+	output: *mut TWTxOutputP2TRKeyPath,
+) -> *mut TWTransactionBuilder {
+	let builder = try_or_else!(TWTransactionBuilder::from_ptr(builder), std::ptr::null_mut);
+	let output = try_or_else!(TWTxOutputP2TRKeyPath::from_ptr(output), std::ptr::null_mut);
+
+	let builder = builder.0.add_output(output.0.into());
 
     TWTransactionBuilder(builder).into_ptr()
 }
