7753081777002af712778abfefe7f65587ad32f7
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-12 16:58:15 +0000

    add test PayToTaprootKeyPath

--- tests/chains/Bitcoin/TaprootBuilder.cpp
+++ tests/chains/Bitcoin/TaprootBuilder.cpp
@@ -84,4 +84,49 @@ TEST(Taproot, PayToWitnessPublicKeyHash) {
 	}
 }
 
+TEST(Taproot, PayToTaprootKeyPath) {
+	// Using the keys from a _regtest_ environment, copied from `rust/tw_bitcoin`.
+    auto alice_wif = "cNDFvH3TXCjxgWeVc7vbu4Jw5m2Lu8FkQ69Z2XvFUD9D9rGjofN1";
+    auto alice_decoded = Base58::decodeCheck(alice_wif);
+    auto alice_seckey = PrivateKey(Data(alice_decoded.begin() + 1, alice_decoded.begin() + 33));
+    auto alice_pubkey = alice_seckey.getPublicKey(TWPublicKeyTypeSECP256k1Extended);
+
+    auto bob_wif = "cNt3XNHiJdJpoX5zt3CXY8ncgrCted8bxmFBzcGeTZbBw6jkByWB";
+    auto bob_decoded = Base58::decodeCheck(bob_wif);
+    auto bob_seckey = PrivateKey(Data(bob_decoded.begin() + 1, bob_decoded.begin() + 33));
+    auto bob_pubkey = alice_seckey.getPublicKey(TWPublicKeyTypeSECP256k1Extended);
+
+	const uint64_t ONE_BTC = 100000000;
+	const uint64_t MINER_FEE = ONE_BTC / 100;
+	const uint64_t SEND_TO_ALICE = 50 * ONE_BTC;
+	const uint64_t SEND_TO_BOB = SEND_TO_ALICE - MINER_FEE;
+	const uint64_t SEND_BACK = SEND_TO_BOB - MINER_FEE;
+
+	{
+		const char* TXID = "c50563913e5a838f937c94232f5a8fc74e58b629fae41dfdffcc9a70f833b53a";
+
+		TaprootBuilder builder = TaprootBuilder();
+		// Alice claims P2PKH input.
+		assert(builder.AddP2PKHInput(parse_hex(TXID).data(), 0, alice_pubkey.bytes.data(), alice_pubkey.bytes.size(), SEND_TO_ALICE).isSuccess());
+		// Alice constructs P2TR key-path output.
+		assert(builder.AddP2TRKeyPathOutput(bob_pubkey.bytes.data(), bob_pubkey.bytes.size(), SEND_TO_BOB).isSuccess());
+		// Alice sign transaction.
+		auto sig = builder.Sign(alice_seckey.bytes.data(), alice_seckey.bytes.size());
+		assert(hex(sig.payload()) == "02000000013ab533f8709accfffd1de4fa29b6584ec78f5a2f23947c938f835a3e916305c5000000006b48304502210086ab2c2192e2738529d6cd9604d8ee75c5b09b0c2f4066a5c5fa3f87a26c0af602202afc7096aaa992235c43e712146057b5ed6a776d82b9129620bc5a21991c0a5301210351e003fdc48e7f31c9bc94996c91f6c3273b7ef4208a1686021bedf7673bb058ffffffff01c0aff62901000000225120e01cfdd05da8fa1d71f987373f3790d45dea9861acb0525c86656fe50f4397a600000000");
+	}
+
+	{
+		const char* TXID_2 = "9a582032f6a50cedaff77d3d5604b33adf8bc31bdaef8de977c2187e395860ac";
+
+		TaprootBuilder builder = TaprootBuilder();
+		// Bob claims P2TR key-path input.
+		assert(builder.AddP2WPKHInput(parse_hex(TXID_2).data(), 0, bob_pubkey.bytes.data(), bob_pubkey.bytes.size(), SEND_TO_BOB).isSuccess());
+		// Bob constructs P2TR key-path output for Alice.
+		assert(builder.AddP2WPKHOutput(alice_pubkey.bytes.data(), alice_pubkey.bytes.size(), SEND_BACK).isSuccess());
+		// Bob signs transaction.
+		auto sig = builder.Sign(bob_seckey.bytes.data(), bob_seckey.bytes.size());
+		assert(hex(sig.payload()) == "02000000000101ac6058397e18c277e98defda1bc38bdf3ab304563d7df7afed0ca5f63220589a0000000000ffffffff01806de72901000000225120a5c027857e359d19f625e52a106b8ac6ca2d6a8728f6cf2107cd7958ee0787c20140ec2d3910d41506b60aaa20520bb72f15e2d2cbd97e3a8e26ee7bad5f4c56b0f2fb0ceaddac33cb2813a33ba017ba6b1d011bab74a0426f12a2bcf47b4ed5bc8600000000");
+	}
+}
+
 } // namespace TW::Taproot::tests
