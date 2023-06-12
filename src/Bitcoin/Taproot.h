788c1e40ca6095163155266fc9f11e397b242835
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-12 15:15:25 +0000

    import and make use of Result type

--- src/Bitcoin/Taproot.h
+++ src/Bitcoin/Taproot.h
@@ -1,19 +1,28 @@
 #include "Data.h"
+#include "Result.h"
 #include "rust/Wrapper.h"
 #include "rust/bindgen/WalletCoreRSBindgen.h"
 
 using namespace TW;
+
+enum TransactionBuilderError {
+	InvalidInput,
+	InvalidOutput,
+	FailedToSign,
+};
+
+using Error = TransactionBuilderError;
 
 class TransactionBuilder {
 public:
     Rust::TWTransactionBuilder* builder;
 
     TransactionBuilder();
-    void AddP2PKHInput(const uint8_t *txid, uint32_t vout, const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
-    void AddP2WPKHInput(const uint8_t *txid, uint32_t vout, const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
-    void AddP2TRKeyPathInput(const uint8_t *txid, uint32_t vout, const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
-    void AddP2PKHOutput(const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
-    void AddP2WPKHOutput(const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
-    void AddP2TRKeyPathOutput(const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
-    Data Sign(const uint8_t *secret_key, size_t secret_key_len);
+    Result<void, Error> AddP2PKHInput(const uint8_t *txid, uint32_t vout, const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
+    Result<void, Error> AddP2WPKHInput(const uint8_t *txid, uint32_t vout, const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
+    Result<void, Error> AddP2TRKeyPathInput(const uint8_t *txid, uint32_t vout, const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
+    Result<void, Error> AddP2PKHOutput(const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
+    Result<void, Error> AddP2WPKHOutput(const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
+    Result<void, Error> AddP2TRKeyPathOutput(const uint8_t *pubkey, size_t pubkey_len, uint64_t satoshis);
+    Result<Data, Error> Sign(const uint8_t *secret_key, size_t secret_key_len);
 };
