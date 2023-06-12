ac002610eb0f91f102c44aa55c532ccca2958c5c
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-12 16:29:00 +0000

    unwrap signature

--- tests/chains/Bitcoin/TaprootBuilder.cpp
+++ tests/chains/Bitcoin/TaprootBuilder.cpp
@@ -10,6 +10,7 @@ using namespace TW;
 namespace TW::Taproot::tests {
 
 TEST(Taproot, PayToPublicKeyHash) {
+	// Using the keys from a _regtest_ environment, copied from `rust/tw_bitcoin`.
     auto alice_wif = "cQUNzeMnF9xPPLqZhH7hMVYGwSuu3b78zznuc5UrxgXnYQBq6Bx1";
     auto alice_decoded = Base58::decodeCheck(alice_wif);
     auto alice_seckey = PrivateKey(Data(alice_decoded.begin() + 1, alice_decoded.begin() + 33));
@@ -26,11 +27,15 @@ TEST(Taproot, PayToPublicKeyHash) {
 	const uint64_t MINER_FEE = ONE_BTC / 100;
 
 	TaprootBuilder builder = TaprootBuilder();
-	assert(builder.AddP2PKHInput(parse_hex(TXID).data(), 0, alice_pubkey.bytes.data(), alice_pubkey.bytes.size(), SEND_TO_ALICE).isSuccess());
-	assert(builder.AddP2PKHOutput(bob_pubkey.bytes.data(), bob_pubkey.bytes.size(), SEND_TO_ALICE - MINER_FEE).isSuccess());
 
+	// Claim P2PKH input.
+	assert(builder.AddP2PKHInput(parse_hex(TXID).data(), 0, alice_pubkey.bytes.data(), alice_pubkey.bytes.size(), SEND_TO_ALICE).isSuccess());
+	// Construct P2PKH output.
+	assert(builder.AddP2PKHOutput(bob_pubkey.bytes.data(), bob_pubkey.bytes.size(), SEND_TO_ALICE - MINER_FEE).isSuccess());
+	// Sign transaction.
 	auto signature = builder.Sign(alice_seckey.bytes.data(), alice_seckey.bytes.size());
-	assert(hex(signature) == "02000000017be4e642bb278018ab12277de9427773ad1c5f5b1d164a157e0d99aa48dc1c1e000000006a473044022078eda020d4b86fcb3af78ef919912e6d79b81164dbbb0b0b96da6ac58a2de4b102201a5fd8d48734d5a02371c4b5ee551a69dca3842edbf577d863cf8ae9fdbbd4590121036666dd712e05a487916384bfcd5973eb53e8038eccbbf97f7eed775b87389536ffffffff01c0aff629010000001976a9145eaaa4f458f9158f86afcba08dd7448d27045e3d88ac00000000");
+
+	assert(hex(signature.payload()) == "02000000017be4e642bb278018ab12277de9427773ad1c5f5b1d164a157e0d99aa48dc1c1e000000006a473044022078eda020d4b86fcb3af78ef919912e6d79b81164dbbb0b0b96da6ac58a2de4b102201a5fd8d48734d5a02371c4b5ee551a69dca3842edbf577d863cf8ae9fdbbd4590121036666dd712e05a487916384bfcd5973eb53e8038eccbbf97f7eed775b87389536ffffffff01c0aff629010000001976a9145eaaa4f458f9158f86afcba08dd7448d27045e3d88ac00000000");
 }
 
 } // namespace TW::Taproot::tests
