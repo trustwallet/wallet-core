be95443501ebf9b773e52e37d1edd96e81714d29
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-14 17:24:08 +0000

    add test proto_sign_input_p2pkh_output_p2pkh

--- rust/tw_bitcoin/src/tests/p2pkh.rs
+++ rust/tw_bitcoin/src/tests/p2pkh.rs
@@ -7,23 +7,23 @@ use tw_encoding::hex;
 pub const ALICE_WIF: &str = "cQUNzeMnF9xPPLqZhH7hMVYGwSuu3b78zznuc5UrxgXnYQBq6Bx1";
 pub const BOB_WIF: &str = "cTk5wSci88FPka7JwHpNEA82dUMjAysdDbCiuYB2fegfgGESAZVn";
 // Mined block, value 50 BTC, vout: 0
-pub const GENESIS_TXID: &str = "1e1cdc48aa990d7e154a161d5b5f1cad737742e97d2712ab188027bb42e6e47b";
+pub const TXID: &str = "1e1cdc48aa990d7e154a161d5b5f1cad737742e97d2712ab188027bb42e6e47b";
+
+pub const FULL_AMOUNT: u64 = ONE_BTC * 50;
+pub const MINER_FEE: u64 = ONE_BTC / 100;
+pub const SEND_AMOUNT: u64 = FULL_AMOUNT - MINER_FEE;
 
 #[test]
 fn sign_input_p2pkh_output_p2pkh() {
     // This passed the `bitcoin-cli -retest testmempoolaccept` command.
     const EXPECTED_RAW_SIGNED: &str = "02000000017be4e642bb278018ab12277de9427773ad1c5f5b1d164a157e0d99aa48dc1c1e000000006a473044022078eda020d4b86fcb3af78ef919912e6d79b81164dbbb0b0b96da6ac58a2de4b102201a5fd8d48734d5a02371c4b5ee551a69dca3842edbf577d863cf8ae9fdbbd4590121036666dd712e05a487916384bfcd5973eb53e8038eccbbf97f7eed775b87389536ffffffff01c0aff629010000001976a9145eaaa4f458f9158f86afcba08dd7448d27045e3d88ac00000000";
 
-    const FULL_AMOUNT: u64 = ONE_BTC * 50;
-    const MINER_FEE: u64 = ONE_BTC / 100;
-    const SEND_AMOUNT: u64 = FULL_AMOUNT - MINER_FEE;
-
     let alice = keypair_from_wif(ALICE_WIF).unwrap();
     let bob = keypair_from_wif(BOB_WIF).unwrap();
 
     // Prepare inputs for Alice.
     let input = TxInputP2PKH::builder()
-        .txid(Txid::from_str(GENESIS_TXID).unwrap())
+        .txid(Txid::from_str(TXID).unwrap())
         .vout(0)
         .recipient(alice)
         .satoshis(FULL_AMOUNT)
