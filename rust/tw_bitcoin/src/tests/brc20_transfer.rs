957c3eb86d2530fe17000f3e56227297a1ef881e
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-15 23:50:59 +0000

    commit, reveal and transfer tokens

--- rust/tw_bitcoin/src/tests/brc20_transfer.rs
+++ rust/tw_bitcoin/src/tests/brc20_transfer.rs
@@ -26,7 +26,7 @@ fn mainnet_brc20_transfer() {
     let BOB_WIF: &str = env!("BOB_WIF");
 
     let alice = keypair_from_wif(ALICE_WIF).unwrap();
-    let bob = keypair_from_wif(ALICE_WIF).unwrap();
+    let bob = keypair_from_wif(BOB_WIF).unwrap();
 
     let txid = Txid::from_str("1121ea22e37b65639a6987b274090e03eaa148dbd02769e616c924148b8fa49b").unwrap();
 
@@ -103,4 +103,55 @@ fn mainnet_brc20_transfer() {
 	assert_ne!(hex[164..292], EXPECTED[164..292]);
 
 	println!("{hex}");
+
+    let txid_for_fees = Txid::from_str("df800a5234945b46574426ff1e99674deaea3749a50838542bd5f909c56ea93c").unwrap();
+    let txid_for_brc20_transfer = Txid::from_str("64b6dda9a545826b906473c9d84f9afc22783c719b5c84b10f77810b292ce75d").unwrap();
+
+	const FEE_AMOUNT: u64 = FULL_AMOUNT - BRC20_INSCRIBE_AMOUNT - MINER_FEE;
+
+	// We use a normal P2WPKH output for this.
+    let input_for_brc20_transfer = TxInputP2WPKH::builder()
+        .txid(txid_for_brc20_transfer)
+        .vout(0)
+        .recipient(alice.try_into().unwrap())
+        .satoshis(BRC20_DUST_AMOUNT)
+        .build()
+        .unwrap();
+
+    let input_for_fee = TxInputP2WPKH::builder()
+        .txid(txid_for_fees)
+        .vout(1)
+        .recipient(alice.try_into().unwrap())
+        .satoshis(FEE_AMOUNT)
+        .build()
+        .unwrap();
+
+	// We transfer the tokens to Bob.
+    let output_brc20_transfer = TxOutputP2WPKH::builder()
+        .recipient(bob.try_into().unwrap())
+        .satoshis(BRC20_DUST_AMOUNT)
+        .build()
+        .unwrap();
+
+    let output_change = TxOutputP2WPKH::builder()
+        .recipient(alice.try_into().unwrap())
+        .satoshis(FEE_AMOUNT - MINER_FEE)
+        .build()
+        .unwrap();
+
+	// We carefully add the BRC20 transfer in the first position for both input and output.
+    let transaction = TransactionBuilder::new()
+        .add_input(input_for_brc20_transfer.into())
+        .add_input(input_for_fee.into())
+        .add_output(output_brc20_transfer.into())
+        .add_output(output_change.into())
+        .sign_inputs(alice)
+        .unwrap()
+        .serialize()
+        .unwrap();
+
+	const EXPECTED_2: &str = "020000000001025de72c290b81770fb1845c9b713c7822fc9a4fd8c97364906b8245a5a9ddb6640000000000ffffffff3ca96ec509f9d52b543808a54937eaea4d67991eff264457465b9434520a80df0100000000ffffffff022202000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d2067000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d02473044022030825dfa9dc9568665c193aeba457266238110d81d9022eafe7a2ca6601d8f8102205a636809f3161f84b9fb8111ef761d72640a8b03ff2b9274aa53e265c774c5c50121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb02483045022100ad575c7ed118bdb762affe535cbc2eed54b5219e4f4379d989113a3cdf2623d402207a5ae68a056189a31f9245efa581d6f04c929ca175cc3476ace8808c61e3d70c0121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000";
+
+	let hex = hex::encode(&transaction, false);
+	println!("> {hex}");
 }
