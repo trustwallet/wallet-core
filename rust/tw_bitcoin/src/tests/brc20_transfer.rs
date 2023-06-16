58576034d20d09f19559e2bb22d1f39ecaa9ff45
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-16 00:19:42 +0000

    update brc20_transfer test

--- rust/tw_bitcoin/src/tests/brc20_transfer.rs
+++ rust/tw_bitcoin/src/tests/brc20_transfer.rs
@@ -12,8 +12,8 @@ use tw_encoding::hex;
 fn mainnet_brc20_transfer() {
     const TXID: &str = "";
 
-	pub const FULL_AMOUNT: u64 = 40_000;
-	pub const MINER_FEE: u64 = 3_300;
+	pub const FULL_AMOUNT: u64 = 26_400;
+	pub const MINER_FEE: u64 = 3_000;
 
 	const BRC20_TICKER: &str = "oadf";
 	const BRC20_AMOUNT: usize = 20;
@@ -28,7 +28,9 @@ fn mainnet_brc20_transfer() {
     let alice = keypair_from_wif(ALICE_WIF).unwrap();
     let bob = keypair_from_wif(BOB_WIF).unwrap();
 
-    let txid = Txid::from_str("1121ea22e37b65639a6987b274090e03eaa148dbd02769e616c924148b8fa49b").unwrap();
+    let txid = Txid::from_str("8ec895b4d30adb01e38471ca1019bfc8c3e5fbd1f28d9e7b5653260d89989008").unwrap();
+
+	// # Make "available" tokens "transferable".
 
 	// Commit transfer.
     let input = TxInputP2WPKH::builder()
@@ -63,11 +65,11 @@ fn mainnet_brc20_transfer() {
         .unwrap();
 
 	let hex = hex::encode(&transaction, false);
-	assert_eq!(hex, "020000000001019ba48f8b1424c916e66927d0db48a1ea030e0974b287699a63657be322ea21110100000000ffffffff02581b000000000000225120e8b706a97732e705e22ae7710703e7f589ed13c636324461afa443016134cc050474000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d02483045022100ed8e58f97f39a4495b5e8701b3a0838b60b9d36ef74dd3feda044fb0c0337ac50220429f32cf166c3d7529c9627aff1c8ddb8802fb2cde8ad1afe61d8aef7b54b5ec0121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000");
-	println!("{hex}");
+	assert_eq!(hex, "02000000000101089098890d2653567b9e8df2d1fbe5c3c8bf1910ca7184e301db0ad3b495c88e0100000000ffffffff02581b000000000000225120e8b706a97732e705e22ae7710703e7f589ed13c636324461afa443016134cc051040000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d02483045022100a44aa28446a9a886b378a4a65e32ad9a3108870bd725dc6105160bed4f317097022069e9de36422e4ce2e42b39884aa5f626f8f94194d1013007d5a1ea9220a06dce0121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000");
+	println!("> {hex}");
 	
 	// Reveal transfer.
-    let txid = Txid::from_str("df800a5234945b46574426ff1e99674deaea3749a50838542bd5f909c56ea93c").unwrap();
+    let txid = Txid::from_str("797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1").unwrap();
 
     let input = TxInputP2TRScriptPath::builder()
         .txid(txid)
@@ -93,21 +95,16 @@ fn mainnet_brc20_transfer() {
         .serialize()
         .unwrap();
 
-	const EXPECTED: &str = "020000000001013ca96ec509f9d52b543808a54937eaea4d67991eff264457465b9434520a80df0000000000ffffffff012202000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d0340eb824d08b9f2c167687c21d5a28705f5066452e19aba05747d80ae019c871d18c666dda1878d40c5e0664254f9dadc587c1538d118eca7a318fc1afbb047581c5b0063036f7264010118746578742f706c61696e3b636861727365743d7574662d3800377b2270223a226272632d3230222c226f70223a227472616e73666572222c227469636b223a226f616466222c22616d74223a223230227d6821c00f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000";
+	const EXPECTED: &str = "02000000000101b11f1782607a1fe5f033ccf9dc17404db020a0dedff94183596ee67ad4177d790000000000ffffffff012202000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d0340de6fd13e43700f59876d305e5a4a5c41ad7ada10bc5a4e4bdd779eb0060c0a78ebae9c33daf77bb3725172edb5bd12e26f00c08f9263e480d53b93818138ad0b5b0063036f7264010118746578742f706c61696e3b636861727365743d7574662d3800377b2270223a226272632d3230222c226f70223a227472616e73666572222c227469636b223a226f616466222c22616d74223a223230227d6821c00f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000";
 	let hex = hex::encode(&transaction, false);
+	println!("> {hex}");
 
-	assert_eq!(hex[..164], EXPECTED[..164]);
-	assert_eq!(hex[292..], EXPECTED[292..]);
-	// We skip the 64-byte Schnorr signature, which uses random data each time
-	// and is therefore not reproducible.
-	assert_ne!(hex[164..292], EXPECTED[164..292]);
+	// # Actually transfer the "transferable" tokens.
 
-	println!("{hex}");
+    let txid_for_fees = Txid::from_str("797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1").unwrap();
+    let txid_for_brc20_transfer = Txid::from_str("7046dc2689a27e143ea2ad1039710885147e9485ab6453fa7e87464aa7dd3eca").unwrap();
 
-    let txid_for_fees = Txid::from_str("df800a5234945b46574426ff1e99674deaea3749a50838542bd5f909c56ea93c").unwrap();
-    let txid_for_brc20_transfer = Txid::from_str("64b6dda9a545826b906473c9d84f9afc22783c719b5c84b10f77810b292ce75d").unwrap();
-
-	const FEE_AMOUNT: u64 = FULL_AMOUNT - BRC20_INSCRIBE_AMOUNT - MINER_FEE;
+	const FOR_FEE_AMOUNT: u64 = FULL_AMOUNT - BRC20_INSCRIBE_AMOUNT - MINER_FEE;
 
 	// We use a normal P2WPKH output for this.
     let input_for_brc20_transfer = TxInputP2WPKH::builder()
@@ -122,7 +119,7 @@ fn mainnet_brc20_transfer() {
         .txid(txid_for_fees)
         .vout(1)
         .recipient(alice.try_into().unwrap())
-        .satoshis(FEE_AMOUNT)
+        .satoshis(FOR_FEE_AMOUNT)
         .build()
         .unwrap();
 
@@ -135,7 +132,7 @@ fn mainnet_brc20_transfer() {
 
     let output_change = TxOutputP2WPKH::builder()
         .recipient(alice.try_into().unwrap())
-        .satoshis(FEE_AMOUNT - MINER_FEE)
+        .satoshis(FOR_FEE_AMOUNT - MINER_FEE)
         .build()
         .unwrap();
 
@@ -150,7 +147,7 @@ fn mainnet_brc20_transfer() {
         .serialize()
         .unwrap();
 
-	const EXPECTED_2: &str = "020000000001025de72c290b81770fb1845c9b713c7822fc9a4fd8c97364906b8245a5a9ddb6640000000000ffffffff3ca96ec509f9d52b543808a54937eaea4d67991eff264457465b9434520a80df0100000000ffffffff022202000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d2067000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d02473044022030825dfa9dc9568665c193aeba457266238110d81d9022eafe7a2ca6601d8f8102205a636809f3161f84b9fb8111ef761d72640a8b03ff2b9274aa53e265c774c5c50121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb02483045022100ad575c7ed118bdb762affe535cbc2eed54b5219e4f4379d989113a3cdf2623d402207a5ae68a056189a31f9245efa581d6f04c929ca175cc3476ace8808c61e3d70c0121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000";
+	const EXPECTED_2: &str = "02000000000102ca3edda74a46877efa5364ab85947e148508713910ada23e147ea28926dc46700000000000ffffffffb11f1782607a1fe5f033ccf9dc17404db020a0dedff94183596ee67ad4177d790100000000ffffffff022202000000000000160014e891850afc55b64aa8247b2076f8894ebdf889015834000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d024830450221008798393eb0b7390217591a8c33abe18dd2f7ea7009766e0d833edeaec63f2ec302200cf876ff52e68dbaf108a3f6da250713a9b04949a8f1dcd1fb867b24052236950121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb0248304502210096bbb9d1f0596d69875646689e46f29485e8ceccacde9d0025db87fd96d3066902206d6de2dd69d965d28df3441b94c76e812384ab9297e69afe3480ee4031e1b2060121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000";
 
 	let hex = hex::encode(&transaction, false);
 	println!("> {hex}");
