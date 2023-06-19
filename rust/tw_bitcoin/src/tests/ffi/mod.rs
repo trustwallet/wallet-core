04655b6d9cd10697fd6ae40066c3664ad58b0125
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-19 18:31:34 +0000

    add convenience wrappers over P2PKH and P2TR key-path FFIs

--- rust/tw_bitcoin/src/tests/ffi/mod.rs
+++ rust/tw_bitcoin/src/tests/ffi/mod.rs
@@ -1,4 +1,4 @@
-use crate::ffi::tw_build_p2wpkh_script;
+use crate::ffi::{tw_build_p2pkh_script, tw_build_p2tr_key_path_script, tw_build_p2wpkh_script};
 use crate::Recipient;
 use bitcoin::PublicKey;
 use std::borrow::Cow;
@@ -32,6 +32,53 @@ fn ffi_build_p2wpkh_script<'a, 'b>(
 
     let raw = unsafe {
         tw_build_p2wpkh_script(satoshis as i64, pubkey.as_ptr(), pubkey.len()).into_vec()
+    };
+
+    let des: TransactionOutput = tw_proto::deserialize(&raw).unwrap();
+
+    // We convert the referenced data into owned data since `raw` goes out of
+    // scope at the end of the function.
+    TransactionOutput {
+        value: des.value,
+        script: des.script.into_owned().into(),
+        spendingScript: des.spendingScript.into_owned().into(),
+    }
+}
+
+/// Convenience wrapper over `tw_build_p2pkh_script` with Protobuf
+/// deserialization support.
+fn ffi_build_p2pkh_script<'a, 'b>(
+    satoshis: u64,
+    // We use 'b to clarify that `recipient` is not tied to the return value.
+    recipient: &'b Recipient<PublicKey>,
+) -> TransactionOutput<'a> {
+    let pubkey = recipient.public_key().to_bytes();
+
+    let raw =
+        unsafe { tw_build_p2pkh_script(satoshis as i64, pubkey.as_ptr(), pubkey.len()).into_vec() };
+
+    let des: TransactionOutput = tw_proto::deserialize(&raw).unwrap();
+
+    // We convert the referenced data into owned data since `raw` goes out of
+    // scope at the end of the function.
+    TransactionOutput {
+        value: des.value,
+        script: des.script.into_owned().into(),
+        spendingScript: des.spendingScript.into_owned().into(),
+    }
+}
+
+/// Convenience wrapper over `tw_build_p2pkh_script` with Protobuf
+/// deserialization support.
+fn ffi_build_p2tr_key_path_script<'a, 'b>(
+    satoshis: u64,
+    // We use 'b to clarify that `recipient` is not tied to the return value.
+    recipient: &'b Recipient<PublicKey>,
+) -> TransactionOutput<'a> {
+    let pubkey = recipient.public_key().to_bytes();
+
+    let raw = unsafe {
+        tw_build_p2tr_key_path_script(satoshis as i64, pubkey.as_ptr(), pubkey.len()).into_vec()
     };
 
     let des: TransactionOutput = tw_proto::deserialize(&raw).unwrap();
