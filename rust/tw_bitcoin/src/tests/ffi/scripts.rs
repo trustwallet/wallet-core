1f0cf47a014fec27321621080f107a85e17d2bcf
lamafab <42901763+lamafab@users.noreply.github.com>
2023-06-19 18:37:11 +0000

    update imports in tests, add comments

--- rust/tw_bitcoin/src/tests/ffi/scripts.rs
+++ rust/tw_bitcoin/src/tests/ffi/scripts.rs
@@ -1,5 +1,5 @@
-use super::ffi_build_p2wpkh_script;
-use crate::ffi::{tw_build_p2pkh_script, tw_build_p2tr_key_path_script};
+use crate::ffi::tw_build_p2pkh_script;
+use crate::tests::ffi::{ffi_build_p2tr_key_path_script, ffi_build_p2wpkh_script};
 use crate::tests::p2pkh::ALICE_WIF;
 use crate::{keypair_from_wif, Recipient, TxOutputP2PKH, TxOutputP2TRKeyPath, TxOutputP2WPKH};
 use bitcoin::PublicKey;
@@ -8,6 +8,7 @@ use tw_proto::Bitcoin::Proto::TransactionOutput;
 
 #[test]
 fn build_ffi_p2pkh_script() {
+    // Prepare keys.
     let keypair: secp256k1::KeyPair = keypair_from_wif(ALICE_WIF).unwrap();
     let recipient = Recipient::<PublicKey>::from(keypair);
 
@@ -34,6 +35,7 @@ fn build_ffi_p2pkh_script() {
 
 #[test]
 fn build_ffi_p2wpkh_script() {
+    // Prepare keys.
     let keypair: secp256k1::KeyPair = keypair_from_wif(ALICE_WIF).unwrap();
     let recipient = Recipient::<PublicKey>::from(keypair);
 
@@ -55,19 +57,14 @@ fn build_ffi_p2wpkh_script() {
 
 #[test]
 fn build_ffi_p2tr_key_path_script() {
+    // Prepare keys.
     let keypair: secp256k1::KeyPair = keypair_from_wif(ALICE_WIF).unwrap();
     let recipient = Recipient::<PublicKey>::from(keypair);
 
     let satoshis: u64 = 1_000;
 
     // Call FFI function.
-    let alice_pubkey = recipient.public_key().to_bytes();
-    let array = unsafe {
-        tw_build_p2tr_key_path_script(satoshis as i64, alice_pubkey.as_ptr(), alice_pubkey.len())
-            .into_vec()
-    };
-
-    let ffi_der: TransactionOutput = tw_proto::deserialize(&array).unwrap();
+    let transaction = ffi_build_p2tr_key_path_script(satoshis, &recipient);
 
     // Compare with native call.
     let tx_out = TxOutputP2TRKeyPath::new(satoshis, recipient.try_into().unwrap());
@@ -77,5 +74,5 @@ fn build_ffi_p2tr_key_path_script() {
         spendingScript: Cow::default(),
     };
 
-    assert_eq!(ffi_der, proto);
+    assert_eq!(transaction, proto);
 }
