751e4d6ea6d19c744dd7852abea3286527ad1e09
Ruslan Serebriakov <rsrbk1@gmail.com>
2023-07-05 22:55:36 +0100

    Update CoinAddressDerivationTests.swift

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -19,6 +19,9 @@ class CoinAddressDerivationTests: XCTestCase {
                 let address = coin.address(string: derivedAddress)
 
                 switch coin {
+                case .acala:
+                    let expectedResult = "25GGezx3LWFQj6HZpYzoWoVzLsHojGtybef3vthC9nd19ms3"
+                    assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .aeternity:
                     let expectedResult = "ak_QDHJSfvHG9sDHBobaWt2TAGhuhipYjEqZEH34bWugpJfJc3GN"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
@@ -101,7 +104,9 @@ class CoinAddressDerivationTests: XCTestCase {
                      .kavaEvm,
                      .klaytn,
                      .meter,
-                     .okxchain:
+                     .okxchain,
+                     .confluxeSpace,
+                     .acalaEVM:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .ronin:
