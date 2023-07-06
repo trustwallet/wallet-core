7d581de10b82e8012608594ed201c8b7c97cae0d
Milerius <rmscastle@gmail.com>
2023-07-06 11:30:31 +0200

    feat(evm): add opbnb testnet

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -106,7 +106,7 @@ class CoinAddressDerivationTests: XCTestCase {
                      .meter,
                      .okxchain,
                      .confluxeSpace,
-                     .opBnbTestnet,
+                     .opBNBtestnet,
                      .acalaEVM:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
