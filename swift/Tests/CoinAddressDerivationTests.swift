d73f179d30d19411881779e00a3ce97004d17378
Pony <weixuefeng1018@gmail.com>
2023-07-14 14:34:27 +0800

    fix: ios test

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -105,13 +105,10 @@ class CoinAddressDerivationTests: XCTestCase {
                      .klaytn,
                      .meter,
                      .okxchain,
-<<<<<<< HEAD
-                     .gatechain:
-=======
                      .confluxeSpace,
                      .opBNBtestnet,
-                     .acalaEVM:
->>>>>>> master
+                     .acalaEVM,
+                     .gatechain:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .ronin:
