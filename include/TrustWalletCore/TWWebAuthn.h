9b50c2921a3c7ebf0866b84ba1aed6f0f1960d7f
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2023-07-17 09:03:47 +0200

    Sync `dev` with `master` (#3308)

--- include/TrustWalletCore/TWWebAuthn.h
+++ include/TrustWalletCore/TWWebAuthn.h
@@ -20,4 +20,19 @@ struct TWWebAuthn;
 /// \return Public key.
 TW_EXPORT_STATIC_METHOD
 struct TWPublicKey *_Nullable TWWebAuthnGetPublicKey(TWData *_Nonnull attestationObject);
+
+/// Uses ASN parser to extract r and s values from a webauthn signature
+///
+/// \param signature ASN encoded webauthn signature: https://www.w3.org/TR/webauthn-2/#sctn-signature-attestation-types
+/// \return Concatenated r and s values.
+TW_EXPORT_STATIC_METHOD
+TWData *_Nonnull TWWebAuthnGetRSValues(TWData *_Nonnull signature);
+
+/// Reconstructs the original message that was signed via P256 curve. Can be used for signature validation.
+///
+/// \param authenticatorData Authenticator Data: https://www.w3.org/TR/webauthn-2/#authenticator-data
+/// \param clientDataJSON clientDataJSON: https://www.w3.org/TR/webauthn-2/#dom-authenticatorresponse-clientdatajson
+/// \return original messages.
+TW_EXPORT_STATIC_METHOD
+TWData *_Nonnull TWWebAuthnReconstructOriginalMessage(TWData* _Nonnull authenticatorData, TWData* _Nonnull clientDataJSON);
 TW_EXTERN_C_END
--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -106,6 +106,7 @@ class CoinAddressDerivationTests: XCTestCase {
                      .meter,
                      .okxchain,
                      .confluxeSpace,
+                     .opBNBtestnet,
                      .acalaEVM:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
