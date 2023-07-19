4588834c3eb3bb4bef3de596af09fa0248a85bf8
lamafab <42901763+lamafab@users.noreply.github.com>
2023-07-19 17:42:02 +0200

    init Kotlin test for fee calculation

---
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/bitcoin/TestBitcoinFee.kt
@@ -0,0 +1,19 @@
+package com.trustwallet.core.app.blockchains.bitcoin
+
+import wallet.core.jni.Bitcoin
+
+class TestBitcoinFeeCalculation {
+
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
+
+    @Test
+    fun P2pkhFee() {
+		val satVb = 19;
+		val tx = Numeric.hexStringToByteArray("02000000017be4e642bb278018ab12277de9427773ad1c5f5b1d164a157e0d99aa48dc1c1e000000006a473044022078eda020d4b86fcb3af78ef919912e6d79b81164dbbb0b0b96da6ac58a2de4b102201a5fd8d48734d5a02371c4b5ee551a69dca3842edbf577d863cf8ae9fdbbd4590121036666dd712e05a487916384bfcd5973eb53e8038eccbbf97f7eed775b87389536ffffffff01c0aff629010000001976a9145eaaa4f458f9158f86afcba08dd7448d27045e3d88ac00000000")
+		var fee = Bitcion.Transaction.calculateFee(tx, satVb)
+
+        assertEquals(fee, 191 * satVb)
+    }
+}
