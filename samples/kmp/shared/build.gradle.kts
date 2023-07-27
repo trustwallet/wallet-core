31c823427f1f7d2de942d9402cc6ff5b520241e4
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-07-27 16:06:01 +0200

    fix(btc): Update `wallet-core-kotlin` dep

--- samples/kmp/shared/build.gradle.kts
+++ samples/kmp/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.trustwallet:wallet-core-kotlin:3.2.5")
+                implementation("com.trustwallet:wallet-core-kotlin:3.2.6")
             }
         }
         val commonTest by getting {
