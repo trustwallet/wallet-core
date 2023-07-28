ac6ab64d6d25763a42040fe815be1822b4a336ec
lamafab <42901763+lamafab@users.noreply.github.com>
2023-07-28 18:02:00 +0000

    simplify utxo proto

--- rust/tw_utxo/src/lib.rs
+++ rust/tw_utxo/src/lib.rs
@@ -1,16 +1,16 @@
 use bitcoin::blockdata::locktime::absolute::{Height, LockTime, Time};
 use bitcoin::hashes::Hash;
-use bitcoin::{secp256k1, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, Txid, Witness};
+use bitcoin::{secp256k1, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Txid, Witness};
 use std::marker::PhantomData;
 use tw_coin_entry::coin_entry::{CoinAddress, CoinEntry, PublicKeyBytes, SignatureBytes};
 use tw_coin_entry::error::SigningResult;
 use tw_proto::Utxo::Proto;
 
-type ProtoLockTimeVariant = Proto::mod_SigningInput::OneOflock_time;
-type ProtoSpendingData<'a> = Proto::mod_TxIn::OneOfspending_data<'a>;
-
 pub mod compiler;
 pub mod entry;
+
+type ProtoLockTimeVariant = Proto::mod_SigningInput::OneOflock_time;
+type ProtoSpendingData<'a> = Proto::mod_TxIn::OneOfspending_data<'a>;
 
 pub trait UtxoContext {
     type SigningInput<'a>;
@@ -41,49 +41,67 @@ impl Signer<StandardBitcoinContext> {
         proto: Proto::SigningInput<'_>,
     ) -> SigningResult<Proto::SigningInput<'static>> {
         let private_key = secp256k1::SecretKey::from_slice(proto.private_key.as_ref()).unwrap();
-        let version = proto.version;
 
-        let lock_time = match proto.lock_time {
-            ProtoLockTimeVariant::blocks(block) => {
-                LockTime::Blocks(Height::from_consensus(block).unwrap())
-            },
-            ProtoLockTimeVariant::seconds(secs) => {
-                LockTime::Seconds(Time::from_consensus(secs).unwrap())
-            },
-            ProtoLockTimeVariant::None => LockTime::Blocks(Height::from_consensus(0).unwrap()),
-        };
+        let tx = convert_proto_to_tx(&proto).unwrap();
 
-        let mut tx = Transaction {
-            version,
-            lock_time,
-            input: vec![],
-            output: vec![],
-        };
-
-        for input in proto.inputs {
-            let txin = input.txin.unwrap();
-            let txid = Txid::from_slice(txin.txid.as_ref()).unwrap();
-            let vout = txin.vout;
-
-            let (script_sig, witness) = match txin.spending_data {
-                ProtoSpendingData::script_isg(script) => {
-                    (ScriptBuf::from_bytes(script.to_vec()), Witness::new())
-                },
-                ProtoSpendingData::witness(witness) => {
-                    // TODO: Should be `repeated Witness...`
-                    (ScriptBuf::new(), Witness::from_slice(&[witness.as_ref()]))
-                },
-                ProtoSpendingData::None => panic!(),
-            };
-
-            tx.input.push(TxIn {
-                previous_output: OutPoint { txid, vout },
-                script_sig,
-                sequence: Sequence::default(),
-                witness,
-            });
+        for (index, input) in tx.input.iter().enumerate() {
+            //match proto.inputs.get(index).expect("index miscalculated").variant {
         }
 
         todo!()
     }
+}
+
+fn convert_proto_to_tx<'a>(proto: &Proto::SigningInput<'a>) -> Result<Transaction, ()> {
+    let version = proto.version;
+
+    // Retreive the lock time. If none is provided, the default lock time is
+    // used (immediately spendable).
+    let lock_time = match proto.lock_time {
+        ProtoLockTimeVariant::blocks(block) => {
+            LockTime::Blocks(Height::from_consensus(block).unwrap())
+        },
+        ProtoLockTimeVariant::seconds(secs) => {
+            LockTime::Seconds(Time::from_consensus(secs).unwrap())
+        },
+        ProtoLockTimeVariant::None => LockTime::Blocks(Height::from_consensus(0).unwrap()),
+    };
+
+    let mut tx = Transaction {
+        version,
+        lock_time,
+        input: vec![],
+        output: vec![],
+    };
+
+    for txin in &proto.inputs {
+        let txid = Txid::from_slice(txin.txid.as_ref()).unwrap();
+        let vout = txin.vout;
+
+        let (script_sig, witness) = match &txin.spending_data {
+            ProtoSpendingData::script_sig(script) => {
+                (ScriptBuf::from_bytes(script.to_vec()), Witness::new())
+            },
+            ProtoSpendingData::witness(witness) => {
+                (ScriptBuf::new(), Witness::from_slice(&witness.items))
+            },
+            ProtoSpendingData::None => panic!(),
+        };
+
+        tx.input.push(TxIn {
+            previous_output: OutPoint { txid, vout },
+            script_sig,
+            sequence: Sequence::default(),
+            witness,
+        });
+    }
+
+    for txout in &proto.outputs {
+        tx.output.push(TxOut {
+            value: txout.value,
+            script_pubkey: ScriptBuf::from_bytes(txout.script_pubkey.to_vec()),
+        });
+    }
+
+    Ok(tx)
 }
