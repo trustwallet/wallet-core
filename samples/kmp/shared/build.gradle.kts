0102a1c812bc97953f74c815b9aff13ea002c11a
Milerius <rmscastle@gmail.com>
2023-08-01 08:47:50 +0200

    feat(kotlin): update to 3.2.7

--- samples/kmp/shared/build.gradle.kts
+++ samples/kmp/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.trustwallet:wallet-core-kotlin:3.2.6")
+                implementation("com.trustwallet:wallet-core-kotlin:3.2.7")
             }
         }
         val commonTest by getting {
