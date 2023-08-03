8bb4112d9a496c2504b70dace5de1cc93b70f222
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-03 08:52:18 +0000

    move error variants to Utxo.proto

--- rust/tw_utxo/src/lib.rs
+++ rust/tw_utxo/src/lib.rs
@@ -1,32 +1,27 @@
-use bitcoin::blockdata::locktime::absolute::{Height, LockTime, Time};
-use bitcoin::hashes::Hash;
-use bitcoin::sighash::{EcdsaSighashType, Prevouts, SighashCache, TapSighashType};
-use bitcoin::taproot::TapLeafHash;
-use bitcoin::{OutPoint, Script, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Txid, Witness};
-use std::borrow::Cow;
-use std::marker::PhantomData;
-use tw_coin_entry::error::SigningResult;
-use tw_proto::Utxo::Proto::{self, SighashType};
+use tw_proto::Utxo::Proto;
 
 pub mod builder;
 pub mod compiler;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
-pub enum Error {
-    SighashFailed(bitcoin::sighash::Error),
-    InvalidWpkhScriptPubkey,
-    InvalidLeafHash,
-    InvalidSighashType,
-    InvalidLockTime,
-    InvalidTxid,
-    OnePrevoutOutOfBound,
-    MissingSighashMethod,
+#[derive(Debug)]
+pub struct Error(Proto::Error);
+
+impl From<Proto::Error> for Error {
+    fn from(value: Proto::Error) -> Self {
+        Error(value)
+    }
 }
 
 impl From<bitcoin::sighash::Error> for Error {
-    fn from(value: bitcoin::sighash::Error) -> Self {
-        Error::SighashFailed(value)
+    fn from(_value: bitcoin::sighash::Error) -> Self {
+        Error(Proto::Error::Error_sighash_failed)
+    }
+}
+
+impl From<Error> for Proto::Error {
+    fn from(value: Error) -> Self {
+        value.0
     }
 }
