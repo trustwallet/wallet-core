69369268438fcf130213d13a2af81b83b4bfb21e
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-09 12:27:14 +0000

    start planner, derive transaction variant from address

--- rust/tw_bitcoin/src/entry.rs
+++ rust/tw_bitcoin/src/entry.rs
@@ -1,5 +1,9 @@
+use crate::Result;
 use std::fmt::Display;
-
+use bitcoin::key::TweakedPublicKey;
+use bitcoin::{ScriptBuf, WPubkeyHash};
+use secp256k1::XOnlyPublicKey;
+use secp256k1::hashes::Hash;
 use tw_coin_entry::coin_context::CoinContext;
 use tw_coin_entry::coin_entry::{CoinAddress, CoinEntry, PublicKeyBytes, SignatureBytes};
 use tw_coin_entry::derivation::Derivation;
@@ -8,6 +12,7 @@ use tw_coin_entry::modules::json_signer::JsonSigner;
 use tw_coin_entry::modules::plan_builder::NoPlanBuilder;
 use tw_coin_entry::prefix::NoPrefix;
 use tw_coin_entry::modules::plan_builder::PlanBuilder;
+use bitcoin::address::{NetworkUnchecked, NetworkChecked, Payload, WitnessVersion};
 use tw_keypair::tw::{PrivateKey, PublicKey};
 use tw_proto::Utxo::Proto as UtxoProto;
 use tw_proto::Bitcoin::Proto as BitcoinProto;
@@ -47,7 +52,64 @@ impl PlanBuilder for LegacyPlanBuilder {
     type SigningInput<'a> = BitcoinProto::SigningInput<'a>;
     type Plan = BitcoinProto::TransactionPlan<'static>;
 
-    fn plan(&self, _coin: &dyn CoinContext, _input: Self::SigningInput<'_>) -> Self::Plan {
+    fn plan(&self, _coin: &dyn CoinContext, input: Self::SigningInput<'_>) -> Self::Plan {
+        let address = Address::from_str(input.to_address.as_ref()).unwrap().0;
+
+        let script_pubkey = match address.address_type().unwrap() {
+            bitcoin::AddressType::P2pkh => {
+                if let Payload::PubkeyHash(ref hash) = address.payload {
+                    ScriptBuf::new_p2pkh(hash)
+                } else {
+                    panic!()
+                }
+            },
+            bitcoin::AddressType::P2sh => {
+                if let Payload::ScriptHash(ref hash) = address.payload {
+                    ScriptBuf::new_p2sh(hash)
+                } else {
+                    panic!()
+                }
+            },
+            bitcoin::AddressType::P2wpkh => {
+                if let Payload::WitnessProgram(program) = address.payload {
+                    // TODO
+                    assert_eq!(program.version(), WitnessVersion::V0);
+                    let slice = program.program().as_bytes();
+                    let wpubkey_hash = WPubkeyHash::from_slice(slice);
+                    ScriptBuf::new_v0_p2wpkh(wpubkey_hash)
+                } else {
+                    panic!()
+                }
+            },
+            bitcoin::AddressType::P2wsh => todo!(),
+            bitcoin::AddressType::P2tr => {
+                if let Payload::WitnessProgram(program) = address.payload {
+                    // TODO
+                    assert_eq!(program.version(), WitnessVersion::V1);
+                    let slice = program.program().as_bytes();
+                    let xonly = XOnlyPublicKey::from_slice(slice).unwrap();
+                    // TODO: another way to do this?
+                    let output_key = TweakedPublicKey::dangerous_assume_tweaked(xonly);
+                    ScriptBuf::new_v1_p2tr_tweaked(output_key)
+                } else {
+                    panic!()
+                }
+            },
+            _ => todo!()
+        };
+
+        let plan = BitcoinProto::TransactionPlan {
+            amount: ,
+            available_amount: ,
+            fee: ,
+            change: ,
+            utxos: ,
+            branch_id: ,
+            error: ,
+            output_op_return: ,
+            
+        };
+
         todo!()
     }
 }
@@ -69,6 +131,21 @@ impl BitcoinEntry {
     fn plan_builder_legacy(&self) -> Option<LegacyPlanBuilder> {
         None
     }
+}
+
+pub struct Address(bitcoin::address::Address<NetworkChecked>);
+
+impl Address {
+    // TODO: Implement `FromStr`
+    pub fn from_str(string: &str) -> Result<Self> {
+        let addr: bitcoin::address::Address<NetworkChecked> = string.parse::<bitcoin::address::Address<_>>().unwrap().require_network(bitcoin::Network::Bitcoin).unwrap();
+        Ok(Address(addr))
+    }
+}
+
+#[test]
+fn address() {
+    let x = Address::from_str("bc1qunq74p3h8425hr6wllevlvqqr6sezfxj262rff");
 }
 
 impl CoinEntry for BitcoinEntry {
