6dc70c9e022917e18fe7f88e74f830a5d5aa0446
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-11 14:18:59 +0000

    handle p2wpkh builder

--- rust/tw_bitcoin/src/entry.rs
+++ rust/tw_bitcoin/src/entry.rs
@@ -4,7 +4,7 @@ use bitcoin::address::{NetworkChecked, Payload};
 use bitcoin::consensus::encode::Encodable;
 use bitcoin::taproot::ControlBlock;
 use bitcoin::{
-    OutPoint, PubkeyHash, ScriptBuf, ScriptHash, Sequence, Transaction, TxIn, TxOut, Txid, Witness,
+    OutPoint, PubkeyHash, ScriptBuf, ScriptHash, Sequence, Transaction, TxIn, TxOut, Txid, Witness, WPubkeyHash,
 };
 use secp256k1::hashes::Hash;
 use std::borrow::Cow;
@@ -145,6 +145,22 @@ impl CoinEntry for BitcoinEntry {
                             };
 
                             ScriptBuf::new_p2pkh(&pubkey_hash)
+                        },
+                        Proto::mod_Builder::OneOftype_pb::p2wsh(_) => {
+                            todo!()
+                        },
+                        Proto::mod_Builder::OneOftype_pb::p2wpkh(pubkey_or_hash) => {
+                            let wpubkey_hash = match &pubkey_or_hash.to_address {
+                                Proto::mod_Builder::mod_ToPublicKeyOrHash::OneOfto_address::hash(hash) => {
+                                    WPubkeyHash::from_slice(hash.as_ref()).unwrap()
+                                }
+                                Proto::mod_Builder::mod_ToPublicKeyOrHash::OneOfto_address::pubkey(pubkey) => {
+                                    bitcoin::PublicKey::from_slice(pubkey.as_ref()).unwrap().wpubkey_hash().unwrap()
+                                }
+                                Proto::mod_Builder::mod_ToPublicKeyOrHash::OneOfto_address::None => todo!(),
+                            };
+
+                            ScriptBuf::new_v0_p2wpkh(&wpubkey_hash)
                         },
                         _ => todo!(),
                     }
