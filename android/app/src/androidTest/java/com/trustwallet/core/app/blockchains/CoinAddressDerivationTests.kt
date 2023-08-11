aa7475536e8c5b0383b1553073139b3498a9e35f
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2023-08-11 11:29:25 +0200

    feat(sei): Add support for Sei cosmos chain (#3362)

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
@@ -145,5 +145,6 @@ class CoinAddressDerivationTests {
         AKASH -> assertEquals("akash142j9u5eaduzd7faumygud6ruhdwme98qal870f", address)
         NOBLE -> assertEquals("noble142j9u5eaduzd7faumygud6ruhdwme98qc8l3wa", address)
         ROOTSTOCK -> assertEquals("0xA2D7065F94F838a3aB9C04D67B312056846424Df", address)
+        SEI -> assertEquals("sei142j9u5eaduzd7faumygud6ruhdwme98qagm0sj", address)
     }
 }
