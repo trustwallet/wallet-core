1f3f321a751b5cefa35cbce14a8fe484b568c4ae
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-08-14 13:29:45 +0200

    feat(sui): Support PreHash and Compile for Sui

--- src/Sui/Entry.cpp
+++ src/Sui/Entry.cpp
@@ -9,6 +9,8 @@
 #include "Address.h"
 #include "Signer.h"
 
+#include "proto/TransactionCompiler.pb.h"
+
 namespace TW::Sui {
 
 bool Entry::validateAddress([[maybe_unused]] TWCoinType coin, const std::string& address, [[maybe_unused]] const PrefixVariant& addressPrefix) const {
@@ -21,6 +23,23 @@ std::string Entry::deriveAddress([[maybe_unused]] TWCoinType coin, const PublicK
 
 void Entry::sign([[maybe_unused]] TWCoinType coin, const TW::Data& dataIn, TW::Data& dataOut) const {
     signTemplate<Signer, Proto::SigningInput>(dataIn, dataOut);
+}
+
+Data Entry::preImageHashes([[maybe_unused]] TWCoinType coin, const Data& txInputData) const {
+    return txCompilerTemplate<Proto::SigningInput, TxCompiler::Proto::PreSigningOutput>(
+        txInputData, [](const auto& input, auto& output) {
+            output = Signer::preImageHashes(input);
+        });
+}
+
+void Entry::compile([[maybe_unused]] TWCoinType coin, const Data& txInputData, const std::vector<Data>& signatures, const std::vector<PublicKey>& publicKeys, Data& dataOut) const {
+    dataOut = txCompilerSingleTemplate<Proto::SigningInput, Proto::SigningOutput>(
+        txInputData, signatures, publicKeys,
+        [](const auto& input, auto& output, const auto& signature, const auto& publicKey) {
+            auto txSignatureScheme = Signer::signatureScheme(signature, publicKey);
+            output.set_unsigned_tx(input.sign_direct_message().unsigned_tx_msg());
+            output.set_signature(txSignatureScheme);
+        });
 }
 
 } // namespace TW::Sui
