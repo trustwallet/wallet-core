f481fc3ccebbb6d82cb096476184a3d8486d628a
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-16 14:42:08 +0000

    add custom From<T> handlers for certain error types

--- rust/tw_bitcoin/src/lib.rs
+++ rust/tw_bitcoin/src/lib.rs
@@ -34,6 +34,24 @@ impl From<Error> for Proto::Error {
     }
 }
 
+impl From<bitcoin::key::Error> for Error {
+    fn from(_: bitcoin::key::Error) -> Self {
+        Error::from(Proto::Error::Error_invalid_public_key)
+    }
+}
+
+impl From<bitcoin::ecdsa::Error> for Error {
+    fn from(_: bitcoin::ecdsa::Error) -> Self {
+        Error::from(Proto::Error::Error_invalid_ecdsa_signature)
+    }
+}
+
+impl From<bitcoin::taproot::Error> for Error {
+    fn from(_: bitcoin::taproot::Error) -> Self {
+        Error::from(Proto::Error::Error_invalid_schnorr_signature)
+    }
+}
+
 use bitcoin::key::PublicKey;
 use bitcoin::script::ScriptBuf;
 use bitcoin::taproot::{TapNodeHash, TaprootSpendInfo};
