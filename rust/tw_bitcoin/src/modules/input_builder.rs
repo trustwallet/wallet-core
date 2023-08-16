aad1f53b44f43184025488dabdb7f329f87e60cc
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-16 15:28:15 +0000

    update error value on wpubkey_hash

--- rust/tw_bitcoin/src/modules/input_builder.rs
+++ rust/tw_bitcoin/src/modules/input_builder.rs
@@ -48,7 +48,7 @@ impl InputBuilder {
                         ScriptBuf::new_v0_p2wpkh(
                             &pubkey
                                 .wpubkey_hash()
-                                .ok_or(Error::from(Proto::Error::Error_invalid_public_key))?,
+                                .ok_or_else(|| Error::from(Proto::Error::Error_invalid_witness_pubkey_hash))?,
                         ),
                         None,
                         // witness bytes, scale factor NOT applied.
