4a3f274ada78878342ef3c630c8faac118a734d8
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-17 13:03:03 +0000

    correctly pass on sighash type for signature construction

--- rust/tw_bitcoin/src/modules/signer.rs
+++ rust/tw_bitcoin/src/modules/signer.rs
@@ -1,5 +1,6 @@
 use crate::{Error, Result};
 use bitcoin::key::{TapTweak, TweakedKeyPair};
+use bitcoin::sighash::{EcdsaSighashType, TapSighashType};
 use secp256k1::{KeyPair, Message, Secp256k1};
 use tw_coin_entry::coin_entry::{PrivateKeyBytes, SignatureBytes};
 use tw_misc::traits::ToBytesVec;
@@ -24,24 +25,34 @@ impl Signer {
                 .map_err(|_| Error::from(Proto::Error::Error_invalid_sighash))?;
 
             match entry.signing_method {
+                // Create a ECDSA signature for legacy and segwit transaction.
                 UtxoProto::SigningMethod::Legacy | UtxoProto::SigningMethod::Segwit => {
+                    let sighash_type =
+                        if let UtxoProto::SighashType::UseDefault = entry.sighash_type {
+                            EcdsaSighashType::All
+                        } else {
+                            EcdsaSighashType::from_consensus(entry.sighash_type as u32)
+                        };
+
                     let sig = bitcoin::ecdsa::Signature {
                         sig: keypair.secret_key().sign_ecdsa(sighash),
-                        // TODO
-                        hash_ty: bitcoin::sighash::EcdsaSighashType::All,
+                        hash_ty: sighash_type,
                     };
 
                     signatures.push(sig.serialize().to_vec());
                 },
+                // Create a Schnorr signature for taproot transactions.
                 UtxoProto::SigningMethod::TaprootAll
                 | UtxoProto::SigningMethod::TaprootOnePrevout => {
+                    // Note that `input.sighash_type = 0` is handled by the underlying library.
+                    let sighash_type = TapSighashType::from_consensus_u8(entry.sighash_type as u8)
+                        .map_err(|_| Error::from(Proto::Error::Error_invalid_sighash_type))?;
+
                     // Any empty leaf hash implies P2TR key-path (balance transfer)
                     if utxo.leaf_hash.is_empty() {
                         // Tweak keypair for P2TR key-path (ie. zeroed Merkle root).
                         let tapped: TweakedKeyPair = keypair.tap_tweak(&secp, None);
                         let tweaked = KeyPair::from(tapped);
-
-                        dbg!(&sighash);
 
                         // Construct the Schnorr signature.
                         #[cfg(not(test))]
@@ -54,19 +65,17 @@ impl Signer {
 
                         let sig = bitcoin::taproot::Signature {
                             sig: schnorr,
-                            // TODO.
-                            hash_ty: bitcoin::sighash::TapSighashType::Default,
+                            hash_ty: sighash_type,
                         };
-
-                        dbg!(&sig);
 
                         signatures.push(sig.to_vec());
                     }
                     // If it has a leaf hash, then it's a P2TR script-path (complex transaction)
                     else {
-                        // We do not tweak the key here since we're passing on
-                        // the "control block" when claiming, hence this signing
-                        // process is simpler that P2TR key-path.
+                        // We do not tweak the key here since the complex
+                        // spending condition(s) must take into account on who
+                        // is allowed to spend the input, hence this signing
+                        // process is simpler than for P2TR key-path.
                         let sig = bitcoin::taproot::Signature {
                             sig: keypair.sign_schnorr(sighash),
                             // TODO.
