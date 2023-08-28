2e2c853f3487bd1697691915a54edb9ecc16988a
lamafab <42901763+lamafab@users.noreply.github.com>
2023-08-28 16:19:30 +0000

    add test input_selector_select_ascending

--- rust/tw_utxo/tests/input_selection.rs
+++ rust/tw_utxo/tests/input_selection.rs
@@ -219,7 +219,7 @@ fn input_selector_one_input_required() {
         lock_time: Default::default(),
         inputs: vec![tx1.clone(), tx2.clone(), tx3.clone()],
         outputs: vec![out1.clone(), out2.clone()],
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: Default::default(),
         // DISABLE change output.
@@ -245,7 +245,7 @@ fn input_selector_one_input_required() {
         lock_time: Default::default(),
         inputs: vec![tx1.clone(), tx2.clone(), tx3.clone()],
         outputs: vec![out1.clone(), out2.clone()],
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: change_script.as_bytes().into(),
         // ENABLE change output.
@@ -315,7 +315,7 @@ fn input_selector_two_inputs_required() {
         outputs: vec![out1.clone(), out2.clone()],
         // We only select the necessary value of inputs to cover the output
         // value.
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: Default::default(),
         // DISABLE change output.
@@ -344,7 +344,7 @@ fn input_selector_two_inputs_required() {
         outputs: vec![out1.clone(), out2.clone()],
         // We only select the necessary value of inputs to cover the output
         // value.
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: change_script.as_bytes().into(),
         // ENABLE change output.
@@ -401,7 +401,7 @@ fn input_selector_one_input_cannot_cover_fees() {
         lock_time: Default::default(),
         inputs: vec![tx1.clone()],
         outputs: vec![out1.clone(), out2.clone()],
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: Default::default(),
         // DISABLE change output.
@@ -425,7 +425,7 @@ fn input_selector_one_input_cannot_cover_fees() {
         lock_time: Default::default(),
         inputs: vec![tx1.clone()],
         outputs: vec![out1.clone(), out2.clone()],
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: change_script.as_bytes().into(),
         // ENABLE change output.
@@ -469,7 +469,7 @@ fn input_selector_exact_balance_no_change() {
         lock_time: Default::default(),
         inputs: vec![tx1.clone()],
         outputs: vec![out1.clone(), out2.clone()],
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         change_script_pubkey: change_script.as_bytes().into(),
         // ENABLE change output.
@@ -518,7 +518,7 @@ fn input_selector_empty_script_bufs() {
         lock_time: Default::default(),
         inputs: vec![tx1.clone()],
         outputs: vec![out1.clone(), out2.clone()],
-        input_selector: Proto::InputSelector::SelectAscending,
+        input_selector: Proto::InputSelector::SelectInOrder,
         weight_base: WEIGHT_BASE,
         // NO change script_pubkey specified, results in an error.
         change_script_pubkey: Default::default(),
@@ -536,4 +536,62 @@ fn input_selector_empty_script_bufs() {
     assert_eq!(output.fee_estimate, 0);
     assert_eq!(output.inputs.len(), 0);
     assert_eq!(output.outputs.len(), 0);
+}
+
+#[test]
+fn input_selector_select_ascending() {
+    // Reusing the txid is fine here, although in production this would mark the transaction invalid.
+    let txid = txid_rev("1e1cdc48aa990d7e154a161d5b5f1cad737742e97d2712ab188027bb42e6e47b");
+
+    let tx1 = Proto::TxIn {
+        txid: txid.as_slice().into(),
+        value: 8_000,
+        sequence: u32::MAX,
+        ..Default::default()
+    };
+
+    let tx2 = Proto::TxIn {
+        txid: txid.as_slice().into(),
+        value: 4_000,
+        sequence: u32::MAX,
+        ..Default::default()
+    };
+
+    let tx3 = Proto::TxIn {
+        txid: txid.as_slice().into(),
+        value: 2_000,
+        sequence: u32::MAX,
+        ..Default::default()
+    };
+
+    let out1 = Proto::TxOut {
+        value: 3_000,
+        script_pubkey: Default::default(),
+    };
+
+    let change_script = change_output();
+    let signing = Proto::SigningInput {
+        version: 2,
+        lock_time: Default::default(),
+        inputs: vec![tx1.clone(), tx2.clone(), tx3.clone()],
+        outputs: vec![out1.clone()],
+        // Select in ASCENDING order:
+        input_selector: Proto::InputSelector::SelectAscending,
+        weight_base: WEIGHT_BASE,
+        change_script_pubkey: change_script.as_bytes().into(),
+        // ENABLE change output.
+        disable_change_output: false,
+    };
+
+    let output = Compiler::<StandardBitcoinContext>::preimage_hashes(signing);
+    assert_eq!(output.error, Proto::Error::OK);
+
+    // Two inputs where selected (in ASCENDING order).
+    assert_eq!(output.inputs.len(), 2);
+    assert_eq!(output.inputs[0], tx3);
+    assert_eq!(output.inputs[1], tx2);
+
+    // Two outputs; target and change.
+    assert_eq!(output.outputs.len(), 2);
+    assert_eq!(output.outputs[0], out1);
 }
