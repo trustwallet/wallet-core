d810b61bcf58ce0167b99e189776052c005de096
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-09-06 11:35:39 +0200

    feat(mantle): Add Mantle EVM-compatible chain

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -112,7 +112,8 @@ class CoinAddressDerivationTests: XCTestCase {
                      .neon,
                      .base,
                      .linea,
-                     .greenfield:
+                     .greenfield,
+                     .mantle:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .ronin:
--- tests/common/CoinAddressDerivationTests.cpp
+++ tests/common/CoinAddressDerivationTests.cpp
@@ -82,6 +82,7 @@ TEST(Coin, DeriveAddress) {
         case TWCoinTypeBase:
         case TWCoinTypeLinea:
         case TWCoinTypeGreenfield:
+        case TWCoinTypeMantle:
             EXPECT_EQ(address, "0x9d8A62f656a8d1615C1294fd71e9CFb3E4855A4F");
             break;
 
