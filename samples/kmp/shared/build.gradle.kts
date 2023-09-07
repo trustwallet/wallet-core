c86ab9d7701241e6bf1e756393ec37301bdf0a36
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2023-09-07 11:51:14 +0200

    [ZenEON]: Add support for ZenEON EVM-compatible chain (#3418)
    * feat(ZenEON): Add support for ZenEON EVM-compatible chain
    
    * feat(ZenEON): Fix Kotlin tests
    
    * feat(ZenEON): Fix kmp

--- samples/kmp/shared/build.gradle.kts
+++ samples/kmp/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.trustwallet:wallet-core-kotlin:3.2.17")
+                implementation("com.trustwallet:wallet-core-kotlin:3.2.20")
             }
         }
         val commonTest by getting {
