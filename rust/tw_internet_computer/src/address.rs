5bf492b779dd77d3cc21480fec2fdc4a1c050555
Ryan Croote <ryan.croote@dfinity.org>
2023-09-11 17:32:35 +0000

    Add docs and tests

--- rust/tw_internet_computer/src/address.rs
+++ rust/tw_internet_computer/src/address.rs
@@ -18,11 +18,22 @@ pub trait IcpAddress: std::str::FromStr<Err = AddressError> + Into<AccountIdenti
     }
 }
 
+/// The ICP ledger keeps track of accounts using account identifiers.
+/// An account identifier is created by `SHA-224` hashing:
+/// * \x0Aaccount-id
+/// * the owner's principal ID
+/// * subaccount (32-bytes)
+/// Then,
+/// * CRC32 the SHA-224 hash
+/// * Prepend the CRC32 to the SHA-224.
+/// https://internetcomputer.org/docs/current/references/ledger/#_accounts
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct AccountIdentifier {
     bytes: H256,
 }
 
 impl AccountIdentifier {
+    /// Create a default account identifier from the given principal owner.
     pub fn new(owner: &Principal) -> Self {
         let mut input = vec![];
         input.extend_from_slice(b"\x0Aaccount-id");
@@ -38,10 +49,12 @@ impl AccountIdentifier {
         Self { bytes }
     }
 
+    /// Return the textual-encoded account identifier.
     pub fn to_hex(&self) -> String {
         hex::encode(self.bytes, false)
     }
 
+    /// Instantiate an account identifier from a hex-encoded string.
     pub fn from_hex(hex_str: &str) -> AddressResult<Self> {
         if hex_str.len() != 64 {
             return Err(AddressError::FromHexError);
--- rust/tw_internet_computer/src/context.rs
+++ rust/tw_internet_computer/src/context.rs
@@ -22,6 +22,7 @@ pub struct StandardInternetComputerLedgerCanister;
 
 impl CanisterId for StandardInternetComputerLedgerCanister {
     fn principal_id() -> Principal {
+        // ICP Ledger Canister: ryjl3-tyaaa-aaaaa-aaaba-cai
         Principal::from_slice(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x01, 0x01])
     }
 }
@@ -32,4 +33,59 @@ pub struct StandardInternetComputerContext;
 impl InternetComputerContext for StandardInternetComputerContext {
     type Address = AccountIdentifier;
     type Canister = StandardInternetComputerLedgerCanister;
+}
+
+#[cfg(test)]
+mod test {
+
+    use std::marker::PhantomData;
+
+    use tw_coin_entry::error::AddressResult;
+
+    use super::*;
+
+    const TEST_OWNER_PRINCIPAL_ID: &str =
+        "t4u4z-y3dur-j63pk-nw4rv-yxdbt-agtt6-nygn7-ywh6y-zm2f4-sdzle-3qe";
+    const TEST_TEXTUAL_ICP_ADDRESS: &str =
+        "943d12e762f43806782f524b8f90297298a6d79e4749b41b585ec427409c826a";
+    const TEXTUAL_ICP_LEDGER_CANISTER_ID: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
+
+    pub struct ContextTest<Context: InternetComputerContext> {
+        _phantom: PhantomData<Context>,
+    }
+
+    impl<Context: InternetComputerContext> ContextTest<Context> {
+        fn canister_id() -> Principal {
+            Context::Canister::principal_id()
+        }
+
+        fn account_identifier_optional(s: &str) -> AddressResult<Option<Context::Address>> {
+            Context::Address::from_str_optional(s)
+        }
+    }
+
+    #[test]
+    fn standard_internet_computer_context_canister_address() {
+        let owner = Principal::from_text(TEST_OWNER_PRINCIPAL_ID).unwrap();
+        let expected_account_identifier = AccountIdentifier::new(&owner);
+        let account_identifier =
+            ContextTest::<StandardInternetComputerContext>::account_identifier_optional(
+                TEST_TEXTUAL_ICP_ADDRESS,
+            )
+            .unwrap()
+            .unwrap();
+        assert_eq!(expected_account_identifier, account_identifier);
+    }
+
+    #[test]
+    fn standard_internet_computer_context_canister_type() {
+        let ledger_canister_id = ContextTest::<StandardInternetComputerContext>::canister_id();
+        assert_eq!(ledger_canister_id.to_text(), TEXTUAL_ICP_LEDGER_CANISTER_ID);
+    }
+
+    #[test]
+    fn standard_internet_computer_ledger_canister() {
+        let ledger_canister_id = StandardInternetComputerLedgerCanister::principal_id();
+        assert_eq!(ledger_canister_id.to_text(), TEXTUAL_ICP_LEDGER_CANISTER_ID);
+    }
 }
