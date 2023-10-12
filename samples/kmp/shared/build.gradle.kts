fa983fa5f1fb83180bed0ea802478a252035de60
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-10-12 20:17:17 +0200

    [Greenfield]: Fix KMP

--- samples/kmp/shared/build.gradle.kts
+++ samples/kmp/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.trustwallet:wallet-core-kotlin:3.2.20")
+                implementation("com.trustwallet:wallet-core-kotlin:3.2.21")
             }
         }
         val commonTest by getting {
