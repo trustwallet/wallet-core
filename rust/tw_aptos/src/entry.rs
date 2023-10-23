158c896682e9a3330c7b3ecbc8f8a026597dac3b
Milerius <rmscastle@gmail.com>
2023-10-23 08:44:07 -0500

    feat(aptos_rust): place holder tw_aptos

---
+++ rust/tw_aptos/src/entry.rs
@@ -0,0 +1,106 @@
+// Copyright © 2017-2023 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+use std::fmt::{Display, Formatter};
+use std::ops::Add;
+use tw_coin_entry::coin_context::CoinContext;
+use tw_coin_entry::coin_entry::{CoinAddress, CoinEntry, PublicKeyBytes, SignatureBytes};
+use tw_coin_entry::derivation::Derivation;
+use tw_coin_entry::error::{AddressError, AddressResult};
+use tw_coin_entry::modules::json_signer::NoJsonSigner;
+use tw_coin_entry::modules::message_signer::NoMessageSigner;
+use tw_coin_entry::modules::plan_builder::NoPlanBuilder;
+use tw_coin_entry::prefix::NoPrefix;
+use tw_keypair::tw::PublicKey;
+use tw_hash::H160;
+use tw_proto::Aptos::Proto;
+use tw_proto::TxCompiler::Proto as CompilerProto;
+use tw_memory::Data;
+
+pub struct AptosEntry;
+
+pub struct Address {
+    bytes: H160,
+}
+
+impl Display for Address {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl CoinAddress for Address {
+    #[inline]
+    fn data(&self) -> Data {
+        todo!()
+    }
+}
+
+impl CoinEntry for AptosEntry {
+    type AddressPrefix = NoPrefix;
+    type Address = Address;
+    type SigningInput<'a> = Proto::SigningInput<'a>;
+    type SigningOutput = Proto::SigningOutput<'static>;
+    type PreSigningOutput = CompilerProto::PreSigningOutput<'static>;
+
+    // Optional modules:
+    type JsonSigner = NoJsonSigner;
+    type PlanBuilder = NoPlanBuilder;
+    type MessageSigner = NoMessageSigner;
+
+    #[inline]
+    fn parse_address(
+        &self,
+        _coin: &dyn CoinContext,
+        address: &str,
+        _prefix: Option<Self::AddressPrefix>,
+    ) -> AddressResult<Self::Address> {
+        todo!()
+    }
+
+    fn derive_address(
+        &self,
+        _coin: &dyn CoinContext,
+        public_key: PublicKey,
+        _derivation: Derivation,
+        _prefix: Option<Self::AddressPrefix>,
+    ) -> AddressResult<Self::Address> {
+        todo!()
+    }
+
+    #[inline]
+    fn sign(&self, _coin: &dyn CoinContext, input: Self::SigningInput<'_>) -> Self::SigningOutput {
+        todo!()
+    }
+
+    #[inline]
+    fn preimage_hashes(
+        &self,
+        _coin: &dyn CoinContext,
+        input: Self::SigningInput<'_>,
+    ) -> Self::PreSigningOutput {
+        todo!()
+    }
+
+    #[inline]
+    fn compile(
+        &self,
+        _coin: &dyn CoinContext,
+        input: Self::SigningInput<'_>,
+        signatures: Vec<SignatureBytes>,
+        public_keys: Vec<PublicKeyBytes>,
+    ) -> Self::SigningOutput {
+        todo!()
+    }
+
+    #[inline]
+    fn json_signer(&self) -> Option<Self::JsonSigner> {
+        None
+    }
+
+    #[inline]
+    fn message_signer(&self) -> Option<Self::MessageSigner> { None }
+}
