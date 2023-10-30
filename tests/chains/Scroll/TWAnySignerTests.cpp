c9dddf9b45bad794124cc0a6435ed0b8fa75619a
zzq0826 <770166635@qq.com>
2023-10-30 21:36:26 +0800

    Update TWAnySignerTests.cpp

--- tests/chains/Scroll/TWAnySignerTests.cpp
+++ tests/chains/Scroll/TWAnySignerTests.cpp
@@ -35,7 +35,7 @@ TEST(TWAnySignerScroll, Sign) {
     auto amount = store(uint256_t(200000000000000));
     transfer.set_amount(amount.data(), amount.size());
 
-    std::string expected = "f86c01830f424083030d4094a6bc5ee0b1e904dd0773c5555d2f6833fe937a6886b5e620f480008083104ec6a0a30f1e29c3d1447d7f7f76c7112af3fb86eb3cb7cc536c932e720cd9813ae274a048ffcadba06d3645829d74fa1cf28744111cfc35893761239fa6b510c84c396e";
+    std::string expected = "f86c01830f424083030d4094a6bc5ee0b1e904dd0773c5555d2f6833fe937a6886b5e620f480008083104ec3a0c43ee3d34f7758e05e2f54df227eb7780ad97d06e91e03ef6a3c91d4bea6e42fa07d075f20776f7f485faca6f057110fd2745a5cdd6cf121682ef7791619a03ade";
 
     // sign test
     Ethereum::Proto::SigningOutput output;
