a32626e502315a466af3530a78f27ff3f2f93d29
iChain <143358269+iChainBlock@users.noreply.github.com>
2023-11-26 12:07:55 +0200

    Update CoinAddressDerivationTests.cpp

--- tests/common/CoinAddressDerivationTests.cpp
+++ tests/common/CoinAddressDerivationTests.cpp
@@ -84,9 +84,11 @@ TEST(Coin, DeriveAddress) {
         case TWCoinTypeGreenfield:
         case TWCoinTypeMantle:
         case TWCoinTypeZenEON:
+        <<<<<<< master
         case TWCoinTypeICHAIN:     
-            EXPECT_EQ(address, "0x9d8A62f656a8d1615C1294fd71e9CFb3E4855A4F");
-            break;
+=======
+            // end_of_evm_address_derivation_tests_marker_do_not_modify
+>>>>>>> master
 
         case TWCoinTypeKin:
         case TWCoinTypeStellar:
