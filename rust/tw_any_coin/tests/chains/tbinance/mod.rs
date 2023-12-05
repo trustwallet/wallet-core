b8257c442259d4ad1a29f809b0a282a5cc503b07
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-12-05 10:11:38 +0100

    [BNB]: Add TBinance address tests

--- rust/tw_any_coin/tests/chains/tbinance/mod.rs
+++ rust/tw_any_coin/tests/chains/tbinance/mod.rs
@@ -4,13 +4,4 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
-mod aptos;
-mod binance;
-mod bitcoin;
-mod cosmos;
-mod ethereum;
-mod internet_computer;
-mod native_evmos;
-mod native_injective;
-mod tbinance;
-mod thorchain;
+mod tbinance_address;
