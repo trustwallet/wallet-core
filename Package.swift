650bd8fb96fa45506f8418ba225a386155f41487
Nhat Dinh <nhatphutho@gmail.com>
2023-12-07 16:15:45 +0900

    Rename SwiftProtobuf to avoid issue

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
+        .library(name: "WalletCoreSwiftProtobuf", targets: ["WalletCoreSwiftProtobuf"])
     ],
     dependencies: [],
     targets: [
@@ -16,7 +16,7 @@ let package = Package(
             checksum: "b7f9fe6b5aa1fd216d43f676400c9db6999c6494e0120db73b2afb49dcc1f013"
         ),
         .binaryTarget(
-            name: "SwiftProtobuf",
+            name: "WalletCoreSwiftProtobuf",
             url: "https://github.com/trustwallet/wallet-core/releases/download/3.2.1/SwiftProtobuf.xcframework.zip",
             checksum: "12bbc92dc2225c661e301cc2826e034fcb69b2a144f8c4ff2e51ce6ccf122124"
         )
