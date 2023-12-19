637f35d4933b78dcf2eceb80ef3fd4f8d299ac35
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2023-12-19 14:23:48 +0100

    [Greenfield]: Update chainId

--- tests/chains/Greenfield/TWCoinTypeTests.cpp
+++ tests/chains/Greenfield/TWCoinTypeTests.cpp
@@ -31,7 +31,7 @@ TEST(TWGreenfieldCoinType, TWCoinType) {
     ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainGreenfield);
     ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 0x0);
     ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0x0);
-    assertStringsEqual(chainId, "9000");
+    assertStringsEqual(chainId, "1017");
     assertStringsEqual(txUrl, "https://greenfieldscan.com/tx/0x150eac42070957115fd538b1f348fadd78d710fb641c248120efcf35d1e7e4f3");
     assertStringsEqual(accUrl, "https://greenfieldscan.com/account/0xcf0f6b88ed72653b00fdebbffc90b98072cb3285");
 }
