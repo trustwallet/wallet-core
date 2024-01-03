61af751b38313f08c3a8a46898cdb9fc2beefc5d
jaimeToca <jaime.toca@binance.com>
2024-01-03 16:51:30 +0100

    Fix warnings

--- kotlin/build-logic/build.gradle.kts
+++ kotlin/build-logic/build.gradle.kts
@@ -12,6 +12,7 @@ allprojects {
     }
     tasks.withType<KotlinCompile> {
         compilerOptions {
+            freeCompilerArgs.add("-Xexpect-actual-classes")
             allWarningsAsErrors.set(true)
             jvmTarget.set(JvmTarget.JVM_17)
         }
