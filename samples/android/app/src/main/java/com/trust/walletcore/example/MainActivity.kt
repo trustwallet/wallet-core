be0dc6f361a48655c48fa2eb35b88a5709b328d3
jaimeToca <jaime.toca@binance.com>
2024-01-03 15:13:59 +0100

    test CI

--- samples/android/app/src/main/java/com/trust/walletcore/example/MainActivity.kt
+++ samples/android/app/src/main/java/com/trust/walletcore/example/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // find layout
         val logView = findViewById<LinearLayout>(R.id.logView)
 
         // 'Import' a wallet
