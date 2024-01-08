9393cfef73b39158fa1a68d1dcc51cdf06f6bf42
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-01-09 01:04:48 +0700

    [WalletConnect/BNB]: Add iOS test

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/binance/TestBinanceWalletConnectSigning.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/binance/TestBinanceWalletConnectSigning.kt
@@ -18,6 +18,8 @@ class TestBinanceWalletConnectSigning {
 
     @Test
     fun testSignBinanceTransactionFromWalletConnectRequest() {
+        // Step 1: Parse a signing request received through WalletConnect.
+
         val parsingInput = WalletConnect.ParseRequestInput.newBuilder().apply {
             method = WalletConnect.Method.CosmosSignAmino
             payload = "{\"signerAddress\":\"bnb1grpf0955h0ykzq3ar5nmum7y6gdfl6lxfn46h2\",\"signDoc\":{\"account_number\":\"19\",\"chain_id\":\"chain-bnb\",\"memo\":\"\",\"data\":null,\"msgs\":[{\"inputs\":[{\"address\":\"bnb1grpf0955h0ykzq3ar5nmum7y6gdfl6lxfn46h2\",\"coins\":[{\"amount\":1001000000,\"denom\":\"BNB\"}]}],\"outputs\":[{\"address\":\"bnb13zeh6hs97d5eu2s5qerguhv8ewwue6u4ywa6yf\",\"coins\":[{\"amount\":1001000000,\"denom\":\"BNB\"}]}]}],\"sequence\":\"23\",\"source\":\"1\"}}"
@@ -28,12 +30,15 @@ class TestBinanceWalletConnectSigning {
 
         assertEquals(parsingOutput.error, Common.SigningError.OK)
 
-        val inpu = parsingOutput.binance.toBuilder().apply {
+        // Step 2: Set missing fields.
+
+        val signingInput = parsingOutput.binance.toBuilder().apply {
             privateKey = ByteString.copyFrom("95949f757db1f57ca94a5dff23314accbe7abee89597bf6a3c7382c84d7eb832".toHexBytes())
         }.build()
 
-        // sign and encode resulting input
-        val output = AnySigner.sign(inpu, BINANCE, SigningOutput.parser())
+        // Step 3: Sign the transaction.
+
+        val output = AnySigner.sign(signingInput, BINANCE, SigningOutput.parser())
 
         assertEquals(output.error, Common.SigningError.OK)
         assertEquals(output.signatureJson, "{\"pub_key\":{\"type\":\"tendermint/PubKeySecp256k1\",\"value\":\"Amo1kgCI2Yw4iMpoxT38k/RWRgJgbLuH8P5e5TPbOOUC\"},\"signature\":\"PCTHhMa7+Z1U/6uxU+3LbTxKd0k231xypdMolyVvjgYvMg+0dTMC+wqW8IxHWXTSDt/Ronu+7ac1h/WN3JWJdQ==\"}")
