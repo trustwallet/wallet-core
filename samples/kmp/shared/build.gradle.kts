134bf69d7a3ce984ce31bd9de2f1d748f06ea6ca
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-01-11 17:26:25 +0700

    [KMP]: Fix KMP CI

--- samples/kmp/shared/build.gradle.kts
+++ samples/kmp/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.trustwallet:wallet-core-kotlin:4.0.14")
+                implementation("com.trustwallet:wallet-core-kotlin:4.0.16")
             }
         }
         val commonTest by getting {
