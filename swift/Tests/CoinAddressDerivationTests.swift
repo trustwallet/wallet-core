2b3cf34bd756c5378d202adce6648632f4cc0686
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-01-25 15:27:11 +0700

    feat(ZetaChain): Fix Swift test

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -113,7 +113,7 @@ class CoinAddressDerivationTests: XCTestCase {
                      .greenfield,
                      .mantle,
                      .zenEON,
-                     .mantaPacific
+                     .mantaPacific,
                      .zetaEVM:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
