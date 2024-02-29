492ee707f9274da62d2863a85055ad163948a317
lamafab <42901763+lamafab@users.noreply.github.com>
2024-02-29 15:06:52 +0000

    build brc20 spending input

--- rust/frameworks/tw_utxo/src/transaction/standard_transaction/builder/spend.rs
+++ rust/frameworks/tw_utxo/src/transaction/standard_transaction/builder/spend.rs
@@ -1,14 +1,14 @@
 use crate::{
-    error::UtxoError,
-    error::{UtxoErrorKind, UtxoResult},
+    error::{UtxoError, UtxoErrorKind, UtxoResult},
     script::{standard_script::claims, Script, Witness},
-    sighash::SighashType,
-    sighash::{BitcoinEcdsaSignature, BitcoinSchnorrSignature},
+    sighash::{BitcoinEcdsaSignature, BitcoinSchnorrSignature, SighashType},
     sighash_computer::SpendingData,
+    transaction::asset::brc20::{BRC20TransferInscription, Brc20Ticker},
 };
 
 use tw_hash::H264;
 use tw_keypair::{ecdsa, schnorr, tw};
+use tw_misc::traits::ToBytesVec;
 
 pub struct SpendingScriptBuilder {
     sighash_ty: Option<SighashType>,
@@ -103,6 +103,47 @@ impl SpendingScriptBuilder {
         .unwrap();
 
         let witness = claims::new_p2tr_script_path(sig.serialize(), payload, control_block);
+
+        Ok(SpendingData {
+            script_sig: Script::default(),
+            witness,
+        })
+    }
+    pub fn brc20_transfer(
+        mut self,
+        sig: schnorr::Signature,
+        pubkey: tw::PublicKey,
+        ticker: String,
+        value: String,
+    ) -> UtxoResult<SpendingData> {
+        let sig = BitcoinSchnorrSignature::new(
+            sig,
+            self.sighash_ty
+                .ok_or(UtxoError(UtxoErrorKind::Error_internal))?,
+        )
+        .unwrap();
+
+        let pubkey: H264 = pubkey
+            .to_bytes()
+            .as_slice()
+            .try_into()
+            .expect("pubkey length is 33 bytes");
+
+        let ticker = Brc20Ticker::new(ticker).unwrap();
+        let transfer = BRC20TransferInscription::new(&pubkey, &ticker, &value).unwrap();
+
+        let control_block = transfer
+            .spend_info
+            .control_block(&(
+                transfer.script.to_owned(),
+                bitcoin::taproot::LeafVersion::TapScript,
+            ))
+            .unwrap();
+
+        let payload = Script::from(transfer.script.to_vec());
+
+        let witness =
+            claims::new_p2tr_script_path(sig.serialize(), payload, control_block.serialize());
 
         Ok(SpendingData {
             script_sig: Script::default(),
