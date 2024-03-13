35378aa5f420cf70319cf58cbadcc8ccc52ce8aa
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2024-03-13 17:32:08 +0100

    [Solana]: Fix Solana delegate stake transaction (#3732)
    * [Solana]: Fix Solana delegate stake transaction
    
    * [CI]: Disable check-binary-sizes PR step if opened from forks

--- rust/chains/tw_solana/src/program/stake_program.rs
+++ rust/chains/tw_solana/src/program/stake_program.rs
@@ -9,15 +9,25 @@ use tw_coin_entry::error::{AddressError, AddressResult};
 use tw_hash::sha2::sha256;
 use tw_hash::H256;
 
+pub const MAX_SEED_LEN: usize = 32;
+
 pub struct StakeProgram;
 
 impl StakeProgram {
+    pub fn recent_blockhash_as_seed(recent_blockhash: &Blockhash) -> String {
+        recent_blockhash
+            .to_string()
+            .chars()
+            .take(MAX_SEED_LEN)
+            .collect()
+    }
+
     pub fn address_from_recent_blockhash(
-        from: SolanaAddress,
-        recent_blockhash: Blockhash,
+        from: &SolanaAddress,
+        recent_blockhash: &Blockhash,
     ) -> SolanaAddress {
         let mut seed = from.bytes().to_vec();
-        seed.extend_from_slice(recent_blockhash.to_string().as_bytes());
+        seed.extend_from_slice(Self::recent_blockhash_as_seed(recent_blockhash).as_bytes());
         seed.extend_from_slice(STAKE_PROGRAM_ID_ADDRESS.bytes().as_slice());
         let bytes =
             H256::try_from(sha256(&seed).as_slice()).expect("sha256 expected to return 32 bytes");
