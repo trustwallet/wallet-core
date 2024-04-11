41bd37302f1485303a9cbe4182049f57d225a33e
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2024-04-11 13:25:29 +0200

    feat(Blast): Add Blast EVM chain (#3785)
    * feat(lightlink): Add Lightlink Phoenix EVM chain
    
    * feat(lightlink): Fix iOS test
    
    * feat(blast): Add Blast EVM chain

--- tests/common/CoinAddressDerivationTests.cpp
+++ tests/common/CoinAddressDerivationTests.cpp
@@ -86,6 +86,7 @@ TEST(Coin, DeriveAddress) {
         case TWCoinTypeZetaEVM:
         case TWCoinTypeMerlin:
         case TWCoinTypeLightlink:
+        case TWCoinTypeBlast:
             // end_of_evm_address_derivation_tests_marker_do_not_modify
             EXPECT_EQ(address, "0x9d8A62f656a8d1615C1294fd71e9CFb3E4855A4F");
             break;
