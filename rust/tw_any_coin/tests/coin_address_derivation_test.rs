cd212c9cb3a186f10ff2cc98fad01f6b9b514d33
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2024-04-19 15:21:25 +0200

    feat(bouncebit): Add support for `BounceBit` EVM (#3807)

--- rust/tw_any_coin/tests/coin_address_derivation_test.rs
+++ rust/tw_any_coin/tests/coin_address_derivation_test.rs
@@ -89,6 +89,7 @@ fn test_coin_address_derivation() {
             | CoinType::Merlin
             | CoinType::Lightlink
             | CoinType::Blast
+            | CoinType::BounceBit
             // end_of_evm_address_derivation_tests_marker_do_not_modify
                 => "0xAc1ec44E4f0ca7D172B7803f6836De87Fb72b309",
             CoinType::Bitcoin
