7d0ccdd21c4b77cbccd702df13bda4ed5f8c2f1b
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-05-22 18:05:23 +0200

    [Bitcoin]: Remove legacy BRC20 API

--- src/Bitcoin/Script.cpp
+++ src/Bitcoin/Script.cpp
@@ -562,27 +562,4 @@ Script Script::lockScriptForAddress(const std::string& string, enum TWCoinType c
     return lockScriptForAddress(string, coin);
 }
 
-Proto::TransactionOutput Script::buildBRC20InscribeTransfer(const std::string& ticker, const std::string& amount, const Data& publicKey) {
-    TW::Bitcoin::Proto::TransactionOutput out;
-    Rust::CByteArrayWrapper res = TW::Rust::tw_bitcoin_legacy_build_brc20_transfer_inscription(ticker.data(), amount.data(), 0, publicKey.data(), publicKey.size());
-    auto result = res.data;
-    out.ParseFromArray(result.data(), static_cast<int>(result.size()));
-    return out;
-}
-
-Proto::TransactionOutput Script::buildOrdinalNftInscription(const std::string& mimeType, const Data& payload, const Data& publicKey) {
-    TW::Bitcoin::Proto::TransactionOutput out;
-    Rust::CByteArrayWrapper res = TW::Rust::tw_bitcoin_legacy_build_nft_inscription(
-        mimeType.data(),
-        payload.data(),
-        payload.size(),
-        0,
-        publicKey.data(),
-        publicKey.size()
-    );
-    auto result = res.data;
-    out.ParseFromArray(result.data(), static_cast<int>(result.size()));
-    return out;
-}
-
 } // namespace TW::Bitcoin
