c934b779f10caeb87a0164cae228e4d840d8b884
doom <clement.doumergue@binance.com>
2024-05-27 14:42:16 +0200

    Apply suggestions from code review, refactor some utility functions

--- rust/tw_ss58_address/src/lib.rs
+++ rust/tw_ss58_address/src/lib.rs
@@ -1,12 +1,13 @@
-use blake2::{Blake2b512, Digest};
 use std::fmt::Formatter;
 use std::str::FromStr;
+
 use tw_coin_entry::error::prelude::*;
 use tw_encoding::{base58, hex};
-use tw_keypair::tw::{PrivateKey, PublicKey};
+use tw_hash::blake2::blake2_b;
+use tw_keypair::ed25519::sha512::PublicKey;
 
 //
-// Most of the materials implemented here is based on the following resources:
+// Most of the materials implemented here are based on the following resources:
 // - https://wiki.polkadot.network/docs/learn-account-advanced#address-format
 // - https://github.com/paritytech/polkadot-sdk/blob/master/substrate/primitives/core/src/crypto.rs
 //
@@ -16,7 +17,7 @@ pub struct NetworkId(u16);
 
 impl NetworkId {
     fn new_unchecked(value: u16) -> Self {
-        return Self(value);
+        Self(value)
     }
 
     pub fn from_u16(value: u16) -> AddressResult<Self> {
@@ -26,8 +27,48 @@ impl NetworkId {
         }
     }
 
+    pub fn from_bytes(bytes: &[u8]) -> AddressResult<Self> {
+        if bytes.is_empty() {
+            return Err(AddressError::MissingPrefix);
+        }
+
+        match bytes[0] {
+            0..=63 => Ok(bytes[0] as u16),
+            64..=127 if bytes.len() >= 2 => {
+                let lower = (bytes[0] << 2) | (bytes[1] >> 6);
+                let upper = bytes[1] & 0b0011_1111;
+                Ok((lower as u16) | ((upper as u16) << 8))
+            },
+            _ => Err(AddressError::UnexpectedAddressPrefix),
+        }
+        .map(Self::new_unchecked)
+    }
+
     pub fn value(&self) -> u16 {
         self.0
+    }
+
+    pub fn prefix_len(&self) -> usize {
+        if self.value() < 64 {
+            1
+        } else {
+            2
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let network = self.value();
+        match network {
+            0..=63 => {
+                vec![network as u8]
+            },
+            _ => {
+                let first = ((network & 0b0000_0000_1111_1100) as u8) >> 2;
+                let second =
+                    ((network >> 8) as u8) | ((network & 0b0000_0000_0000_0011) as u8) << 6;
+                vec![first | 0b01000000, second]
+            },
+        }
     }
 }
 
@@ -36,6 +77,14 @@ impl TryFrom<u16> for NetworkId {
 
     fn try_from(value: u16) -> Result<Self, Self::Error> {
         Self::from_u16(value)
+    }
+}
+
+impl TryFrom<&[u8]> for NetworkId {
+    type Error = AddressError;
+
+    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
+        Self::from_bytes(bytes)
     }
 }
 
@@ -50,44 +99,11 @@ impl SS58Address {
     const KEY_SIZE: usize = 32;
     const SS58_PREFIX: &'static [u8] = b"SS58PRE";
 
-    fn extract_network(bytes: &[u8]) -> AddressResult<(usize, u16)> {
-        if bytes.len() == 0 {
-            return Err(AddressError::MissingPrefix);
-        }
-
-        match bytes[0] {
-            0..=63 => Ok((1, bytes[0] as u16)),
-            64..=127 if bytes.len() >= 2 => {
-                let lower = (bytes[0] << 2) | (bytes[1] >> 6);
-                let upper = bytes[1] & 0b0011_1111;
-                Ok((2, (lower as u16) | ((upper as u16) << 8)))
-            },
-            _ => Err(AddressError::UnexpectedAddressPrefix),
-        }
-    }
-
-    fn encode_network(network_id: NetworkId) -> Vec<u8> {
-        let network = network_id.value();
-        match network {
-            0..=63 => {
-                vec![network as u8]
-            },
-            _ => {
-                let first = ((network & 0b0000_0000_1111_1100) as u8) >> 2;
-                let second =
-                    ((network >> 8) as u8) | ((network & 0b0000_0000_0000_0011) as u8) << 6;
-                vec![first | 0b01000000, second]
-            },
-        }
-    }
-
     fn compute_expected_checksum(decoded: &[u8]) -> Vec<u8> {
-        // XXX: tried using tw_hash::blake2::blake2_b, but it did not produce the correct result
-        let mut ctx = Blake2b512::new();
-        ctx.update(Self::SS58_PREFIX);
-        ctx.update(decoded);
+        let mut data = Vec::from(Self::SS58_PREFIX);
+        data.extend(decoded);
 
-        let mut bytes = ctx.finalize().to_vec();
+        let mut bytes = blake2_b(&data, 64).expect("hash length should be valid");
         bytes.truncate(Self::CHECKSUM_SIZE);
         bytes
     }
@@ -96,9 +112,9 @@ impl SS58Address {
         let decoded = base58::decode(repr, base58::Alphabet::BITCOIN)
             .map_err(|_| AddressError::FromBase58Error)?;
 
-        let (network_prefix_len, network) = Self::extract_network(&decoded)?;
+        let network = NetworkId::from_bytes(&decoded)?;
 
-        if network_prefix_len + Self::KEY_SIZE + Self::CHECKSUM_SIZE != decoded.len() {
+        if network.prefix_len() + Self::KEY_SIZE + Self::CHECKSUM_SIZE != decoded.len() {
             return Err(AddressError::FromBase58Error);
         }
 
@@ -111,21 +127,16 @@ impl SS58Address {
         }
 
         Ok(Self {
-            key: decoded[network_prefix_len..network_prefix_len + Self::KEY_SIZE].to_owned(),
-            network: NetworkId::new_unchecked(network),
+            key: decoded[network.prefix_len()..network.prefix_len() + Self::KEY_SIZE].to_owned(),
+            network,
         })
     }
 
     pub fn from_public_key(key: &PublicKey, network: u16) -> AddressResult<Self> {
         let network = NetworkId::try_from(network)?;
 
-        let bytes = match key {
-            PublicKey::Ed25519(k) => k.as_slice(),
-            _ => return Err(AddressError::InvalidInput),
-        };
-
         Ok(Self {
-            key: bytes.to_owned(),
+            key: key.as_slice().to_owned(),
             network,
         })
     }
@@ -138,19 +149,19 @@ impl SS58Address {
         &self.key
     }
 
-    fn as_bytes(&self) -> Vec<u8> {
-        let mut res = Self::encode_network(self.network);
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut res = self.network.to_bytes();
         res.extend(self.key_bytes());
         res.extend(Self::compute_expected_checksum(&res));
         res
     }
 
-    fn as_base58_string(&self) -> String {
-        base58::encode(&self.as_bytes(), base58::Alphabet::BITCOIN)
+    pub fn to_base58_string(&self) -> String {
+        base58::encode(&self.to_bytes(), base58::Alphabet::BITCOIN)
     }
 
-    fn as_hex_string(&self) -> String {
-        hex::encode(&self.as_bytes(), false)
+    pub fn to_hex_string(&self) -> String {
+        hex::encode(self.to_bytes(), false)
     }
 }
 
@@ -164,17 +175,15 @@ impl FromStr for SS58Address {
 
 impl std::fmt::Display for SS58Address {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.as_base58_string())
+        f.write_str(&self.to_base58_string())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::{NetworkId, SS58Address};
-    use std::string::ToString;
     use tw_coin_entry::error::prelude::AddressError;
-    use tw_encoding::hex;
-    use tw_keypair::tw::{PublicKey, PublicKeyType};
+    use tw_keypair::ed25519::sha512::PublicKey;
 
     fn networks() -> [(Vec<u8>, u16); 27] {
         [
@@ -219,7 +228,62 @@ mod tests {
     }
 
     #[test]
-    fn test_address_from_str_valid() {
+    fn test_network_from_bytes() {
+        fn test_case(prefix: &[u8], expected_network: u16) {
+            let network = NetworkId::from_bytes(prefix).expect("error extracting network");
+            let expected_prefix_length = if network.value() < 64 { 1 } else { 2 } as usize;
+            assert_eq!(
+                network.prefix_len(),
+                expected_prefix_length,
+                "for expected network {}",
+                expected_network
+            );
+            assert_eq!(
+                network.value(),
+                expected_network,
+                "for expected network {}",
+                expected_network
+            );
+        }
+
+        for (prefix, network) in networks() {
+            test_case(&prefix, network);
+        }
+
+        // ensure prefix length is returned as expected
+        test_case(&[0x00, 0x00], 0x00)
+    }
+
+    #[test]
+    fn test_network_from_bytes_invalid() {
+        // at least one byte is expected
+        let res = NetworkId::from_bytes(&[]);
+        assert_eq!(res, Err(AddressError::MissingPrefix));
+
+        // the first byte should be in 0..=127
+        let res = NetworkId::from_bytes(&[0xFF]);
+        assert_eq!(res, Err(AddressError::UnexpectedAddressPrefix));
+
+        // a second byte should follow
+        let res = NetworkId::from_bytes(&[0x40]);
+        assert_eq!(res, Err(AddressError::UnexpectedAddressPrefix));
+    }
+
+    #[test]
+    fn test_network_as_bytes() {
+        fn test_case(expected_prefix: &[u8], network: u16) {
+            let prefix = NetworkId::new_unchecked(network).to_bytes();
+
+            assert_eq!(prefix, expected_prefix, "for network {}", network);
+        }
+
+        for (prefix, network) in networks() {
+            test_case(&prefix, network);
+        }
+    }
+
+    #[test]
+    fn test_address_from_str() {
         fn test_case(repr: &str, expected_network: u16) {
             let addr = SS58Address::from_str(repr).expect("error parsing address");
             assert_eq!(addr.network().value(), expected_network);
@@ -239,93 +303,35 @@ mod tests {
     #[test]
     fn test_address_from_public_key() {
         let key_hex = "92fd9c237030356e26cfcc4568dc71055d5ec92dfe0ff903767e00611971bad3";
-        let key_bytes = hex::decode(key_hex).expect("error decoding public key");
-        let key = PublicKey::new(key_bytes, PublicKeyType::Ed25519)
-            .expect("error creating test public key");
+        let key = PublicKey::try_from(key_hex).expect("error creating test public key");
 
         let addr = SS58Address::from_public_key(&key, 0).expect("error creating address");
         assert_eq!(addr.network().value(), 0);
-        assert_eq!(addr.key_bytes(), key.to_bytes());
+        assert_eq!(addr.key_bytes(), key.as_slice());
 
         let addr = SS58Address::from_public_key(&key, 5).expect("error creating address");
         assert_eq!(addr.network().value(), 5);
-        assert_eq!(addr.key_bytes(), key.to_bytes());
+        assert_eq!(addr.key_bytes(), key.as_slice());
 
         let addr = SS58Address::from_public_key(&key, 172).expect("error creating address");
         assert_eq!(addr.network().value(), 172);
-        assert_eq!(addr.key_bytes(), key.to_bytes());
+        assert_eq!(addr.key_bytes(), key.as_slice());
     }
 
     #[test]
     fn test_address_from_public_key_with_invalid_network() {
         let key_hex = "92fd9c237030356e26cfcc4568dc71055d5ec92dfe0ff903767e00611971bad3";
-        let key_bytes = hex::decode(key_hex).expect("error decoding public key");
-        let key = PublicKey::new(key_bytes, PublicKeyType::Ed25519)
-            .expect("error creating test public key");
+        let key = PublicKey::try_from(key_hex).expect("error creating test public key");
 
         let res = SS58Address::from_public_key(&key, 32771);
         assert_eq!(res, Err(AddressError::InvalidInput));
     }
 
     #[test]
-    fn test_extract_network_valid() {
-        fn test_case(prefix: &[u8], expected_network: u16) {
-            let (prefix_length, network) =
-                SS58Address::extract_network(prefix).expect("error extracting network");
-            let expected_prefix_length = if network < 64 { 1 } else { 2 } as usize;
-            assert_eq!(
-                prefix_length, expected_prefix_length,
-                "for expected network {}",
-                expected_network
-            );
-            assert_eq!(
-                network, expected_network,
-                "for expected network {}",
-                expected_network
-            );
-        }
-
-        for (prefix, network) in networks() {
-            test_case(&prefix, network);
-        }
-
-        // ensure prefix length is returned as expected
-        test_case(&[0x00, 0x00], 0x00)
-    }
-
-    #[test]
-    fn test_extract_network_invalid() {
-        // at least one byte is expected
-        let res = SS58Address::extract_network(&[]);
-        assert_eq!(res, Err(AddressError::MissingPrefix));
-
-        // the first byte should be in 0..=127
-        let res = SS58Address::extract_network(&[0xFF]);
-        assert_eq!(res, Err(AddressError::UnexpectedAddressPrefix));
-
-        // a second byte should follow
-        let res = SS58Address::extract_network(&[0x40]);
-        assert_eq!(res, Err(AddressError::UnexpectedAddressPrefix));
-    }
-
-    #[test]
-    fn test_encode_network() {
-        fn test_case(expected_prefix: &[u8], network: u16) {
-            let prefix = SS58Address::encode_network(NetworkId::new_unchecked(network));
-
-            assert_eq!(prefix, expected_prefix, "for network {}", network);
-        }
-
-        for (prefix, network) in networks() {
-            test_case(&prefix, network);
-        }
-    }
-
-    #[test]
     fn test_as_base58_string() {
         fn test_case(repr: &str) {
             let addr = SS58Address::from_str(repr).expect("error parsing address");
-            assert_eq!(addr.as_base58_string(), repr);
+            assert_eq!(addr.to_base58_string(), repr);
         }
 
         test_case("Fu3r514w83euSVV7q1MyFGWErUR2xDzXS2goHzimUn4S12D");
@@ -345,7 +351,7 @@ mod tests {
         let addr = SS58Address::from_str("1FRMM8PEiWXYax7rpS6X4XZX1aAAxSWx1CrKTyrVYhV24fg")
             .expect("error parsing address");
         assert_eq!(
-            addr.as_hex_string(),
+            addr.to_hex_string(),
             "000aff6865635ae11013a83835c019d44ec3f865145943f487ae82a8e7bed3a66b29d7"
         );
     }
