904e0daf64f40d89883cb4d59a35a73cdc01c19a
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-07-09 17:46:04 +0200

    [Bitcoin]: Uncomment out Android tests

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/bitcoin/TestBitcoinSigning.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/bitcoin/TestBitcoinSigning.kt
@@ -162,14 +162,131 @@ class TestBitcoinSigning {
     }
 
     @Test
+    fun testSignBrc20Commit() {
+        // Successfully broadcasted: https://www.blockchain.com/explorer/transactions/btc/797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1
+        val privateKeyData = (Numeric.hexStringToByteArray("e253373989199da27c48680e3a3fc0f648d50f9a727ef17a7fe6a4dc3b159129"))
+        val dustSatoshis = 546.toLong()
+        val txId = Numeric.hexStringToByteArray("8ec895b4d30adb01e38471ca1019bfc8c3e5fbd1f28d9e7b5653260d89989008").reversedArray()
+
+        val privateKey = PrivateKey(privateKeyData)
+        val publicKey = ByteString.copyFrom(privateKey.getPublicKeySecp256k1(true).data())
+
+        val utxo0 = BitcoinV2.Input.newBuilder()
+            .setOutPoint(BitcoinV2.OutPoint.newBuilder().apply {
+                hash = ByteString.copyFrom(txId)
+                vout = 1
+            })
+            .setValue(26_400)
+            .setSighashType(BitcoinSigHashType.ALL.value())
+            .setScriptBuilder(BitcoinV2.Input.InputBuilder.newBuilder().apply {
+                p2Wpkh = BitcoinV2.PublicKeyOrHash.newBuilder().setPubkey(publicKey).build()
+            })
+
+        val out0 = BitcoinV2.Output.newBuilder()
+            .setValue(7_000)
+            .setBuilder(BitcoinV2.Output.OutputBuilder.newBuilder().apply {
+                brc20Inscribe = BitcoinV2.Output.OutputBrc20Inscription.newBuilder().apply {
+                    inscribeTo = publicKey
+                    ticker = "oadf"
+                    transferAmount = "20"
+                }.build()
+            })
+
+        val changeOutput = BitcoinV2.Output.newBuilder()
+            .setValue(16_400)
+            .setBuilder(BitcoinV2.Output.OutputBuilder.newBuilder().apply {
+                p2Wpkh = BitcoinV2.PublicKeyOrHash.newBuilder().setPubkey(publicKey).build()
+            })
+
+        val signingInput = BitcoinV2.SigningInput.newBuilder()
+            .setVersion(BitcoinV2.TransactionVersion.V2)
+            .addPrivateKeys(ByteString.copyFrom(privateKeyData))
+            .addInputs(utxo0)
+            .addOutputs(out0)
+            .addOutputs(changeOutput)
+            .setInputSelector(BitcoinV2.InputSelector.UseAll)
+            .setChainInfo(BitcoinV2.ChainInfo.newBuilder().apply {
+                p2PkhPrefix = 0
+                p2ShPrefix = 5
+            })
+            .setDangerousUseFixedSchnorrRng(true)
+            .setFixedDustThreshold(dustSatoshis)
+            .build()
+
+        val legacySigningInput = Bitcoin.SigningInput.newBuilder().apply {
+            signingV2 = signingInput
+        }
+
+        val output = AnySigner.sign(legacySigningInput.build(), BITCOIN, SigningOutput.parser())
+
+        assertEquals(output.error, SigningError.OK)
+        assertEquals(output.signingResultV2.error, SigningError.OK)
+        assertEquals(Numeric.toHexString(output.signingResultV2.encoded.toByteArray()), "0x02000000000101089098890d2653567b9e8df2d1fbe5c3c8bf1910ca7184e301db0ad3b495c88e0100000000ffffffff02581b000000000000225120e8b706a97732e705e22ae7710703e7f589ed13c636324461afa443016134cc051040000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d02483045022100a44aa28446a9a886b378a4a65e32ad9a3108870bd725dc6105160bed4f317097022069e9de36422e4ce2e42b39884aa5f626f8f94194d1013007d5a1ea9220a06dce0121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000")
+        assertEquals(Numeric.toHexString(output.signingResultV2.txid.toByteArray()), "0x797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1")
+    }
+
+    @Test
+    fun testSignBrc20Reveal() {
+        // Successfully broadcasted: https://www.blockchain.com/explorer/transactions/btc/7046dc2689a27e143ea2ad1039710885147e9485ab6453fa7e87464aa7dd3eca
+        val privateKeyData = (Numeric.hexStringToByteArray("e253373989199da27c48680e3a3fc0f648d50f9a727ef17a7fe6a4dc3b159129"))
+        val dustSatoshis = 546.toLong()
+        val txIdCommit = Numeric.hexStringToByteArray("797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1").reversedArray()
+
+        val privateKey = PrivateKey(privateKeyData)
+        val publicKey = ByteString.copyFrom(privateKey.getPublicKeySecp256k1(true).data())
+
+        val utxo0 = BitcoinV2.Input.newBuilder()
+            .setOutPoint(BitcoinV2.OutPoint.newBuilder().apply {
+                hash = ByteString.copyFrom(txIdCommit)
+                vout = 0
+            })
+            .setValue(7_000)
+            .setSighashType(BitcoinSigHashType.ALL.value())
+            .setScriptBuilder(BitcoinV2.Input.InputBuilder.newBuilder().apply {
+                brc20Inscribe = BitcoinV2.Input.InputBrc20Inscription.newBuilder().apply {
+                    inscribeTo = publicKey
+                    ticker = "oadf"
+                    transferAmount = "20"
+                }.build()
+            })
+
+        val out0 = BitcoinV2.Output.newBuilder()
+            .setValue(dustSatoshis)
+            .setBuilder(BitcoinV2.Output.OutputBuilder.newBuilder().apply {
+                p2Wpkh = BitcoinV2.PublicKeyOrHash.newBuilder().setPubkey(publicKey).build()
+            })
+
+        val signingInput = BitcoinV2.SigningInput.newBuilder()
+            .setVersion(BitcoinV2.TransactionVersion.V2)
+            .addPrivateKeys(ByteString.copyFrom(privateKeyData))
+            .addInputs(utxo0)
+            .addOutputs(out0)
+            .setInputSelector(BitcoinV2.InputSelector.UseAll)
+            .setChainInfo(BitcoinV2.ChainInfo.newBuilder().apply {
+                p2PkhPrefix = 0
+                p2ShPrefix = 5
+            })
+            .setDangerousUseFixedSchnorrRng(true)
+            .setFixedDustThreshold(dustSatoshis)
+            .build()
+
+        val legacySigningInput = Bitcoin.SigningInput.newBuilder().apply {
+            signingV2 = signingInput
+        }
+
+        val output = AnySigner.sign(legacySigningInput.build(), BITCOIN, SigningOutput.parser())
+
+        assertEquals(output.error, SigningError.OK)
+        assertEquals(output.signingResultV2.error, SigningError.OK)
+        assertEquals(Numeric.toHexString(output.signingResultV2.encoded.toByteArray()), "0x02000000000101b11f1782607a1fe5f033ccf9dc17404db020a0dedff94183596ee67ad4177d790000000000ffffffff012202000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d03406a35548b8fa4620028e021a944c1d3dc6e947243a7bfc901bf63fefae0d2460efa149a6440cab51966aa4f09faef2d1e5efcba23ab4ca6e669da598022dbcfe35b0063036f7264010118746578742f706c61696e3b636861727365743d7574662d3800377b2270223a226272632d3230222c226f70223a227472616e73666572222c227469636b223a226f616466222c22616d74223a223230227d6821c00f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000")
+        assertEquals(Numeric.toHexString(output.signingResultV2.txid.toByteArray()), "0x7046dc2689a27e143ea2ad1039710885147e9485ab6453fa7e87464aa7dd3eca")
+    }
+
+    @Test
     fun testSignBrc20Transfer() {
         // Successfully broadcasted: https://www.blockchain.com/explorer/transactions/btc/3e3576eb02667fac284a5ecfcb25768969680cc4c597784602d0a33ba7c654b7
         val privateKeyData = (Numeric.hexStringToByteArray("e253373989199da27c48680e3a3fc0f648d50f9a727ef17a7fe6a4dc3b159129"))
-        val fullAmount = 26400
-        val minerFee = 3000
-        val brcInscribeAmount = 7000
         val dustSatoshis = 546.toLong()
-        val forFeeAmount = fullAmount - brcInscribeAmount - minerFee
         val txIdInscription = Numeric.hexStringToByteArray("7046dc2689a27e143ea2ad1039710885147e9485ab6453fa7e87464aa7dd3eca").reversedArray()
         val txIdForFees = Numeric.hexStringToByteArray("797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1").reversedArray()
 
@@ -193,7 +310,7 @@ class TestBitcoinSigning {
                 hash = ByteString.copyFrom(txIdForFees)
                 vout = 1
             })
-            .setValue(16400)
+            .setValue(16_400)
             .setSighashType(BitcoinSigHashType.ALL.value())
             .setScriptBuilder(BitcoinV2.Input.InputBuilder.newBuilder().apply {
                 p2Wpkh = BitcoinV2.PublicKeyOrHash.newBuilder().setPubkey(publicKey).build()
@@ -204,7 +321,7 @@ class TestBitcoinSigning {
             .setToAddress(bobAddress)
 
         val changeOutput = BitcoinV2.Output.newBuilder()
-            .setValue(13400)
+            .setValue(13_400)
             .setBuilder(BitcoinV2.Output.OutputBuilder.newBuilder().apply {
                 p2Wpkh = BitcoinV2.PublicKeyOrHash.newBuilder().setPubkey(publicKey).build()
             })
@@ -236,128 +353,6 @@ class TestBitcoinSigning {
         assertEquals(Numeric.toHexString(output.signingResultV2.encoded.toByteArray()), "0x02000000000102ca3edda74a46877efa5364ab85947e148508713910ada23e147ea28926dc46700000000000ffffffffb11f1782607a1fe5f033ccf9dc17404db020a0dedff94183596ee67ad4177d790100000000ffffffff022202000000000000160014e891850afc55b64aa8247b2076f8894ebdf889015834000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d024830450221008798393eb0b7390217591a8c33abe18dd2f7ea7009766e0d833edeaec63f2ec302200cf876ff52e68dbaf108a3f6da250713a9b04949a8f1dcd1fb867b24052236950121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb0248304502210096bbb9d1f0596d69875646689e46f29485e8ceccacde9d0025db87fd96d3066902206d6de2dd69d965d28df3441b94c76e812384ab9297e69afe3480ee4031e1b2060121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000")
         assertEquals(Numeric.toHexString(output.signingResultV2.txid.toByteArray()), "0x3e3576eb02667fac284a5ecfcb25768969680cc4c597784602d0a33ba7c654b7")
     }
-
-//    @Test
-//    fun testSignBrc20Commit() {
-//        // Successfully broadcasted: https://www.blockchain.com/explorer/transactions/btc/797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1
-//        val privateKeyData = (Numeric.hexStringToByteArray("e253373989199da27c48680e3a3fc0f648d50f9a727ef17a7fe6a4dc3b159129"))
-//        val fullAmount = 26400
-//        val minerFee = 3000
-//        val brcInscribeAmount = 7000
-//        val forFeeAmount = fullAmount - brcInscribeAmount - minerFee
-//        val txId = Numeric.hexStringToByteArray("089098890d2653567b9e8df2d1fbe5c3c8bf1910ca7184e301db0ad3b495c88e")
-//        val brc20Ticker = "oadf"
-//        val brc20Amount = "20"
-//
-//        val privateKey = PrivateKey(privateKeyData)
-//        val publicKey = privateKey.getPublicKeySecp256k1(true)
-//        val pubKeyHash = Hash.ripemd(Hash.sha256(publicKey.data()))
-//
-//        val p2wpkh = BitcoinScript.buildPayToWitnessPubkeyHash(pubKeyHash)
-//        val outputInscribe = BitcoinScript.buildBRC20InscribeTransfer(brc20Ticker, brc20Amount, publicKey.data())
-//        val outputInscribeProto = Bitcoin.TransactionOutput.parseFrom(outputInscribe)
-//
-//        val input = Bitcoin.SigningInput.newBuilder()
-//            .setIsItBrcOperation(true)
-//            .addPrivateKey(ByteString.copyFrom(privateKeyData))
-//
-//        val unspentOutputPoint = Bitcoin.OutPoint.newBuilder()
-//            .setHash(ByteString.copyFrom(txId))
-//            .setIndex(1)
-//            .build()
-//
-//        val utxo0 = Bitcoin.UnspentTransaction.newBuilder()
-//            .setScript(ByteString.copyFrom(p2wpkh.data()))
-//            .setAmount(fullAmount.toLong())
-//            .setVariant(Bitcoin.TransactionVariant.P2WPKH)
-//            .setOutPoint(unspentOutputPoint)
-//            .build()
-//        input.addUtxo(utxo0)
-//
-//        val utxoPlan0 = Bitcoin.UnspentTransaction.newBuilder()
-//            .setScript(outputInscribeProto.script)
-//            .setAmount(brcInscribeAmount.toLong())
-//            .setVariant(Bitcoin.TransactionVariant.BRC20TRANSFER)
-//            .build()
-//        val utxoPlan1 = Bitcoin.UnspentTransaction.newBuilder()
-//            .setScript(ByteString.copyFrom(p2wpkh.data()))
-//            .setAmount(forFeeAmount.toLong())
-//            .setVariant(Bitcoin.TransactionVariant.P2WPKH)
-//            .build()
-//
-//        val plan = Bitcoin.TransactionPlan.newBuilder()
-//            .addUtxos(utxoPlan0)
-//            .addUtxos(utxoPlan1)
-//            .build()
-//        input.plan = plan
-//
-//        val output = AnySigner.sign(input.build(), BITCOIN, SigningOutput.parser())
-//
-//        assertEquals(output.error, SigningError.OK)
-//        assertEquals(output.transactionId, "797d17d47ae66e598341f9dfdea020b04d4017dcf9cc33f0e51f7a6082171fb1")
-//        assertEquals(Numeric.toHexString(output.encoded.toByteArray()), "0x02000000000101089098890d2653567b9e8df2d1fbe5c3c8bf1910ca7184e301db0ad3b495c88e0100000000ffffffff02581b000000000000225120e8b706a97732e705e22ae7710703e7f589ed13c636324461afa443016134cc051040000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d02483045022100a44aa28446a9a886b378a4a65e32ad9a3108870bd725dc6105160bed4f317097022069e9de36422e4ce2e42b39884aa5f626f8f94194d1013007d5a1ea9220a06dce0121030f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000")
-//
-//        val signedTransaction = output.transaction
-//        assert(signedTransaction.isInitialized)
-//    }
-//
-//    @Test
-//    fun testSignBrc20Reveal() {
-//        // Successfully broadcasted: https://www.blockchain.com/explorer/transactions/btc/7046dc2689a27e143ea2ad1039710885147e9485ab6453fa7e87464aa7dd3eca
-//        val privateKeyData = (Numeric.hexStringToByteArray("e253373989199da27c48680e3a3fc0f648d50f9a727ef17a7fe6a4dc3b159129"))
-//        val brcInscribeAmount = 7000
-//        val dustSatoshis = 546
-//        val txId = Numeric.hexStringToByteArray("b11f1782607a1fe5f033ccf9dc17404db020a0dedff94183596ee67ad4177d79")
-//        val brc20Ticker = "oadf"
-//        val brc20Amount = "20"
-//
-//        val privateKey = PrivateKey(privateKeyData)
-//        val publicKey = privateKey.getPublicKeySecp256k1(true)
-//        val pubKeyHash = Hash.ripemd(Hash.sha256(publicKey.data()))
-//        val p2wpkh = BitcoinScript.buildPayToWitnessPubkeyHash(pubKeyHash)
-//        val outputInscribe = BitcoinScript.buildBRC20InscribeTransfer(brc20Ticker, brc20Amount, publicKey.data())
-//        val outputInscribeProto = Bitcoin.TransactionOutput.parseFrom(outputInscribe)
-//
-//        val input = Bitcoin.SigningInput.newBuilder()
-//            .setIsItBrcOperation(true)
-//            .addPrivateKey(ByteString.copyFrom(privateKeyData))
-//
-//        val unspentOutputPoint0 = Bitcoin.OutPoint.newBuilder()
-//            .setHash(ByteString.copyFrom(txId))
-//            .setIndex(0)
-//            .build()
-//        val utxo0 = Bitcoin.UnspentTransaction.newBuilder()
-//            .setScript(outputInscribeProto.script)
-//            .setAmount(brcInscribeAmount.toLong())
-//            .setVariant(Bitcoin.TransactionVariant.BRC20TRANSFER)
-//            .setSpendingScript(outputInscribeProto.spendingScript)
-//            .setOutPoint(unspentOutputPoint0)
-//            .build()
-//
-//        input.addUtxo(utxo0)
-//
-//        val utxoPlan0 = Bitcoin.UnspentTransaction.newBuilder()
-//            .setScript(ByteString.copyFrom(p2wpkh.data()))
-//            .setAmount(dustSatoshis.toLong())
-//            .setVariant(Bitcoin.TransactionVariant.P2WPKH)
-//            .build()
-//
-//        val plan = Bitcoin.TransactionPlan.newBuilder()
-//            .addUtxos(utxoPlan0)
-//            .build()
-//        input.plan = plan
-//
-//        val output = AnySigner.sign(input.build(), BITCOIN, SigningOutput.parser())
-//
-//        assertEquals(output.error, SigningError.OK)
-//        assertEquals(output.transactionId, "7046dc2689a27e143ea2ad1039710885147e9485ab6453fa7e87464aa7dd3eca")
-//        val encodedHex = Numeric.toHexString(output.encoded.toByteArray())
-//        assert(encodedHex.startsWith("0x02000000000101b11f1782607a1fe5f033ccf9dc17404db020a0dedff94183596ee67ad4177d790000000000ffffffff012202000000000000160014e311b8d6ddff856ce8e9a4e03bc6d4fe5050a83d0340"))
-//        assert(encodedHex.endsWith("5b0063036f7264010118746578742f706c61696e3b636861727365743d7574662d3800377b2270223a226272632d3230222c226f70223a227472616e73666572222c227469636b223a226f616466222c22616d74223a223230227d6821c00f209b6ada5edb42c77fd2bc64ad650ae38314c8f451f3e36d80bc8e26f132cb00000000"))
-//
-//        val signedTransaction = output.transaction
-//        assert(signedTransaction.isInitialized)
-//    }
 }
 
 const val nftInscriptionImageData =
