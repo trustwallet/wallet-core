35e4f59f5c401d8700881d16d2aca8ceb03bdac2
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2024-08-14 13:02:50 +0200

    feat(ton): Add `tw_ton_message_signer_sign_message` in Rust

---
+++ rust/wallet_core_rs/tests/solana.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2017 Trust Wallet.
+
+#[path = "solana/solana_address.rs"]
+mod solana_address;
+#[path = "solana/solana_transaction.rs"]
+mod solana_transaction;
