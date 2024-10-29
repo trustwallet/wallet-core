dd09db52d0e60e22563ebacb55cb8853b52c618e
Javad <ja7ad@live.com>
2024-10-29 11:32:11 +0330

    add bond payload for sign transaction

--- rust/chains/tw_pactus/src/modules/tx_builder.rs
+++ rust/chains/tw_pactus/src/modules/tx_builder.rs
@@ -2,9 +2,9 @@
 //
 // Copyright © 2017 Trust Wallet.
 
-use crate::transaction::payload::TransferPayload;
+use crate::transaction::payload::{TransferPayload, BondPayload, Payload};
 use crate::transaction::Transaction;
-use crate::types::{Address, Amount};
+use crate::types::{Address, Amount, ValidatorPublicKey};
 use std::str::FromStr;
 use tw_coin_entry::error::prelude::*;
 use tw_proto::Pactus;
@@ -16,14 +16,22 @@ impl TxBuilder {
         match &input.transaction {
             None => SigningError::err(SigningErrorType::Error_invalid_params),
             Some(trx) => {
-                let payload = match &trx.payload {
+                let payload: Box<dyn Payload> = match &trx.payload {
                     Pactus::Proto::mod_TransactionMessage::OneOfpayload::transfer(pld) => {
                         let sender = Address::from_str(&pld.sender)?;
                         let receiver = Address::from_str(&pld.receiver)?;
                         Box::new(TransferPayload::new(sender, receiver, Amount(pld.amount)))
                     },
-                    Pactus::Proto::mod_TransactionMessage::OneOfpayload::bond(_pld) => {
-                        return SigningError::err(SigningErrorType::Error_not_supported)
+                    Pactus::Proto::mod_TransactionMessage::OneOfpayload::bond(pld) => {
+                        let sender = Address::from_str(&pld.sender)?;
+                        let receiver = Address::from_str(&pld.receiver)?;
+                        let public_key = if !pld.public_key.is_empty() {
+                            Some(ValidatorPublicKey::from_str(&pld.public_key)?)
+                        } else {
+                            None
+                        };
+
+                        Box::new(BondPayload::new(sender, receiver, Amount(pld.stake), public_key))
                     },
                     Pactus::Proto::mod_TransactionMessage::OneOfpayload::None => {
                         return SigningError::err(SigningErrorType::Error_invalid_params)
