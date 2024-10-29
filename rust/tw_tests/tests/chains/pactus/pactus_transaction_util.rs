7b12db552fd63ae6443b6c66cf56d501c47acf1a
Javad <ja7ad@live.com>
2024-10-29 11:35:05 +0330

    refactor transfer sign payload

--- rust/tw_tests/tests/chains/pactus/pactus_transaction_util.rs
+++ rust/tw_tests/tests/chains/pactus/pactus_transaction_util.rs
@@ -1,22 +1,22 @@
 use tw_any_coin::test_utils::transaction_calc_tx_hash_utils::TransactionUtilHelper;
 use tw_coin_registry::coin_type::CoinType;
 
-use crate::chains::pactus::test_cases::transfer_1::{
-    TRANSACTION_NOT_SIGNED_DATA, TRANSACTION_SIGNED_DATA, TX_ID,
+use crate::chains::pactus::test_cases::transfer::{
+    TRANSACTION_TRANSFER_NOT_SIGNED_DATA, TRANSACTION_TRANSFER_SIGNED_DATA, TRANSFER_TX_ID,
 };
 
 #[test]
 fn test_pactus_transaction_util_calc_tx_hash_signed() {
-    let encoded_tx = TRANSACTION_SIGNED_DATA;
+    let encoded_tx = TRANSACTION_TRANSFER_SIGNED_DATA;
     let tx_hash = TransactionUtilHelper::calc_tx_hash(CoinType::Pactus, encoded_tx);
 
-    assert_eq!(tx_hash, TX_ID);
+    assert_eq!(tx_hash, TRANSFER_TX_ID);
 }
 
 #[test]
 fn test_pactus_transaction_util_calc_tx_hash_not_signed() {
-    let encoded_tx = TRANSACTION_NOT_SIGNED_DATA;
+    let encoded_tx = TRANSACTION_TRANSFER_NOT_SIGNED_DATA;
     let tx_hash = TransactionUtilHelper::calc_tx_hash(CoinType::Pactus, encoded_tx);
 
-    assert_eq!(tx_hash, TX_ID);
+    assert_eq!(tx_hash, TRANSFER_TX_ID);
 }
