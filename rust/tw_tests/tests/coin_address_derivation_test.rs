2b4c3d87a1b1419f149f1dfa7e37b7c729135db4
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-11-01 21:12:07 +0100

    Update coin_address_derivation_test.rs

--- rust/tw_tests/tests/coin_address_derivation_test.rs
+++ rust/tw_tests/tests/coin_address_derivation_test.rs
@@ -157,6 +157,7 @@ fn test_coin_address_derivation() {
             CoinType::Polkadot => "12dyy3fArMPDXLsnRtapTqZsC2KCEimeqs1dop4AEERaKC6x",
             CoinType::Acala => "22WaYy5ChG8V5vvRVDP4ErE7esk8nZ4rjGYwxeVArnNT8dU3",
             CoinType::Kusama => "EDJV2jycw8fqTgiExLsDe6iUzbnM62hDk7u3BLm9wcYswkY",
+            CoinType::MAYAChain => "maya1ten42eesehw0ktddcp0fws7d3ycsqez3e55grf",
             // end_of_coin_address_derivation_tests_marker_do_not_modify
             _ => panic!("{:?} must be covered", coin),
         };
