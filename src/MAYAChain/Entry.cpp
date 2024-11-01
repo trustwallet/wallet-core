44c4dc81cbd5a6841c4e20032dccf240885dde57
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-11-01 21:25:53 +0100

    Create Entry.cpp

---
+++ src/MAYAChain/Entry.cpp
@@ -0,0 +1,29 @@
+// Copyright © 2017-2024 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Entry.h"
+
+#include "Signer.h"
+#include "../proto/Cosmos.pb.h"
+
+using namespace std;
+
+namespace TW::MAYAChain {
+
+// Note: avoid business logic from here, rather just call into classes like Address, Signer, etc.
+
+void Entry::sign([[maybe_unused]] TWCoinType coin, const TW::Data& dataIn, TW::Data& dataOut) const {
+    auto input = Cosmos::Proto::SigningInput();
+    input.ParseFromArray(dataIn.data(), (int)dataIn.size());
+    auto serializedOut = Signer::sign(input).SerializeAsString();
+    dataOut.insert(dataOut.end(), serializedOut.begin(), serializedOut.end());
+}
+
+string Entry::signJSON([[maybe_unused]] TWCoinType coin, const std::string& json, const Data& key) const {
+    return Signer::signJSON(json, key);
+}
+
+} // namespace TW::MAYAChain
