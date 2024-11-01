6ac12c160000ffbb7fb8f81ecfeab92eb39b4ad6
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-11-01 21:26:12 +0100

    Create Entry.h

---
+++ src/MAYAChain/Entry.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2017 Trust Wallet.
+
+#pragma once
+
+#include "../CoinEntry.h"
+#include "../Cosmos/Entry.h"
+
+namespace TW::MAYAChain {
+
+/// Entry point for implementation of MAYAChain coin.
+/// Note: do not put the implementation here (no matter how simple), to avoid having coin-specific includes in this file
+class Entry final : public Cosmos::Entry {
+};
+
+} // namespace TW::MAYAChain
