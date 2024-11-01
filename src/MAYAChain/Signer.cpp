f6ff40b1ede9c79dcc2f3a9966498acbcbedf772
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-11-01 21:26:35 +0100

    Create Signer.cpp

---
+++ src/MAYAChain/Signer.cpp
@@ -0,0 +1,36 @@
+// Copyright © 2017-2024 Trust Wallet.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "Signer.h"
+#include "../Cosmos/Signer.h"
+#include "../proto/Cosmos.pb.h"
+
+#include <TrustWalletCore/TWCoinType.h>
+#include <google/protobuf/util/json_util.h>
+
+using namespace TW;
+
+namespace TW::MAYAChain {
+const std::string TYPE_PREFIX_MSG_SEND = "mayachain/MsgSend";
+
+Cosmos::Proto::SigningOutput Signer::sign(Cosmos::Proto::SigningInput& input) noexcept {
+    for (auto i = 0; i < input.messages_size(); ++i) {
+        if (input.messages(i).has_send_coins_message()) {
+            input.mutable_messages(i)->mutable_send_coins_message()->set_type_prefix(TYPE_PREFIX_MSG_SEND);
+        }
+    }
+    return Cosmos::Signer::sign(input, TWCoinTypeMAYAChain);
+}
+
+std::string Signer::signJSON(const std::string& json, const Data& key) {
+    auto input = Cosmos::Proto::SigningInput();
+    google::protobuf::util::JsonStringToMessage(json, &input);
+    input.set_private_key(key.data(), key.size());
+    auto output = Signer::sign(input);
+    return output.json();
+}
+
+} // namespace TW::MAYAChain
