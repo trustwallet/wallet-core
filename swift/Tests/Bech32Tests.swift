588758e050f4d42d43d5b69cecb56f12bf5a1b69
10gic <2391796+10gic@users.noreply.github.com>
2024-11-09 00:20:59 +0800

    Fix failed swift test case

--- swift/Tests/Bech32Tests.swift
+++ swift/Tests/Bech32Tests.swift
@@ -19,7 +19,7 @@ class Bech32Tests: XCTestCase {
 
     func testDecodeWrongChecksumVariant()  {
         // This is a Bech32m variant, not Bech32 variant. So it should fail using Bech32 decoder.
-        let decoded = Bech32.decode(string: "abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw")
+        let decoded = Bech32.decode(string: "abcdef1l7aum6echk45nj3s0wdvt2fg8x9yrzpqzd3ryx")
         XCTAssertNil(decoded)
     }
 
