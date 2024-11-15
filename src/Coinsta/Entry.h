3aef85dcc6d9a0c9f49488da5ed4a2d3c788bc19
Coinat1 <keekends@gmail.com>
2024-11-15 17:32:59 +0200

    Create Entry.h

---
+++ src/Coinsta/Entry.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2017 Trust Wallet.
+
+#pragma once
+
+#include "Bitcoin/Entry.h"
+
+namespace TW::Coinsta {
+
+/// Entry point for Coinsta coin.
+/// Note: do not put the implementation here (no matter how simple), to avoid having coin-specific includes in this file
+///
+/// Currently, we must support the legacy `Bitcoin.proto` API,
+/// but `BitcoinV2.proto` still can be used through `Bitcoin.SigningInput.signing_v2`.
+/// TODO inherit Rust::RustCoinEntry directly when `Bitcoin.proto` is deprecated.
+class Entry : public Bitcoin::Entry {
+};
+
+} // namespace TW::Coinsta
