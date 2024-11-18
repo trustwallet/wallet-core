45124dd5881d9bf8ccb36dfbeb8f41a2b0d46139
satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
2024-11-19 01:57:35 +0700

    [Nimiq]: Add support for Nimiq PoS Albatross Mainnet (#4115)
    * Add support for Nimiq Albatross PoS transaction serialization (#4110)
    
    * Update Nimiq Transaction encoding for Albatross PoS
    
    * Update tests
    
    * [Nimiq]: Add networkId parameter to SigningInput
    
    * [Nimiq]: Fix iOS test
    
    * [Nimiq]: Fix invalid integer type cast
    
    * [Nimiq]: Fix iOS test
    
    ---------
    
    Co-authored-by: Sören Schwert <hello@soerenschwert.de>

--- src/Nimiq/Transaction.cpp
+++ src/Nimiq/Transaction.cpp
@@ -9,25 +9,33 @@
 
 namespace TW::Nimiq {
 
-const uint8_t NETWORK_ID = 42;
 const uint8_t EMPTY_FLAGS = 0;
 
 std::vector<uint8_t> Transaction::serialize() const {
+    // Source code:
+    // https://github.com/nimiq/core-rs-albatross/blame/b8ed402c9096ffb54afea52347b91ab7831e75de/primitives/transaction/src/lib.rs#L699
+
     std::vector<uint8_t> data;
 
     data.push_back(0x00); // Basic TX type
+    if (isAlbatross()) {
+        data.push_back(0x00); // Signature Proof type and flags (Ed25519 type and no flags)
+    }
     data.insert(data.end(), sender_pub_key.begin(), sender_pub_key.end());
     data.insert(data.end(), destination.bytes.begin(), destination.bytes.end());
     encode64BE(amount, data);
     encode64BE(fee, data);
     encode32BE(vsh, data);
-    data.push_back(NETWORK_ID);
+    data.push_back(consensusNetworkId());
     data.insert(data.end(), signature.begin(), signature.end());
 
     return data;
 }
 
 std::vector<uint8_t> Transaction::getPreImage() const {
+    // Source code:
+    // https://github.com/nimiq/core-rs-albatross/blame/b8ed402c9096ffb54afea52347b91ab7831e75de/primitives/transaction/src/lib.rs#L582
+
     std::vector<uint8_t> data;
 
     // Build pre-image
@@ -40,10 +48,32 @@ std::vector<uint8_t> Transaction::getPreImage() const {
     encode64BE(amount, data);
     encode64BE(fee, data);
     encode32BE(vsh, data);
-    data.push_back(NETWORK_ID);
+    data.push_back(consensusNetworkId());
     data.push_back(EMPTY_FLAGS);
+    if (isAlbatross()) {
+        data.push_back(0x00); // Sender Data size (+ 0 bytes of data)
+    }
 
     return data;
+}
+
+bool Transaction::isAlbatross() const {
+    if (networkId == Proto::NetworkId::MainnetAlbatross) {
+        return true;
+    }
+    return false;
+}
+
+uint8_t Transaction::consensusNetworkId() const {
+    switch (networkId) {
+        case Proto::NetworkId::UseDefault:
+        case Proto::NetworkId::Mainnet:
+            return static_cast<uint8_t>(Proto::NetworkId::Mainnet);
+        case Proto::NetworkId::MainnetAlbatross:
+            return static_cast<uint8_t>(Proto::NetworkId::MainnetAlbatross);
+        default:
+            throw std::invalid_argument("Invalid network ID");
+    }
 }
 
 } // namespace TW::Nimiq
--- tests/chains/Nimiq/TWAnySignerTests.cpp
+++ tests/chains/Nimiq/TWAnySignerTests.cpp
@@ -28,4 +28,22 @@ TEST(TWAnySignerNimiq, Sign) {
     EXPECT_EQ(hex(output.encoded()), "0070c7492aaa9c9ac7a05bc0d9c5db2dae9372029654f71f0c7f95deed5099b7021450ffc385cd4e7c6ac9a7e91614ca67ff90568a00000000028182ba00000000000003e80004cb2f2a74dc7f6e0ab58a0bf52cc6e8801b0cca132dd4229d9a3e3a3d2f90e4d8f045d981b771bf5fc3851a98f3c617b1a943228f963e910e061808a721cfa0e3cad50b");
 }
 
+TEST(TWAnySignerNimiq, SignPoS) {
+    auto privateKey = parse_hex("e3cc33575834add098f8487123cd4bca543ee859b3e8cfe624e7e6a97202b756");
+
+    Proto::SigningInput input;
+
+    input.set_destination("NQ86 2H8F YGU5 RM77 QSN9 LYLH C56A CYYR 0MLA");
+    input.set_fee(1000);
+    input.set_value(42042042);
+    input.set_validity_start_height(314159);
+    input.set_private_key(privateKey.data(), privateKey.size());
+    input.set_network_id(Proto::NetworkId::MainnetAlbatross);
+
+    Proto::SigningOutput output;
+    ANY_SIGN(input, TWCoinTypeNimiq);
+
+    EXPECT_EQ(hex(output.encoded()), "000070c7492aaa9c9ac7a05bc0d9c5db2dae9372029654f71f0c7f95deed5099b7021450ffc385cd4e7c6ac9a7e91614ca67ff90568a00000000028182ba00000000000003e80004cb2f180ba678744be3bf9cd44fbcdabfb5be209f21739934836e26055610ab02720fa99489219d9f3581664473a1b40b30ad1f6e13150d59f8234a42c3f0de3d505405");
+}
+
 } // namespace TW::Nimiq::tests
