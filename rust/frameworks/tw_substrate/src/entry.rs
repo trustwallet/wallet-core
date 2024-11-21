50ffb2b5ba22f4e74c9034f420d8e61ee3599db3
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-11-21 20:50:56 +0800

    Remove unused error variant.

--- rust/frameworks/tw_substrate/src/entry.rs
+++ rust/frameworks/tw_substrate/src/entry.rs
@@ -43,8 +43,11 @@ impl<T: SubstrateCoinEntry> SubstrateEntry<T> {
         coin: &dyn CoinContext,
         input: T::SigningInput<'_>,
     ) -> SigningResult<RawOwned> {
-        let keypair = self.0.get_keypair(coin, &input).ok();
-        let public_key = keypair.map(|p| p.public().clone());
+        let public_key = self
+            .0
+            .get_keypair(coin, &input)
+            .ok()
+            .map(|p| p.public().clone());
         let unsigned_tx = self.0.build_transaction(coin, public_key, &input)?;
         let pre_image = unsigned_tx.encode_payload()?;
         Ok(RawOwned(pre_image))
