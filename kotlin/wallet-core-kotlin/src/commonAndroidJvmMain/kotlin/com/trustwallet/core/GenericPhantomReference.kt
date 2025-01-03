11d320b29af83c3a990ab461c4a8bd1de189b468
Sergei Boiko <127754187+satoshiotomakan@users.noreply.github.com>
2025-01-03 21:42:46 +0700

    [Dev/UnderAudit]: Dev<->Master (#4046)
    * Kotlin multiplatform leaking memory (#4037)
    
    * Add deinit for KMP iOS and JVM targets
    
    * Add deinit for JS target
    
    * Add deinit for JS target
    
    * Fix JVM native name
    
    * Reuse one thread on JVM
    
    ---------
    
    Co-authored-by: satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
    
    * [KMP] Fix issue: memory leak found in Base58.decode in iOS (#4031)
    
    * Fix kmp issue: memory leak found in Base58.decode in iOS
    
    * Remove unused functions
    
    * Fix failed test cases
    
    * Revert "Fix failed test cases"
    
    This reverts commit 57eee395df508d95a5e570b1f54143b7de20eb31.
    
    * Revert val -> value argument name refactoring
    
    * Output better indentation
    
    * Revert changes in TWEthereumAbiFunction.h
    
    * Fix inconsistent naming
    
    ---------
    
    Co-authored-by: satoshiotomakan <127754187+satoshiotomakan@users.noreply.github.com>
    
    * [TON]: Add support for TON 24-words mnemonic (#3998)
    
    * feat(ton): Add support for TON 24-words mnemonic in Rust
    
    * feat(ton): Add tw_ton_wallet FFIs
    
    * feat(ton): Add TWTONWallet FFI in C++
    
    * feat(ton): Add tonMnemonic StoredKey type
    
    * feat(ton): Add StoredKey TON tests
    
    * feat(ton): Add TWStoredKey TON tests
    
    * feat(ton): Add TONWallet support in Swift
    
    * TODO add iOS tests
    
    * feat(ton): Add `KeyStore` iOS tests
    
    * feat(ton): Add TONWallet support in JavaScript
    
    * Add `KeyStore` TypeScript tests
    
    * feat(ton): Remove `TonMnemonic` structure, replace with a `validate_mnemonic_words` function
    
    * [CI] Trigger CI
    
    * feat(ton): Fix rustfmt
    
    * feat(ton): Fix C++ build
    
    * feat(ton): Fix C++ build
    
    * feat(ton): Fix C++ build
    
    * feat(ton): Fix C++ address analyzer
    
    * feat(ton): Fix C++ tests
    
    * feat(ton): Add Android tests
    
    * feat(ton): Bump `actions/upload-artifact` to v4
    
    * Bump `dawidd6/action-download-artifact` to v6
    
    * feat(eth): Fix PR comments
    
    * Fix Java JVM leak (#4092)
    
    * [Chore]: Fix Android bindings (#4095)
    
    * [Chore]: Add GenericPhantomReference.java
    
    * [Chore]: Fix unnecessary null assertion in WalletCoreLibLoader.kt
    
    * Fix memory lead found in public key in kmp binding (#4097)
    
    * Update Callisto explorer (#4131)
    
    * Revert "[TON]: Add support for TON 24-words mnemonic (#3998)" (#4148)
    
    This reverts commit 0b167710b325d299398ca82ef57ee8e630e54633.
    
    ---------
    
    Co-authored-by: Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
    Co-authored-by: 10gic <github10gic@proton.me>

---
+++ kotlin/wallet-core-kotlin/src/commonAndroidJvmMain/kotlin/com/trustwallet/core/GenericPhantomReference.kt
@@ -0,0 +1,47 @@
+package com.trustwallet.core
+
+import java.lang.ref.PhantomReference
+import java.lang.ref.ReferenceQueue
+
+internal class GenericPhantomReference private constructor(
+    referent: Any,
+    private val handle: Long,
+    private val onDelete: (Long) -> Unit,
+) : PhantomReference<Any>(referent, queue) {
+
+    companion object {
+        private val references: MutableSet<GenericPhantomReference> = HashSet()
+        private val queue: ReferenceQueue<Any> = ReferenceQueue()
+
+        init {
+            Thread {
+                try {
+                    doDeletes()
+                } catch (e: InterruptedException) {
+                    Thread.currentThread().interrupt()
+                }
+            }.apply {
+                name = "WCFinalizingDaemon"
+                isDaemon = true
+                priority = Thread.NORM_PRIORITY
+                start()
+            }
+        }
+
+        fun register(
+            referent: Any,
+            handle: Long,
+            onDelete: (Long) -> Unit,
+        ) {
+            references.add(GenericPhantomReference(referent, handle, onDelete))
+        }
+
+        private fun doDeletes() {
+            while (true) {
+                val ref = queue.remove() as GenericPhantomReference
+                ref.onDelete(ref.handle)
+                references.remove(ref)
+            }
+        }
+    }
+}
