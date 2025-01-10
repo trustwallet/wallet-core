3fd969af48375dd2922a845867e073444d1646c9
Victor Sima <vic.sima@gmail.com>
2025-01-10 03:48:28 -0500

    KMP sample update (#4150)
    - android sdk 35 & ios 18
     - kotlin 2.1.0
     - gradle 8.10
     - AGP 8.7
    
    Co-authored-by: Szymon Rząd <sz.rzad@gmail.com>

--- samples/kmp/androidApp/build.gradle.kts
+++ samples/kmp/androidApp/build.gradle.kts
@@ -1,25 +1,23 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    kotlin("plugin.compose")
 }
 
 android {
     namespace = "com.example.kmpsample.android"
-    compileSdk = 33
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.kmpsample.android"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.12"
-    }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -30,20 +28,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.3.1")
-    implementation("androidx.compose.ui:ui-tooling:1.3.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
-    implementation("androidx.compose.foundation:foundation:1.3.1")
-    implementation("androidx.compose.material:material:1.3.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.ui:ui:1.7.5")
+    implementation("androidx.compose.ui:ui-tooling:1.7.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    implementation("androidx.compose.foundation:foundation:1.7.5")
+    implementation("androidx.compose.material:material:1.7.5")
+    implementation("androidx.activity:activity-compose:1.9.3")
 }
