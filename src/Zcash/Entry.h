466d681c20376815cc14a546f8350d37a7306fba
Satoshi Otomakan <satoshiotomakan8@gmail.com>
2025-01-23 16:35:10 +0100

    refactor(zcash): Add support for BitcoinV2 protocol for Zcash implementation in C++

--- src/Zcash/Entry.h
+++ src/Zcash/Entry.h
@@ -10,16 +10,16 @@ namespace TW::Zcash {
 
 /// Zcash entry dispatcher.
 /// Note: do not put the implementation here (no matter how simple), to avoid having coin-specific includes in this file
-class Entry final : public CoinEntry {
+class Entry : public CoinEntry {
 public:
-    bool validateAddress(TWCoinType coin, const std::string& address, const PrefixVariant& addressPrefix) const;
-    std::string deriveAddress(TWCoinType coin, const PublicKey& publicKey, TWDerivation derivation, const PrefixVariant& addressPrefix) const;
-    Data addressToData(TWCoinType coin, const std::string& address) const;
-    void sign(TWCoinType coin, const Data& dataIn, Data& dataOut) const;
-    void plan(TWCoinType coin, const Data& dataIn, Data& dataOut) const;
+    bool validateAddress(TWCoinType coin, const std::string& address, const PrefixVariant& addressPrefix) const final;
+    std::string deriveAddress(TWCoinType coin, const PublicKey& publicKey, TWDerivation derivation, const PrefixVariant& addressPrefix) const final;
+    Data addressToData(TWCoinType coin, const std::string& address) const final;
+    void sign(TWCoinType coin, const Data& dataIn, Data& dataOut) const final;
+    void plan(TWCoinType coin, const Data& dataIn, Data& dataOut) const final;
 
-    Data preImageHashes(TWCoinType coin, const Data& txInputData) const;
-    void compile(TWCoinType coin, const Data& txInputData, const std::vector<Data>& signatures, const std::vector<PublicKey>& publicKeys, Data& dataOut) const;
+    Data preImageHashes(TWCoinType coin, const Data& txInputData) const final;
+    void compile(TWCoinType coin, const Data& txInputData, const std::vector<Data>& signatures, const std::vector<PublicKey>& publicKeys, Data& dataOut) const final;
 };
 
 } // namespace TW::Zcash
