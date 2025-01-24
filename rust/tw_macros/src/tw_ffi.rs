4b7d462c979b687f86dcc24fa214ae3e5a709482
gupnik <17176722+gupnik@users.noreply.github.com>
2025-01-24 11:43:12 +0530

    FMT

--- rust/tw_macros/src/tw_ffi.rs
+++ rust/tw_macros/src/tw_ffi.rs
@@ -98,16 +98,24 @@ pub fn tw_ffi(attr: TokenStream2, item: TokenStream2) -> Result<TokenStream2> {
     };
 
     let class = args.class.unwrap().to_string();
-    let docs = func.attrs.iter().flat_map(|attr| {
-        if let syn::Meta::NameValue(meta) = &attr.meta {
-            if meta.path.is_ident("doc") {
-                if let syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Str(lit), .. }) = &meta.value {
-                    return Some(lit.value().trim().to_string());
+    let docs = func
+        .attrs
+        .iter()
+        .flat_map(|attr| {
+            if let syn::Meta::NameValue(meta) = &attr.meta {
+                if meta.path.is_ident("doc") {
+                    if let syn::Expr::Lit(syn::ExprLit {
+                        lit: syn::Lit::Str(lit),
+                        ..
+                    }) = &meta.value
+                    {
+                        return Some(lit.value().trim().to_string());
+                    }
                 }
             }
-        }
-        None
-    }).collect::<Vec<_>>();
+            None
+        })
+        .collect::<Vec<_>>();
     let static_function = TWStaticFunction {
         name: args.name.unwrap().to_string(),
         rust_name: func_name,
