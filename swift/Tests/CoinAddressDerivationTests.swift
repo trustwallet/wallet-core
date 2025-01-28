b154f9d7bc06f5f7020319b0200fd041b355f2ad
Sergei Boiko <127754187+satoshiotomakan@users.noreply.github.com>
2025-01-28 20:14:50 +0700

    feat(sonic): Add support for Sonic EVM chain (#4239)

--- swift/Tests/CoinAddressDerivationTests.swift
+++ swift/Tests/CoinAddressDerivationTests.swift
@@ -119,7 +119,8 @@ class CoinAddressDerivationTests: XCTestCase {
                      .lightlink,
                      .blast,
                      .bounceBit,
-                     .zkLinkNova:
+                     .zkLinkNova,
+                     .sonic:
                     let expectedResult = "0x8f348F300873Fd5DA36950B2aC75a26584584feE"
                     assertCoinDerivation(coin, expectedResult, derivedAddress, address)
                 case .ronin:
