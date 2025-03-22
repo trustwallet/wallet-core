84e987416ba98335adffbbed15b69a0a2572fe42
Mostafa <mostafa.sedaghat@gmail.com>
2025-03-23 03:19:32 +0800

    test(pactus): fix TS broken tests

--- wasm/tests/CoinType.test.ts
+++ wasm/tests/CoinType.test.ts
@@ -64,7 +64,7 @@ describe("CoinType", () => {
     assert.equal(CoinTypeExt.purpose(CoinType.pactus), Purpose.bip44);
     assert.equal(CoinTypeExt.curve(CoinType.pactus), Curve.ed25519);
     assert.isTrue(CoinTypeExt.validate(CoinType.pactus, "pc1rnvlc4wa73lc0rydmgfswz4j5wad4un376vv2d7"))
-    assert.equal(CoinTypeExt.derivationPath(CoinType.pactus), "m/44'/21888'/3'");
+    assert.equal(CoinTypeExt.derivationPath(CoinType.pactus), "m/44'/21888'/3'/0'");
     assert.equal(CoinTypeExt.derivationPathWithDerivation(CoinType.pactus, Derivation.pactusMainnet), "m/44'/21888'/3'/0'");
     assert.equal(CoinTypeExt.derivationPathWithDerivation(CoinType.pactus, Derivation.pactustestnet), "m/44'/21777'/3'/0'");
   });
