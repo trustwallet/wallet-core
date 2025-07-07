e83495a8f2b65f7d3bc3443534033db2ecc33d06
gupnik <17176722+gupnik@users.noreply.github.com>
2025-07-07 14:23:54 +0530

    Integrates Tezos in coin registry

--- src/Tezos/Entry.h
+++ src/Tezos/Entry.h
@@ -4,22 +4,14 @@
 
 #pragma once
 
-#include "../CoinEntry.h"
+#include "rust/RustCoinEntry.h"
 
 namespace TW::Tezos {
 
-/// Entry point for implementation of Tezos coin.
-/// Note: do not put the implementation here (no matter how simple), to avoid having coin-specific includes in this file
-class Entry final : public CoinEntry {
+class Entry final : public Rust::RustCoinEntryWithSignJSON {
 public:
-    bool validateAddress(TWCoinType coin, const std::string& address, const PrefixVariant& addressPrefix) const override;
-    std::string deriveAddress(TWCoinType coin, const PublicKey& publicKey, TWDerivation derivation, const PrefixVariant& addressPrefix) const override;
-    void sign(TWCoinType coin, const Data& dataIn, Data& dataOut) const override;
-    bool supportsJSONSigning() const override { return true; }
-    std::string signJSON(TWCoinType coin, const std::string& json, const Data& key) const override;
-
-    Data preImageHashes(TWCoinType coin, const Data& txInputData) const override;
-    void compile(TWCoinType coin, const Data& txInputData, const std::vector<Data>& signatures, const std::vector<PublicKey>& publicKeys, Data& dataOut) const override;
+    bool supportsJSONSigning() const final { return true; }
+    std::string signJSON(TWCoinType coin, const std::string& json, const Data& key) const final;
 };
 
 } // namespace TW::Tezos
