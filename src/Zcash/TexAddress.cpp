b5b6e515609e016a35134c10b8424e36e78a31c3
Sergei Boiko <satoshiotomakan8@gmail.com>
2025-10-14 10:21:36 +0200

    feat(zcash): Reuse code in `TexAddress::isValid`

--- src/Zcash/TexAddress.cpp
+++ src/Zcash/TexAddress.cpp
@@ -10,25 +10,12 @@ bool TexAddress::isValid(const std::string& addr) {
 }
 
 bool TexAddress::isValid(const std::string& addr, const std::string& hrp) {
-    auto dec = Bech32::decode(addr);
-    // check hrp prefix (if given)
-    if (hrp.length() > 0 && std::get<0>(dec).compare(0, hrp.length(), hrp) != 0) {
+    try {
+        TexAddress address(addr);
+        return address.getHrp() == hrp;
+    } catch (...) {
         return false;
     }
-    if (std::get<1>(dec).empty()) {
-        return false;
-    }
-    if (std::get<2>(dec) != Bech32::Bech32M) {
-        return false;
-    }
-
-    Data conv;
-    auto success = Bech32::convertBits<5, 8, false>(conv, std::get<1>(dec));
-    if (!success || conv.size() != length) {
-        return false;
-    }
-
-    return true;
 }
 
 TexAddress::TexAddress(const std::string& addr) {
