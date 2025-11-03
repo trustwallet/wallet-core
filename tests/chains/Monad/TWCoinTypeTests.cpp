55a973cebbd80def82fcf97a73505242185a7719
Sergei Boiko <127754187+satoshiotomakan@users.noreply.github.com>
2025-11-03 17:33:19 +0100

    feat(monad): Update Monad to mainnet (#4550)
    * TODO update explorer

--- tests/chains/Monad/TWCoinTypeTests.cpp
+++ tests/chains/Monad/TWCoinTypeTests.cpp
@@ -11,19 +11,19 @@ TEST(TWMonadCoinType, TWCoinType) {
     const auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(coin));
     const auto id = WRAPS(TWCoinTypeConfigurationGetID(coin));
     const auto name = WRAPS(TWCoinTypeConfigurationGetName(coin));
-    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0xfffe6b1c1238be8d5f4779f97af8fffc89530e2634c61be7a5241dc08f41f07f"));
+    const auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0x8394f9f01bc2ae2cc93e19170bf80c303210f6f4198e5ec3cc99b0cba04962b6"));
     const auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(coin, txId.get()));
-    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0xa161C1d86b26ac692604E0cDbc5EcB2D1d85759c"));
+    const auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0x6ab69B482987b0BA1f1c96BDbDC192a80CB09132"));
     const auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(coin, accId.get()));
 
     assertStringsEqual(id, "monad");
-    assertStringsEqual(name, "Monad Testnet");
+    assertStringsEqual(name, "Monad");
     assertStringsEqual(symbol, "MON");
     ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(coin), 18);
     ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainEthereum);
     ASSERT_EQ(TWCoinTypeP2pkhPrefix(coin), 0);
     ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 0);
     ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0);
-    assertStringsEqual(txUrl, "https://testnet.monadexplorer.com/tx/0xfffe6b1c1238be8d5f4779f97af8fffc89530e2634c61be7a5241dc08f41f07f");
-    assertStringsEqual(accUrl, "https://testnet.monadexplorer.com/address/0xa161C1d86b26ac692604E0cDbc5EcB2D1d85759c");
+    assertStringsEqual(txUrl, "https://monadexplorer.com/tx/0x8394f9f01bc2ae2cc93e19170bf80c303210f6f4198e5ec3cc99b0cba04962b6");
+    assertStringsEqual(accUrl, "https://monadexplorer.com/address/0x6ab69B482987b0BA1f1c96BDbDC192a80CB09132");
 }
