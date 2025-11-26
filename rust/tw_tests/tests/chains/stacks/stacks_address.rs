c5ae159541a1fc492b78cd7fbdb55fe642c9c899
Joey Yandle <xoloki@gmail.com>
2025-11-25 23:42:01 -0500

    add two well known test vectors for derive

--- rust/tw_tests/tests/chains/stacks/stacks_address.rs
+++ rust/tw_tests/tests/chains/stacks/stacks_address.rs
@@ -11,12 +11,24 @@ use tw_coin_registry::coin_type::CoinType;
 #[test]
 fn test_stacks_address_derive() {
     let private_key_hexstr = "5ee478841eee47c23c164530d3df3469168431af52569c3dcc5d715666a7321b";
-    //let private_key = tw_keypair::ecdsa::secp256k1::PrivateKey::from();
-
     test_address_derive(
         CoinType::Stacks,
         KeyType::PrivateKey(private_key_hexstr),
         "SP2VR2M51AC00TKNMF08XE9WSJDZ3WT4QGQNEQPDQA",
+    );
+
+    let private_key_hexstr = "0000000000000000000000000000000000000000000000000000000000000001";
+    test_address_derive(
+        CoinType::Stacks,
+        KeyType::PrivateKey(private_key_hexstr),
+        "SP2STHWXQ8368SDN2MJGE4BMDKMCHZ2GSVTS1X0BPM",
+    );
+
+    let private_key_hexstr = "1111111111111111111111111111111111111111111111111111111111111111";
+    test_address_derive(
+        CoinType::Stacks,
+        KeyType::PrivateKey(private_key_hexstr),
+        "SP2VY74M5227FDVHREWPH773F5Y1W1ED8WXY3RAVG4",
     );
 }
 
