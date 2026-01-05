e6149218d10ca1bf93bebe91d9df438ee7e4b2f1
Joey Yandle <joey@stackslabs.com>
2026-01-05 05:15:30 +0100

    fix stacks coin type test by removing prefix checks since they were removed from registry.json

--- tests/chains/Stacks/TWCoinTypeTests.cpp
+++ tests/chains/Stacks/TWCoinTypeTests.cpp
@@ -21,8 +21,6 @@ TEST(TWStacksCoinType, TWCoinType) {
     assertStringsEqual(symbol, "STX");
     ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(coin), 8);
     ASSERT_EQ(TWCoinTypeBlockchain(coin), TWBlockchainStacks);
-    ASSERT_EQ(TWCoinTypeP2pkhPrefix(coin), 0);
-    ASSERT_EQ(TWCoinTypeP2shPrefix(coin), 1);
     ASSERT_EQ(TWCoinTypeStaticPrefix(coin), 0);
     assertStringsEqual(txUrl, "https://explorer.hiro.so/txid/0x2aeb646c13a0261ebc02003877d2db6c839d3bcbeea6ba7ede877f484f6ce70c");
     assertStringsEqual(accUrl, "https://explorer.hiro.so/address/SP3XXK8BG5X7CRH7W07RRJK3JZJXJ799WX3Y0SMCR");
