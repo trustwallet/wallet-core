a6eb3677cbc1f366c7f5032096b4db48527cefe3
Joey Yandle <joey@stackslabs.com>
2026-01-05 03:44:55 +0100

    implement C++ signing test

--- tests/chains/Stacks/TWAnySignerTests.cpp
+++ tests/chains/Stacks/TWAnySignerTests.cpp
@@ -3,15 +3,29 @@
 // Copyright © 2017 Trust Wallet.
 
 #include <TrustWalletCore/TWAnySigner.h>
-#include "HexCoding.h"
 
+#include "HexCoding.h"
+#include "PrivateKey.h"
+#include "PublicKey.h"
+#include "proto/Stacks.pb.h"
 #include "TestUtilities.h"
+
 #include <gtest/gtest.h>
 
 using namespace TW;
 
 // TODO: Finalize tests
 
-TEST(TWAnySignerStacks, Sign) {
-    // TODO: Finalize test implementation
+TEST(TWStacks, Sign) {
+    Stacks::Proto::SigningInput input;
+    auto& tf = *input.mutable_transfer();
+    tf.set_to("SP2VR2M51AC00TKNMF08XE9WSJDZ3WT4QGQNEQPDQA");
+    tf.set_amount(100000);
+    tf.set_fee(10000);
+    tf.set_memo("hello");
+    auto privateKey = PrivateKey(parse_hex("a1b2c3d4e5f60000000000000000000000000000000000000000000000000001"));
+    input.set_private_key(privateKey.bytes.data(), privateKey.bytes.size());
+    Stacks::Proto::SigningOutput output;
+    ANY_SIGN(input, TWCoinTypeAptos);
+    //ASSERT_EQ(hex(output.raw_txn()), "07968dab936c1bad187c60ce4082f307d03}
 }
