ca6049609bfb013a6a0e65db7fe3c4034b702477
Sergei <251985147+sergei-boiko-trustwallet@users.noreply.github.com>
2026-04-24 17:00:07 +0200

    fix(binance): update Go version and enhance protobuf definitions

--- samples/go/compile.sh
+++ samples/go/compile.sh
@@ -23,5 +23,14 @@ for FILE in "$PROTO_PATH"/*.proto; do
     --go_opt=MBitcoin.proto=tw/protos/bitcoin \
     --go_opt=MEthereum.proto=tw/protos/ethereum \
     --go_opt=MBinance.proto=tw/protos/binance \
+    --go_opt=MBabylonStaking.proto=tw/protos/babylonstaking \
+    --go_opt=MCosmos.proto=tw/protos/cosmos \
+    --go_opt=MPolkadot.proto=tw/protos/polkadot \
+    --go_opt=MSolana.proto=tw/protos/solana \
+    --go_opt=MUtxo.proto=tw/protos/utxo \
+    --go_opt=MDecredV2.proto=tw/protos/decredv2 \
+    --go_opt=MAptos.proto=tw/protos/aptos \
+    --go_opt=MZcash.proto=tw/protos/zcash \
+    --go_opt=MBitcoinV2.proto=tw/protos/bitcoinv2 \
     "$PROTO_PATH"/"$FILE_NAME"
 done
